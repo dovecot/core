cb9d8bccb3342b10e7e8d907c2c34d12f01da438
Timo Sirainen <tss@iki.fi>
2002-08-23 17:52:26 +0300

    comment update
    --HG--
    branch : HEAD

--- src/lib/hex-binary.h
+++ src/lib/hex-binary.h
@@ -6,8 +6,9 @@
 const char *binary_to_hex(const unsigned char *data, unsigned int size);
 
 /* Convert hex to binary. data and dest may point to same value.
-   Returns TRUE if successful. Returns number of bytes writte to dest,
-   or -1 if error occured. */
+   Returns TRUE if successful. Returns number of bytes written to dest,
+   or -1 if error occured. Make sure dest is at least half the size of
+   strlen(data). */
 int hex_to_binary(const char *data, unsigned char *dest);
 
 #endif
