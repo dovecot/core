adb665e5f5befd533d1bce2fa60db3f6d02de5c7
Timo Sirainen <tss@iki.fi>
2002-09-23 13:30:21 +0300

    close mailbox before sending "OK Logout completed", because the closing may take some time with mbox
    --HG--
    branch : HEAD

--- src/imap/cmd-logout.c
+++ src/imap/cmd-logout.c
@@ -6,6 +6,15 @@
 int cmd_logout(Client *client)
 {
 	client_send_line(client, "* BYE Logging out");
+
+	if (client->mailbox != NULL) {
+		/* this could be done at client_disconnect() as well,
+		   but eg. mbox rewrite takes a while so the waiting is
+		   better to happen before "OK" message. */
+		client->mailbox->close(client->mailbox);
+		client->mailbox = NULL;
+	}
+
 	client_send_tagline(client, "OK Logout completed.");
 	client_disconnect(client);
 	return TRUE;
