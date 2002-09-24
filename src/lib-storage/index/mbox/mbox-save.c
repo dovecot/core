8f3bd370094ea99ad559be6aecd5ed130025a01f
Timo Sirainen <tss@iki.fi>
2002-09-24 20:38:37 +0300

    mbox ignored message flags for APPEND.
    --HG--
    branch : HEAD

--- src/lib-storage/index/mbox/mbox-save.c
+++ src/lib-storage/index/mbox/mbox-save.c
@@ -15,7 +15,7 @@
 
 static char my_hostdomain[256] = "";
 
-static void set_error(MailStorage *storage, const char *mbox_path)
+static int set_error(MailStorage *storage, const char *mbox_path)
 {
 	if (errno == ENOSPC)
 		mail_storage_set_error(storage, "Not enough disk space");
@@ -23,6 +23,8 @@ static void set_error(MailStorage *storage, const char *mbox_path)
 		mail_storage_set_critical(storage, "Error writing to "
 					  "mbox file %s: %m", mbox_path);
 	}
+
+	return FALSE;
 }
 
 static int mbox_check_ending_lf(MailStorage *storage, int fd, off_t pos,
@@ -48,16 +50,13 @@ static int mbox_check_ending_lf(MailStorage *storage, int fd, off_t pos,
 		return TRUE;
 	} while (0);
 
-	set_error(storage, mbox_path);
-	return FALSE;
+	return set_error(storage, mbox_path);
 }
 
 static int mbox_append_lf(MailStorage *storage, int fd, const char *mbox_path)
 {
-	if (write_full(fd, "\n", 1) < 0) {
-		set_error(storage, mbox_path);
-		return FALSE;
-	}
+	if (write_full(fd, "\n", 1) < 0)
+		return set_error(storage, mbox_path);
 
 	return TRUE;
 }
@@ -89,9 +88,57 @@ static int write_from_line(MailStorage *storage, int fd, const char *mbox_path,
 	line = mbox_from_create(sender, internal_date);
 	len = strlen(line);
 
-	if (write_full(fd, line, len) < 0) {
-		set_error(storage, mbox_path);
-		return FALSE;
+	if (write_full(fd, line, len) < 0)
+		return set_error(storage, mbox_path);
+
+	return TRUE;
+}
+
+static int write_flags(MailStorage *storage, int fd, const char *mbox_path,
+		       MailFlags flags, const char *custom_flags[])
+{
+	const char *str;
+	unsigned int field;
+	int i;
+
+	if (flags == 0)
+		return TRUE;
+
+	if (flags & MAIL_SEEN) {
+		if (write_full(fd, "Status: R\n", 10) < 0)
+			return set_error(storage, mbox_path);
+	}
+
+	if (flags & (MAIL_ANSWERED|MAIL_DRAFT|MAIL_FLAGGED|MAIL_DELETED)) {
+		str = t_strconcat("X-Status: ",
+				  (flags & MAIL_ANSWERED) ? "A" : "",
+				  (flags & MAIL_DRAFT) ? "D" : "",
+				  (flags & MAIL_FLAGGED) ? "F" : "",
+				  (flags & MAIL_DELETED) ? "T" : "",
+				  "\n", NULL);
+
+		if (write_full(fd, str, strlen(str)) < 0)
+			return set_error(storage, mbox_path);
+	}
+
+	if (flags & MAIL_CUSTOM_FLAGS_MASK) {
+		if (write_full(fd, "X-Keywords:", 11) < 0)
+			return set_error(storage, mbox_path);
+
+		field = 1 << MAIL_CUSTOM_FLAG_1_BIT;
+		for (i = 0; i < MAIL_CUSTOM_FLAGS_COUNT; i++, field <<= 1) {
+			if ((flags & field) && custom_flags[i] != NULL) {
+				if (write_full(fd, " ", 1) < 0)
+					return set_error(storage, mbox_path);
+
+				if (write_full(fd, custom_flags[i],
+					       strlen(custom_flags[i])) < 0)
+					return set_error(storage, mbox_path);
+			}
+		}
+
+		if (write_full(fd, "\n", 1) < 0)
+			return set_error(storage, mbox_path);
 	}
 
 	return TRUE;
@@ -140,6 +187,8 @@ int mbox_storage_save(Mailbox *box, MailFlags flags, const char *custom_flags[],
 		if (!mbox_check_ending_lf(box->storage, fd, pos, mbox_path) ||
 		    !write_from_line(box->storage, fd, mbox_path,
 				     internal_date) ||
+		    !write_flags(box->storage, fd, mbox_path, flags,
+				 custom_flags) ||
 		    !index_storage_save_into_fd(box->storage, fd, mbox_path,
 						data, data_size) ||
 		    !mbox_append_lf(box->storage, fd, mbox_path)) {
