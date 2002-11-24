850f5063cc1228fa19e91323b51e1f997577f3f8
Timo Sirainen <tss@iki.fi>
2002-11-25 00:07:53 +0200

    Added script to easily generate self-signed certificate.
    --HG--
    branch : HEAD

---
+++ doc/mkcert.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Generates a self-signed certificate.
+
+OPENSSL=${OPENSSL-openssl}
+SSLDIR=${SSLDIR-/etc/ssl}
+OPENSSLCONFIG=${OPENSSLCONFIG-dovecot-openssl.cnf}
+
+CERTFILE=$SSLDIR/certs/dovecot.cer
+KEYFILE=$SSLDIR/private/dovecot.key
+
+if [ ! -d $SSLDIR/certs ]; then
+  echo $SSLDIR/certs directory doesn't exist
+fi
+
+if [ ! -d $SSLDIR/private ]; then
+  echo $SSLDIR/private directory doesn't exist
+fi
+
+if [ -f $CERTFILE ]; then
+  echo "$CERTFILE already exists, won't overwrite"
+  exit 1
+fi
+
+if [ -f $KEYFILE ]; then
+  echo "$KEYFILE already exists, won't overwrite"
+  exit 1
+fi
+
+$OPENSSL req -new -x509 -nodes -config $OPENSSLCONFIG -out $CERTFILE -keyout $KEYFILE || exit 2
+chmod 0600 $KEYFILE
+echo 
+$OPENSSL x509 -subject -fingerprint -noout -in $SSLDIR/certs/dovecot.cer || exit 2
