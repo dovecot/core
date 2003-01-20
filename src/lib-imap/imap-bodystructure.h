ab5efa23c60cdc394ea4a7508364ff5f0e8e005b
Timo Sirainen <tss@iki.fi>
2003-01-20 15:54:42 +0200

    comment update
    --HG--
    branch : HEAD

--- src/lib-imap/imap-bodystructure.h
+++ src/lib-imap/imap-bodystructure.h
@@ -4,7 +4,9 @@
 struct message_part;
 
 /* If *part is non-NULL, it's used as base for building the body structure.
-   Otherwise it's set to the root message_part and parsed. */
+   Otherwise it's set to the root message_part and parsed. pool is used only
+   for allocating message_part, not the return value which is from data
+   stack. */
 const char *imap_part_get_bodystructure(pool_t pool, struct message_part **part,
 					struct istream *input, int extended);
 
