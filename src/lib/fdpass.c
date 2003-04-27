61d810cccf3590dc3b84e50df29538f8f6196c4a
Timo Sirainen <tss@iki.fi>
2003-04-28 02:55:32 +0300

    AIX fix
    --HG--
    branch : HEAD

--- src/lib/fdpass.c
+++ src/lib/fdpass.c
@@ -32,9 +32,9 @@
 */
 
 #define _XPG4_2
-#define _XOPEN_SOURCE /* for IRIX */
 
 #ifndef _AIX
+#  define _XOPEN_SOURCE /* for IRIX, breaks AIX */
 #  define _XOPEN_SOURCE_EXTENDED /* for Tru64, breaks AIX */
 #endif
 
