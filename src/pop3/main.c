aa78a2506ee69a6bd58cc706210ee79b62ec0954
Timo Sirainen <tss@iki.fi>
2003-07-15 12:44:56 +0300

    Added missing randgen initialization.
    --HG--
    branch : HEAD

--- src/pop3/main.c
+++ src/pop3/main.c
@@ -6,6 +6,7 @@
 #include "restrict-access.h"
 #include "fd-close-on-exec.h"
 #include "process-title.h"
+#include "randgen.h"
 #include "module-dir.h"
 #include "mail-storage.h"
 
@@ -55,6 +56,10 @@ static void drop_privileges(void)
 {
 	/* Log file or syslog opening probably requires roots */
 	open_logfile();
+
+	/* Most likely needed. Have to open /dev/urandom before possible
+	   chrooting. */
+	random_init();
 
 	restrict_access_by_env(!IS_STANDALONE());
 }
@@ -113,6 +118,7 @@ static void main_deinit(void)
 
 	clients_deinit();
         mail_storage_deinit();
+	random_deinit();
 
 	closelog();
 }
