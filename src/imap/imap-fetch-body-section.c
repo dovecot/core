006abdf1d83c078ad7a053274dda852060b5773f
Timo Sirainen <tss@iki.fi>
2004-04-26 01:30:25 +0300

    Fixes for fetching BODY[n], patch by Rick Jones
    --HG--
    branch : HEAD

--- src/imap/imap-fetch-body-section.c
+++ src/imap/imap-fetch-body-section.c
@@ -462,8 +462,10 @@ static int part_find(struct mail *mail, const struct imap_fetch_body_data *body,
 		}
 
 		if (part != NULL &&
-		    (part->flags & MESSAGE_PART_FLAG_MESSAGE_RFC822)) {
-			/* skip the message/rfc822 part */
+		    (part->flags & MESSAGE_PART_FLAG_MESSAGE_RFC822) &&
+		    (*path >= '0' && *path <= '9' || strncmp(path, "HEADER", 6) == 0)) {
+			/* if remainder of path is a number or "HEADER",
+				skip the message/rfc822 part */
 			part = part->children;
 		}
 	}
