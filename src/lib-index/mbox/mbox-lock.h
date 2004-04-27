0d6c06233b2cdc5d854329ea6de3e1a0e244cdb4
Timo Sirainen <tss@iki.fi>
2004-04-27 23:14:15 +0300

    removed out of the way of new index code
    --HG--
    branch : HEAD

--- src/lib-index/mbox/mbox-lock.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MBOX_LOCK_H
-#define __MBOX_LOCK_H
-
-/* NOTE: if mbox file is not open, it's opened. if it is open but file has
-   been overwritten (ie. inode has changed), it's reopened. */
-int mbox_lock(struct mail_index *index, enum mail_lock_type lock_type);
-int mbox_unlock(struct mail_index *index);
-
-#endif
