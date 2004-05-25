54d88358e596ed91fdb741ece5a10f482da9cc13
Timo Sirainen <tss@iki.fi>
2004-05-25 16:01:25 +0300

    compile fix
    --HG--
    branch : HEAD

--- src/login-common/ssl-proxy.c
+++ src/login-common/ssl-proxy.c
@@ -9,9 +9,15 @@ int ssl_initialized = FALSE;
 
 /* no SSL support */
 
-int ssl_proxy_new(int fd __attr_unused__, struct ip_addr *ip __attr_unused__)
+int ssl_proxy_new(int fd __attr_unused__, struct ip_addr *ip __attr_unused__,
+		  struct ssl_proxy **proxy_r __attr_unused__)
 {
 	return -1;
+}
+
+int ssl_proxy_has_valid_client_cert(struct ssl_proxy *proxy __attr_unused__)
+{
+	return FALSE;
 }
 
 void ssl_proxy_init(void) {}
