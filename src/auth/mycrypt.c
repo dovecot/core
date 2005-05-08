8ec00f653e1d4a0af107d107d333d8bc8ad8bccb
Timo Sirainen <tss@iki.fi>
2005-05-08 20:35:47 +0300

    No, The _XPG6 define is still needed with final Solaris 10 release..
    --HG--
    branch : HEAD

--- src/auth/mycrypt.c
+++ src/auth/mycrypt.c
@@ -2,6 +2,7 @@
 #define _XOPEN_SOURCE_EXTENDED 1
 #define _XOPEN_VERSION 4
 #define _XPG4_2
+#define _XPG6 /* for Solaris 10 to avoid #error in feature_tests.h */
 #include <unistd.h>
 
 #include "mycrypt.h"
