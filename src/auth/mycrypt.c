ea86515671db4776e9f70ba27c5bfdf899da31de
Timo Sirainen <tss@iki.fi>
2005-05-08 20:42:04 +0300

    Added comment
    --HG--
    branch : HEAD

--- src/auth/mycrypt.c
+++ src/auth/mycrypt.c
@@ -1,5 +1,5 @@
 #define _XOPEN_SOURCE 4
-#define _XOPEN_SOURCE_EXTENDED 1
+#define _XOPEN_SOURCE_EXTENDED 1 /* 1 needed for AIX */
 #define _XOPEN_VERSION 4
 #define _XPG4_2
 #define _XPG6 /* for Solaris 10 to avoid #error in feature_tests.h */
