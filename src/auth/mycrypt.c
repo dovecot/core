e6281f57ea8ea965db6dfa7ed45f07c1ac95aeaa
Timo Sirainen <tss@iki.fi>
2005-09-18 20:37:17 +0300

    Add _XPG6 macro if needed..
    --HG--
    branch : HEAD

--- src/auth/mycrypt.c
+++ src/auth/mycrypt.c
@@ -2,6 +2,9 @@
 #define _XOPEN_SOURCE_EXTENDED 1 /* 1 needed for AIX */
 #define _XOPEN_VERSION 4
 #define _XPG4_2
+#ifdef CRYPT_USE_XPG6
+#  define _XPG6 /* Some Solaris versions require this, some break with this */
+#endif
 #include <unistd.h>
 
 #include "mycrypt.h"
