43e116129adf73eeca96d26132d2ecc8081b0bb3
Timo Sirainen <tss@iki.fi>
2005-09-18 20:01:14 +0300

    typofix in error message
    --HG--
    branch : HEAD

--- src/lib-storage/index/maildir/maildir-save.c
+++ src/lib-storage/index/maildir/maildir-save.c
@@ -218,7 +218,7 @@ int maildir_save_continue(struct mail_save_context *_ctx)
 
 	if (o_stream_send_istream(ctx->output, ctx->input) < 0) {
 		mail_storage_set_critical(STORAGE(ctx->mbox->storage),
-			"o_stream_send_istrea(%s) failed: %m",
+			"o_stream_send_istream(%s) failed: %m",
 			t_strconcat(ctx->tmpdir, "/",
 				    ctx->files->basename, NULL));
 		ctx->failed = TRUE;
