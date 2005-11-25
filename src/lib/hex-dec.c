e6055ac974358d31bb2269731dcac57fbe910cc2
Timo Sirainen <tss@iki.fi>
2005-11-25 17:19:49 +0200

    Added decimal <-> hex string translation functions.
    --HG--
    branch : HEAD

---
+++ src/lib/hex-dec.c
@@ -0,0 +1,36 @@
+/* Copyright (c) 2005 Timo Sirainen */
+
+#include "lib.h"
+#include "hex-dec.h"
+
+void dec2hex(unsigned char *hexstr, uintmax_t dec, unsigned int hexstr_size)
+{
+	unsigned int i;
+
+	for (i = 0; i < hexstr_size; i++) {
+		unsigned int value = dec & 0x0f;
+		if (value < 10)
+			hexstr[hexstr_size-i-1] = value + '0';
+		else
+			hexstr[hexstr_size-i-1] = value - 10 + 'A';
+		dec >>= 4;
+	}
+}
+
+uintmax_t hex2dec(const unsigned char *data, unsigned int len)
+{
+	unsigned int i;
+	uintmax_t value = 0;
+
+	for (i = 0; i < len; i++) {
+		value = value*0x10;
+		if (data[i] >= '0' && data[i] <= '9')
+			value += data[i]-'0';
+		else if (data[i] >= 'A' && data[i] <= 'F')
+			value += data[i]-'A' + 10;
+		else
+			return 0;
+	}
+	return value;
+}
+
---
+++ src/lib/hex-dec.h
@@ -0,0 +1,12 @@
+#ifndef __HEX_DEC_H
+#define __HEX_DEC_H
+
+#define DEC2HEX(hexstr, str) \
+	dec2hex(hexstr, str, sizeof(hexstr))
+
+/* Decimal -> hex string translation. The result isn't NUL-terminated. */
+void dec2hex(unsigned char *hexstr, uintmax_t dec, unsigned int hexstr_size);
+/* Parses hex string and returns its decimal value, or 0 in case of errors */
+uintmax_t hex2dec(const unsigned char *data, unsigned int len);
+
+#endif
