95098bec42f5383615bcae840b717c4c29888d9a
Timo Sirainen <tss@iki.fi>
2006-04-13 03:56:22 +0300

    pop3_lock_session setting wasn't transferred to pop3 process
    --HG--
    branch : HEAD

--- src/master/mail-process.c
+++ src/master/mail-process.c
@@ -241,6 +241,8 @@ mail_process_set_environment(struct settings *set, const char *mail,
 		env_put("POP3_REUSE_XUIDL=1");
 	if (set->pop3_enable_last)
 		env_put("POP3_ENABLE_LAST=1");
+	if (set->pop3_lock_session)
+		env_put("POP3_LOCK_SESSION=1");
 	if (set->mbox_dirty_syncs)
 		env_put("MBOX_DIRTY_SYNCS=1");
 	if (set->mbox_very_dirty_syncs)
