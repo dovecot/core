b4931fc08faa0079f32f29286f4c3abd0d8788bc
Timo Sirainen <timo.sirainen@movial.fi>
2006-04-24 12:17:20 +0300

    Added assert
    --HG--
    branch : HEAD

--- src/lib-index/mail-index.c
+++ src/lib-index/mail-index.c
@@ -402,6 +402,7 @@ int mail_index_map_parse_keywords(struct mail_index *index,
 	   - struct mail_index_keyword_header_rec * keywords_count
 	   - const char names[] * keywords_count
 	*/
+	i_assert(ext->hdr_offset < map->hdr.header_size);
 	kw_hdr = CONST_PTR_OFFSET(map->hdr_base, ext->hdr_offset);
 	kw_rec = (const void *)(kw_hdr + 1);
 	name = (const char *)(kw_rec + kw_hdr->keywords_count);
