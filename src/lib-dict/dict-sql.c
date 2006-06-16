0d977de2f7090f42aa3ebe58e053335ad932f015
Timo Sirainen <tss@iki.fi>
2006-06-16 12:54:14 +0300

    Fixes to SQL update statements
    --HG--
    branch : HEAD

--- src/lib-dict/dict-sql.c
+++ src/lib-dict/dict-sql.c
@@ -306,23 +306,23 @@ static void sql_dict_set(struct dict_transaction_context *_ctx,
 	t_push();
 	if (priv) {
 		query = t_strdup_printf(
-			"INSERT INTO %s (%s, %s, %s) VALUES (%s, %s, %s) "
+			"INSERT INTO %s (%s, %s, %s) VALUES ('%s', '%s', '%s') "
 			"ON DUPLICATE KEY UPDATE %s = '%s'",
 			dict->table, dict->select_field, dict->where_field,
 			dict->username_field,
 			sql_escape_string(dict->db, key),
 			sql_escape_string(dict->db, value),
 			sql_escape_string(dict->db, dict->username),
-			sql_escape_string(dict->db, key),
+			dict->select_field,
 			sql_escape_string(dict->db, value));
 	} else {
 		query = t_strdup_printf(
-			"INSERT INTO %s (%s, %s) VALUES (%s, %s) "
+			"INSERT INTO %s (%s, %s) VALUES ('%s', '%s') "
 			"ON DUPLICATE KEY UPDATE %s = '%s'",
 			dict->table, dict->select_field, dict->where_field,
 			sql_escape_string(dict->db, key),
 			sql_escape_string(dict->db, value),
-                        sql_escape_string(dict->db, key),
+                        dict->select_field,
 			sql_escape_string(dict->db, value));
 	}
 	sql_update(ctx->sql_ctx, query);
@@ -344,22 +344,20 @@ static void sql_dict_atomic_inc(struct dict_transaction_context *_ctx,
 	t_push();
 	if (priv) {
 		query = t_strdup_printf(
-			"INSERT INTO %s (%s, %s, %s) VALUES (%s, %lld, %s) "
+			"INSERT INTO %s (%s, %s, %s) VALUES ('%s', %lld, '%s') "
 			"ON DUPLICATE KEY UPDATE %s = %s + %lld",
 			dict->table, dict->select_field, dict->where_field,
 			dict->username_field,
                         sql_escape_string(dict->db, key), diff,
 			sql_escape_string(dict->db, dict->username),
-                        sql_escape_string(dict->db, key),
-			sql_escape_string(dict->db, key), diff);
+                        dict->select_field, dict->select_field, diff);
 	} else {
 		query = t_strdup_printf(
 			"INSERT INTO %s (%s, %s) VALUES (%s, %lld) "
 			"ON DUPLICATE KEY UPDATE %s = %s + %lld",
 			dict->table, dict->select_field, dict->where_field,
                         sql_escape_string(dict->db, key), diff,
-                        sql_escape_string(dict->db, key),
-			sql_escape_string(dict->db, key), diff);
+                        dict->select_field, dict->select_field, diff);
 	}
 	sql_update(ctx->sql_ctx, query);
 	t_pop();
