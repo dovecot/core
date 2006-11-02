6e4cd4ba520bc22ce375de378f4751136ebcf75a
Timo Sirainen <tss@iki.fi>
2006-11-02 19:26:31 +0200

    Last changes broke it badly.
    --HG--
    branch : HEAD

--- src/lib-storage/index/mbox/mbox-sync.c
+++ src/lib-storage/index/mbox/mbox-sync.c
@@ -799,8 +799,8 @@ mbox_sync_handle_missing_space(struct mbox_sync_mail_context *mail_ctx)
 			     array_count(&mail_ctx->mail.keywords));
 		array_append_array(&keywords_copy, &mail_ctx->mail.keywords);
 		mail_ctx->mail.keywords = keywords_copy;
-		array_append(&sync_ctx->mails, &mail_ctx->mail, 1);
 	}
+	array_append(&sync_ctx->mails, &mail_ctx->mail, 1);
 
 	sync_ctx->space_diff += mail_ctx->mail.space;
 	if (sync_ctx->space_diff < 0) {
