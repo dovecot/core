497bf5aec8a81e310077f74bc4d8025c50ec32f7
Timo Sirainen <tss@iki.fi>
2007-01-04 15:22:01 +0200

    Comment out the configurable dir environments.
    --HG--
    branch : HEAD

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # If you've non-standard directories, set these
-ACLOCAL_DIR=
-GETTEXT_DIR=
+#ACLOCAL_DIR=
+#GETTEXT_DIR=
 
 if test "$ACLOCAL_DIR" != ""; then
   ACLOCAL="aclocal -I $ACLOCAL_DIR"
