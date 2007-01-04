ec4e08ad363aa5057faa4886f4c195c56b85140a
Timo Sirainen <tss@iki.fi>
2007-01-04 02:53:33 +0200

    Rewrite. Use autoreconf now. Also copy config.rpath from gettext's directory.
    --HG--
    branch : HEAD

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,20 @@
-aclocal
-autoheader
-libtoolize --force
-automake --add-missing
-autoconf
+#!/bin/sh
+
+# If you've non-standard directories, set these
+ACLOCAL_DIR=
+GETTEXT_DIR=
+
+if test "$ACLOCAL_DIR" != ""; then
+  ACLOCAL="aclocal -I $ACLOCAL_DIR"
+  export ACLOCAL
+fi
+
+autoreconf -i -f
+
+for dir in $GETTEXT_DIR /usr/share/gettext; do
+  if test -f $dir/config.rpath; then
+    /bin/cp -f $dir/config.rpath .
+    break
+  fi
+done
+
