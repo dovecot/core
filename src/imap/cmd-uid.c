fc58b668a28ad8f21efa737f969027518725ad9a
Timo Sirainen <tss@iki.fi>
2007-02-06 13:44:57 +0200

    Don't crash with unknown UID commands.
    --HG--
    branch : HEAD

--- src/imap/cmd-uid.c
+++ src/imap/cmd-uid.c
@@ -14,15 +14,14 @@ bool cmd_uid(struct client_command_context *cmd)
 		return FALSE;
 
 	command = command_find(t_strconcat("UID ", cmd_name, NULL));
-	cmd->cmd_flags = command->flags;
-	cmd->func = command->func;
-
-	if (cmd->func != NULL) {
-		cmd->uid = TRUE;
-		return cmd->func(cmd);
-	} else {
+	if (command == NULL) {
 		client_send_tagline(cmd, t_strconcat(
 			"BAD Unknown UID command ", cmd_name, NULL));
 		return TRUE;
 	}
+
+	cmd->cmd_flags = command->flags;
+	cmd->func = command->func;
+	cmd->uid = TRUE;
+	return cmd->func(cmd);
 }
