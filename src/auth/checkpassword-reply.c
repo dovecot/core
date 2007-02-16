b0b381251dfbfe04ea2763b6d4a359222e3a047d
Timo Sirainen <tss@iki.fi>
2007-02-16 19:13:07 +0200

    Don't crash if USER or HOME environment is missing. Allow HOME environment not to exist.
    --HG--
    branch : HEAD

--- src/auth/checkpassword-reply.c
+++ src/auth/checkpassword-reply.c
@@ -10,26 +10,33 @@
 int main(void)
 {
 	string_t *str;
+	const char *user, *home;
 	const char *extra_env, *value, *const *tmp;
 
 	lib_init();
 	str = t_str_new(1024);
 
-	if (strchr(getenv("USER"), '\t') != NULL) {
-		i_error("USER contains TAB");
-		return 1;
-	}
-	if (strchr(getenv("HOME"), '\t') != NULL) {
-		i_error("HOME contains TAB");
+	user = getenv("USER");
+	if (user == NULL)
+		i_fatal("checkpassword: USER environment missing");
+	if (strchr(user, '\t') != NULL) {
+		i_error("checkpassword: USER contains TAB");
 		return 1;
 	}
 
 	str_printfa(str, "userdb_user=%s\t"
-		    "userdb_home=%s\t"
 		    "userdb_uid=%s\t"
 		    "userdb_gid=%s\t",
-		    getenv("USER"), getenv("HOME"),
-		    dec2str(getuid()), dec2str(getgid()));
+		    user, dec2str(getuid()), dec2str(getgid()));
+
+	home = getenv("HOME");
+	if (home != NULL) {
+		if (strchr(home, '\t') != NULL) {
+			i_error("checkpassword: HOME contains TAB");
+			return 1;
+		}
+		str_printfa(str, "userdb_home=%s\t", home);
+	}
 
 	extra_env = getenv("EXTRA");
 	if (extra_env != NULL) {
@@ -43,7 +50,7 @@ int main(void)
 	}
 
 	if (write_full(4, str_data(str), str_len(str)) < 0) {
-		i_error("write_full() failed: %m");
+		i_error("checkpassword: write_full() failed: %m");
 		exit(111);
 	}
 	return 0;
