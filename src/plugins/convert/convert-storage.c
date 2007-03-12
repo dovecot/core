7f9998b6a46e82196bb9997e454d7f6f543073ad
Timo Sirainen <tss@iki.fi>
2007-03-12 14:35:27 +0200

    Mailbox name conversion wasn't done for directories.
    --HG--
    branch : HEAD

--- src/plugins/convert/convert-storage.c
+++ src/plugins/convert/convert-storage.c
@@ -137,9 +137,12 @@ static int mailbox_convert_list_item(struct mail_storage *source_storage,
 	name = strcasecmp(info->name, "INBOX") == 0 ? "INBOX" : info->name;
 	if ((info->flags & MAILBOX_NOSELECT) != 0) {
 		/* \NoSelect mailbox, so it's probably a "directory" */
-		if (mail_storage_mailbox_create(dest_storage, name, TRUE) < 0) {
+		dest_name = mailbox_name_convert(dest_storage, source_storage,
+						 name);
+		if (mail_storage_mailbox_create(dest_storage, dest_name,
+						TRUE) < 0) {
 			i_error("Mailbox conversion: Couldn't create mailbox "
-				"directory %s", name);
+				"directory %s", dest_name);
 			return -1;
 		}
 		return 0;
