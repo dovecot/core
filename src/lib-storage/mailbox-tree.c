031f4a59407a157a920aef36d070a8becfc6688a
Timo Sirainen <tss@iki.fi>
2007-04-11 22:26:59 +0300

    Keep children/nochildren flags updated.
    --HG--
    branch : HEAD

--- src/lib-storage/mailbox-tree.c
+++ src/lib-storage/mailbox-tree.c
@@ -93,8 +93,7 @@ mailbox_tree_traverse(struct mailbox_tree_context *tree, const char *path,
 			(*node)->name = p_strdup(tree->pool, name);
 
 			if (*path != '\0') {
-				(*node)->flags = MAILBOX_NONEXISTENT |
-					MAILBOX_CHILDREN;
+				(*node)->flags = MAILBOX_NONEXISTENT;
 			} else {
 				if (created != NULL)
 					*created = TRUE;
@@ -104,6 +103,8 @@ mailbox_tree_traverse(struct mailbox_tree_context *tree, const char *path,
 		if (*path == '\0')
 			break;
 
+		(*node)->flags |= MAILBOX_CHILDREN;
+		(*node)->flags &= ~(MAILBOX_NOCHILDREN | MAILBOX_NOINFERIORS);
 		name = path+1;
 		node = &(*node)->children;
 	}
