62b1acbf187f5d611c82be168d716fa2db1f6edd
Timo Sirainen <tss@iki.fi>
2007-06-11 02:27:55 +0300

    These files are MIT licensed nowadays.
    --HG--
    branch : HEAD

--- src/lib/ioloop-notify-kqueue.c
+++ src/lib/ioloop-notify-kqueue.c
@@ -2,11 +2,6 @@
  * BSD kqueue() based ioloop notify handler.
  *
  * Copyright (c) 2005 Vaclav Haisman <v.haisman@sh.cvut.cz>
- *
- * This library is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
  */
 
 #define _GNU_SOURCE
