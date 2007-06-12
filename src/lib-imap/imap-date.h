30349af4b3815daf8db125db98917a73469b599d
Timo Sirainen <tss@iki.fi>
2007-06-12 18:06:38 +0300

    Fixed comments.
    --HG--
    branch : HEAD

--- src/lib-imap/imap-date.h
+++ src/lib-imap/imap-date.h
@@ -1,13 +1,13 @@
 #ifndef __IMAP_DATE_H
 #define __IMAP_DATE_H
 
-/* Parses IMAP date/time string. time_t is filled with UTC date.
-   timezone_offset is filled with parsed timezone. If no timezone is given,
-   local timezone is assumed.
+/* Parses IMAP date/time string and returns TRUE if the string is valid.
+   time_t is filled with UTC date. timezone_offset is filled with parsed
+   timezone. If no timezone is given, local timezone is assumed.
 
-   Returns TRUE if string is valid. If date is outside valid range for time_t
-   (usually only with 32bit time_t), the function still return TRUE but
-   returned time is (time_t)-1. */
+   If date is too low or too high to fit to time_t, it's set to lowest/highest
+   allowed value. This allows you to use the value directly for comparing
+   timestamps. */
 bool imap_parse_date(const char *str, time_t *time);
 bool imap_parse_datetime(const char *str, time_t *time, int *timezone_offset);
 
