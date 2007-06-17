dd0e6d4d8a01a67457091cdcef5f322df0805879
Timo Sirainen <tss@iki.fi>
2007-06-17 22:08:50 +0300

    Even if mailbox sync fails, commit the transaction so that cache file gets updated.
    --HG--
    branch : HEAD

--- src/pop3/client.c
+++ src/pop3/client.c
@@ -101,7 +101,7 @@ static int init_mailbox(struct client *client)
 		mail_free(&mail);
 		if (mailbox_search_deinit(&ctx) < 0) {
 			client_send_storage_error(client);
-			mailbox_transaction_rollback(&t);
+			(void)mailbox_transaction_commit(&t, 0);
 			break;
 		}
 
@@ -112,8 +112,9 @@ static int init_mailbox(struct client *client)
 			return TRUE;
 		}
 
-		/* well, sync and try again */
-		mailbox_transaction_rollback(&t);
+		/* well, sync and try again. we might have cached virtual
+		   sizes, make sure they get committed. */
+		(void)mailbox_transaction_commit(&t, 0);
 	}
 
 	if (i == 2)
