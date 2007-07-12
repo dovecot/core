a89b1c5c1485b8ed9dc2c611ad1f72f20c0ed6af
Timo Sirainen <tss@iki.fi>
2007-07-13 02:52:09 +0300

    Compile fix.
    --HG--
    branch : HEAD

--- src/lib/ioloop-notify-none.c
+++ src/lib/ioloop-notify-none.c
@@ -6,11 +6,13 @@
 #ifdef IOLOOP_NOTIFY_NONE
 
 #undef io_add_notify
-struct io *io_add_notify(const char *path __attr_unused__,
-			 io_callback_t *callback __attr_unused__,
-			 void *context __attr_unused__)
+enum io_notify_result
+io_add_notify(const char *path __attr_unused__,
+	      io_callback_t *callback __attr_unused__,
+	      void *context __attr_unused__, struct io **io_r)
 {
-	return NULL;
+	*io_r = NULL;
+	return IO_NOTIFY_DISABLED;
 }
 
 void io_loop_notify_remove(struct ioloop *ioloop __attr_unused__,
