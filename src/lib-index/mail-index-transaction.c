b5efcae1db660e69ed7126521735a3f3f1ddd10c
Timo Sirainen <tss@iki.fi>
2007-07-15 00:37:57 +0300

    mail_index_ext_reset() and mail_index_ext_set_reset_id() need to clear ext header updates and resizes. mail_index_ext_set_reset_id() also needs to clear ext resets.
    --HG--
    branch : HEAD

--- src/lib-index/mail-index-transaction.c
+++ src/lib-index/mail-index-transaction.c
@@ -868,6 +868,25 @@ void mail_index_ext_set_reset_id(struct mail_index_transaction *t,
 		if (array_is_created(array))
 			array_clear(array);
 	}
+	if (array_is_created(&t->ext_hdr_updates) &&
+	    ext_id < array_count(&t->ext_hdr_updates)) {
+		/* if extension headers have been updated, clear them */
+		struct mail_index_transaction_ext_hdr_update **hdr;
+
+		hdr = array_idx_modifiable(&t->ext_hdr_updates, ext_id);
+		if (*hdr != NULL)
+			i_free_and_null(*hdr);
+	}
+	if (array_is_created(&t->ext_resets) &&
+	    ext_id < array_count(&t->ext_resets)) {
+		/* clear resets */
+		array_idx_clear(&t->ext_resets, ext_id);
+	}
+	if (array_is_created(&t->ext_resizes) &&
+	    ext_id < array_count(&t->ext_resizes)) {
+		/* clear resizes */
+		array_idx_clear(&t->ext_resets, ext_id);
+	}
 
 	if (!array_is_created(&t->ext_reset_ids))
 		i_array_init(&t->ext_reset_ids, ext_id + 2);
