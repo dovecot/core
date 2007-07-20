810bf6e564ebef9655fc0ca3b5273fbb6022c098
Timo Sirainen <tss@iki.fi>
2007-07-20 17:21:41 +0300

    Added BINARY_NUMBER_SEARCH() macro.
    --HG--
    branch : HEAD

--- src/lib/bsearch-insert-pos.h
+++ src/lib/bsearch-insert-pos.h
@@ -1,6 +1,25 @@
 #ifndef __BSEARCH_INSERT_POS
 #define __BSEARCH_INSERT_POS
 
+/* Binary search template */
+#define BINARY_NUMBER_SEARCH(data, count, value, idx_r) \
+	unsigned int idx, left_idx, right_idx;        \
+						      \
+	idx = 0; left_idx = 0; right_idx = (count);   \
+	while (left_idx < right_idx) {                \
+		idx = (left_idx + right_idx) / 2;     \
+						      \
+		if ((data)[idx] < (value))            \
+			left_idx = idx+1;             \
+		else if ((data)[idx] > (value))       \
+			right_idx = idx;              \
+		else {                                \
+			*(idx_r) = idx;               \
+			return TRUE;                  \
+		}                                     \
+	}                                             \
+	return FALSE
+
 /* If key is found, returns TRUE and sets idx_r to the position where the key
    was found. If key isn't found, returns FALSE and sets idx_r to the position
    where the key should be inserted. */
