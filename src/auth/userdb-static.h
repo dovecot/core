0afb344c73989fd7692cfee1ff03ac1f765b29a5
Timo Sirainen <tss@iki.fi>
2007-08-11 14:27:40 +0300

    Forgot to add.
    --HG--
    branch : HEAD

---
+++ src/auth/userdb-static.h
@@ -0,0 +1,14 @@
+#ifndef __USERDB_STATIC_H
+#define __USERDB_STATIC_H
+
+struct userdb_static_template *
+userdb_static_template_build(pool_t pool, const char *userdb_name,
+			     const char *args);
+bool userdb_static_template_isset(struct userdb_static_template *tmpl,
+				  const char *key);
+bool userdb_static_template_remove(struct userdb_static_template *tmpl,
+				   const char *key, const char **value_r);
+void userdb_static_template_export(struct userdb_static_template *tmpl,
+				   struct auth_request *auth_request);
+
+#endif
