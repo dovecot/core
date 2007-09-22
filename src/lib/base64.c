b54adb1e9dff8400d8277bff525c9c013751574d
Timo Sirainen <tss@iki.fi>
2007-09-22 23:14:30 +0300

    Rewrote base64 functions so they're now MIT licensed.
    --HG--
    branch : HEAD

--- src/lib/base64.c
+++ src/lib/base64.c
@@ -1,104 +1,83 @@
-/* Based on the sources of Cyrus IMAP:
- *
- * Copyright (c) 2000 Carnegie Mellon University.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The name "Carnegie Mellon University" must not be used to
- *    endorse or promote products derived from this software without
- *    prior written permission. For permission or any other legal
- *    details, please contact  
- *      Office of Technology Transfer
- *      Carnegie Mellon University
- *      5000 Forbes Avenue
- *      Pittsburgh, PA  15213-3890
- *      (412) 268-4387, fax: (412) 268-7395
- *      tech-transfer@andrew.cmu.edu
- *
- * 4. Redistributions of any form whatsoever must retain the following
- *    acknowledgment:
- *    "This product includes software developed by Computing Services
- *     at Carnegie Mellon University (http://www.cmu.edu/computing/)."
- *
- * CARNEGIE MELLON UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO
- * THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS, IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY BE LIABLE
- * FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
- * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
- * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
+/* Copyright (c) 2007 Dovecot authors, see the included COPYING file */
 
 #include "lib.h"
 #include "base64.h"
 #include "buffer.h"
 
-static const char basis_64[] =
-   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+static const char b64enc[] =
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+static const unsigned char b64dec[256] = {
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, /* 0-7 */
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, /* 8-15 */
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, /* 16-23 */
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, /* 24-31 */
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, /* 32-39 */
+	0xff, 0xff, 0xff, 0x3e, 0xff, 0xff, 0xff, 0x3f, /* 40-47 */
+	0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x3b, /* 48-55 */
+	0x3c, 0x3d, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, /* 56-63 */
+	0xff, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, /* 64-71 */
+	0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, /* 72-79 */
+	0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, /* 80-87 */
+	0x17, 0x18, 0x19, 0xff, 0xff, 0xff, 0xff, 0xff, /* 88-95 */
+	0xff, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, /* 96-103 */
+	0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, /* 104-111 */
+	0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f, 0x30, /* 112-119 */
+	0x31, 0x32, 0x33, 0xff, 0xff, 0xff, 0xff, 0xff, /* 120-127 */
+
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, /* 128-255 */
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+};
 
 void base64_encode(const void *src, size_t src_size, buffer_t *dest)
 {
 	const unsigned char *src_c = src;
+	unsigned char tmp[4];
 	size_t src_pos;
-	int c1, c2, c3;
 
 	for (src_pos = 0; src_pos < src_size; ) {
-		c1 = src_c[src_pos++];
-		buffer_append_c(dest, basis_64[c1 >> 2]);
-
-		c2 = src_pos == src_size ? 0 : src_c[src_pos];
-		buffer_append_c(dest, basis_64[((c1 & 0x03) << 4) |
-					       ((c2 & 0xf0) >> 4)]);
-
-		if (src_pos++ == src_size) {
-			buffer_append(dest, "==", 2);
+		tmp[0] = b64enc[src_c[src_pos] >> 2];
+		switch (src_size - src_pos) {
+		case 1:
+			tmp[1] = b64enc[(src_c[src_pos] & 0x03) << 4];
+			tmp[2] = '=';
+			tmp[3] = '=';
+			src_pos++;
+			break;
+		case 2:
+			tmp[1] = b64enc[((src_c[src_pos] & 0x03) << 4) |
+					((src_c[src_pos+1] & 0xf0) >> 4)];
+			tmp[2] = b64enc[((src_c[src_pos+1] & 0x0f) << 2) |
+					((src_c[src_pos+2] & 0xc0) >> 6)];
+			tmp[3] = '=';
+			src_pos += 2;
+			break;
+		default:
+			tmp[1] = b64enc[((src_c[src_pos] & 0x03) << 4) |
+					((src_c[src_pos+1] & 0xf0) >> 4)];
+			tmp[2] = b64enc[((src_c[src_pos+1] & 0x0f) << 2) |
+					((src_c[src_pos+2] & 0xc0) >> 6)];
+			tmp[3] = b64enc[src_c[src_pos+2] & 0x3f];
+			src_pos += 3;
 			break;
 		}
-
-		c3 = src_pos == src_size ? 0 : src_c[src_pos];
-		buffer_append_c(dest, basis_64[((c2 & 0x0f) << 2) |
-					       ((c3 & 0xc0) >> 6)]);
-
-		if (src_pos++ == src_size) {
-			buffer_append_c(dest, '=');
-			break;
-		}
-
-		buffer_append_c(dest, basis_64[c3 & 0x3f]);
+		buffer_append(dest, tmp, 4);
 	}
 }
-
-#define XX 127
-
-/* Table for decoding base64 */
-static const char index_64[256] = {
-    XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX,
-    XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX,
-    XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,62, XX,XX,XX,63,
-    52,53,54,55, 56,57,58,59, 60,61,XX,XX, XX,XX,XX,XX,
-    XX, 0, 1, 2,  3, 4, 5, 6,  7, 8, 9,10, 11,12,13,14,
-    15,16,17,18, 19,20,21,22, 23,24,25,XX, XX,XX,XX,XX,
-    XX,26,27,28, 29,30,31,32, 33,34,35,36, 37,38,39,40,
-    41,42,43,44, 45,46,47,48, 49,50,51,XX, XX,XX,XX,XX,
-    XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX,
-    XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX,
-    XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX,
-    XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX,
-    XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX,
-    XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX,
-    XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX,
-    XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX, XX,XX,XX,XX,
-};
 
 #define IS_EMPTY(c) \
 	((c) == '\n' || (c) == '\r' || (c) == ' ' || (c) == '\t')
@@ -108,56 +87,45 @@ int base64_decode(const void *src, size_t src_size,
 {
 	const unsigned char *src_c = src;
 	size_t src_pos;
-	unsigned char buf[4];
-	int c1, c2, c3, c4;
-	size_t size;
+	unsigned char input[4], output[3];
 	int ret = 1;
 
-	for (src_pos = 0; src_pos+3 < src_size; ) {
-		c1 = src_c[src_pos++];
-
-		if (IS_EMPTY(c1))
-			continue;
-
-		if (index_64[c1] == XX)
+	for (src_pos = 0; src_pos+3 < src_size; src_pos += 4) {
+		input[0] = b64dec[src_c[src_pos]];
+		if (input[0] == 0xff) {
+			if (IS_EMPTY(src_c[src_pos++]))
+				continue;
 			return -1;
-
-		c2 = src_c[src_pos++];
-		if (index_64[c2] == XX)
-			return -1;
-
-		c3 = src_c[src_pos++];
-		if (c3 != '=' && index_64[c3] == XX)
-			return -1;
-
-		c4 = src_c[src_pos++];
-		if (c4 != '=' && index_64[c4] == XX)
-			return -1;
-
-		buf[0] = (index_64[c1] << 2) | ((index_64[c2] & 0x30) >> 4);
-		if (c3 == '=') {
-			if (c4 != '=')
-				return -1;
-			size = 1;
-		} else {
-			buf[1] = ((index_64[c2] & 0xf) << 4) |
-				((index_64[c3] & 0x3c) >> 2);
-
-			if (c4 == '=')
-				size = 2;
-			else {
-				buf[2] = ((index_64[c3] & 0x3) << 6) |
-					index_64[c4];
-				size = 3;
-			}
 		}
 
-		buffer_append(dest, buf, size);
-		if (size < 3) {
-			/* end of base64 data */
+		input[1] = b64dec[src_c[src_pos+1]];
+		if (input[1] == 0xff)
+			return -1;
+		output[0] = (input[0] << 2) | (input[1] >> 4);
+
+		input[2] = b64dec[src_c[src_pos+2]];
+		if (input[2] == 0xff) {
+			if (src_c[src_pos+2] != '=' || src_c[src_pos+3] != '=')
+				return -1;
+			buffer_append(dest, output, 1);
 			ret = 0;
+			src_pos += 4;
 			break;
 		}
+
+		output[1] = (input[1] << 4) | (input[2] >> 2);
+		input[3] = b64dec[src_c[src_pos+3]];
+		if (input[3] == 0xff) {
+			if (src_c[src_pos+3] != '=')
+				return -1;
+			buffer_append(dest, output, 2);
+			ret = 0;
+			src_pos += 4;
+			break;
+		}
+
+		output[2] = ((input[2] << 6) & 0xc0) | input[3];
+		buffer_append(dest, output, 3);
 	}
 
 	for (; src_pos < src_size; src_pos++) {
