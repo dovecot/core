37f2d421e07c55edcc1b40c53be37eec637c0c1e
Timo Sirainen <tss@iki.fi>
2007-10-06 01:50:52 +0300

    Removed CRLF/LF output streams. They're no longer needed.
    --HG--
    branch : HEAD

--- src/lib/ostream-crlf.h
+++
@@ -1,9 +0,0 @@
-#ifndef OSTREAM_CRLF_H
-#define OSTREAM_CRLF_H
-
-/* Replace all plain LFs with CRLF. */
-struct ostream *o_stream_create_crlf(struct ostream *output);
-/* Replace all CRLF pairs with plain LFs. */
-struct ostream *o_stream_create_lf(struct ostream *output);
-
-#endif
