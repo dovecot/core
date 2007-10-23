5b574e118288049a929fc3bc82c4c2380fc47d9c
Timo Sirainen <tss@iki.fi>
2007-10-23 16:49:37 +0300

    Include CRLFs in searched text between headers.
    --HG--
    branch : HEAD

--- src/lib-mail/message-search.c
+++ src/lib-mail/message-search.c
@@ -106,12 +106,16 @@ static void handle_header(struct message_search_context *ctx,
 static bool search_header(struct message_search_context *ctx,
 			  const struct message_header_line *hdr)
 {
+	static const unsigned char crlf[2] = { '\r', '\n' };
+
 	return str_find_more(ctx->str_find_ctx,
 			     (const unsigned char *)hdr->name, hdr->name_len) ||
 		str_find_more(ctx->str_find_ctx,
 			      hdr->middle, hdr->middle_len) ||
 		str_find_more(ctx->str_find_ctx, hdr->full_value,
-			      hdr->full_value_len);
+			      hdr->full_value_len) ||
+		(!hdr->no_newline &&
+		 str_find_more(ctx->str_find_ctx, crlf, 2));
 }
 
 int message_search_more(struct message_search_context *ctx,
