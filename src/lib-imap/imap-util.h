1f84cd9073025883e19b1892dc0ae1c243dd63bd
Timo Sirainen <tss@iki.fi>
2007-12-02 13:39:25 +0200

    Compiler warning fix.
    --HG--
    branch : HEAD

--- src/lib-imap/imap-util.h
+++ src/lib-imap/imap-util.h
@@ -1,6 +1,8 @@
 #ifndef IMAP_UTIL_H
 #define IMAP_UTIL_H
 
+enum mail_flags;
+
 /* Write flags as a space separated string. */
 void imap_write_flags(string_t *dest, enum mail_flags flags,
 		      const char *const *keywords);
