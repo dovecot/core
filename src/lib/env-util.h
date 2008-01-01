77ebdd4ea458350c492f042a76a2056e013e69be
Timo Sirainen <tss@iki.fi>
2008-01-02 01:35:21 +0200

    Added functions for building an "environment array". It can be passed later to execve().
    --HG--
    branch : HEAD

--- src/lib/env-util.h
+++ src/lib/env-util.h
@@ -7,4 +7,12 @@ void env_put(const char *env);
 /* Clear all environment variables. */
 void env_clean(void);
 
+/* Append a string containing key=value to the array */
+void envarr_add(ARRAY_TYPE(const_string) *arr,
+		const char *key, const char *value);
+void envarr_addi(ARRAY_TYPE(const_string) *arr, const char *key,
+		 unsigned int value);
+/* Append a string containing key=1 to the array */
+void envarr_addb(ARRAY_TYPE(const_string) *arr, const char *key);
+
 #endif
