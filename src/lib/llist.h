04052710485ccc4a89c177527066b9c2a511f221
Timo Sirainen <tss@iki.fi>
2008-01-06 02:18:20 +0200

    Added macros for handling a doubly linked list.
    --HG--
    branch : HEAD

---
+++ src/lib/llist.h
@@ -0,0 +1,21 @@
+#ifndef LLIST_H
+#define LLIST_H
+
+/* Doubly linked list */
+#define DLLIST_PREPEND(list, item) STMT_START { \
+	(item)->prev = NULL; \
+	(item)->next = *(list); \
+	if (*(list) != NULL) (*(list))->prev = (item); \
+	*(list) = (item); \
+	} STMT_END
+
+#define DLLIST_REMOVE(list, item) STMT_START { \
+	if ((item)->prev == NULL) \
+		*(list) = (item)->next; \
+	else \
+		(item)->prev->next = (item)->next; \
+	if ((item)->next != NULL) \
+		(item)->next->prev = (item)->prev; \
+	} STMT_END
+
+#endif
