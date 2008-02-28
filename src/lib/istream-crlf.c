b52ab39e0900187a1b0d675922a8c91766373fb7
Timo Sirainen <tss@iki.fi>
2008-02-28 06:28:51 +0200

    Added support for seeking forwards in the stream. Also mark the stream as non-seekable since seeking backwards doesn't work.
    --HG--
    branch : HEAD

--- src/lib/istream-crlf.c
+++ src/lib/istream-crlf.c
@@ -151,11 +151,30 @@ static ssize_t i_stream_crlf_read_lf(struct istream_private *stream)
 	return ret;
 }
 
-static void ATTR_NORETURN
-i_stream_crlf_seek(struct istream_private *stream ATTR_UNUSED,
-		   uoff_t v_offset ATTR_UNUSED, bool mark ATTR_UNUSED)
+static void
+i_stream_crlf_seek(struct istream_private *stream,
+		   uoff_t v_offset, bool mark ATTR_UNUSED)
 {
-	i_panic("crlf-istream: seeking unsupported currently");
+	size_t available;
+
+	if (stream->istream.v_offset > v_offset)
+		i_panic("crlf-istream: seeking unsupported currently");
+
+	while (stream->istream.v_offset < v_offset) {
+		(void)i_stream_crlf_read_crlf(stream);
+
+		available = stream->pos - stream->skip;
+		if (available == 0) {
+			stream->istream.stream_errno = ESPIPE;
+			return;
+		}
+		if (available <= v_offset - stream->istream.v_offset)
+			i_stream_skip(&stream->istream, available);
+		else {
+			i_stream_skip(&stream->istream,
+				      v_offset - stream->istream.v_offset);
+		}
+	}
 }
 
 static const struct stat *
@@ -184,7 +203,7 @@ i_stream_create_crlf_full(struct istream *input, bool crlf)
 	cstream->istream.stat = i_stream_crlf_stat;
 
 	cstream->istream.istream.blocking = input->blocking;
-	cstream->istream.istream.seekable = input->seekable;
+	cstream->istream.istream.seekable = FALSE;
 	return i_stream_create(&cstream->istream, input,
 			       i_stream_get_fd(input));
 }
