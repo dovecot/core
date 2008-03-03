410b1f3afefe36b9df985c91788533716c47f7ce
Timo Sirainen <tss@iki.fi>
2008-03-03 02:54:46 +0200

    Moved mbox From_-line handling to lib-mail, since it's also used by deliver.
    --HG--
    branch : HEAD

--- src/lib-storage/index/mbox/mbox-from.h
+++
@@ -1,8 +0,0 @@
-#ifndef MBOX_FROM_H
-#define MBOX_FROM_H
-
-int mbox_from_parse(const unsigned char *msg, size_t size,
-		    time_t *time_r, char **sender_r);
-const char *mbox_from_create(const char *sender, time_t time);
-
-#endif
