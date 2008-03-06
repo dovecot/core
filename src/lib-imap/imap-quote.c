25ba4509dca8dcaa2a4636884110ce566cbe459b
Timo Sirainen <tss@iki.fi>
2008-03-06 09:42:16 +0200

    imap_quote(): Don't crash if allocating from data stack pool.
    --HG--
    branch : HEAD

--- src/lib-imap/imap-quote.c
+++ src/lib-imap/imap-quote.c
@@ -91,18 +91,20 @@ void imap_quote_append(string_t *str, const unsigned char *value,
 const char *imap_quote(pool_t pool, const unsigned char *value,
 		       size_t value_len)
 {
+	string_t *str;
 	char *ret;
 
 	if (value == NULL)
 		return "NIL";
 
-	T_BEGIN {
-		   string_t *str;
+	if (!pool->datastack_pool)
+		t_push();
 
-		   str = t_str_new(value_len + MAX_INT_STRLEN + 5);
-		   imap_quote_append(str, value, value_len, TRUE);
-		   ret = p_strndup(pool, str_data(str), str_len(str));
-	} T_END;
+	str = t_str_new(value_len + MAX_INT_STRLEN + 5);
+	imap_quote_append(str, value, value_len, TRUE);
+	ret = p_strndup(pool, str_data(str), str_len(str));
 
+	if (!pool->datastack_pool)
+		t_pop();
 	return ret;
 }
