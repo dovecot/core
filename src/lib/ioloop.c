421bd1f3e8f7ce7cc7879f2209d0293ce9cce0c0
Timo Sirainen <tss@iki.fi>
2008-11-30 15:04:29 +0200

    ioloop: 0 ms timeouts could still have caused infinite looping.
    --HG--
    branch : HEAD

--- src/lib/ioloop.c
+++ src/lib/ioloop.c
@@ -157,8 +157,9 @@ timeout_reset_timeval(struct timeout *timeout, struct timeval *tv_now)
 	if (timeout->msecs == 0) {
 		/* if we came here from io_loop_handle_timeouts(),
 		   next_run must be larger than tv_now or we could go to
-		   infinite loop */
-		timeout->next_run.tv_usec += 1000;
+		   infinite loop. +1000 to get 1 ms further, another +1000 to
+		   account for timeout_update_next()'s truncation. */
+		timeout->next_run.tv_usec += 2000;
 		if (timeout->next_run.tv_usec >= 1000000) {
 			timeout->next_run.tv_sec++;
 			timeout->next_run.tv_usec -= 1000000;
