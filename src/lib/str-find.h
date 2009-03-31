49a7e4dba84bbf35d82669d1ae79ad43949eed19
Timo Sirainen <tss@iki.fi>
2009-03-31 17:34:28 -0400

    Added str_find_get_match_end_pos(). Added unit testing.
    --HG--
    branch : HEAD

--- src/lib/str-find.h
+++ src/lib/str-find.h
@@ -10,6 +10,9 @@ void str_find_deinit(struct str_find_context **ctx);
    blocks and have the key still match. */
 bool str_find_more(struct str_find_context *ctx,
 		   const unsigned char *data, size_t size);
+/* After str_find_more() has returned TRUE, this function returns the end
+   position in the previous data block where the key had matched. */
+size_t str_find_get_match_end_pos(struct str_find_context *ctx);
 /* Reset input data. The next str_find_more() call won't try to match the key
    to earlier data. */
 void str_find_reset(struct str_find_context *ctx);
--- src/tests/test-lib.c
+++ src/tests/test-lib.c
@@ -10,6 +10,7 @@
 #include "primes.h"
 #include "priorityq.h"
 #include "seq-range-array.h"
+#include "str-find.h"
 #include "str-sanitize.h"
 #include "utc-mktime.h"
 
@@ -725,6 +726,86 @@ static void test_seq_range_array(void)
 	test_seq_range_array_random();
 }
 
+static const char *str_find_text = "xababcd";
+
+static bool test_str_find_substring(const char *key, int expected_pos)
+{
+	const unsigned char *text = (const unsigned char *)str_find_text;
+	const unsigned int text_len = strlen(str_find_text);
+	struct str_find_context *ctx;
+	unsigned int i, j, pos, max, offset;
+	bool ret;
+
+	ctx = str_find_init(pool_datastack_create(), key);
+	/* divide text into every possible block combination and test that
+	   it matches */
+	max = 1 << (text_len-1);
+	for (i = 0; i < max; i++) {
+		str_find_reset(ctx);
+		pos = 0; offset = 0; ret = FALSE;
+		for (j = 0; j < text_len; j++) {
+			if ((i & (1 << j)) != 0) {
+				if (str_find_more(ctx, text+pos, j-pos+1)) {
+					ret = TRUE;
+					break;
+				}
+				offset += j-pos + 1;
+				pos = j + 1;
+			}
+		}
+		if (pos != text_len && !ret) {
+			if (str_find_more(ctx, text+pos, j-pos))
+				ret = TRUE;
+		}
+		if (expected_pos < 0) {
+			if (ret)
+				return FALSE;
+		} else {
+			if (!ret)
+				return FALSE;
+
+			pos = str_find_get_match_end_pos(ctx) +
+				offset - strlen(key);
+			if ((int)pos != expected_pos)
+				return FALSE;
+		}
+	}
+	return TRUE;
+}
+
+struct str_find_input {
+	const char *str;
+	int pos;
+};
+static void test_str_find(void)
+{
+	static const char *fail_input[] = {
+		"xabc",
+		"xabd",
+		"abd"
+	};
+	unsigned int idx, len;
+	const char *key, *p;
+	unsigned int i;
+	bool success = TRUE;
+
+	for (idx = 0; idx < strlen(str_find_text); idx++) {
+		for (len = strlen(str_find_text)-idx; len > 0; len--) {
+			/* we'll get a search key for all substrings of text */
+			T_BEGIN {
+				key = t_strndup(str_find_text + idx, len);
+				p = strstr(str_find_text, key);
+				success = test_str_find_substring(key, p - str_find_text);
+			} T_END;
+			if (!success)
+				break;
+		}
+	}
+	for (i = 0; i < N_ELEMENTS(fail_input) && success; i++)
+		success = test_str_find_substring(fail_input[i], -1);
+	test_out("str_find()", success);
+}
+
 struct str_sanitize_input {
 	const char *str;
 	unsigned int max_len;
@@ -832,6 +913,7 @@ int main(void)
 		test_primes,
 		test_priorityq,
 		test_seq_range_array,
+		test_str_find,
 		test_str_sanitize,
 		test_utc_mktime,
 
