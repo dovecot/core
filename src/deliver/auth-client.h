c1f1b82bf90ccec67c2af620ed76d21330f917ec
Timo Sirainen <tss@iki.fi>
2009-04-09 20:26:30 -0400

    Removed unnecessary code.
    --HG--
    branch : HEAD

--- src/deliver/auth-client.h
+++
@@ -1,8 +0,0 @@
-#ifndef AUTH_CLIENT_H
-#define AUTH_CLIENT_H
-
-int auth_client_lookup_and_restrict(const char *auth_socket, bool debug,
-				    const char **user, uid_t euid, pool_t pool,
-				    ARRAY_TYPE(const_string) *extra_fields_r);
-
-#endif
