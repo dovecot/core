af72f81339761a688afbf3113d8a8c2a1b25b4fe
Timo Sirainen <tss@iki.fi>
2009-06-24 14:20:11 -0400

    auth: Increased auth workers' max. input line length to 8192.
    --HG--
    branch : HEAD

--- src/auth/auth-worker-client.h
+++ src/auth/auth-worker-client.h
@@ -1,7 +1,7 @@
 #ifndef AUTH_WORKER_CLIENT_H
 #define AUTH_WORKER_CLIENT_H
 
-#define AUTH_WORKER_MAX_LINE_LENGTH 1024
+#define AUTH_WORKER_MAX_LINE_LENGTH 8192
 
 struct auth_worker_client *auth_worker_client_create(struct auth *auth, int fd);
 void auth_worker_client_destroy(struct auth_worker_client **client);
