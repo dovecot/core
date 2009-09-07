8233fad76e4dfaa302aceb5130e86a5fed32b8e5
Timo Sirainen <tss@iki.fi>
2009-09-06 20:51:58 -0400

    dict: Removed unused files.
    --HG--
    branch : HEAD

--- src/dict/dict-server.h
+++
@@ -1,9 +0,0 @@
-#ifndef DICT_SERVER_H
-#define DICT_SERVER_H
-
-struct dict;
-
-struct dict_server *dict_server_init(const char *path, int fd);
-void dict_server_deinit(struct dict_server *server);
-
-#endif
