7ac087a7fc8cf8c16b2145e5e8be048db94068f7
Timo Sirainen <tss@iki.fi>
2010-04-13 14:34:25 +0300

    run-test: Fixed to work correctly with older Valgrind versions.
    --HG--
    branch : HEAD

--- run-test.sh
+++ run-test.sh
@@ -2,7 +2,13 @@
 
 trap "rm -f test.out.$$" 0 1 2 3 15
 
-valgrind -q --log-file=test.out.$$ $*
+if valgrind --version | grep '^valgrind-3.[012]'; then
+  # RHEL 5.4 still has Valgrind v3.2
+  valgrind -q --log-file-exactly=test.out.$$ $*
+else
+  # v3.3+
+  valgrind -q --log-file=test.out.$$ $*
+fi
 if [ -s test.out.$$ ]; then
   cat test.out.$$
   exit 1
