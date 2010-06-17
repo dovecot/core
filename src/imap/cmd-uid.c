09caab22d29a60d7178557097d667ebbbcfc983a
Timo Sirainen <tss@iki.fi>
2010-06-17 13:53:14 +0100

    imap: UID commands could have written garbage to process title. Patch by Mike Abbott / Apple
    --HG--
    branch : HEAD

--- src/imap/cmd-uid.c
+++ src/imap/cmd-uid.c
@@ -20,7 +20,7 @@ bool cmd_uid(struct client_command_context *cmd)
 		return TRUE;
 	}
 
-	cmd->name = t_strconcat("UID ", cmd_name, NULL);
+	cmd->name = p_strconcat(cmd->pool, "UID ", cmd_name, NULL);
 	cmd->cmd_flags = command->flags;
 	cmd->func = command->func;
 	cmd->uid = TRUE;
