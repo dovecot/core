1e50a4f930a028ab5fcf690503bfd13b54ff6787
Timo Sirainen <tss@iki.fi>
2010-09-08 14:47:45 +0100

    auth: Don't crash if auth_cache_size has a very small value.

--- src/auth/auth-cache.c
+++ src/auth/auth-cache.c
@@ -242,7 +242,7 @@ void auth_cache_insert(struct auth_cache *cache, struct auth_request *request,
 		sizeof(node->data) + data_size;
 
 	/* make sure we have enough space */
-	while (cache->size_left < alloc_size)
+	while (cache->size_left < alloc_size && cache->tail != NULL)
 		auth_cache_node_destroy(cache, cache->tail);
 
 	node = hash_table_lookup(cache->hash, str_c(str));
