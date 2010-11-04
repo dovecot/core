a640b061437e19f502de679cab713bdaab7dcc9b
Timo Sirainen <tss@iki.fi>
2010-11-04 16:37:49 +0000

    ssl: Call OpenSSL_add_all_algorithms() to make some OpenSSL versions happy.

--- src/login-common/ssl-proxy-openssl.c
+++ src/login-common/ssl-proxy-openssl.c
@@ -1170,6 +1170,7 @@ void ssl_proxy_init(void)
 
 	SSL_library_init();
 	SSL_load_error_strings();
+	OpenSSL_add_all_algorithms();
 
 	extdata_index = SSL_get_ex_new_index(0, dovecot, NULL, NULL, NULL);
 
