a31ee53e11fb7ecbc37b024d0cad6250e3d06fae
Timo Sirainen <tss@iki.fi>
2011-01-04 12:04:42 +0200

    dsync: Saved messages' save-date was set to 1970-01-01.

--- src/dsync/dsync-worker-local.c
+++ src/dsync/dsync-worker-local.c
@@ -1000,6 +1000,8 @@ local_worker_msg_iter_next(struct dsync_worker_msg_iter *_iter,
 	msg_r->flags = mail_get_flags(iter->mail);
 	msg_r->keywords = mail_get_keywords(iter->mail);
 	msg_r->modseq = mail_get_modseq(iter->mail);
+	if (mail_get_save_date(iter->mail, &msg_r->save_date) < 0)
+		msg_r->save_date = (time_t)-1;
 	return 1;
 }
 
