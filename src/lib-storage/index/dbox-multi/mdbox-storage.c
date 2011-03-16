7d20b3ad93863c638f5929e38994da1fc6a529b9
Timo Sirainen <tss@iki.fi>
2011-03-16 17:59:07 +0200

    lib-storage: Moved mailbox_update() assert check from mdbox to common, plus fixed it.

--- src/lib-storage/index/dbox-multi/mdbox-storage.c
+++ src/lib-storage/index/dbox-multi/mdbox-storage.c
@@ -229,8 +229,6 @@ static int mdbox_write_index_header(struct mailbox *box,
 	    hdr->first_recent_uid < update->min_first_recent_uid) {
 		uint32_t first_recent_uid = update->min_first_recent_uid;
 
-		i_assert(update->min_next_uid == 0 ||
-			 first_recent_uid < update->min_next_uid);
 		mail_index_update_header(trans,
 			offsetof(struct mail_index_header, first_recent_uid),
 			&first_recent_uid, sizeof(first_recent_uid), FALSE);
