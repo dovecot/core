884275d777a79af11109a92f080253a2518f37b8
Timo Sirainen <tss@iki.fi>
2012-01-28 19:25:32 +0200

    smtp client: Do DNS blocking lookups. We don't currently have access to base_dir, so we can't know what the path to dns-client is. Also smtp client is a blocking operation anyway, so doing a blocking DNS lookup isn't a problem.

--- src/lib-lda/smtp-client.c
+++ src/lib-lda/smtp-client.c
@@ -261,7 +261,6 @@ static int smtp_client_send(struct smtp_client *smtp_client)
 	client_set.mail_from = smtp_client->return_path == NULL ? "<>" :
 		t_strconcat("<", smtp_client->return_path, ">", NULL);
 	client_set.my_hostname = smtp_client->set->hostname;
-	client_set.dns_client_socket_path = "dns-client";
 
 	ioloop = io_loop_create();
 	client = lmtp_client_init(&client_set, smtp_client_send_finished,
