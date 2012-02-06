b849b2f39b86dd1e23bd13b597dffe33ebd94185
Timo Sirainen <tss@iki.fi>
2012-02-06 21:42:37 +0200

    master: If instance_name doesn't begin with "dovecot", add "dovecot-" prefix to process names.

--- src/master/main.c
+++ src/master/main.c
@@ -85,6 +85,8 @@ void process_exec(const char *cmd, const char *extra_args[])
 	/* prefix with dovecot/ */
 	argv[0] = t_strdup_printf("%s/%s", services->set->instance_name,
 				  argv[0]);
+	if (strncmp(argv[0], PACKAGE, strlen(PACKAGE)) != 0)
+		argv[0] = t_strconcat(PACKAGE"-", argv[0], NULL);
 	(void)execv_const(executable, argv);
 }
 
