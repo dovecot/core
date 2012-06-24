bef515a6f5cce9cf036861b2e409966e5d884973
Timo Sirainen <tss@iki.fi>
2012-06-24 03:19:17 +0300

    director: Fix to handling duplicate USER-WEAK event.

--- src/director/director-connection.c
+++ src/director/director-connection.c
@@ -711,7 +711,7 @@ director_cmd_user_weak(struct director_connection *conn,
 	bool weak = TRUE;
 	int ret;
 
-	if ((ret = director_cmd_is_seen(conn, &args, &dir_host)) < 0)
+	if ((ret = director_cmd_is_seen(conn, &args, &dir_host)) != 0)
 		return FALSE;
 
 	if (str_array_length(args) != 2 ||
