c48b861822f077e22723809e45dc1adda868200f
Timo Sirainen <tss@iki.fi>
2012-07-25 19:30:59 +0300

    Comment update.

--- src/lib-mail/istream-attachment-connector.h
+++ src/lib-mail/istream-attachment-connector.h
@@ -11,7 +11,8 @@ istream_attachment_connector_begin(struct istream *base_input, uoff_t msg_size);
    non-zero, the input is base64-encoded with the given settings. The
    (resulting base64-encoded) input must have exactly encoded_size bytes.
 
-   Returns 0 if the input was ok, -1 if we've already reached msg_size */
+   Returns 0 if the input was ok, -1 if we've already reached msg_size or
+   attachment offsets/sizes aren't valid. */
 int istream_attachment_connector_add(struct istream_attachment_connector *conn,
 				     struct istream *decoded_input,
 				     uoff_t start_offset, uoff_t encoded_size,
