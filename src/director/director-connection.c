33e3cf377566707fbe2054cbcff4370a3fd2282c
Timo Sirainen <tss@iki.fi>
2012-09-12 00:04:34 +0300

    director: Already seen USER-WEAK command caused director to disconnect.

--- src/director/director-connection.c
+++ src/director/director-connection.c
@@ -712,7 +712,7 @@ director_cmd_user_weak(struct director_connection *conn,
 	int ret;
 
 	if ((ret = director_cmd_is_seen(conn, &args, &dir_host)) != 0)
-		return FALSE;
+		return ret > 0;
 
 	if (str_array_length(args) != 2 ||
 	    str_to_uint(args[0], &username_hash) < 0 ||
