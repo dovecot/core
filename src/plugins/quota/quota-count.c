09b4a50740d34c3f951dc1f9df8edf8283ac532a
Timo Sirainen <tss@iki.fi>
2012-09-18 18:40:46 +0300

    quota: Skip alias symlinks when calculating quota

--- src/plugins/quota/quota-count.c
+++ src/plugins/quota/quota-count.c
@@ -71,6 +71,7 @@ quota_count_namespace(struct quota_root *root, struct mail_namespace *ns,
 	int ret = 0;
 
 	ctx = mailbox_list_iter_init(ns->list, "*",
+				     MAILBOX_LIST_ITER_SKIP_ALIASES |
 				     MAILBOX_LIST_ITER_RETURN_NO_FLAGS);
 	while ((info = mailbox_list_iter_next(ctx)) != NULL) {
 		if ((info->flags & (MAILBOX_NONEXISTENT |
