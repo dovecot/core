da5bcae5bb5a4216fabdaaba3b9b786967944996
Timo Sirainen <tss@iki.fi>
2012-10-02 22:37:49 +0300

    doveadm: Fixed printing large input from doveadm-server.

--- src/doveadm/server-connection.c
+++ src/doveadm/server-connection.c
@@ -100,7 +100,8 @@ static void server_flush_field(struct server_connection *conn, string_t *str,
 {
 	if (conn->streaming) {
 		conn->streaming = FALSE;
-		stream_data(str, data, size);
+		if (size > 0)
+			stream_data(str, data, size);
 		doveadm_print_stream("", 0);
 	} else {
 		const char *text;
