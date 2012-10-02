60f5757c647cb65d0be79ef6e853d3cb0e2eb047
Timo Sirainen <tss@iki.fi>
2012-10-03 01:26:42 +0300

    mdbox: Fix to handling transactions with partially failed saves.

--- src/lib-storage/index/dbox-common/dbox-file.c
+++ src/lib-storage/index/dbox-common/dbox-file.c
@@ -532,12 +532,21 @@ int dbox_file_append_flush(struct dbox_file_append_context *ctx)
 {
 	struct mail_storage *storage = &ctx->file->storage->storage;
 
-	if (ctx->last_flush_offset == ctx->output->offset)
+	if (ctx->last_flush_offset == ctx->output->offset &&
+	    ctx->last_checkpoint_offset == ctx->output->offset)
 		return 0;
 
 	if (o_stream_flush(ctx->output) < 0) {
 		dbox_file_set_syscall_error(ctx->file, "write()");
 		return -1;
+	}
+
+	if (ctx->last_checkpoint_offset != ctx->output->offset) {
+		if (ftruncate(ctx->file->fd, ctx->last_checkpoint_offset) < 0) {
+			dbox_file_set_syscall_error(ctx->file, "ftruncate()");
+			return -1;
+		}
+		o_stream_seek(ctx->output, ctx->last_checkpoint_offset);
 	}
 
 	if (storage->set->parsed_fsync_mode != FSYNC_MODE_NEVER) {
