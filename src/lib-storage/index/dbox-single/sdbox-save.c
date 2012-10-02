9a790c932a9e5d52939c5d9e56e8b0ef02166d43
Timo Sirainen <tss@iki.fi>
2012-10-03 01:38:20 +0300

    sdbox: Fix to handling transactions with partially failed saves.

--- src/lib-storage/index/dbox-single/sdbox-save.c
+++ src/lib-storage/index/dbox-single/sdbox-save.c
@@ -168,7 +168,7 @@ static int dbox_save_mail_write_metadata(struct dbox_save_context *ctx,
 static int dbox_save_finish_write(struct mail_save_context *_ctx)
 {
 	struct sdbox_save_context *ctx = (struct sdbox_save_context *)_ctx;
-	struct dbox_file *const *files;
+	struct dbox_file **files;
 
 	ctx->ctx.finished = TRUE;
 	if (ctx->ctx.dbox_output == NULL)
@@ -192,14 +192,17 @@ static int dbox_save_finish_write(struct mail_save_context *_ctx)
 	if (ctx->ctx.failed) {
 		mail_index_expunge(ctx->ctx.trans, ctx->ctx.seq);
 		dbox_file_append_rollback(&ctx->append_ctx);
+		dbox_file_unlink(*files);
+		dbox_file_unref(files);
+		array_delete(&ctx->files, array_count(&ctx->files) - 1, 1);
 	} else {
 		dbox_file_append_checkpoint(ctx->append_ctx);
 		if (dbox_file_append_commit(&ctx->append_ctx) < 0)
 			ctx->ctx.failed = TRUE;
+		dbox_file_close(*files);
 	}
 
 	i_stream_unref(&ctx->ctx.input);
-	dbox_file_close(*files);
 	ctx->ctx.dbox_output = NULL;
 
 	return ctx->ctx.failed ? -1 : 0;
