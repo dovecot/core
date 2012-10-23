81b1d14891415fef0c2f37ef1ef3680cdcc600f1
Timo Sirainen <tss@iki.fi>
2012-10-23 20:09:35 +0300

    istream-seekable: When read() reaches EOF, unreference underlying streams. This was already done when the stream was kept in memory, but not when when the stream was written to temporary file.

--- src/lib/istream-seekable.c
+++ src/lib/istream-seekable.c
@@ -153,6 +153,7 @@ static ssize_t read_more(struct seekable_istream *sstream)
 			/* last one, EOF */
 			sstream->size = sstream->istream.istream.v_offset;
 			sstream->istream.istream.eof = TRUE;
+			unref_streams(sstream);
 			return -1;
 		}
 
