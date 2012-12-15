bca1c5f83108993e8e49b77aec5f07c797cda1cf
Timo Sirainen <tss@iki.fi>
2012-12-15 13:56:16 +0200

    dsync: Fixed remote syncing due to recent changes

--- src/doveadm/dsync/dsync-ibc-stream.c
+++ src/doveadm/dsync/dsync-ibc-stream.c
@@ -81,9 +81,9 @@ static const struct {
 	},
 	{ .name = "mailbox",
 	  .chr = 'B',
-	  .required_keys = "mailbox_lost mailbox_guid uid_validity uid_next "
+	  .required_keys = "mailbox_guid uid_validity uid_next "
 		"messages_count first_recent_uid highest_modseq",
-	  .optional_keys = "cache_fields"
+	  .optional_keys = "mailbox_lost cache_fields"
 	},
 	{ .name = "mail_change",
 	  .chr = 'C',
