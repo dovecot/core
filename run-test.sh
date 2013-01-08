54073bf3354f35b25091a89fb33f2ab5d9c8bfbc
Timo Sirainen <tss@iki.fi>
2013-01-08 03:25:55 +0200

    run-test.sh didn't exit with failure code when the test failed

--- run-test.sh
+++ run-test.sh
@@ -5,11 +5,14 @@ trap "rm -f test.out.$$" 0 1 2 3 15
 if valgrind --version | grep '^valgrind-3.[012]'; then
   # RHEL 5.4 still has Valgrind v3.2
   valgrind -q --log-file-exactly=test.out.$$ $*
+  ret=$?
 else
   # v3.3+
   valgrind -q --log-file=test.out.$$ $*
+  ret=$?
 fi
 if [ -s test.out.$$ ]; then
   cat test.out.$$
   exit 1
 fi
+exit $ret
