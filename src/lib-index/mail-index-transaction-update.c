4a49e252826cfe5326527fe4f13e879bf96b6ee3
Timo Sirainen <tss@iki.fi>
2013-01-15 08:39:30 +0200

    lib-index: Newly added messages weren't always sorted by UIDs.

--- src/lib-index/mail-index-transaction-update.c
+++ src/lib-index/mail-index-transaction-update.c
@@ -201,6 +201,7 @@ void mail_index_append_finish_uids(struct mail_index_transaction *t,
 	unsigned int i, count;
 	struct seq_range *range;
 	uint32_t next_uid;
+	bool used_existing_uids;
 	
 	if (!array_is_created(&t->appends))
 		return;
@@ -216,9 +217,15 @@ void mail_index_append_finish_uids(struct mail_index_transaction *t,
 	}
 
 	/* assign missing uids */
+	used_existing_uids = FALSE;
 	for (i = 0; i < count; i++) {
-		if (recs[i].uid == 0 || recs[i].uid < first_uid)
+		if (recs[i].uid == 0 || recs[i].uid < first_uid) {
 			recs[i].uid = next_uid++;
+			if (used_existing_uids)
+				t->appends_nonsorted = TRUE;
+		} else {
+			used_existing_uids = TRUE;
+		}
 	}
 
 	/* write the saved uids range */
