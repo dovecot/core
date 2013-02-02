cca4ba2a504d70a9fe9fee37f8433997359de52c
Timo Sirainen <tss@iki.fi>
2013-02-02 17:01:07 +0200

    Updated copyright notices to include year 2013.

--- src/util/maildirlock.c
+++ src/util/maildirlock.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008-2012 Dovecot authors, see the included COPYING file */
+* Copyright (c) 2008-2013 Dovecot authors, see the included COPYING file */
 
 #include "lib.h"
 #include "lib-signals.h"
