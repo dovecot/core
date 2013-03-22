44a529ca5ae82d6b11dc3096a51c3748856dcd08
Timo Sirainen <tss@iki.fi>
2013-03-22 12:48:21 +0200

    dsync: Use fcntl() locking instead of flock().

--- src/doveadm/dsync/dsync-brain.c
+++ src/doveadm/dsync/dsync-brain.c
@@ -232,7 +232,7 @@ dsync_brain_lock(struct dsync_brain *brain, const char *remote_hostname)
 		}
 
 		if (file_wait_lock(brain->lock_fd, brain->lock_path, F_WRLCK,
-				   FILE_LOCK_METHOD_FLOCK, brain->lock_timeout,
+				   FILE_LOCK_METHOD_FCNTL, brain->lock_timeout,
 				   &brain->lock) <= 0) {
 			i_error("Couldn't lock %s: %m", brain->lock_path);
 			break;
