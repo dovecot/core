2e01cfaa0acc57a7f44478ecf82be8b8521a3fcf
Timo Sirainen <tss@iki.fi>
2013-06-06 12:42:34 +0300

    var_expand*(): Added small unit tests for %H and %N

--- src/lib/test-var-expand.c
+++ src/lib/test-var-expand.c
@@ -21,9 +21,15 @@ static void test_var_expand_builtin(void)
 	static struct var_expand_test tests[] = {
 		{ "%{hostname}", NULL },
 		{ "%{pid}", NULL },
-		{ "a%{env:FOO}b", "abaRb" }
+		{ "a%{env:FOO}b", "abaRb" },
+		{ "%50Hv", "1f" },
+		{ "%50Hw", "2e" },
+		{ "%50Nv", "25" },
+		{ "%50Nw", "e" }
 	};
 	static struct var_expand_table table[] = {
+		{ 'v', "value", NULL },
+		{ 'w', "value2", NULL },
 		{ '\0', NULL, NULL }
 	};
 	string_t *str = t_str_new(128);
