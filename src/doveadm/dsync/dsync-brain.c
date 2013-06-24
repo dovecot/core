08bbf28bf9ca4965020fe8b29a3f8be5f0d03dff
Timo Sirainen <tss@iki.fi>
2013-06-24 23:06:45 +0300

    dsync: Don't crash at deinit when dsync fails early.

--- src/doveadm/dsync/dsync-brain.c
+++ src/doveadm/dsync/dsync-brain.c
@@ -242,8 +242,10 @@ int dsync_brain_deinit(struct dsync_brain **_brain)
 		dsync_brain_sync_mailbox_deinit(brain);
 	if (brain->local_tree_iter != NULL)
 		dsync_mailbox_tree_iter_deinit(&brain->local_tree_iter);
-	dsync_mailbox_tree_deinit(&brain->local_mailbox_tree);
-	dsync_mailbox_tree_deinit(&brain->remote_mailbox_tree);
+	if (brain->local_mailbox_tree != NULL)
+		dsync_mailbox_tree_deinit(&brain->local_mailbox_tree);
+	if (brain->remote_mailbox_tree != NULL)
+		dsync_mailbox_tree_deinit(&brain->remote_mailbox_tree);
 	if (brain->mailbox_states_iter != NULL)
 		hash_table_iterate_deinit(&brain->mailbox_states_iter);
 	hash_table_destroy(&brain->mailbox_states);
