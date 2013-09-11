6523f54d1521edf894880f2d45e75cef5dd31c3d
Timo Sirainen <tss@iki.fi>
2013-09-12 01:38:54 +0300

    auth: auth-client socket should be owned by $default_internal_user It's mostly the same as auth-login, which is also owned by it.

--- src/auth/auth-settings.c
+++ src/auth/auth-settings.c
@@ -19,7 +19,7 @@ static struct file_listener_settings auth_unix_listeners_array[] = {
 	{ "login/login", 0666, "", "" },
 	{ "token-login/tokenlogin", 0666, "", "" },
 	{ "auth-login", 0600, "$default_internal_user", "" },
-	{ "auth-client", 0600, "", "" },
+	{ "auth-client", 0600, "$default_internal_user", "" },
 	{ "auth-userdb", 0666, "$default_internal_user", "" },
 	{ "auth-master", 0600, "", "" }
 };
