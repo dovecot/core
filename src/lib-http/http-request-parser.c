101e78ae4d5705f196fba22db3261db042bfb3c2
Stephan Bosch <stephan@rename-it.nl>
2013-09-15 03:46:25 +0300

    lib-http: Improved display of invalid characters in parse error messages.

--- src/lib-http/http-request-parser.c
+++ src/lib-http/http-request-parser.c
@@ -96,8 +96,12 @@ static int http_request_parse_target(struct http_request_parser *parser)
 static inline const char *_chr_sanitize(unsigned char c)
 {
 	if (c >= 0x20 && c < 0x7F)
-		return t_strdup_printf("'%c'", c);
-	return t_strdup_printf("0x%02x", c);
+		return t_strdup_printf("`%c'", c);
+	if (c == 0x0a)
+		return "<LF>";
+	if (c == 0x0d)
+		return "<CR>";
+	return t_strdup_printf("<0x%02x>", c);
 }
 
 static int http_request_parse(struct http_request_parser *parser,
