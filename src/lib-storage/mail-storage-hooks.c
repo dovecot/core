c7db4d0d3caa2fc7a2afd7135fa5645862554156
Timo Sirainen <tss@iki.fi>
2013-11-01 16:44:25 +0200

    lib-storage: If hook is forced, don't bother checking if it's in user's mail_plugins.

--- src/lib-storage/mail-storage-hooks.c
+++ src/lib-storage/mail-storage-hooks.c
@@ -146,9 +146,12 @@ static void mail_user_add_plugin_hooks(struct mail_user *user)
 	t_array_init(&tmp_hooks, array_count(&module_hooks));
 	plugins = t_strsplit_spaces(user->set->mail_plugins, ", ");
 	array_foreach(&module_hooks, module_hook) {
-		name = module_get_plugin_name(module_hook->module);
-		if (str_array_find(plugins, name) || module_hook->forced)
-			array_append(&tmp_hooks, module_hook, 1);
+		if (!module_hook->forced) {
+			name = module_get_plugin_name(module_hook->module);
+			if (!str_array_find(plugins, name))
+				continue;
+		}
+		array_append(&tmp_hooks, module_hook, 1);
 	}
 
 	/* next we have to sort them by the modules' priority (based on name) */
