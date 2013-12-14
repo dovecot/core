1c0462c156a7b1184acecbfb137d2d6a4af93f15
Timo Sirainen <tss@iki.fi>
2013-12-14 14:40:13 +0200

    Compiler warning fix

--- src/lib-master/master-auth.c
+++ src/lib-master/master-auth.c
@@ -111,7 +111,7 @@ static void master_auth_connection_input(struct master_auth_connection *conn)
 		if (ret == 0 || errno == ECONNRESET) {
 			i_error("read(%s) failed: Remote closed connection "
 				"(service's process_limit reached?)",
-				conn->auth->path, conn->auth->path);
+				conn->auth->path);
 		} else {
 			if (errno == EAGAIN)
 				return;
