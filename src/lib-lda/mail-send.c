3a61b68f1b3166e6f63dcd5d7d996133e6571c5b
Timo Sirainen <tss@iki.fi>
2014-04-25 15:31:08 +0300

    lib-lda: Removed unused mail_send_forward()

--- src/lib-lda/mail-send.c
+++ src/lib-lda/mail-send.c
@@ -182,36 +182,3 @@ int mail_send_rejection(struct mail_deliver_context *ctx, const char *recipient,
     o_stream_nsend(output, str_data(str), str_len(str));
     return smtp_client_close(smtp_client);
 }
-
-int mail_send_forward(struct mail_deliver_context *ctx, const char *forwardto)
-{
-    static const char *hide_headers[] = {
-        "Return-Path"
-    };
-    struct istream *input;
-    struct ostream *output;
-    struct smtp_client *smtp_client;
-    const char *return_path;
-
-    if (mail_get_stream(ctx->src_mail, NULL, NULL, &input) < 0)
-	    return -1;
-
-    return_path = mail_deliver_get_return_address(ctx);
-    if (mailbox_get_settings(ctx->src_mail->box)->mail_debug) {
-	    i_debug("Sending a forward to <%s> with return path <%s>",
-		    forwardto, return_path);
-    }
-
-    smtp_client = smtp_client_open(ctx->set, forwardto, return_path, &output);
-
-    input = i_stream_create_header_filter(input, HEADER_FILTER_EXCLUDE |
-                                          HEADER_FILTER_NO_CR, hide_headers,
-                                          N_ELEMENTS(hide_headers),
-					  *null_header_filter_callback,
-					  (void *)NULL);
-
-    (void)o_stream_send_istream(output, input);
-    i_stream_unref(&input);
-
-    return smtp_client_close(smtp_client);
-}
