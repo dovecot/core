2262531bc5fe37a8675b430cc5aae10ee397cb4d
Timo Sirainen <tss@iki.fi>
2014-04-25 15:52:37 +0300

    safe_memset(size=0) triggered read access to data[0], possibly crashing.

--- src/lib/safe-memset.c
+++ src/lib/safe-memset.c
@@ -8,6 +8,9 @@ void safe_memset(void *data, int c, size_t size)
 	volatile unsigned int volatile_zero_idx = 0;
 	volatile unsigned char *p = data;
 
+	if (size == 0)
+		return;
+
 	do {
 		memset(data, c, size);
 	} while (p[volatile_zero_idx] != c);
