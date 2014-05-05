36f614569bbff9a10717c9362b070ded8836f973
Phil Carmody <phil@dovecot.fi>
2014-05-05 15:09:27 +0300

    lib: quell sparse "warning: Using plain integer as NULL pointer"
    The first element of the structure is a pointer, make that explicit.

--- src/lib/array-decl.h
+++ src/lib/array-decl.h
@@ -2,7 +2,7 @@
 #define ARRAY_DECL_H
 
 #define ARRAY(array_type) union { struct array arr; array_type const *const *v; array_type **v_modifiable; }
-#define ARRAY_INIT { { 0, 0 } }
+#define ARRAY_INIT { { NULL, 0 } }
 
 #define ARRAY_DEFINE_TYPE(name, array_type) \
 	union array ## __ ## name { struct array arr; array_type const *const *v; array_type **v_modifiable; }
