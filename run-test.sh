99399f061df83272be34fdf61c404b8b8f6943e3
Teemu Huovila <teemu.huovila@dovecot.fi>
2014-05-08 14:57:56 +0300

    run-tests.sh: Add support for reading valgrind suppressions if they exist.

--- run-test.sh
+++ run-test.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 trap "rm -f test.out.$$" 0 1 2 3 15
-
-valgrind -q --log-file=test.out.$$ $*
+supp_path="`dirname $0`/run-test-valgrind.supp"
+if [ -r "$supp_path" ]; then
+  valgrind -q --suppressions="$supp_path" --log-file=test.out.$$ $*
+else
+  valgrind -q --log-file=test.out.$$ $*
+fi
 ret=$?
 if [ -s test.out.$$ ]; then
   cat test.out.$$
