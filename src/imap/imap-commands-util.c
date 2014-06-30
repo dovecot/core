e63b559252ea54722e52d717737f47c035c1992f
Timo Sirainen <tss@iki.fi>
2014-06-30 14:29:26 +0300

    imap: If we disconnect client because mailbox is inconsistent, send the error message before BYE.

--- src/imap/imap-commands-util.c
+++ src/imap/imap-commands-util.c
@@ -182,17 +182,16 @@ void client_send_storage_error(struct client_command_context *cmd,
 	const char *error_string;
 	enum mail_error error;
 
+	error_string = mail_storage_get_last_error(storage, &error);
+	client_send_tagline(cmd, imap_get_error_string(cmd, error_string,
+						       error));
+
 	if (cmd->client->mailbox != NULL &&
 	    mailbox_is_inconsistent(cmd->client->mailbox)) {
 		/* we can't do forced CLOSE, so have to disconnect */
 		client_disconnect_with_error(cmd->client,
 			"IMAP session state is inconsistent, please relogin.");
-		return;
 	}
-
-	error_string = mail_storage_get_last_error(storage, &error);
-	client_send_tagline(cmd, imap_get_error_string(cmd, error_string,
-						       error));
 }
 
 void client_send_untagged_storage_error(struct client *client,
@@ -201,16 +200,15 @@ void client_send_untagged_storage_error(struct client *client,
 	const char *error_string;
 	enum mail_error error;
 
+	error_string = mail_storage_get_last_error(storage, &error);
+	client_send_line(client, t_strconcat("* NO ", error_string, NULL));
+
 	if (client->mailbox != NULL &&
 	    mailbox_is_inconsistent(client->mailbox)) {
 		/* we can't do forced CLOSE, so have to disconnect */
 		client_disconnect_with_error(client,
 			"IMAP session state is inconsistent, please relogin.");
-		return;
 	}
-
-	error_string = mail_storage_get_last_error(storage, &error);
-	client_send_line(client, t_strconcat("* NO ", error_string, NULL));
 }
 
 bool client_parse_mail_flags(struct client_command_context *cmd,
