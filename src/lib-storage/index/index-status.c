7ebb549522e87b4691c1b6e4c906cfc08f5e8db8
Timo Sirainen <tss@iki.fi>
2014-07-03 15:26:32 +0300

    lib-storage: mailbox_get_metadata() now opens the mailbox only if it's necessary.

--- src/lib-storage/index/index-status.c
+++ src/lib-storage/index/index-status.c
@@ -375,6 +375,17 @@ int index_mailbox_get_metadata(struct mailbox *box,
 			       enum mailbox_metadata_items items,
 			       struct mailbox_metadata *metadata_r)
 {
+	/* handle items that don't require opening the mailbox */
+	if ((items & MAILBOX_METADATA_BACKEND_NAMESPACE) != 0) {
+		metadata_r->backend_ns_prefix = "";
+		metadata_r->backend_ns_type =
+			mailbox_list_get_namespace(box->list)->type;
+		items &= ~MAILBOX_METADATA_BACKEND_NAMESPACE;
+	}
+	if (items == 0)
+		return 0;
+
+	/* handle items that require opening the mailbox */
 	if (!box->opened) {
 		if (mailbox_open(box) < 0)
 			return -1;
@@ -392,10 +403,5 @@ int index_mailbox_get_metadata(struct mailbox *box,
 		get_metadata_cache_fields(box, metadata_r);
 	if ((items & MAILBOX_METADATA_PRECACHE_FIELDS) != 0)
 		get_metadata_precache_fields(box, metadata_r);
-	if ((items & MAILBOX_METADATA_BACKEND_NAMESPACE) != 0) {
-		metadata_r->backend_ns_prefix = "";
-		metadata_r->backend_ns_type =
-			mailbox_list_get_namespace(box->list)->type;
-	}
 	return 0;
 }
