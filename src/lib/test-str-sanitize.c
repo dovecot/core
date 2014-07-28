83060e2c2b3d75bda70ac9cc3e55ff1f677acc0f
Phil Carmody <phil@dovecot.fi>
2014-07-28 16:49:47 +0300

    lib: test-str-sanitize - tighten tests Check that the identical string is returned in the no-change case, not just a copy.
    Signed-off-by: Phil Carmody <phil@dovecot.fi>

--- src/lib/test-str-sanitize.c
+++ src/lib/test-str-sanitize.c
@@ -3,44 +3,36 @@
 #include "test-lib.h"
 #include "str-sanitize.h"
 
-struct str_sanitize_input {
+struct str_sanitize_test {
 	const char *str;
 	unsigned int max_len;
+	const char *sanitized; /* NULL for no change */
 };
 
 void test_str_sanitize(void)
 {
-	static struct str_sanitize_input input[] = {
-		{ NULL, 2 },
-		{ "", 2 },
-		{ "a", 2 },
-		{ "ab", 2 },
-		{ "abc", 2 },
-		{ "abcd", 3 },
-		{ "abcde", 4 },
-		{ "с", 10 },
-		{ "с", 1 },
-		{ "\001x\x1fy\x81", 10 }
-	};
-	static const char *output[] = {
-		NULL,
-		"",
-		"a",
-		"ab",
-		"...",
-		"...",
-		"a...",
-		"с",
-		"с",
-		"?x?y?"
+	static struct str_sanitize_test tests[] = {
+		{ NULL,    2, NULL },
+		{ "",      2, NULL },
+		{ "a",     2, NULL },
+		{ "ab",    2, NULL },
+		{ "abc",   2, "..." },
+		{ "abcd",  3, "..." },
+		{ "abcde", 4, "a..." },
+		{ "с",    10, NULL },
+		{ "с",     1, NULL },
+		{ "\001x\x1fy\x81", 10, "?x?y?" }
 	};
 	const char *str;
 	unsigned int i;
 
 	test_begin("str_sanitize");
-	for (i = 0; i < N_ELEMENTS(input); i++) {
-		str = str_sanitize(input[i].str, input[i].max_len);
-		test_assert(null_strcmp(output[i], str) == 0);
+	for (i = 0; i < N_ELEMENTS(tests); i++) {
+		str = str_sanitize(tests[i].str, tests[i].max_len);
+		if (tests[i].sanitized != NULL)
+			test_assert_idx(null_strcmp(str, tests[i].sanitized) == 0, i);
+		else
+			test_assert_idx(str == tests[i].str, i);
 	}
 	test_end();
 }
