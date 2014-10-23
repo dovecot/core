80bc6f4feafd94bfaf3bf77e7b4038c03b81de3a
Timo Sirainen <tss@iki.fi>
2014-10-23 06:37:30 +0300

    doveadm: Added doveadm_print_istream() for easily printing istreams.

--- src/doveadm/doveadm-print.c
+++ src/doveadm/doveadm-print.c
@@ -2,6 +2,7 @@
 
 #include "lib.h"
 #include "array.h"
+#include "istream.h"
 #include "doveadm-print-private.h"
 
 #include <stdio.h>
@@ -99,6 +100,26 @@ void doveadm_print_stream(const void *value, size_t size)
 		ctx->header_idx++;
 		ctx->print_stream_open = FALSE;
 	}
+}
+
+int doveadm_print_istream(struct istream *input)
+{
+	const unsigned char *data;
+	size_t size;
+	ssize_t ret;
+
+	while ((ret = i_stream_read_data(input, &data, &size, 0)) > 0) {
+		doveadm_print_stream(data, size);
+		i_stream_skip(input, size);
+	}
+	i_assert(ret == -1);
+	doveadm_print_stream("", 0);
+	if (input->stream_errno != 0) {
+		i_error("read(%s) failed: %s", i_stream_get_name(input),
+			i_stream_get_error(input));
+		return -1;
+	}
+	return 0;
 }
 
 void doveadm_print_sticky(const char *key, const char *value)
