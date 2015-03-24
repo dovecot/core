4ce25b6f7ff304d40916dadc3341973eb88b0a50
Timo Sirainen <tss@iki.fi>
2015-03-24 11:27:26 +0200

    imapc: Fix to previous change - labels are strings and not atoms. Allow also atoms just in case

--- src/lib-storage/index/imapc/imapc-mailbox.c
+++ src/lib-storage/index/imapc/imapc-mailbox.c
@@ -315,12 +315,17 @@ static void imapc_untagged_fetch(const struct imapc_untagged_reply *reply,
 			   IMAPC_BOX_HAS_FEATURE(mbox, IMAPC_FEATURE_GMAIL_MIGRATION)) {
 			if (!imap_arg_get_list(&list[i+1], &flags_list))
 				return;
+#if 0
+			if (flags_list[0].type != IMAP_ARG_EOL)
+				have_labels = TRUE;
+#else
 			for (j = 0; flags_list[j].type != IMAP_ARG_EOL; j++) {
-				if (!imap_arg_get_atom(&flags_list[j], &atom))
+				if (!imap_arg_get_astring(&flags_list[j], &atom))
 					return;
 				if (strcasecmp(atom, "\\Muted") != 0)
 					have_labels = TRUE;
 			}
+#endif
 		}
 	}
 
