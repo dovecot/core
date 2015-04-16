c17957c8504a8ae862d76c29defd893e7459648a
Timo Sirainen <tss@iki.fi>
2015-04-16 14:19:51 +0300

    doveadm mailbox metadata: The keys must always be lowercased. The keys need to be case-insensitive in IMAP, so the IMAP code always also lowercases them.

--- src/doveadm/doveadm-mail-mailbox-metadata.c
+++ src/doveadm/doveadm-mail-mailbox-metadata.c
@@ -78,6 +78,7 @@ cmd_mailbox_metadata_parse_key(const char *arg,
 		i_fatal_status(EX_USAGE, "Invalid metadata key '%s': "
 			       "Must begin with /private or /shared", arg);
 	}
+	*key_r = t_str_lcase(*key_r);
 }
 
 static void
