971f89b14c748f2338091c82ad377e93c58598c6
Timo Sirainen <tss@iki.fi>
2015-05-03 16:26:46 +0300

    lib-mail: Removed quoted_printable_decode*() quoted_printable_q_decode() was still left in quoted-printable.h, but maybe it should be moved to qp-decoder.c as well.

--- src/lib-mail/quoted-printable.h
+++ src/lib-mail/quoted-printable.h
@@ -1,19 +1,6 @@
 #ifndef QUOTED_PRINTABLE_H
 #define QUOTED_PRINTABLE_H
 
-/* Translates quoted printable data into binary. dest must be at least the
-   size of src, and may be same as src. Returns 0 if input was valid, -1 if
-   there were some decoding errors (which were skipped over). LFs without
-   preceding CR are returned as CRLF (but =0A isn't).
-
-   This function may be called multiple times for parsing the same stream.
-   src_pos is updated to first non-translated character in src. */
-int quoted_printable_decode(const unsigned char *src, size_t src_size,
-			    size_t *src_pos_r, buffer_t *dest);
-/* Like quoted_printable_decode(), but handle src as the final block.
-   This allows src to end without LF. */
-int quoted_printable_decode_final(const unsigned char *src, size_t src_size,
-				  size_t *src_pos_r, buffer_t *dest);
 /* Decode MIME "Q" encoding. */
 int quoted_printable_q_decode(const unsigned char *src, size_t src_size,
 			      buffer_t *dest);
--- src/lib-mail/test-quoted-printable.c
+++ src/lib-mail/test-quoted-printable.c
@@ -6,92 +6,6 @@
 #include "quoted-printable.h"
 #include "test-common.h"
 
-struct test_quoted_printable_decode_data {
-	const char *input;
-	const char *output;
-	int end_skip;
-	int ret;
-};
-
-static void test_quoted_printable_decode(void)
-{
-	static struct test_quoted_printable_decode_data data[] = {
-		{ "foo  \r\nbar=", "foo\r\nbar", 1, 0 },
-		{ "foo\t=\nbar", "foo\tbar", 0, 0 },
-		{ "foo = \n=01", "foo \001", 0, 0 },
-		{ "foo =\t\r\nbar", "foo bar", 0, 0 },
-		{ "foo =\r\n=01", "foo \001", 0, 0 },
-		{ "foo  \nbar=", "foo\r\nbar", 1, 0 },
-		{ "=0A=0D  ", "\n\r", 2, 0 },
-		{ "foo_bar", "foo_bar", 0, 0 },
-		{ "foo=", "foo", 1, 0 },
-		{ "foo=  ", "foo", 3, 0 },
-		{ "foo=A", "foo", 2, 0 },
-		{ "foo=Ax", "foo=Ax", 0, -1 },
-		{ "foo=Ax=xy", "foo=Ax=xy", 0, -1 }
-	};
-	buffer_t *buf;
-	unsigned int i, start, end, len;
-	size_t src_pos;
-	int ret;
-
-	test_begin("quoted printable decode");
-	buf = buffer_create_dynamic(pool_datastack_create(), 128);
-	for (i = 0; i < N_ELEMENTS(data); i++) {
-		len = strlen(data[i].input);
-		ret = quoted_printable_decode((const void *)data[i].input, len,
-					      &src_pos, buf);
-		test_assert(ret == data[i].ret);
-		test_assert(src_pos + data[i].end_skip == len);
-		test_assert(strcmp(data[i].output, str_c(buf)) == 0);
-
-		buffer_set_used_size(buf, 0);
-		for (start = 0, end = 1; end <= len; ) {
-			quoted_printable_decode(CONST_PTR_OFFSET(data[i].input, start),
-						end - start, &src_pos, buf);
-			src_pos += start;
-			start = src_pos;
-			if (src_pos <= end)
-				end++;
-			else
-				end = src_pos + 1;
-		}
-		test_assert(src_pos + data[i].end_skip == len);
-		test_assert(strcmp(data[i].output, str_c(buf)) == 0);
-		buffer_set_used_size(buf, 0);
-	}
-	test_end();
-}
-
-static void test_quoted_printable_decode_final(void)
-{
-	static struct test_quoted_printable_decode_data data[] = {
-		{ "=0A=0D  ", "\n\r", 2, 0 },
-		{ "foo=", "foo", 1, 0 },
-		{ "foo  ", "foo", 2, 0 },
-		{ "foo=  ", "foo", 3, 0 },
-		{ "foo=A", "foo", 2, -1 }
-	};
-	buffer_t *buf;
-	unsigned int i, len;
-	size_t src_pos;
-	int ret;
-
-	test_begin("quoted printable decode final");
-	buf = buffer_create_dynamic(pool_datastack_create(), 128);
-	for (i = 0; i < N_ELEMENTS(data); i++) {
-		len = strlen(data[i].input);
-		ret = quoted_printable_decode_final((const void *)data[i].input,
-						    len, &src_pos, buf);
-		test_assert(ret == data[i].ret);
-		test_assert(src_pos + data[i].end_skip == len);
-		test_assert(strcmp(data[i].output, str_c(buf)) == 0);
-
-		buffer_set_used_size(buf, 0);
-	}
-	test_end();
-}
-
 static void test_quoted_printable_q_decode(void)
 {
 	const char *data[] = {
@@ -119,8 +33,6 @@ static void test_quoted_printable_q_decode(void)
 int main(void)
 {
 	static void (*test_functions[])(void) = {
-		test_quoted_printable_decode,
-		test_quoted_printable_decode_final,
 		test_quoted_printable_q_decode,
 		NULL
 	};
