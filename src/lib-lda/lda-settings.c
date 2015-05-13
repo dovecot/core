7fb35b357a0271f6bd07e1be74ce8347b6dd8dce
Timo Sirainen <tss@iki.fi>
2015-05-13 19:36:29 +0300

    lib-lda: Don't crash if postmaster_address is given as command line parameter.

--- src/lib-lda/lda-settings.c
+++ src/lib-lda/lda-settings.c
@@ -77,8 +77,8 @@ static bool lda_settings_check(void *_set, pool_t pool, const char **error_r)
 
 	if (*set->hostname == '\0')
 		set->hostname = p_strdup(pool, my_hostdomain());
-	i_assert(set->postmaster_address[0] == SETTING_STRVAR_UNEXPANDED[0]);
-	if (set->postmaster_address[1] == '\0') {
+	if (set->postmaster_address[0] == SETTING_STRVAR_UNEXPANDED[0] &&
+	    set->postmaster_address[1] == '\0') {
 		/* check for valid looking fqdn in hostname */
 		if (strchr(set->hostname, '.') == NULL) {
 			*error_r = "postmaster_address setting not given";
