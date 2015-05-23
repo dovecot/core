d32fce86c0667b5e9028c141305d0bbe7f3f9166
Timo Sirainen <tss@iki.fi>
2015-05-23 16:51:52 -0400

    lib: file_create_locked() wasn't deleting the temp file after successful link()

--- src/lib/file-create-locked.c
+++ src/lib/file-create-locked.c
@@ -72,6 +72,8 @@ try_create_new(const char *path, const struct file_create_settings *set,
 						   str_c(temp_path), path);
 		}
 	} else {
+		if (unlink(str_c(temp_path)) < 0)
+			i_error("unlink(%s) failed: %m", str_c(temp_path));
 		*fd_r = fd;
 		return 1;
 	}
