9a5ff31ae3164cce08162f7a24a20617f05a0a1c
Timo Sirainen <tss@iki.fi>
2015-06-03 00:43:51 +0300

    lib-fts: fts-lowercase can now use the default destroy function.

--- src/lib-fts/fts-filter-lowercase.c
+++ src/lib-fts/fts-filter-lowercase.c
@@ -9,13 +9,6 @@
 #  include "fts-icu.h"
 #endif
 
-static void
-fts_filter_lowercase_destroy(struct fts_filter *filter)
-{
-	str_free(&filter->token);
-	i_free(filter);
-}
-
 static int
 fts_filter_lowercase_create(const struct fts_language *lang ATTR_UNUSED,
 			    const char *const *settings,
@@ -54,7 +47,7 @@ fts_filter_lowercase_filter(struct fts_filter *filter ATTR_UNUSED,
 static const struct fts_filter_vfuncs lowercase_filter_vfuncs = {
 	fts_filter_lowercase_create,
 	fts_filter_lowercase_filter,
-	fts_filter_lowercase_destroy
+	NULL
 };
 
 static const struct fts_filter fts_filter_lowercase_real = {
