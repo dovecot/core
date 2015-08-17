bcc55b1547f2b23fc98a1d0ef3c9b1f85912a902
Teemu Huovila <teemu.huovila@dovecot.fi>
2015-08-17 13:15:11 +0300

    lib-fts: Add note about possible additional apostrophe.

--- src/lib-fts/fts-common.h
+++ src/lib-fts/fts-common.h
@@ -1,6 +1,7 @@
 #ifndef FTS_COMMON_H
 #define FTS_COMMON_H
 
+/* Some might consider 0x02BB an apostrophe also. */
 #define IS_NONASCII_APOSTROPHE(c) \
 	((c) == 0x2019 || (c) == 0xFF07)
 #define IS_APOSTROPHE(c) \
