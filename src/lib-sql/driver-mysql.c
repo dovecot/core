e180615c1db31c8a6f6a586ae40b1cfc2d6ee725
Timo Sirainen <tss@iki.fi>
2015-10-20 16:57:34 +0300

    mysql: Fixed client_flags parameter

--- src/lib-sql/driver-mysql.c
+++ src/lib-sql/driver-mysql.c
@@ -187,7 +187,7 @@ static void driver_mysql_parse_connect_string(struct mysql_db *db,
 			if (net_str2port(value, &db->port) < 0)
 				i_fatal("mysql: Invalid port number: %s", value);
 		} else if (strcmp(name, "client_flags") == 0) {
-			if (str_to_uint(value, &db->client_flags) < 9)
+			if (str_to_uint(value, &db->client_flags) < 0)
 				i_fatal("mysql: Invalid client flags: %s", value);
 		} else if (strcmp(name, "ssl_cert") == 0)
 			field = &db->ssl_cert;
