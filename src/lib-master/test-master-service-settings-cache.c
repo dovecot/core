ded6d682080c7b9c3698a7d066904c445f51ae70
Phil Carmody <phil@dovecot.fi>
2015-11-16 12:26:50 +0200

    lib-master: stop tests from leaking memory This permits Valgrind to run without complaint.
    Signed-off-by: Phil Carmody <phil@dovecot.fi>

--- src/lib-master/test-master-service-settings-cache.c
+++ src/lib-master/test-master-service-settings-cache.c
@@ -98,15 +98,20 @@ int main(void)
 		test_master_service_settings_cache,
 		NULL
 	};
+	pool_t pool;
+	int ret;
 
 	memset(&input, 0, sizeof(input));
 	input.module = "module";
 	input.service = "service_name";
 
 	set.config_cache_size = 1024*4;
+	pool = pool_alloconly_create("set pool", 1024);
 	test_master_service.set_parser =
-		settings_parser_init(pool_alloconly_create("set pool", 1024),
-			&test_setting_parser_info, 0);
+		settings_parser_init(pool, &test_setting_parser_info, 0);
 	master_service = &test_master_service;
-	return test_run(test_functions);
+	ret = test_run(test_functions);
+	settings_parser_deinit(&test_master_service.set_parser);
+	pool_unref(&pool);
+	return ret;
 }
