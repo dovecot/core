e99bcf69a330388423c85f60952f7127c52b7723
Timo Sirainen <tss@iki.fi>
2015-11-30 13:15:44 +0200

    lib-imap: Added unit tests for imap_append_[an]string()

--- src/lib-imap/test-imap-quote.c
+++ src/lib-imap/test-imap-quote.c
@@ -37,10 +37,76 @@ static void test_imap_append_string_for_humans(void)
 	test_end();
 }
 
+static void test_imap_append_astring(void)
+{
+	static struct {
+		const char *input, *output;
+	} tests[] = {
+		{ "", "\"\"" },
+		{ "NIL", "\"NIL\"" },
+		{ "niL", "\"niL\"" },
+		{ "ni", "ni" },
+		{ "\\", "\"\\\\\"" },
+		{ "\\\\", "\"\\\\\\\\\"" },
+		{ "\\\\\\", "\"\\\\\\\\\\\\\"" },
+		{ "\\\\\\\\", "\"\\\\\\\\\\\\\\\\\"" },
+		{ "\\\\\\\\\\", "{5}\r\n\\\\\\\\\\" },
+		{ "\\\\\\\\\\\\", "{6}\r\n\\\\\\\\\\\\" },
+		{ "\"", "\"\\\"\"" },
+		{ "\"\"", "\"\\\"\\\"\"" },
+		{ "\"\"\"", "\"\\\"\\\"\\\"\"" },
+		{ "\"\"\"\"", "\"\\\"\\\"\\\"\\\"\"" },
+		{ "\"\"\"\"\"", "{5}\r\n\"\"\"\"\"" },
+		{ "\"\"\"\"\"\"", "{6}\r\n\"\"\"\"\"\"" },
+		{ "\r", "{1}\r\n\r" },
+		{ "\n", "{1}\r\n\n" },
+		{ "\r\n", "{2}\r\n\r\n" },
+		{ "\x7f", "\"\x7f\"" },
+		{ "\x80", "{1}\r\n\x80" },
+		{ "\xff", "{1}\r\n\xff" },
+	};
+	string_t *str = t_str_new(128);
+	unsigned int i;
+
+	test_begin("test_imap_append_astring()");
+
+	for (i = 0; i < N_ELEMENTS(tests); i++) {
+		str_truncate(str, 0);
+		imap_append_astring(str, tests[i].input);
+		test_assert_idx(strcmp(tests[i].output, str_c(str)) == 0, i);
+	}
+	test_end();
+}
+
+static void test_imap_append_nstring(void)
+{
+	static struct {
+		const char *input, *output;
+	} tests[] = {
+		{ "", "\"\"" },
+		{ NULL, "NIL" },
+		{ "NIL", "\"NIL\"" },
+		{ "ni", "\"ni\"" }
+	};
+	string_t *str = t_str_new(128);
+	unsigned int i;
+
+	test_begin("test_imap_append_nstring()");
+
+	for (i = 0; i < N_ELEMENTS(tests); i++) {
+		str_truncate(str, 0);
+		imap_append_nstring(str, tests[i].input);
+		test_assert_idx(strcmp(tests[i].output, str_c(str)) == 0, i);
+	}
+	test_end();
+}
+
 int main(void)
 {
 	static void (*test_functions[])(void) = {
 		test_imap_append_string_for_humans,
+		test_imap_append_astring,
+		test_imap_append_nstring,
 		NULL
 	};
 	return test_run(test_functions);
