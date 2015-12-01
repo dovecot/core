1b7cb001c0ddbb632d42cfa124424901d9227ba5
Timo Sirainen <tss@iki.fi>
2015-12-01 16:45:37 +0200

    lib-mail: test-rfc822-parser unit test fix We didn't check that all the output was necessarily verified. Also this makes static analyzer happier.

--- src/lib-mail/test-rfc822-parser.c
+++ src/lib-mail/test-rfc822-parser.c
@@ -54,12 +54,14 @@ static void test_rfc822_parse_content_param(void)
 
 	test_begin("rfc822 parse content param");
 	rfc822_parser_init(&parser, (const void *)input, strlen(input), NULL);
-	while ((ret = rfc822_parse_content_param(&parser, &key, &value)) > 0) {
+	while ((ret = rfc822_parse_content_param(&parser, &key, &value)) > 0 &&
+	       i < N_ELEMENTS(output)) {
 		test_assert_idx(strcmp(output[i].key, key) == 0, i);
 		test_assert_idx(strcmp(output[i].value, value) == 0, i);
 		i++;
 	}
 	test_assert(ret == 0);
+	test_assert(i == N_ELEMENTS(output));
 	test_end();
 }
 
