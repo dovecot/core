9058f3006fffd25835ad701e1b2c3c8faafd3c80
Timo Sirainen <tss@iki.fi>
2015-12-09 13:02:46 +0200

    director: If doveadm attempts to change tag for an existing host, return error.

--- src/director/doveadm-connection.c
+++ src/director/doveadm-connection.c
@@ -282,6 +282,9 @@ doveadm_cmd_host_set_or_update(struct doveadm_connection *conn, const char *line
 			return TRUE;
 		}
 		host = mail_host_add_ip(dir->mail_hosts, &ip, tag);
+	} else if (tag[0] != '\0' && strcmp(mail_host_get_tag(host), tag) != 0) {
+		o_stream_nsend_str(conn->output, "host tag can't be changed\n");
+		return TRUE;
 	} else if (host->desynced) {
 		o_stream_nsend_str(conn->output,
 			"host is already being updated - try again later\n");
