51338d9d63b71585aef4a01c6e4f1a09fa6a9597
Aki Tuomi <aki.tuomi@dovecot.fi>
2016-03-02 16:08:06 +0200

    doveadm-http: Set HTTP client's max idletime to 5 seconds

--- src/doveadm/client-connection-http.c
+++ src/doveadm/client-connection-http.c
@@ -754,6 +754,7 @@ static void doveadm_http_server_send_response(void *context)
 }
 
 static const struct http_server_settings http_server_set = {
+	.max_client_idle_time_msecs = 5000,
         .max_pipelined_requests = 0
 };
 
