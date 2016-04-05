dovecot/core

commit 146f6f85db29296edb42cfbee135cd816117b876

fts: If Tika returns 500, retry it a couple of times and then fallback to ignoring the problem.

src/plugins/fts/fts-parser-tika.c
@@ -103,6 +103,21 @@ fts_tika_parser_response(const struct http_response *response,
 		}
 		parser->payload = i_stream_create_from_data("", 0);
 		break;
+	case 500:
+		/* Server Error - the problem could be anything (in Tika or
+		   HTTP server or proxy) and might be retriable, but Tika has
+		   trouble processing some documents and throws up this error
+		   every time for those documents. So we try retrying this a
+		   couple of times, but if that doesn't work we'll just ignore
+		   it. */
+		if (http_client_request_try_retry(parser->http_req))
+			return;
+		i_info("fts_tika: PUT %s failed: %u %s - ignoring",
+		       mail_user_plugin_getenv(parser->user, "fts_tika"),
+		       response->status, response->reason);
+		parser->payload = i_stream_create_from_data("", 0);
+		break;
+
 	default:
 		i_error("fts_tika: PUT %s failed: %u %s",
 			mail_user_plugin_getenv(parser->user, "fts_tika"),

commit dca6152ae37045a2cb4f5d06e7849869e4244da5

fts-tika: Don't crash on 500 errors (caused by 146f6f85d)
http_req=NULL here set by http_client_request_send_payload(). Even if it
wasn't, the payload wouldn't be there.

src/plugins/fts/fts-parser-tika.c
@@ -107,11 +107,13 @@ fts_tika_parser_response(const struct http_response *response,
 		/* Server Error - the problem could be anything (in Tika or
 		   HTTP server or proxy) and might be retriable, but Tika has
 		   trouble processing some documents and throws up this error
-		   every time for those documents. So we try retrying this a
-		   couple of times, but if that doesn't work we'll just ignore
-		   it. */
-		if (http_client_request_try_retry(parser->http_req))
-			return;
+		   every time for those documents.
+
+		   Unfortunately we can't easily re-send the request here,
+		   because we would have to re-send the entire payload, which
+		   isn't available anymore here. So we'd need to indicate
+		   in fts_parser_deinit() that we want to retry.
+		   FIXME: do this in v2.3. For now we'll just ignore it. */
 		i_info("fts_tika: PUT %s failed: %u %s - ignoring",
 		       mail_user_plugin_getenv(parser->user, "fts_tika"),
 		       response->status, response->reason);