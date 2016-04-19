dovecot/core

commit 84f697c5e30565823619abaaeb57164c789d4b66

lib: bits - GCC (and clang) provide bit-twiddle intrinsics, use them
Signed-off-by: Phil Carmody <phil@dovecot.fi>

src/lib/bits.h
@@ -8,6 +8,24 @@
 
 size_t nearest_power(size_t num) ATTR_CONST;
 
+#if __GNUC__ > 2
+static inline unsigned int ATTR_CONST
+bits_required32(uint32_t num)
+{
+	return num == 0 ? 0 : 32 - __builtin_clz(num);
+}
+static inline unsigned int ATTR_CONST
+bits_required8(uint8_t num)   { return bits_required32(num); }
+
+static inline unsigned int ATTR_CONST
+bits_required16(uint16_t num) { return bits_required32(num); }
+
+static inline unsigned int ATTR_CONST
+bits_required64(uint64_t num)
+{
+	return num == 0 ? 0 : 64 - __builtin_clzl(num);
+}
+#else
 unsigned int bits_required8(uint8_t num) ATTR_CONST;
 
 static inline
@@ -28,6 +46,7 @@ unsigned int bits_required64(uint64_t num)
 	return (num <= 0xffffffff) ? bits_required32(num)
 		: 32 + bits_required32(num >> 32);
 }
+#endif
 
 /* These functions look too big to be inline, but in almost all expected
    uses, 'fracbits' will be a compile-time constant, and most of the

src/lib/bits.c
@@ -12,6 +12,9 @@ size_t nearest_power(size_t num)
 	return n;
 }
 
+#if __GNUC__ > 2
+/* Lucky you, it's all inline intrinsics */
+#else
 unsigned int bits_required8(uint8_t num)
 {
 	int ret = 0;
@@ -20,3 +23,4 @@ unsigned int bits_required8(uint8_t num)
 	num &= ~(num>>1); /* 3->2, else unchanged */
 	return ret + num;
 }
+#endif

commit 9e9711ebed27c8efeece02d57cf91bb00f10015c

lib: Fixed bits_required64() with 32bit systems.
Broken by 84f697c5e30565823619abaaeb57164c789d4b66.

src/lib/bits.h
@@ -23,7 +23,7 @@ bits_required16(uint16_t num) { return bits_required32(num); }
 static inline unsigned int ATTR_CONST
 bits_required64(uint64_t num)
 {
-	return num == 0 ? 0 : 64 - __builtin_clzl(num);
+	return num == 0 ? 0 : 64 - __builtin_clzll(num);
 }
 #else
 unsigned int bits_required8(uint8_t num) ATTR_CONST;