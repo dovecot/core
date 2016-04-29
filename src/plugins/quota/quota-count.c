dovecot/core

commit 464db6d9d6d9126ac87143fc7a466120ff5b70f4

quota-count: Cache quota usage within the same ioloop run.
This mainly means that when both STORAGE and MESSAGES quota is looked up we
don't need to calculate them twice.

src/plugins/quota/quota-count.c
@@ -1,8 +1,16 @@
 /* Copyright (c) 2006-2016 Dovecot authors, see the included COPYING file */
 
 #include "lib.h"
+#include "ioloop.h"
 #include "mailbox-list-iter.h"
 #include "quota-private.h"
+
+struct count_quota_root {
+	struct quota_root root;
+
+	struct timeval cache_timeval;
+	uint64_t cached_bytes, cached_count;
+};
 
 struct quota_mailbox_iter {
 	struct quota_root *root;
@@ -137,9 +145,18 @@ quota_mailbox_iter_next(struct quota_mailbox_iter *iter)
 
 int quota_count(struct quota_root *root, uint64_t *bytes_r, uint64_t *count_r)
 {
+	struct count_quota_root *croot = (struct count_quota_root *)root;
 	struct quota_mailbox_iter *iter;
 	const struct mailbox_info *info;
 	int ret = 0, ret2;
+
+	if (croot->cache_timeval.tv_usec == ioloop_timeval.tv_usec &&
+	    croot->cache_timeval.tv_sec == ioloop_timeval.tv_sec &&
+	    ioloop_timeval.tv_sec != 0) {
+		*bytes_r = croot->cached_bytes;
+		*count_r = croot->cached_count;
+		return 1;
+	}
 
 	*bytes_r = *count_r = 0;
 	if (root->recounting)
@@ -159,12 +176,20 @@ int quota_count(struct quota_root *root, uint64_t *bytes_r, uint64_t *count_r)
 	}
 	quota_mailbox_iter_deinit(&iter);
 	root->recounting = FALSE;
+	if (ret > 0) {
+		croot->cache_timeval = ioloop_timeval;
+		croot->cached_bytes = *bytes_r;
+		croot->cached_count = *count_r;
+	}
 	return ret < 0 ? -1 : 0;
 }
 
 static struct quota_root *count_quota_alloc(void)
 {
-	return i_new(struct quota_root, 1);
+	struct count_quota_root *root;
+
+	root = i_new(struct count_quota_root, 1);
+	return &root->root;
 }
 
 static int count_quota_init(struct quota_root *root, const char *args,
@@ -273,6 +298,9 @@ static int
 count_quota_update(struct quota_root *root,
 		   struct quota_transaction_context *ctx)
 {
+	struct count_quota_root *croot = (struct count_quota_root *)root;
+
+	croot->cache_timeval.tv_sec = 0;
 	if (ctx->recalculate) {
 		if (quota_count_recalculate(root) < 0)
 			return -1;

commit ad3bf867168121a1255720487457d8bffc3f5e48

quota: dict-quota was broken by 464db6d9d
We can't assume that the quota_root given to quota_count() is
count_quota_root, because dict_quota_root also calls it.

src/plugins/quota/quota-count.c
@@ -145,18 +145,9 @@ quota_mailbox_iter_next(struct quota_mailbox_iter *iter)
 
 int quota_count(struct quota_root *root, uint64_t *bytes_r, uint64_t *count_r)
 {
-	struct count_quota_root *croot = (struct count_quota_root *)root;
 	struct quota_mailbox_iter *iter;
 	const struct mailbox_info *info;
 	int ret = 0, ret2;
-
-	if (croot->cache_timeval.tv_usec == ioloop_timeval.tv_usec &&
-	    croot->cache_timeval.tv_sec == ioloop_timeval.tv_sec &&
-	    ioloop_timeval.tv_sec != 0) {
-		*bytes_r = croot->cached_bytes;
-		*count_r = croot->cached_count;
-		return 1;
-	}
 
 	*bytes_r = *count_r = 0;
 	if (root->recounting)
@@ -176,10 +167,26 @@ int quota_count(struct quota_root *root, uint64_t *bytes_r, uint64_t *count_r)
 	}
 	quota_mailbox_iter_deinit(&iter);
 	root->recounting = FALSE;
+	return ret;
+}
+
+static int quota_count_cached(struct count_quota_root *root,
+			      uint64_t *bytes_r, uint64_t *count_r)
+{
+	int ret;
+
+	if (root->cache_timeval.tv_usec == ioloop_timeval.tv_usec &&
+	    root->cache_timeval.tv_sec == ioloop_timeval.tv_sec &&
+	    ioloop_timeval.tv_sec != 0) {
+		*bytes_r = root->cached_bytes;
+		*count_r = root->cached_count;
+		return 1;
+	}
+	ret = quota_count(&root->root, bytes_r, count_r);
 	if (ret > 0) {
-		croot->cache_timeval = ioloop_timeval;
-		croot->cached_bytes = *bytes_r;
-		croot->cached_count = *count_r;
+		root->cache_timeval = ioloop_timeval;
+		root->cached_bytes = *bytes_r;
+		root->cached_count = *count_r;
 	}
 	return ret < 0 ? -1 : 0;
 }
@@ -217,12 +224,13 @@ count_quota_root_get_resources(struct quota_root *root ATTR_UNUSED)
 }
 
 static int
-count_quota_get_resource(struct quota_root *root,
+count_quota_get_resource(struct quota_root *_root,
 			 const char *name, uint64_t *value_r)
 {
+	struct count_quota_root *root = (struct count_quota_root *)_root;
 	uint64_t bytes, count;
 
-	if (quota_count(root, &bytes, &count) < 0)
+	if (quota_count_cached(root, &bytes, &count) < 0)
 		return -1;
 
 	if (strcmp(name, QUOTA_NAME_STORAGE_BYTES) == 0)