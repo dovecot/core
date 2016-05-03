dovecot/core

commit 26a6ff7f0e1e020212c68e97110b0a3a9d2af628

config: Remove old_namespace()
From config/old-set-parser.c, and its caller. The namespace section
named "private", "shared" or "public" is obsolete accordingly.

src/config/old-set-parser.c
@@ -510,21 +510,6 @@ static bool old_auth_section(struct config_parser_context *ctx,
 	return FALSE;
 }
 
-static bool old_namespace(struct config_parser_context *ctx,
-			  const char *value)
-{
-	if (strcmp(value, "private") != 0 &&
-	    strcmp(value, "shared") != 0 &&
-	    strcmp(value, "public") != 0)
-		return FALSE;
-
-	obsolete(ctx, "namespace %s {} has been replaced by namespace { type=%s }", value, value);
-	config_parser_apply_line(ctx, CONFIG_LINE_TYPE_SECTION_BEGIN, "namespace", "");
-	config_parser_apply_line(ctx, CONFIG_LINE_TYPE_KEYVALUE,
-				 "type", value);
-	return TRUE;
-}
-
 static void socket_apply(struct config_parser_context *ctx)
 {
 	const struct socket_set *set = &ctx->old->socket_set;
@@ -608,7 +593,7 @@ bool old_settings_handle(struct config_parser_context *ctx,
 			ctx->old->auth_section = 1;
 			return TRUE;
 		} else if (ctx->pathlen == 0 && strcmp(key, "namespace") == 0)
-			return old_namespace(ctx, value);
+			return TRUE;
 		else if (ctx->pathlen == 0 && strcmp(key, "protocol") == 0 &&
 			 strcmp(value, "managesieve") == 0) {
 			obsolete(ctx, "protocol managesieve {} has been replaced by protocol sieve { }");

commit cb06054f863550b766950fe2949b69b7047bcd81

config: Fixed namespaces to work again
Broken by 26a6ff7f0. old_settings_handle() should have returned FALSE for
settings it didn't handle.

src/config/old-set-parser.c
@@ -592,9 +592,7 @@ bool old_settings_handle(struct config_parser_context *ctx,
 			obsolete(ctx, "add auth_ prefix to all settings inside auth {} and remove the auth {} section completely");
 			ctx->old->auth_section = 1;
 			return TRUE;
-		} else if (ctx->pathlen == 0 && strcmp(key, "namespace") == 0)
-			return TRUE;
-		else if (ctx->pathlen == 0 && strcmp(key, "protocol") == 0 &&
+		} else if (ctx->pathlen == 0 && strcmp(key, "protocol") == 0 &&
 			 strcmp(value, "managesieve") == 0) {
 			obsolete(ctx, "protocol managesieve {} has been replaced by protocol sieve { }");
 			config_parser_apply_line(ctx, CONFIG_LINE_TYPE_SECTION_BEGIN,