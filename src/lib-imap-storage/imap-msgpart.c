dovecot/core

commit 333602c4048025afffabfbc57be2df6fc0ac7f4b

lib-imap-storage: Don't double-filter HEADER.FIELDS (..) headers.

src/lib-imap-storage/imap-msgpart.c
@@ -377,12 +377,10 @@ imap_msgpart_get_partial_header(struct mail *mail, struct istream *mail_input,
 	bool has_nuls;
 
 	if (msgpart->fetch_type == FETCH_HEADER_FIELDS) {
-		input = i_stream_create_header_filter(mail_input,
-						      HEADER_FILTER_INCLUDE |
-						      HEADER_FILTER_HIDE_BODY,
-						      hdr_fields, hdr_count,
-						      *null_header_filter_callback,
-						      (void *)NULL);
+		/* mail_get_header_stream() already filtered out the
+		   unwanted headers. */
+		input = mail_input;
+		i_stream_ref(input);
 	} else {
 		i_assert(msgpart->fetch_type == FETCH_HEADER_FIELDS_NOT);
 		input = i_stream_create_header_filter(mail_input,

commit 564b73a662e20d0b4cae22d4d180cd1d7023923a

lib-imap-storage: Removed wrong assert.
Now that we're no longer putting the headers through istream-header-filter
it doesn't necessarily have CRLFs. Broken by 333602c40.

src/lib-imap-storage/imap-msgpart.c
@@ -417,8 +417,6 @@ imap_msgpart_crlf_seek(struct mail *mail, struct istream *input,
 	uoff_t virtual_skip = msgpart->partial_offset;
 	bool cr_skipped;
 
-	i_assert(msgpart->headers == NULL); /* HEADER.FIELDS returns CRLFs */
-
 	if (virtual_skip == 0) {
 		/* no need to seek */
 	} else if (cache->uid == mail->uid &&