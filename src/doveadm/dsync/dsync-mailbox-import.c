dovecot/core

commit 79490ec1a58241d6011fa36713ca651d795855c3

doveadm backup: If local mailbox has more mails than remote, detect and delete it earlier.

src/doveadm/dsync/dsync-mailbox-import.c
@@ -751,6 +751,11 @@ static bool dsync_mailbox_try_save_cur(struct dsync_mailbox_importer *importer,
 		/* add a record for local mail */
 		i_assert(importer->cur_mail != NULL);
 		if (importer->revert_local_changes) {
+			if (save_change == NULL) {
+				dsync_mailbox_revert_existing_uid(importer, importer->cur_mail->uid,
+					t_strdup_printf("highest than remote's UIDs (remote UIDNEXT=%u)", importer->remote_uid_next));
+				return TRUE;
+			}
 			mail_expunge(importer->cur_mail);
 			importer->cur_mail_skip = TRUE;
 			importer->next_local_seq++;

commit 8c8f6ed0a46db6589c08d8db1f4ed779ef5bc19b

doveadm backup: Fixed unnecessary mailbox deletion.
Broken by 79490ec1a. The idea is to delete the mailbox early to avoid
unnecessary work, but we deleted it too early. If the last mail(s) were
deleted instead of just expunging them we deleted the whole mailbox.

src/doveadm/dsync/dsync-mailbox-import.c
@@ -783,9 +783,10 @@ static bool dsync_mailbox_try_save_cur(struct dsync_mailbox_importer *importer,
 		/* add a record for local mail */
 		i_assert(importer->cur_mail != NULL);
 		if (importer->revert_local_changes) {
-			if (save_change == NULL) {
+			if (save_change == NULL &&
+			    importer->cur_mail->uid >= importer->remote_uid_next) {
 				dsync_mailbox_revert_existing_uid(importer, importer->cur_mail->uid,
-					t_strdup_printf("highest than remote's UIDs (remote UIDNEXT=%u)", importer->remote_uid_next));
+					t_strdup_printf("higher than remote's UIDs (remote UIDNEXT=%u)", importer->remote_uid_next));
 				return TRUE;
 			}
 			mail_expunge(importer->cur_mail);