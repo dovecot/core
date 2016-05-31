0f56c2ae3318c2d0ccba824f7fa85c5e4a5d5e7c
Timo Sirainen <timo.sirainen@dovecot.fi>
2016-06-01 00:12:44 +0300

    lib: Fixed file_copy() after recent changes.

--- src/lib/file-copy.c
+++ src/lib/file-copy.c
@@ -18,7 +18,7 @@ static int file_copy_to_tmp(const char *srcpath, const char *tmppath,
 	struct stat st;
 	mode_t old_umask;
 	int fd_in, fd_out;
-	off_t ret;
+	int ret = -1;
 
 	if (try_hardlink) {
 		/* see if hardlinking works */
@@ -72,6 +72,7 @@ static int file_copy_to_tmp(const char *srcpath, const char *tmppath,
 
 	switch (o_stream_send_istream(output, input)) {
 	case OSTREAM_SEND_ISTREAM_RESULT_FINISHED:
+		ret = 0;
 		break;
 	case OSTREAM_SEND_ISTREAM_RESULT_WAIT_INPUT:
 	case OSTREAM_SEND_ISTREAM_RESULT_WAIT_OUTPUT:
