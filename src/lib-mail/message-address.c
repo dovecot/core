dovecot/core

commit 91fdb25b5a77defecbbc0cb9b7f15f28e9dab1f0

lib-mail: parse_mailbox: Set display name instead mailbox when parsing failed
It does not make sense to set mailbox without domain on incorrect input.
Rather set display name which is more likely useable value.

Test case:

  test

is parsed as:

  { name = "test", mailbox = NULL, domain = NULL }

src/lib-mail/message-address.c
@@ -205,6 +205,11 @@ static int parse_mailbox(struct message_address_parser_context *ctx)
 		/* nope, should be addr-spec */
 		ctx->parser.data = start;
 		ret = parse_addr_spec(ctx);
+		if (ctx->addr.invalid_syntax && ctx->addr.name == NULL &&
+		    ctx->addr.mailbox == NULL && ctx->addr.domain == NULL) {
+			ctx->addr.name = ctx->addr.mailbox;
+			ctx->addr.mailbox = NULL;
+		}
 	}
 
 	if (ret < 0)

commit 64a07a73e7ce05753fe94ad1d4d7954ed45b8f6d

lib-mail: Fixed to 91fdb25b5
Caused by my manual edits to avoid the -Wstrict-bool warnings.

src/lib-mail/message-address.c
@@ -250,7 +250,7 @@ static int parse_mailbox(struct message_address_parser_context *ctx)
 		ctx->parser.data = start;
 		ret = parse_addr_spec(ctx);
 		if (ctx->addr.invalid_syntax && ctx->addr.name == NULL &&
-		    ctx->addr.mailbox == NULL && ctx->addr.domain == NULL) {
+		    ctx->addr.mailbox != NULL && ctx->addr.domain == NULL) {
 			ctx->addr.name = ctx->addr.mailbox;
 			ctx->addr.mailbox = NULL;
 		}