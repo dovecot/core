5165bc0ffcdadc07af9e7b37a149492f09777d14
Timo Sirainen <timo.sirainen@dovecot.fi>
2016-06-29 14:31:28 +0300

    auth: Avoid nonstandard #pragma once

--- src/auth/auth-policy.h
+++ src/auth/auth-policy.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef AUTH_POLICY_H
+#define AUTH_POLICY_H
 
 typedef void (*auth_policy_callback_t)(int, void *);
 
@@ -6,3 +7,5 @@ void auth_policy_check(struct auth_request *request, const char *password, auth_
 void auth_policy_report(struct auth_request *request);
 void auth_policy_init(void);
 void auth_policy_deinit(void);
+
+#endif
