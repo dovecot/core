dovecot/core

commit 042668c0cd5a7d35ce6373ae493695e8f12d3157

maildir: Avoid extra memory usage on duplicate uidlist entries
Also fixes assert-crashing in hash_table_insert() when it happens.

src/lib-storage/index/maildir/maildir-uidlist.c
@@ -1701,8 +1701,11 @@ maildir_uidlist_sync_next_partial(struct maildir_uidlist_sync_ctx *ctx,
 		rec = p_new(uidlist->record_pool,
 			    struct maildir_uidlist_rec, 1);
 		rec->uid = (uint32_t)-1;
+		rec->filename = p_strdup(uidlist->record_pool, filename);
 		array_append(&uidlist->records, &rec, 1);
 		uidlist->change_counter++;
+
+		hash_table_insert(uidlist->files, rec->filename, rec);
 	}
 	if (uid != 0) {
 		if (rec->uid != uid && rec->uid != (uint32_t)-1) {
@@ -1724,8 +1727,6 @@ maildir_uidlist_sync_next_partial(struct maildir_uidlist_sync_ctx *ctx,
 	rec->flags &= ~MAILDIR_UIDLIST_REC_FLAG_NEW_DIR;
 	rec->flags = (rec->flags | flags) &
 		~MAILDIR_UIDLIST_REC_FLAG_NONSYNCED;
-	rec->filename = p_strdup(uidlist->record_pool, filename);
-	hash_table_insert(uidlist->files, rec->filename, rec);
 
 	ctx->finished = FALSE;
 	*rec_r = rec;
@@ -1816,6 +1817,8 @@ int maildir_uidlist_sync_next_uid(struct maildir_uidlist_sync_ctx *ctx,
 			/* didn't exist in uidlist, it's recent */
 			flags |= MAILDIR_UIDLIST_REC_FLAG_RECENT;
 		}
+		rec->filename = p_strdup(ctx->record_pool, filename);
+		hash_table_insert(ctx->files, rec->filename, rec);
 
 		array_append(&ctx->records, &rec, 1);
 	}
@@ -1826,8 +1829,6 @@ int maildir_uidlist_sync_next_uid(struct maildir_uidlist_sync_ctx *ctx,
 	}
 
 	rec->flags = (rec->flags | flags) & ~MAILDIR_UIDLIST_REC_FLAG_NONSYNCED;
-	rec->filename = p_strdup(ctx->record_pool, filename);
-	hash_table_insert(ctx->files, rec->filename, rec);
 	*rec_r = rec;
 	return 1;
 }

commit 38920bff33eaa2acef5c200df5ce7088fd61e673

maildir: Fixed updating filenames in existing uidlist
Broken by 042668c0c.

This could have caused errors like:

 - maildir_file_do(...): Filename keeps changing
 - Expunged message reappeared, giving a new UID

src/lib-storage/index/maildir/maildir-uidlist.c
@@ -1706,6 +1706,8 @@ maildir_uidlist_sync_next_partial(struct maildir_uidlist_sync_ctx *ctx,
 		uidlist->change_counter++;
 
 		hash_table_insert(uidlist->files, rec->filename, rec);
+	} else if (strcmp(rec->filename, filename) != 0) {
+		rec->filename = p_strdup(uidlist->record_pool, filename);
 	}
 	if (uid != 0) {
 		if (rec->uid != uid && rec->uid != (uint32_t)-1) {
@@ -1800,6 +1802,8 @@ int maildir_uidlist_sync_next_uid(struct maildir_uidlist_sync_ctx *ctx,
 		   to check for duplicates. */
 		rec->flags &= ~(MAILDIR_UIDLIST_REC_FLAG_NEW_DIR |
 				MAILDIR_UIDLIST_REC_FLAG_MOVED);
+		if (strcmp(rec->filename, filename) != 0)
+			rec->filename = p_strdup(ctx->record_pool, filename);
 	} else {
 		old_rec = hash_table_lookup(uidlist->files, filename);
 		i_assert(old_rec != NULL || UIDLIST_IS_LOCKED(uidlist));