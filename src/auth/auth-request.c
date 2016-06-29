dovecot/core

commit e82511362eb29f75fd940bb3377589c62fb2984a

auth-policy: Do not do policy checks every time

src/auth/auth-request.c
@@ -872,19 +872,19 @@ void auth_request_verify_plain(struct auth_request *request,
 
 	i_assert(request->state == AUTH_REQUEST_STATE_MECH_CONTINUE);
 
-	ctx = p_new(request->pool, struct auth_policy_check_ctx, 1);
-	ctx->request = request;
-	if (request->mech_password == NULL)
-		ctx->password = p_strdup(request->pool, password);
-	else
-		ctx->password = request->mech_password;
-	ctx->callback_plain = callback;
-	ctx->type = AUTH_POLICY_CHECK_TYPE_PLAIN;
-
 	if (request->policy_processed)
 		auth_request_verify_plain_continue(request, ctx->password, callback);
-	else
+	else {
+		ctx = p_new(request->pool, struct auth_policy_check_ctx, 1);
+		ctx->request = request;
+		if (request->mech_password == NULL)
+			ctx->password = p_strdup(request->pool, password);
+		else
+			ctx->password = request->mech_password;
+		ctx->callback_plain = callback;
+		ctx->type = AUTH_POLICY_CHECK_TYPE_PLAIN;
 		auth_policy_check(request, ctx->password, auth_request_policy_check_callback, ctx);
+	}
 }
 
 static
@@ -1038,16 +1038,19 @@ void auth_request_lookup_credentials(struct auth_request *request,
 
 	i_assert(request->state == AUTH_REQUEST_STATE_MECH_CONTINUE);
 
-	ctx = p_new(request->pool, struct auth_policy_check_ctx, 1);
-	ctx->request = request;
-	if (request->credentials_scheme == NULL)
-		ctx->scheme = p_strdup(request->pool, scheme);
-	else
-		ctx->scheme = request->credentials_scheme;
-	ctx->callback_lookup = callback;
-	ctx->type = AUTH_POLICY_CHECK_TYPE_LOOKUP;
-
-	auth_policy_check(request, ctx->password, auth_request_policy_check_callback, ctx);
+	if (request->policy_processed)
+		auth_request_lookup_credentials_policy_continue(request, scheme, callback);
+	else {
+		ctx = p_new(request->pool, struct auth_policy_check_ctx, 1);
+		ctx->request = request;
+		if (request->credentials_scheme == NULL)
+			ctx->scheme = p_strdup(request->pool, scheme);
+		else
+			ctx->scheme = request->credentials_scheme;
+		ctx->callback_lookup = callback;
+		ctx->type = AUTH_POLICY_CHECK_TYPE_LOOKUP;
+		auth_policy_check(request, ctx->password, auth_request_policy_check_callback, ctx);
+	}
 }
 
 static

commit f000a3cda907a489ce643f9bea9b1dcefd4961eb

auth: Fixed plaintext authentication when auth policy was already processed.
Broken by e82511362.

src/auth/auth-request.c
@@ -872,9 +872,10 @@ void auth_request_verify_plain(struct auth_request *request,
 
 	i_assert(request->state == AUTH_REQUEST_STATE_MECH_CONTINUE);
 
-	if (request->policy_processed)
-		auth_request_verify_plain_continue(request, ctx->password, callback);
-	else {
+	if (request->policy_processed) {
+		auth_request_verify_plain_continue(request,
+			p_strdup(request->pool, password), callback);
+	} else {
 		ctx = p_new(request->pool, struct auth_policy_check_ctx, 1);
 		ctx->request = request;
 		if (request->mech_password == NULL)