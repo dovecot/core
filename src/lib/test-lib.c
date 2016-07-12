6335b1be387359ac461b0c34087bbba98a925a49
Phil Carmody <phil@dovecot.fi>
2016-07-12 13:31:32 +0300

    lib: tests - give the tests names, using the new macros
    The lists of test function declarations and of test functions to call was
    basically the same list. Use the new test macros to squash them into one
    list, and then chose to name all the tests, so that you can select which
    ones to run. This can simplify the debug cycle when some of the tests are
    lengthy.
    
    e.g.:
      phil@phil:~/repos/dovecot-core$ src/lib/test-lib --match crc
      crc32 ................................................................ : ok
      0 / 1 tests failed
    
    src/lib is clearly the most complicated case, conversions of others where
    filtering would be useful should be trivial following this example.
    
    Signed-off-by: Phil Carmody <phil@dovecot.fi>

--- src/lib/test-lib.c
+++ src/lib/test-lib.c
@@ -2,73 +2,27 @@
 
 #include "test-lib.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
-	static void (*test_functions[])(void) = {
-		test_aqueue,
-		test_array,
-		test_base32,
-		test_base64,
-		test_bits,
-		test_bsearch_insert_pos,
-		test_buffer,
-		test_crc32,
-		test_data_stack,
-		test_failures,
-		test_guid,
-		test_hash,
-		test_hash_format,
-		test_hash_method,
-		test_hex_binary,
-		test_ioloop,
-		test_iso8601_date,
-		test_iostream_temp,
-		test_istream,
-		test_istream_base64_decoder,
-		test_istream_base64_encoder,
-		test_istream_chain,
-		test_istream_concat,
-		test_istream_crlf,
-		test_istream_failure_at,
-		test_istream_seekable,
-		test_istream_tee,
-		test_istream_unix,
-		test_json_parser,
-		test_json_tree,
-		test_llist,
-		test_log_throttle,
-		test_mempool_alloconly,
-		test_net,
-		test_numpack,
-		test_pkcs5_pbkdf2,
-		test_ostream_escaped,
-		test_ostream_failure_at,
-		test_ostream_file,
-		test_primes,
-		test_printf_format_fix,
-		test_priorityq,
-		test_seq_range_array,
-		test_str,
-		test_strescape,
-		test_strfuncs,
-		test_strnum,
-		test_str_find,
-		test_str_sanitize,
-		test_str_table,
-		test_time_util,
-		test_timing,
-		test_unichar,
-		test_utc_mktime,
-		test_var_expand,
-		test_wildcard_match,
-		NULL
+	const char *match = "";
+	if (argc > 2 && strcmp(argv[1], "--match") == 0)
+		match = argv[2];
+
+	static struct named_test test_functions[] = {
+#define TEST(x) TEST_NAMED(x)
+#define FATAL(x)
+#include "test-lib.inc"
+#undef TEST
+#undef FATAL
+		{ NULL, NULL }
 	};
-	static enum fatal_test_state (*fatal_functions[])(int) = {
-		fatal_array,
-		fatal_data_stack,
-		fatal_mempool,
-		fatal_printf_format_fix,
-		NULL
+	static struct named_fatal fatal_functions[] = {
+#define TEST(x)
+#define FATAL(x) FATAL_NAMED(x)
+#include "test-lib.inc"
+#undef TEST
+#undef FATAL
+		{ NULL, NULL }
 	};
-	return test_run_with_fatals(test_functions, fatal_functions);
+	return test_run_named_with_fatals(match, test_functions, fatal_functions);
 }
--- src/lib/test-lib.h
+++ src/lib/test-lib.h
@@ -4,65 +4,10 @@
 #include "lib.h"
 #include "test-common.h"
 
-void test_aqueue(void);
-void test_array(void);
-enum fatal_test_state fatal_array(int);
-void test_base32(void);
-void test_base64(void);
-void test_bits(void);
-void test_bsearch_insert_pos(void);
-void test_buffer(void);
-void test_crc32(void);
-void test_data_stack(void);
-enum fatal_test_state fatal_data_stack(int);
-void test_failures(void);
-void test_guid(void);
-void test_hash(void);
-void test_hash_format(void);
-void test_hash_method(void);
-void test_hex_binary(void);
-void test_ioloop(void);
-void test_iso8601_date(void);
-void test_iostream_temp(void);
-void test_istream(void);
-void test_istream_base64_decoder(void);
-void test_istream_base64_encoder(void);
-void test_istream_chain(void);
-void test_istream_concat(void);
-void test_istream_crlf(void);
-void test_istream_failure_at(void);
-void test_istream_seekable(void);
-void test_istream_tee(void);
-void test_istream_unix(void);
-void test_json_parser(void);
-void test_json_tree(void);
-void test_llist(void);
-void test_log_throttle(void);
-void test_mempool_alloconly(void);
-enum fatal_test_state fatal_mempool(int);
-void test_pkcs5_pbkdf2(void);
-void test_net(void);
-void test_numpack(void);
-void test_ostream_escaped(void);
-void test_ostream_failure_at(void);
-void test_ostream_file(void);
-void test_primes(void);
-void test_printf_format_fix(void);
-enum fatal_test_state fatal_printf_format_fix(int);
-void test_priorityq(void);
-void test_seq_range_array(void);
-void test_str(void);
-void test_strescape(void);
-void test_strfuncs(void);
-void test_strnum(void);
-void test_str_find(void);
-void test_str_sanitize(void);
-void test_str_table(void);
-void test_time_util(void);
-void test_timing(void);
-void test_unichar(void);
-void test_utc_mktime(void);
-void test_var_expand(void);
-void test_wildcard_match(void);
+#define TEST(x) TEST_DECL(x)
+#define FATAL(x) FATAL_DECL(x)
+#include "test-lib.inc"
+#undef TEST
+#undef FATAL
 
 #endif
