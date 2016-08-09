dovecot/core

commit dfe53c824fb6f6e3736ee2958665dd6f545724b1

lmtp: Use port 24 if no port has been provided
This allows using the LMTP, IMAP and POP3 proxy on the same
Dovecot installation without the userdb providing the port
to connect to.

TCP port 24 is registered at IANA as: "any private mail system"

LMTP being the Local Mail Transfer Protocol classifies as a private
mail system and thus justifies the usage of port 24.

Prior to this patch the LTMP client would connect to TCP port 0 by
default if the userdb did not provide a port to connect to.

src/lmtp/commands.c
@@ -263,6 +263,8 @@ client_proxy_rcpt_parse_fields(struct lmtp_proxy_rcpt_settings *set,
 		} else if (strcmp(key, "protocol") == 0) {
 			if (strcmp(value, "lmtp") == 0)
 				set->protocol = LMTP_CLIENT_PROTOCOL_LMTP;
+				if (!port_set)
+					set->port = 24;
 			else if (strcmp(value, "smtp") == 0) {
 				set->protocol = LMTP_CLIENT_PROTOCOL_SMTP;
 				if (!port_set)

commit accba2f170529d73097e098d0bb6c13b7b4d6abf

lmtp: Fixed using default port 24
Broken by dfe53c824.

src/lmtp/commands.c
@@ -261,11 +261,11 @@ client_proxy_rcpt_parse_fields(struct lmtp_proxy_rcpt_settings *set,
 			}
 			set->timeout_msecs *= 1000;
 		} else if (strcmp(key, "protocol") == 0) {
-			if (strcmp(value, "lmtp") == 0)
+			if (strcmp(value, "lmtp") == 0) {
 				set->protocol = LMTP_CLIENT_PROTOCOL_LMTP;
 				if (!port_set)
 					set->port = 24;
-			else if (strcmp(value, "smtp") == 0) {
+			} else if (strcmp(value, "smtp") == 0) {
 				set->protocol = LMTP_CLIENT_PROTOCOL_SMTP;
 				if (!port_set)
 					set->port = 25;