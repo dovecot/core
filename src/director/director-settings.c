a64e49f98394012ccd41b80810da3c3a710b5d74
Aki Tuomi <aki.tuomi@dovecot.fi>
2016-10-13 16:11:48 +0300

    director: Support flush socket
    This allows specifying an URI to execute
    on user kill. It can be of form
    exec:/path/to/bin, unix:/path/to/socket or
    tcp:ip:port
    
    The location is sent FLUSH username-hash
    per killed user. You can execute some
    action there, and you are expected to
    return '+\nOK\n' as reply once you are
    done.

--- src/director/director-settings.c
+++ src/director/director-settings.c
@@ -71,6 +71,7 @@ static const struct setting_define director_setting_defines[] = {
 	DEF(SET_STR, director_servers),
 	DEF(SET_STR, director_mail_servers),
 	DEF(SET_STR, director_username_hash),
+	DEF(SET_STR, director_flush_socket),
 	DEF(SET_TIME, director_user_expire),
 	DEF(SET_TIME, director_user_kick_delay),
 	DEF(SET_IN_PORT, director_doveadm_port),
@@ -85,6 +86,7 @@ const struct director_settings director_default_settings = {
 	.director_servers = "",
 	.director_mail_servers = "",
 	.director_username_hash = "%Lu",
+	.director_flush_socket = "",
 	.director_user_expire = 60*15,
 	.director_user_kick_delay = 2,
 	.director_doveadm_port = 0
--- src/director/director-settings.h
+++ src/director/director-settings.h
@@ -9,6 +9,8 @@ struct director_settings {
 	const char *director_servers;
 	const char *director_mail_servers;
 	const char *director_username_hash;
+	const char *director_flush_socket;
+
 	unsigned int director_user_expire;
 	unsigned int director_user_kick_delay;
 	in_port_t director_doveadm_port;
