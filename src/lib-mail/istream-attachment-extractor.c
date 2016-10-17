dovecot/core

commit 7f74811b78f8915e73dffc88bb49009e98b6846d

global: Make sure i_stream_read() calls handle 0 and -2 return values correctly.

src/lib-mail/istream-attachment-extractor.c
@@ -375,8 +375,7 @@ static int astream_decode_base64(struct attachment_istream *astream)
 	o_stream_cork(output);
 
 	hash_format_reset(astream->set.hash_format);
-	while ((ret = i_stream_read(base64_input)) > 0) {
-		data = i_stream_get_data(base64_input, &size);
+	while ((ret = i_stream_read_more(base64_input, &data, &size)) > 0) {
 		buffer_set_used_size(buf, 0);
 		if (base64_decode(data, size, &size, buf) < 0) {
 			i_error("istream-attachment: BUG: "

src/doveadm/client-connection-http.c
@@ -200,8 +200,11 @@ static void doveadm_http_server_json_success(void *context, struct istream *resu
 
 static int doveadm_http_server_istream_read(struct client_connection_http *conn)
 {
-	while (i_stream_read(conn->cmd_param->value.v_istream) > 0)
-		i_stream_skip(conn->cmd_param->value.v_istream, i_stream_get_data_size(conn->cmd_param->value.v_istream));
+	const unsigned char *data;
+	size_t size;
+
+	while (i_stream_read_more(conn->cmd_param->value.v_istream, &data, &size) > 0)
+		i_stream_skip(conn->cmd_param->value.v_istream, size);
 	if (!conn->cmd_param->value.v_istream->eof)
 		return 0;
 

src/doveadm/doveadm-mail-save.c
@@ -35,13 +35,13 @@ cmd_save_to_mailbox(struct save_cmd_context *ctx, struct mailbox *box,
 		mailbox_transaction_rollback(&trans);
 		return -1;
 	}
-	while ((ret = i_stream_read(input)) > 0 || ret == -2) {
+	do {
 		if (mailbox_save_continue(save_ctx) < 0) {
 			save_failed = TRUE;
 			ret = -1;
 			break;
 		}
-	}
+	} while ((ret = i_stream_read(input)) > 0);
 	i_assert(ret == -1);
 
 	if (input->stream_errno != 0) {

src/doveadm/doveadm-mail.c
@@ -163,8 +163,11 @@ static struct doveadm_mail_cmd_context *cmd_purge_alloc(void)
 
 static void doveadm_mail_cmd_input_input(struct doveadm_mail_cmd_context *ctx)
 {
-	while (i_stream_read(ctx->cmd_input) > 0)
-		i_stream_skip(ctx->cmd_input, i_stream_get_data_size(ctx->cmd_input));
+	const unsigned char *data;
+	size_t size;
+
+	while (i_stream_read_more(ctx->cmd_input, &data, &size) > 0)
+		i_stream_skip(ctx->cmd_input, size);
 	if (!ctx->cmd_input->eof)
 		return;
 

src/doveadm/dsync/dsync-mail.c
@@ -35,6 +35,7 @@ int dsync_mail_get_hdr_hash(struct mail *mail, unsigned int version,
 	unsigned char md5_result[MD5_RESULTLEN];
 	const unsigned char *data;
 	size_t size;
+	ssize_t sret;
 	int ret = 0;
 
 	hdr_ctx = mailbox_header_lookup_init(mail->box, hashed_headers);
@@ -47,15 +48,12 @@ int dsync_mail_get_hdr_hash(struct mail *mail, unsigned int version,
 
 	md5_init(&md5_ctx);
 	memset(&hash_ctx, 0, sizeof(hash_ctx));
-	while (!i_stream_is_eof(input)) {
-		if (i_stream_read_more(input, &data, &size) == -1)
-			break;
-		if (size == 0)
-			break;
+	while ((sret = i_stream_read_more(input, &data, &size)) > 0) {
 		message_header_hash_more(&hash_ctx, &hash_method_md5, &md5_ctx,
 					 version, data, size);
 		i_stream_skip(input, size);
 	}
+	i_assert(sret == -1);
 	if (input->stream_errno != 0)
 		ret = -1;
 	i_stream_unref(&input);

src/imap/cmd-append.c
@@ -205,12 +205,10 @@ cmd_append_catenate_mpurl(struct client_command_context *cmd,
 	/* add this input stream to chain */
 	i_stream_chain_append(ctx->catchain, mpresult.input);
 	/* save by reading the chain stream */
-	while (!i_stream_is_eof(mpresult.input)) {
+	do {
 		ret = i_stream_read(mpresult.input);
 		i_assert(ret != 0); /* we can handle only blocking input here */
-		if (mailbox_save_continue(ctx->save_ctx) < 0 || ret == -1)
-			break;
-	}
+	} while (mailbox_save_continue(ctx->save_ctx) == 0 && ret != -1);
 
 	if (mpresult.input->stream_errno != 0) {
 		mail_storage_set_critical(ctx->box->storage,

src/lib-compression/istream-bzlib.c
@@ -225,6 +225,8 @@ i_stream_bzlib_seek(struct istream_private *stream, uoff_t v_offset, bool mark)
 		stream->pos = stream->skip;
 	} else {
 		/* read and cache forward */
+		ssize_t ret;
+
 		do {
 			size_t avail = stream->pos - stream->skip;
 
@@ -236,7 +238,8 @@ i_stream_bzlib_seek(struct istream_private *stream, uoff_t v_offset, bool mark)
 			}
 
 			i_stream_skip(&stream->istream, avail);
-		} while (i_stream_read(&stream->istream) >= 0);
+		} while ((ret = i_stream_read(&stream->istream)) > 0);
+		i_assert(ret == -1);
 
 		if (stream->istream.v_offset != v_offset) {
 			/* some failure, we've broken it */
@@ -279,11 +282,13 @@ i_stream_bzlib_stat(struct istream_private *stream, bool exact)
 
 	if (zstream->stream_size == (uoff_t)-1) {
 		uoff_t old_offset = stream->istream.v_offset;
+		ssize_t ret;
 
 		do {
 			size = i_stream_get_data_size(&stream->istream);
 			i_stream_skip(&stream->istream, size);
-		} while (i_stream_read(&stream->istream) > 0);
+		} while ((ret = i_stream_read(&stream->istream)) > 0);
+		i_assert(ret == -1);
 
 		i_stream_seek(&stream->istream, old_offset);
 		if (zstream->stream_size == (uoff_t)-1)

src/lib-compression/istream-lz4.c
@@ -208,6 +208,8 @@ i_stream_lz4_seek(struct istream_private *stream, uoff_t v_offset, bool mark)
 		stream->pos = stream->skip;
 	} else {
 		/* read and cache forward */
+		ssize_t ret;
+
 		do {
 			size_t avail = stream->pos - stream->skip;
 
@@ -219,7 +221,8 @@ i_stream_lz4_seek(struct istream_private *stream, uoff_t v_offset, bool mark)
 			}
 
 			i_stream_skip(&stream->istream, avail);
-		} while (i_stream_read(&stream->istream) >= 0);
+		} while ((ret = i_stream_read(&stream->istream)) > 0);
+		i_assert(ret == -1);
 
 		if (stream->istream.v_offset != v_offset) {
 			/* some failure, we've broken it */
@@ -262,11 +265,13 @@ i_stream_lz4_stat(struct istream_private *stream, bool exact)
 
 	if (zstream->stream_size == (uoff_t)-1) {
 		uoff_t old_offset = stream->istream.v_offset;
+		ssize_t ret;
 
 		do {
 			size = i_stream_get_data_size(&stream->istream);
 			i_stream_skip(&stream->istream, size);
-		} while (i_stream_read(&stream->istream) > 0);
+		} while ((ret = i_stream_read(&stream->istream)) > 0);
+		i_assert(ret == -1);
 
 		i_stream_seek(&stream->istream, old_offset);
 		if (zstream->stream_size == (uoff_t)-1)

commit 9b5fa7fdd9b9f1f61eaddda48036df200fc5e56e

lib-mail: Fixed infinite loop in istream-attachment-extractor
Broken by 7f74811b7. Pretty much any larger attachment going through
this istream had a good potential of going into an infinite loop.

src/lib-mail/istream-attachment-extractor.c
@@ -375,7 +375,9 @@ static int astream_decode_base64(struct attachment_istream *astream)
 	o_stream_cork(output);
 
 	hash_format_reset(astream->set.hash_format);
-	while ((ret = i_stream_read_more(base64_input, &data, &size)) > 0) {
+	size_t bytes_needed = 1;
+	while ((ret = i_stream_read_bytes(base64_input, &data, &size,
+					  bytes_needed)) > 0) {
 		buffer_set_used_size(buf, 0);
 		if (base64_decode(data, size, &size, buf) < 0) {
 			i_error("istream-attachment: BUG: "
@@ -387,6 +389,7 @@ static int astream_decode_base64(struct attachment_istream *astream)
 		o_stream_nsend(output, buf->data, buf->used);
 		hash_format_loop(astream->set.hash_format,
 				 buf->data, buf->used);
+		bytes_needed = i_stream_get_data_size(base64_input) + 1;
 	}
 	if (ret != -1) {
 		i_assert(failed);