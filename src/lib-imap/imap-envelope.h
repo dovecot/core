5dc17f032d0e3b1586a87c0762c3aab4562a2d18
Stephan Bosch <stephan.bosch@dovecot.fi>
2016-10-23 12:10:44 +0200

    lib-imap: Removed existing envelope parsing function.
    This is currently only used by imaptest. Alternative API is provided in later commit.

--- src/lib-imap/imap-envelope.h
+++ src/lib-imap/imap-envelope.h
@@ -19,11 +19,6 @@ enum imap_envelope_field {
 	IMAP_ENVELOPE_FIELDS
 };
 
-enum imap_envelope_result_type {
-	IMAP_ENVELOPE_RESULT_TYPE_STRING,
-	IMAP_ENVELOPE_RESULT_TYPE_FIRST_MAILBOX
-};
-
 struct message_part_envelope_data;
 
 extern const char *imap_envelope_headers[];
@@ -38,11 +33,5 @@ void imap_envelope_parse_header(pool_t pool,
 /* Write envelope to given string */
 void imap_envelope_write_part_data(struct message_part_envelope_data *data,
 				   string_t *str);
-
-/* Parse envelope and store specified field to result. NIL is stored as NULL.
-   Returns TRUE if successful. */
-bool imap_envelope_parse(const char *envelope, enum imap_envelope_field field,
-			 enum imap_envelope_result_type result_type,
-			 const char **result);
 
 #endif
