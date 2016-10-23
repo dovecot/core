535dbe3c376347b6c7a593dca70643ffdbba95f7
Stephan Bosch <stephan.bosch@dovecot.fi>
2016-10-23 18:10:01 +0200

    lib-imap: imap-evenvelope: Changed imap_envelope_get_field() into a private equivalent.

--- src/lib-imap/imap-envelope.h
+++ src/lib-imap/imap-envelope.h
@@ -3,28 +3,9 @@
 
 struct imap_arg;
 struct message_header_line;
-
-enum imap_envelope_field {
-	/* NOTE: in the same order as listed in ENVELOPE */
-	IMAP_ENVELOPE_DATE = 0,
-	IMAP_ENVELOPE_SUBJECT,
-	IMAP_ENVELOPE_FROM,
-	IMAP_ENVELOPE_SENDER,
-	IMAP_ENVELOPE_REPLY_TO,
-	IMAP_ENVELOPE_TO,
-	IMAP_ENVELOPE_CC,
-	IMAP_ENVELOPE_BCC,
-	IMAP_ENVELOPE_IN_REPLY_TO,
-	IMAP_ENVELOPE_MESSAGE_ID,
-
-	IMAP_ENVELOPE_FIELDS
-};
-
 struct message_part_envelope_data;
 
 extern const char *imap_envelope_headers[];
-
-bool imap_envelope_get_field(const char *name, enum imap_envelope_field *ret);
 
 /* Update envelope data based from given header field */
 void imap_envelope_parse_header(pool_t pool,
