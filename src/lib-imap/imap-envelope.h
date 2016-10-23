300daf3c174deeabc0b734b033d1f6ccb148671e
Stephan Bosch <stephan.bosch@dovecot.fi>
2016-10-23 16:49:37 +0200

    lib-imap: imap-envelope: Added function to parse struct message_part_envelope_data from parsed imap args.

--- src/lib-imap/imap-envelope.h
+++ src/lib-imap/imap-envelope.h
@@ -1,6 +1,7 @@
 #ifndef IMAP_ENVELOPE_H
 #define IMAP_ENVELOPE_H
 
+struct imap_arg;
 struct message_header_line;
 
 enum imap_envelope_field {
@@ -33,5 +34,10 @@ void imap_envelope_parse_header(pool_t pool,
 /* Write envelope to given string */
 void imap_envelope_write_part_data(struct message_part_envelope_data *data,
 				   string_t *str);
+
+/* Parse envelope from arguments */
+bool imap_envelope_parse_args(const struct imap_arg *args,
+	pool_t pool, struct message_part_envelope_data **envlp_r,
+	const char **error_r);
 
 #endif
