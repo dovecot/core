dovecot/core

commit acfda38b75d0f0e899ef692fef01593bd56ed85e

global: Try to initialize var_expand_tab[] directly.
This avoids accidents with the array numbering being wrong.

src/lib-lda/mail-send.c
@@ -24,26 +24,21 @@ static const struct var_expand_table *
 get_var_expand_table(struct mail *mail, const char *reason,
 		     const char *recipient)
 {
-	static struct var_expand_table static_tab[] = {
-		{ 'n', NULL, "crlf" },
-		{ 'r', NULL, "reason" },
-		{ 's', NULL, "subject" },
-		{ 't', NULL, "to" },
+	const char *subject;
+	if (mail_get_first_header(mail, "Subject", &subject) <= 0)
+		subject = "";
+
+	const struct var_expand_table stack_tab[] = {
+		{ 'n', "\r\n", "crlf" },
+		{ 'r', reason, "reason" },
+		{ 's', subject, "subject" },
+		{ 't', recipient, "to" },
 		{ '\0', NULL, NULL }
 	};
 	struct var_expand_table *tab;
-	const char *subject;
 
-	tab = t_malloc_no0(sizeof(static_tab));
-	memcpy(tab, static_tab, sizeof(static_tab));
-
-	tab[0].value = "\r\n";
-	tab[1].value = reason;
-	if (mail_get_first_header(mail, "Subject", &subject) <= 0)
-		subject = "";
-	tab[2].value = str_sanitize(subject, 80);
-	tab[3].value = recipient;
-
+	tab = t_malloc_no0(sizeof(stack_tab));
+	memcpy(tab, stack_tab, sizeof(stack_tab));
 	return tab;
 }
 

src/imap-hibernate/imap-client.c
@@ -436,55 +436,47 @@ static void imap_client_add_idle_keepalive_timeout(struct imap_client *client)
 static const struct var_expand_table *
 imap_client_get_var_expand_table(struct imap_client *client)
 {
-	static struct var_expand_table static_tab[] = {
-		{ 'u', NULL, "user" },
-		{ 'n', NULL, "username" },
-		{ 'd', NULL, "domain" },
-		{ 's', NULL, "service" },
-		{ 'h', NULL, "home" },
-		{ 'l', NULL, "lip" },
-		{ 'r', NULL, "rip" },
-		{ 'p', NULL, "pid" },
-		{ 'i', NULL, "uid" },
-		{ '\0', NULL, "gid" },
-		{ '\0', NULL, "session" },
-		{ '\0', NULL, "auth_user" },
-		{ '\0', NULL, "auth_username" },
-		{ '\0', NULL, "auth_domain" },
+	const char *username = t_strcut(client->state.username, '@');
+	const char *domain = i_strchr_to_next(client->state.username, '@');
+	const char *local_ip = client->state.local_ip.family == 0 ? NULL :
+		net_ip2addr(&client->state.local_ip);
+	const char *remote_ip = client->state.remote_ip.family == 0 ? NULL :
+		net_ip2addr(&client->state.remote_ip);
+
+	const char *auth_user, *auth_username, *auth_domain;
+	imap_client_parse_userdb_fields(client, &auth_user);
+	if (auth_user == NULL) {
+		auth_user = client->state.username;
+		auth_username = username;
+		auth_domain = domain;
+	} else {
+		auth_username = t_strcut(auth_user, '@');
+		auth_domain = i_strchr_to_next(auth_user, '@');
+	}
+
+	const struct var_expand_table stack_tab[] = {
+		{ 'u', client->state.username, "user" },
+		{ 'n', username, "username" },
+		{ 'd', domain, "domain" },
+		{ 's', "imap-hibernate", "service" },
+		{ 'h', NULL /* we shouldn't need this */, "home" },
+		{ 'l', local_ip, "lip" },
+		{ 'r', remote_ip, "rip" },
+		{ 'p', my_pid, "pid" },
+		{ 'i', dec2str(client->state.uid), "uid" },
+		{ '\0', dec2str(client->state.gid), "gid" },
+		{ '\0', client->state.session_id, "session" },
+		{ '\0', auth_user, "auth_user" },
+		{ '\0', auth_username, "auth_username" },
+		{ '\0', auth_domain, "auth_domain" },
 		/* NOTE: keep this synced with lib-storage's
 		   mail_user_var_expand_table() */
 		{ '\0', NULL, NULL }
 	};
 	struct var_expand_table *tab;
-	const char *auth_user;
 
-	tab = t_malloc_no0(sizeof(static_tab));
-	memcpy(tab, static_tab, sizeof(static_tab));
-
-	tab[0].value = client->state.username;
-	tab[1].value = t_strcut(client->state.username, '@');
-	tab[2].value = i_strchr_to_next(client->state.username, '@');
-	tab[3].value = "imap-hibernate";
-	tab[4].value = NULL; /* we shouldn't need this */
-	tab[5].value = client->state.local_ip.family == 0 ? NULL :
-		net_ip2addr(&client->state.local_ip);
-	tab[6].value = client->state.remote_ip.family == 0 ? NULL :
-		net_ip2addr(&client->state.remote_ip);
-	tab[7].value = my_pid;
-	tab[8].value = dec2str(client->state.uid);
-	tab[9].value = dec2str(client->state.gid);
-	tab[10].value = client->state.session_id;
-
-	imap_client_parse_userdb_fields(client, &auth_user);
-	if (auth_user == NULL) {
-		tab[11].value = tab[0].value;
-		tab[12].value = tab[1].value;
-		tab[13].value = tab[2].value;
-	} else {
-		tab[11].value = auth_user;
-		tab[12].value = t_strcut(auth_user, '@');
-		tab[13].value = i_strchr_to_next(auth_user, '@');
-	}
+	tab = t_malloc_no0(sizeof(stack_tab));
+	memcpy(tab, stack_tab, sizeof(stack_tab));
 	return tab;
 }
 

src/imap/imap-client.c
@@ -237,35 +237,20 @@ void client_command_cancel(struct client_command_context **_cmd)
 
 const char *client_stats(struct client *client)
 {
-	static struct var_expand_table static_tab[] = {
-		{ 'i', NULL, "input" },
-		{ 'o', NULL, "output" },
-		{ '\0', NULL, "session" },
-		{ '\0', NULL, "fetch_hdr_count" },
-		{ '\0', NULL, "fetch_hdr_bytes" },
-		{ '\0', NULL, "fetch_body_count" },
-		{ '\0', NULL, "fetch_body_bytes" },
-		{ '\0', NULL, "deleted" },
-		{ '\0', NULL, "expunged" },
-		{ '\0', NULL, "trashed" },
+	const struct var_expand_table tab[] = {
+		{ 'i', dec2str(i_stream_get_absolute_offset(client->input)), "input" },
+		{ 'o', dec2str(client->output->offset), "output" },
+		{ '\0', client->session_id, "session" },
+		{ '\0', dec2str(client->fetch_hdr_count), "fetch_hdr_count" },
+		{ '\0', dec2str(client->fetch_hdr_bytes), "fetch_hdr_bytes" },
+		{ '\0', dec2str(client->fetch_body_count), "fetch_body_count" },
+		{ '\0', dec2str(client->fetch_body_bytes), "fetch_body_bytes" },
+		{ '\0', dec2str(client->deleted_count), "deleted" },
+		{ '\0', dec2str(client->expunged_count), "expunged" },
+		{ '\0', dec2str(client->trashed_count), "trashed" },
 		{ '\0', NULL, NULL }
 	};
-	struct var_expand_table *tab;
 	string_t *str;
-
-	tab = t_malloc_no0(sizeof(static_tab));
-	memcpy(tab, static_tab, sizeof(static_tab));
-
-	tab[0].value = dec2str(i_stream_get_absolute_offset(client->input));
-	tab[1].value = dec2str(client->output->offset);
-	tab[2].value = client->session_id;
-	tab[3].value = dec2str(client->fetch_hdr_count);
-	tab[4].value = dec2str(client->fetch_hdr_bytes);
-	tab[5].value = dec2str(client->fetch_body_count);
-	tab[6].value = dec2str(client->fetch_body_bytes);
-	tab[7].value = dec2str(client->deleted_count);
-	tab[8].value = dec2str(client->expunged_count);
-	tab[9].value = dec2str(client->trashed_count);
 
 	str = t_str_new(128);
 	var_expand(str, client->set->imap_logout_format, tab);

src/lib-lda/mail-deliver.c
@@ -43,13 +43,13 @@ static void
 mail_deliver_log_var_expand_table_update_times(struct mail_deliver_context *ctx,
 					       struct var_expand_table *tab)
 {
-#define VAR_EXPAND_SESSION_TIME_IDX 7
+#define VAR_EXPAND_DELIVERY_TIME_IDX 7
 	int delivery_time_msecs;
 
 	io_loop_time_refresh();
 	delivery_time_msecs = timeval_diff_msecs(&ioloop_timeval,
 						 &ctx->delivery_time_started);
-	tab[VAR_EXPAND_SESSION_TIME_IDX].value = dec2str(delivery_time_msecs);
+	tab[VAR_EXPAND_DELIVERY_TIME_IDX].value = dec2str(delivery_time_msecs);
 }
 
 static const struct var_expand_table *
@@ -57,50 +57,54 @@ mail_deliver_get_log_var_expand_table_full(struct mail_deliver_context *ctx,
 					   struct mail *mail,
 					   const char *message)
 {
-	static struct var_expand_table static_tab[] = {
-		{ '$', NULL, NULL },
-		{ 'm', NULL, "msgid" },
-		{ 's', NULL, "subject" },
-		{ 'f', NULL, "from" },
-		{ 'e', NULL, "from_envelope" },
-		{ 'p', NULL, "size" },
-		{ 'w', NULL, "vsize" },
+	const char *message_id, *subject = NULL, *from_envelope = NULL;
+	const char *from, *psize = NULL, *vsize = NULL, *storage_id = NULL;
+	const char *session_time = NULL, *to_envelope = NULL;
+	uoff_t size;
+
+	if (mail_get_first_header(mail, "Message-ID", &message_id) <= 0)
+		message_id = "unspecified";
+	else
+		message_id = str_sanitize(message_id, 200);
+
+	if (mail_get_first_header_utf8(mail, "Subject", &subject) > 0)
+		subject = str_sanitize(subject, 80);
+	from = str_sanitize(mail_deliver_get_address(mail, "From"), 80);
+
+	if (mail_get_special(mail, MAIL_FETCH_FROM_ENVELOPE, &from_envelope) > 0)
+		from_envelope = str_sanitize(from_envelope, 80);
+
+	if (mail_get_physical_size(mail, &size) == 0)
+		psize = dec2str(size);
+	if (mail_get_virtual_size(mail, &size) == 0)
+		vsize = dec2str(size);
+	if (ctx != NULL) {
+		session_time = dec2str(ctx->session_time_msecs);
+		to_envelope = ctx->dest_addr;
+	}
+	(void)mail_get_special(mail, MAIL_FETCH_STORAGE_ID, &storage_id);
+
+	const struct var_expand_table stack_tab[] = {
+		{ '$', message, NULL },
+		{ 'm', message_id, "msgid" },
+		{ 's', subject, "subject" },
+		{ 'f', from, "from" },
+		{ 'e', from_envelope, "from_envelope" },
+		{ 'p', psize, "size" },
+		{ 'w', vsize, "vsize" },
+		/* must be VAR_EXPAND_DELIVERY_TIME_IDX */
 		{ '\0', NULL, "delivery_time" },
-		{ '\0', NULL, "session_time" },
-		{ '\0', NULL, "to_envelope" },
-		{ '\0', NULL, "storage_id" },
+		{ '\0', session_time, "session_time" },
+		{ '\0', to_envelope, "to_envelope" },
+		{ '\0', storage_id, "storage_id" },
 		{ '\0', NULL, NULL }
 	};
 	struct var_expand_table *tab;
-	const char *str;
-	uoff_t size;
 
-	tab = t_malloc_no0(sizeof(static_tab));
-	memcpy(tab, static_tab, sizeof(static_tab));
-
-	tab[0].value = message;
-	(void)mail_get_first_header(mail, "Message-ID", &tab[1].value);
-	tab[1].value = tab[1].value == NULL ? "unspecified" :
-		str_sanitize(tab[1].value, 200);
-
-	(void)mail_get_first_header_utf8(mail, "Subject", &tab[2].value);
-	tab[2].value = str_sanitize(tab[2].value, 80);
-	tab[3].value = str_sanitize(mail_deliver_get_address(mail, "From"), 80);
-
-	if (mail_get_special(mail, MAIL_FETCH_FROM_ENVELOPE, &str) < 0)
-		str = "";
-	tab[4].value = str_sanitize(str, 80);
-
-	if (mail_get_physical_size(mail, &size) == 0)
-		tab[5].value = dec2str(size);
-	if (mail_get_virtual_size(mail, &size) == 0)
-		tab[6].value = dec2str(size);
-	if (ctx != NULL) {
+	tab = t_malloc_no0(sizeof(stack_tab));
+	memcpy(tab, stack_tab, sizeof(stack_tab));
+	if (ctx != NULL)
 		mail_deliver_log_var_expand_table_update_times(ctx, tab);
-		tab[8].value = dec2str(ctx->session_time_msecs);
-		tab[9].value = ctx->dest_addr;
-	}
-	(void)mail_get_special(mail, MAIL_FETCH_STORAGE_ID, &tab[10].value);
 	return tab;
 }
 
@@ -159,7 +163,7 @@ void mail_deliver_log(struct mail_deliver_context *ctx, const char *fmt, ...)
 	mail_deliver_log_var_expand_table_update_times(ctx, ctx->var_expand_table);
 	var_expand(str, ctx->set->deliver_log_format, ctx->var_expand_table);
 	ctx->var_expand_table[0].value = "";
-	ctx->var_expand_table[VAR_EXPAND_SESSION_TIME_IDX].value = "";
+	ctx->var_expand_table[VAR_EXPAND_DELIVERY_TIME_IDX].value = "";
 
 	i_info("%s", str_c(str));
 	va_end(args);

src/lib-mail/mail-user-hash.c
@@ -8,13 +8,6 @@
 
 unsigned int mail_user_hash(const char *username, const char *format)
 {
-	static struct var_expand_table static_tab[] = {
-		{ 'u', NULL, "user" },
-		{ 'n', NULL, "username" },
-		{ 'd', NULL, "domain" },
-		{ '\0', NULL, NULL }
-	};
-	struct var_expand_table *tab;
 	unsigned char md5[MD5_RESULTLEN];
 	unsigned int i, hash = 0;
 
@@ -28,13 +21,13 @@ unsigned int mail_user_hash(const char *username, const char *format)
 				       strlen(username), md5);
 		} T_END;
 	} else T_BEGIN {
+		const struct var_expand_table tab[] = {
+			{ 'u', username, "user" },
+			{ 'n', t_strcut(username, '@'), "username" },
+			{ 'd', i_strchr_to_next(username, '@'), "domain" },
+			{ '\0', NULL, NULL }
+		};
 		string_t *str = t_str_new(128);
-
-		tab = t_malloc_no0(sizeof(static_tab));
-		memcpy(tab, static_tab, sizeof(static_tab));
-		tab[0].value = username;
-		tab[1].value = t_strcut(username, '@');
-		tab[2].value = i_strchr_to_next(username, '@');
 
 		var_expand(str, format, tab);
 		md5_get_digest(str_data(str), str_len(str), md5);

src/lib-storage/index/shared/shared-storage.c
@@ -139,14 +139,6 @@ int shared_storage_get_namespace(struct mail_namespace **_ns,
 	struct mailbox_list *list = (*_ns)->list;
 	struct shared_storage *storage = (struct shared_storage *)_storage;
 	struct mail_user *user = _storage->user;
-	static struct var_expand_table static_tab[] = {
-		{ 'u', NULL, "user" },
-		{ 'n', NULL, "username" },
-		{ 'd', NULL, "domain" },
-		{ 'h', NULL, "home" },
-		{ '\0', NULL, NULL }
-	};
-	struct var_expand_table *tab;
 	struct mail_namespace *new_ns, *ns = *_ns;
 	struct mail_namespace_settings *ns_set, *unexpanded_ns_set;
 	struct mail_user *owner;
@@ -234,11 +226,13 @@ int shared_storage_get_namespace(struct mail_namespace **_ns,
 
 	/* expand the namespace prefix and see if it already exists.
 	   this should normally happen only when the mailbox is being opened */
-	tab = t_malloc_no0(sizeof(static_tab));
-	memcpy(tab, static_tab, sizeof(static_tab));
-	tab[0].value = userdomain;
-	tab[1].value = username;
-	tab[2].value = domain;
+	struct var_expand_table tab[] = {
+		{ 'u', userdomain, "user" },
+		{ 'n', username, "username" },
+		{ 'd', domain, "domain" },
+		{ 'h', NULL, "home" },
+		{ '\0', NULL, NULL }
+	};
 
 	prefix = t_str_new(128);
 	str_append(prefix, ns->prefix);

src/lib-storage/mail-storage-service.c
@@ -394,48 +394,44 @@ get_var_expand_table(struct master_service *service,
 		     const struct mail_storage_service_input *input,
 		     const struct mail_storage_service_privileges *priv)
 {
-	static struct var_expand_table static_tab[] = {
-		{ 'u', NULL, "user" },
-		{ 'n', NULL, "username" },
-		{ 'd', NULL, "domain" },
-		{ 's', NULL, "service" },
-		{ 'l', NULL, "lip" },
-		{ 'r', NULL, "rip" },
-		{ 'p', NULL, "pid" },
-		{ 'i', NULL, "uid" },
-		{ '\0', NULL, "gid" },
-		{ '\0', NULL, "session" },
-		{ '\0', NULL, "auth_user" },
-		{ '\0', NULL, "auth_username" },
-		{ '\0', NULL, "auth_domain" },
+	const char *username = t_strcut(input->username, '@');
+	const char *domain = i_strchr_to_next(input->username, '@');
+	const char *uid = priv == NULL ? NULL :
+		dec2str(priv->uid == (uid_t)-1 ? geteuid() : priv->uid);
+	const char *gid = priv == NULL ? NULL :
+		dec2str(priv->gid == (gid_t)-1 ? getegid() : priv->gid);
+
+	const char *auth_user, *auth_username, *auth_domain;
+	if (user == NULL || user->auth_user == NULL) {
+		auth_user = input->username;
+		auth_username = username;
+		auth_domain = domain;
+	} else {
+		auth_user = user->auth_user;
+		auth_username = t_strcut(user->auth_user, '@');
+		auth_domain = i_strchr_to_next(user->auth_user, '@');
+	}
+
+	const struct var_expand_table stack_tab[] = {
+		{ 'u', input->username, "user" },
+		{ 'n', username, "username" },
+		{ 'd', domain, "domain" },
+		{ 's', service->name, "service" },
+		{ 'l', net_ip2addr(&input->local_ip), "lip" },
+		{ 'r', net_ip2addr(&input->remote_ip), "rip" },
+		{ 'p', my_pid, "pid" },
+		{ 'i', uid, "uid" },
+		{ '\0', gid, "gid" },
+		{ '\0', input->session_id, "session" },
+		{ '\0', auth_user, "auth_user" },
+		{ '\0', auth_username, "auth_username" },
+		{ '\0', auth_domain, "auth_domain" },
 		{ '\0', NULL, NULL }
 	};
 	struct var_expand_table *tab;
 
-	tab = t_malloc_no0(sizeof(static_tab));
-	memcpy(tab, static_tab, sizeof(static_tab));
-
-	tab[0].value = input->username;
-	tab[1].value = t_strcut(input->username, '@');
-	tab[2].value = i_strchr_to_next(input->username, '@');
-	tab[3].value = service->name;
-	tab[4].value = net_ip2addr(&input->local_ip);
-	tab[5].value = net_ip2addr(&input->remote_ip);
-	tab[6].value = my_pid;
-	tab[7].value = priv == NULL ? NULL :
-		dec2str(priv->uid == (uid_t)-1 ? geteuid() : priv->uid);
-	tab[8].value = priv == NULL ? NULL :
-		dec2str(priv->gid == (gid_t)-1 ? getegid() : priv->gid);
-	tab[9].value = input->session_id;
-	if (user == NULL || user->auth_user == NULL) {
-		tab[10].value = tab[0].value;
-		tab[11].value = tab[1].value;
-		tab[12].value = tab[2].value;
-	} else {
-		tab[10].value = user->auth_user;
-		tab[11].value = t_strcut(user->auth_user, '@');
-		tab[12].value = i_strchr_to_next(user->auth_user, '@');
-	}
+	tab = t_malloc_no0(sizeof(stack_tab));
+	memcpy(tab, stack_tab, sizeof(stack_tab));
 	return tab;
 }
 

src/lib-storage/mail-user.c
@@ -209,57 +209,54 @@ void mail_user_set_vars(struct mail_user *user, const char *service,
 const struct var_expand_table *
 mail_user_var_expand_table(struct mail_user *user)
 {
-	static struct var_expand_table static_tab[] = {
-		{ 'u', NULL, "user" },
-		{ 'n', NULL, "username" },
-		{ 'd', NULL, "domain" },
-		{ 's', NULL, "service" },
-		{ 'h', NULL, "home" },
-		{ 'l', NULL, "lip" },
-		{ 'r', NULL, "rip" },
-		{ 'p', NULL, "pid" },
-		{ 'i', NULL, "uid" },
-		{ '\0', NULL, "gid" },
-		{ '\0', NULL, "session" },
-		{ '\0', NULL, "auth_user" },
-		{ '\0', NULL, "auth_username" },
-		{ '\0', NULL, "auth_domain" },
+	/* use a cached table, unless home directory has been set afterwards */
+	if (user->var_expand_table != NULL &&
+	    user->var_expand_table[4].value == user->_home)
+		return user->var_expand_table;
+
+	const char *username =
+		p_strdup(user->pool, t_strcut(user->username, '@'));
+	const char *domain = i_strchr_to_next(user->username, '@');
+	const char *local_ip = user->local_ip == NULL ? NULL :
+		p_strdup(user->pool, net_ip2addr(user->local_ip));
+	const char *remote_ip = user->remote_ip == NULL ? NULL :
+		p_strdup(user->pool, net_ip2addr(user->remote_ip));
+
+	const char *auth_user, *auth_username, *auth_domain;
+	if (user->auth_user == NULL) {
+		auth_user = user->username;
+		auth_username = username;
+		auth_domain = domain;
+	} else {
+		auth_user = user->auth_user;
+		auth_username =
+			p_strdup(user->pool, t_strcut(user->auth_user, '@'));
+		auth_domain = i_strchr_to_next(user->auth_user, '@');
+	}
+
+	const struct var_expand_table stack_tab[] = {
+		{ 'u', user->username, "user" },
+		{ 'n', username, "username" },
+		{ 'd', domain, "domain" },
+		{ 's', user->service, "service" },
+		{ 'h', user->_home /* don't look it up unless we need it */, "home" },
+		{ 'l', local_ip, "lip" },
+		{ 'r', remote_ip, "rip" },
+		{ 'p', my_pid, "pid" },
+		{ 'i', p_strdup(user->pool, dec2str(user->uid)), "uid" },
+		{ '\0', p_strdup(user->pool, dec2str(user->gid)), "gid" },
+		{ '\0', user->session_id, "session" },
+		{ '\0', auth_user, "auth_user" },
+		{ '\0', auth_username, "auth_username" },
+		{ '\0', auth_domain, "auth_domain" },
 		/* NOTE: keep this synced with imap-hibernate's
 		   imap_client_var_expand_table() */
 		{ '\0', NULL, NULL }
 	};
 	struct var_expand_table *tab;
 
-	/* use a cached table, unless home directory has been set afterwards */
-	if (user->var_expand_table != NULL &&
-	    user->var_expand_table[4].value == user->_home)
-		return user->var_expand_table;
-
-	tab = p_malloc(user->pool, sizeof(static_tab));
-	memcpy(tab, static_tab, sizeof(static_tab));
-
-	tab[0].value = user->username;
-	tab[1].value = p_strdup(user->pool, t_strcut(user->username, '@'));
-	tab[2].value = i_strchr_to_next(user->username, '@');
-	tab[3].value = user->service;
-	tab[4].value = user->_home; /* don't look it up unless we need it */
-	tab[5].value = user->local_ip == NULL ? NULL :
-		p_strdup(user->pool, net_ip2addr(user->local_ip));
-	tab[6].value = user->remote_ip == NULL ? NULL :
-		p_strdup(user->pool, net_ip2addr(user->remote_ip));
-	tab[7].value = my_pid;
-	tab[8].value = p_strdup(user->pool, dec2str(user->uid));
-	tab[9].value = p_strdup(user->pool, dec2str(user->gid));
-	tab[10].value = user->session_id;
-	if (user->auth_user == NULL) {
-		tab[11].value = tab[0].value;
-		tab[12].value = tab[1].value;
-		tab[13].value = tab[2].value;
-	} else {
-		tab[11].value = user->auth_user;
-		tab[12].value = p_strdup(user->pool, t_strcut(user->auth_user, '@'));
-		tab[13].value = i_strchr_to_next(user->auth_user, '@');
-	}
+	tab = p_malloc(user->pool, sizeof(stack_tab));
+	memcpy(tab, stack_tab, sizeof(stack_tab));
 
 	user->var_expand_table = tab;
 	return user->var_expand_table;

commit b1d5d687ddc3ebf65b9416c435321062e57c98b9

lib-lda: Add back subject sanitization to mail sending.
Broken by acfda38b75d0f0e899ef692fef01593bd56ed85e

src/lib-lda/mail-send.c
@@ -31,7 +31,7 @@ get_var_expand_table(struct mail *mail, const char *reason,
 	const struct var_expand_table stack_tab[] = {
 		{ 'n', "\r\n", "crlf" },
 		{ 'r', reason, "reason" },
-		{ 's', subject, "subject" },
+		{ 's', str_sanitize(subject, 80), "subject" },
 		{ 't', recipient, "to" },
 		{ '\0', NULL, NULL }
 	};