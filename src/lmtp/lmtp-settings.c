d13bee3f3d66a6298fac37f0e16a414b1b1e7ac3
Stephan Bosch <stephan.bosch@dovecot.fi>
2016-11-04 18:16:52 +0100

    lmtp: Removed lmtp_address_translate setting.
    It is assumed not to be used.

--- src/lmtp/lmtp-settings.c
+++ src/lmtp/lmtp-settings.c
@@ -63,7 +63,6 @@ static const struct setting_define lmtp_setting_defines[] = {
 	DEF(SET_BOOL, lmtp_save_to_detail_mailbox),
 	DEF(SET_BOOL, lmtp_rcpt_check_quota),
 	DEF(SET_UINT, lmtp_user_concurrency_limit),
-	DEF(SET_STR, lmtp_address_translate),
 	DEF(SET_ENUM, lmtp_hdr_delivery_address),
 	DEF(SET_STR_VARS, login_greeting),
 	DEF(SET_STR, login_trusted_networks),
@@ -76,7 +75,6 @@ static const struct lmtp_settings lmtp_default_settings = {
 	.lmtp_save_to_detail_mailbox = FALSE,
 	.lmtp_rcpt_check_quota = FALSE,
 	.lmtp_user_concurrency_limit = 0,
-	.lmtp_address_translate = "",
 	.lmtp_hdr_delivery_address = "final:none:original",
 	.login_greeting = PACKAGE_NAME" ready.",
 	.login_trusted_networks = ""
--- src/lmtp/lmtp-settings.h
+++ src/lmtp/lmtp-settings.h
@@ -17,7 +17,6 @@ struct lmtp_settings {
 	bool lmtp_save_to_detail_mailbox;
 	bool lmtp_rcpt_check_quota;
 	unsigned int lmtp_user_concurrency_limit;
-	const char *lmtp_address_translate;
 	const char *lmtp_hdr_delivery_address;
 	const char *login_greeting;
 	const char *login_trusted_networks;
