c38f69924b6fd2ff697ee87a3371ade4fd66ae15
Timo Sirainen <timo.sirainen@dovecot.fi>
2016-11-29 23:21:17 +0200

    lib: Optimize printf_format_fix_noalloc()
    Using strchr() is faster than looping through the characters manually.
    Since this function is being called a lot, it's worth optimizing.

--- src/lib/printf-format-fix.c
+++ src/lib/printf-format-fix.c
@@ -35,28 +35,27 @@ fix_format_real(const char *fmt, const char *p, size_t *len_r)
 static const char *
 printf_format_fix_noalloc(const char *format, size_t *len_r)
 {
-	const char *p;
-	const char *ret = format;
+	const char *ret, *p, *p2;
 
-	for (p = format; *p != '\0'; ) {
-		if (*p++ == '%') {
-			switch (*p) {
-			case 'n':
-				i_panic("%%n modifier used");
-			case 'm':
-				if (ret != format)
-					i_panic("%%m used twice");
-				ret = fix_format_real(format, p-1, len_r);
-				break;
-			case '\0':
-				i_panic("%% modifier missing in '%s'", format);
-			}
-			p++;
+	p = ret = format;
+	while ((p2 = strchr(p, '%')) != NULL) {
+		p = p2+1;
+		switch (*p) {
+		case 'n':
+			i_panic("%%n modifier used");
+		case 'm':
+			if (ret != format)
+				i_panic("%%m used twice");
+			ret = fix_format_real(format, p-1, len_r);
+			break;
+		case '\0':
+			i_panic("%% modifier missing in '%s'", format);
 		}
+		p++;
 	}
 
 	if (ret == format)
-		*len_r = p - format;
+		*len_r = p - format + strlen(p);
 	return ret;
 }
 
