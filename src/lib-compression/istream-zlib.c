391e998096c5a821138495d5b8f873b21791aec4
Timo Sirainen <timo.sirainen@dovecot.fi>
2017-02-13 18:06:46 +0200

    lib-compress: Fix missing .gz header error message

--- src/lib-compression/istream-zlib.c
+++ src/lib-compression/istream-zlib.c
@@ -78,7 +78,7 @@ static int i_stream_zlib_read_header(struct istream_private *stream)
 	if (size == zstream->prev_size) {
 		stream->istream.stream_errno = stream->parent->stream_errno;
 		if (ret == -1 && stream->istream.stream_errno == 0) {
-			zlib_read_error(zstream, "missing gz trailer");
+			zlib_read_error(zstream, "missing gz header");
 			stream->istream.stream_errno = EINVAL;
 		}
 		if (ret == -2) {
