d466d48bda68cb8c368ca88527e86f1bfa245ab9
Aki Tuomi <aki.tuomi@dovecot.fi>
2017-02-27 19:12:32 +0200

    doveadm: Ensure -- is added to command line before positional arguments
    This prevents parser from choking on dash.

--- src/doveadm/doveadm-mail.c
+++ src/doveadm/doveadm-mail.c
@@ -1051,6 +1051,9 @@ doveadm_cmd_ver2_to_mail_cmd_wrapper(struct doveadm_cmd_context *cctx)
 		}
 	}
 
+	const char *dashdash = "--";
+	array_append(&full_args, &dashdash, 1);
+
 	array_append_zero(&pargv);
 	/* All the -parameters need to be included in full_args so that
 	   they're sent to doveadm-server. */
