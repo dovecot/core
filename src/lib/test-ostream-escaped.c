f76d3786a74496476c2db402f324e439abee99d3
Aki Tuomi <aki.tuomi@dovecot.fi>
2017-03-17 10:13:59 +0200

    test: lib: Check o_stream_send return value
    Make static analyzers happier

--- src/lib/test-ostream-escaped.c
+++ src/lib/test-ostream-escaped.c
@@ -71,7 +71,7 @@ static void test_ostream_escaped_hex(void)
 	os_encode = o_stream_create_escaped(os_sink, ostream_escaped_hex_format);
 
 	test_begin("test_ostream_escaped_hex()");
-	o_stream_send_str(os_encode, "hello, world");
+	test_assert(o_stream_send_str(os_encode, "hello, world") == 12);
 	o_stream_flush(os_encode);
 
 	test_assert(strcmp(str_c(str), "68656c6c6f2c20776f726c64") == 0);
