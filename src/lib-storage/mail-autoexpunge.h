35b5cca7a320676f82a8f5a185e4ea890740d44e
Timo Sirainen <timo.sirainen@dovecot.fi>
2017-03-23 14:27:28 +0200

    lib-storage: Change mail_user_autoexpunge() to return number of autoexpunged mails

--- src/lib-storage/mail-autoexpunge.h
+++ src/lib-storage/mail-autoexpunge.h
@@ -1,6 +1,8 @@
 #ifndef MAIL_AUTOEXPUNGE_H
 #define MAIL_AUTOEXPUNGE_H
 
-void mail_user_autoexpunge(struct mail_user *user);
+/* Perform autoexpunging for all the user's mailboxes that have autoexpunging
+   configured. Returns number of mails that were autoexpunged. */
+unsigned int mail_user_autoexpunge(struct mail_user *user);
 
 #endif
