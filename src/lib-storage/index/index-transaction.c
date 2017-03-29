acfeae4e2b5c2acdab88a1377ff6cd4a9b5f0a3a
Timo Sirainen <timo.sirainen@dovecot.fi>
2017-03-29 13:28:36 +0300

    lib-storage: Fix potential dict transaction leak on transaction rollback.

--- src/lib-storage/index/index-transaction.c
+++ src/lib-storage/index/index-transaction.c
@@ -103,6 +103,11 @@ index_transaction_index_rollback(struct mail_index_transaction *index_trans)
 	struct mailbox_transaction_context *t =
 		MAIL_STORAGE_CONTEXT(index_trans);
 
+	if (t->attr_pvt_trans != NULL)
+		dict_transaction_rollback(&t->attr_pvt_trans);
+	if (t->attr_shared_trans != NULL)
+		dict_transaction_rollback(&t->attr_shared_trans);
+
 	if (t->save_ctx != NULL) {
 		mailbox_save_context_deinit(t->save_ctx);
 		t->box->v.transaction_save_rollback(t->save_ctx);
