e56789765832ffbddb6ac42a869a99754dfb274c
Aki Tuomi <aki.tuomi@dovecot.fi>
2017-05-09 12:25:49 +0300

    auth: Do not use AUTH_SUBSYS_MECH in logging when request->mech is NULL
    This can happen when a lookup is done instead of authentication.
    
    Fixes auth: Panic: file auth-request.c: line 2560 (get_log_prefix): assertion failed: (auth_request->mech != NULL)

--- src/auth/auth-request.c
+++ src/auth/auth-request.c
@@ -674,14 +674,18 @@ auth_request_want_skip_passdb(struct auth_request *request,
 	username = request->user;
 
 	if (!auth_request_mechanism_accepted(mechs, request->mech)) {
-		auth_request_log_debug(request, AUTH_SUBSYS_MECH,
+		auth_request_log_debug(request,
+				       request->mech != NULL ? AUTH_SUBSYS_MECH
+							      : "none",
 				       "skipping passdb: mechanism filtered");
 		return TRUE;
 	}
 
 	if (passdb->passdb->username_filter != NULL &&
 	    !auth_request_username_accepted(username_filter, username)) {
-		auth_request_log_debug(request, AUTH_SUBSYS_MECH,
+		auth_request_log_debug(request,
+				       request->mech != NULL ? AUTH_SUBSYS_MECH
+							      : "none",
 				       "skipping passdb: username filtered");
 		return TRUE;
 	}
