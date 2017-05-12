dovecot/core

commit 4792d4bddbe2341de4be778b541db3958bc7e56b

lib: Add dec2str_buf()
Conflicts:
	src/lib/strfuncs.c

src/lib/strfuncs.c
@@ -733,12 +733,14 @@ const char **p_strarray_dup(pool_t pool, const char *const *arr)
 
 const char *dec2str(uintmax_t number)
 {
-	char *buffer;
+	return dec2str_buf(t_malloc_no0(MAX_INT_STRLEN), number);
+}
+
+char *dec2str_buf(char buffer[STATIC_ARRAY MAX_INT_STRLEN], uintmax_t number)
+{
 	int pos;
 
 	pos = MAX_INT_STRLEN;
-	buffer = t_malloc(pos);
-
 	buffer[--pos] = '\0';
 	do {
 		buffer[--pos] = (number % 10) + '0';

src/lib/strfuncs.h
@@ -94,6 +94,10 @@ void p_strsplit_free(pool_t pool, char **arr);
 const char **t_strsplit_tab(const char *data) ATTR_MALLOC ATTR_RETURNS_NONNULL;
 
 const char *dec2str(uintmax_t number);
+/* Use the given buffer to write out the number. Returns pointer to the
+   written number in the buffer. Note that this isn't the same as the beginning
+   of the buffer. */
+char *dec2str_buf(char buffer[STATIC_ARRAY MAX_INT_STRLEN], uintmax_t number);
 
 /* Return length of NULL-terminated list string array */
 unsigned int str_array_length(const char *const *arr) ATTR_PURE;

commit cacb6b19ebeaf7344a09f8d75f04f5c3e626bce5

lib: Use t_malloc instead of t_malloc_no0
Broken in 4792d4bd

src/lib/strfuncs.c
@@ -733,7 +733,7 @@ const char **p_strarray_dup(pool_t pool, const char *const *arr)
 
 const char *dec2str(uintmax_t number)
 {
-	return dec2str_buf(t_malloc_no0(MAX_INT_STRLEN), number);
+	return dec2str_buf(t_malloc(MAX_INT_STRLEN), number);
 }
 
 char *dec2str_buf(char buffer[STATIC_ARRAY MAX_INT_STRLEN], uintmax_t number)