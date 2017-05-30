8df38b03aa84198e9e78c7ccfe63fcb445fa8058
Timo Sirainen <timo.sirainen@dovecot.fi>
2017-05-30 11:30:10 +0300

    auth: Fix crash on passdb lookup when all passdbs were skipped
    Finishes the fix in 614f5b6febaf3c825f9200ab3b62d9d047197b0e
    
    Fixes:
    auth: Panic: file auth-request.c: line 2493 (get_log_prefix): assertion failed: (auth_request->mech != NULL)

--- src/auth/auth-request.c
+++ src/auth/auth-request.c
@@ -1077,7 +1077,8 @@ void auth_request_verify_plain_continue(struct auth_request *request,
 	request->passdb = passdb;
 
 	if (passdb == NULL) {
-		auth_request_log_error(request, AUTH_SUBSYS_MECH,
+		auth_request_log_error(request,
+			request->mech != NULL ? AUTH_SUBSYS_MECH : "none",
 			"All password databases were skipped");
 		callback(PASSDB_RESULT_INTERNAL_FAILURE, request);
 		return;
@@ -1256,7 +1257,8 @@ void auth_request_lookup_credentials_policy_continue(struct auth_request *reques
 	request->passdb = passdb;
 
 	if (passdb == NULL) {
-		auth_request_log_error(request, AUTH_SUBSYS_MECH,
+		auth_request_log_error(request,
+			request->mech != NULL ? AUTH_SUBSYS_MECH : "none",
 			"All password databases were skipped");
 		callback(PASSDB_RESULT_INTERNAL_FAILURE, NULL, 0, request);
 		return;
