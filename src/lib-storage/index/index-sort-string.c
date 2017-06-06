dovecot/core

commit 0a6a527f0c42b5478d80ac53ab357885676fd516

lib-storage: Optimize error handling for SORT
If a lot of mails have failed in a large mailbox, this check reduces the
CPU usage significantly.

src/lib-storage/index/index-sort-string.c
@@ -355,6 +355,11 @@ index_sort_get_string(struct sort_string_context *ctx,
 	uint32_t seq = node->seq;
 	int ret = 1;
 
+	if (node->no_update) {
+		/* we've already determined that we can't do this lookup */
+		return FALSE;
+	}
+
 	if (ctx->sort_strings[seq] == NULL) T_BEGIN {
 		string_t *str;
 		const char *result;

commit f976c38f07e6da867000c4c0e74afe235bc763d5

lib-storage: Fix assert-crash in SORT caused by earlier changes
Broken by 0a6a527f0c42b5478d80ac53ab357885676fd516

Fixes:
Panic: file index-sort-string.c: line 668 (index_sort_add_ids_range): assertion failed: (str != NULL)

src/lib-storage/index/index-sort-string.c
@@ -357,6 +357,7 @@ index_sort_get_string(struct sort_string_context *ctx,
 
 	if (node->no_update) {
 		/* we've already determined that we can't do this lookup */
+		*str_r = ctx->sort_strings[seq];
 		return FALSE;
 	}
 