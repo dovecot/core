52818a09c8c0e9319e6425467f01aa99cd2d017c
Timo Sirainen <timo.sirainen@dovecot.fi>
2017-06-07 15:21:10 +0300

    lib-mail: message_address_parse() - don't stop at <> when parsing address lists
    And similarly don't stop at <@domain>

--- src/lib-mail/message-address.c
+++ src/lib-mail/message-address.c
@@ -153,18 +153,25 @@ static int parse_angle_addr(struct message_address_parser_context *ctx)
 		if (parse_domain_list(ctx) <= 0 || *ctx->parser.data != ':') {
 			if (ctx->fill_missing)
 				ctx->addr.route = "INVALID_ROUTE";
-			return -1;
+			if (ctx->parser.data == ctx->parser.end)
+				return -1;
+			/* try to continue anyway */
+		} else {
+			ctx->parser.data++;
 		}
-		ctx->parser.data++;
 		if ((ret = rfc822_skip_lwsp(&ctx->parser)) <= 0)
 			return ret;
 	}
 
-	if ((ret = parse_local_part(ctx)) <= 0)
-		return ret;
-	if (*ctx->parser.data == '@') {
-		if ((ret = parse_domain(ctx)) <= 0)
+	if (*ctx->parser.data == '>') {
+		/* <> address isn't valid */
+	} else {
+		if ((ret = parse_local_part(ctx)) <= 0)
 			return ret;
+		if (*ctx->parser.data == '@') {
+			if ((ret = parse_domain(ctx)) <= 0)
+				return ret;
+		}
 	}
 
 	if (*ctx->parser.data != '>')
