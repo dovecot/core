05a4fe649671ac5ca6b3beb085c73e6e85bc4f8b
Aki Tuomi <aki.tuomi@dovecot.fi>
2017-06-08 12:18:30 +0300

    lib-auth: Require being connected before adding requests

--- src/lib-auth/auth-server-connection.c
+++ src/lib-auth/auth-server-connection.c
@@ -472,6 +472,8 @@ auth_server_connection_add_request(struct auth_server_connection *conn,
 {
 	unsigned int id;
 
+	i_assert(conn->handshake_received);
+
 	id = ++conn->client->request_id_counter;
 	if (id == 0) {
 		/* wrapped - ID 0 not allowed */
