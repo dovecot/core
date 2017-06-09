eec34b3861508e337fae4cfaff50a61300558020
Josef 'Jeff' Sipek <jeff.sipek@dovecot.fi>
2017-06-09 15:30:02 +0300

    lib: correct comment in path_normalize
    Don't blame static analyzers for doing their job.

--- src/lib/path-util.c
+++ src/lib/path-util.c
@@ -161,7 +161,7 @@ static int path_normalize(const char *path, bool resolve_links,
 						return -1;
 					}
 					if ((size_t)ret < lsize) {
-						/* make static analyzers happy */
+						/* POSIX doesn't guarantee the presence of a NIL */
 						npath_link[ret] = '\0';
 						break;
 					}
