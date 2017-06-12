dovecot/core

commit f38b0dafbffa9d79542e36b4b3e598ed4115e5a3

welcome: tabescape args, this is required by script client 4

src/plugins/welcome/welcome-plugin.c
@@ -3,6 +3,7 @@
 #include "lib.h"
 #include "net.h"
 #include "str.h"
+#include "strescape.h"
 #include "eacces-error.h"
 #include "write-full.h"
 #include "module-context.h"
@@ -58,8 +59,8 @@ static void script_execute(struct mail_user *user, const char *cmd, bool wait)
 	else
 		str_append(str, "-\n");
 	for (; *args != NULL; args++) {
-		str_append(str, *args);
-		str_append_c(str, '\n');
+		str_append_tabescaped(str, *args);
+		str_append_c(str, '\t');
 	}
 	str_append_c(str, '\n');
 

commit 5209c4303c9bb02ba9d4d1a9ba192d4288086663

welcome: Fix sending parameters to welcome script.
Broken by f38b0dafbffa9d79542e36b4b3e598ed4115e5a3

src/plugins/welcome/welcome-plugin.c
@@ -60,7 +60,7 @@ static void script_execute(struct mail_user *user, const char *cmd, bool wait)
 		str_append(str, "-\n");
 	for (; *args != NULL; args++) {
 		str_append_tabescaped(str, *args);
-		str_append_c(str, '\t');
+		str_append_c(str, '\n');
 	}
 	str_append_c(str, '\n');
 