d6e3deea85740b3af60e9f5bab32e98beba12d93
Timo Sirainen <timo.sirainen@dovecot.fi>
2017-06-28 17:48:01 +0300

    lib: file_create_locked() - Add settings to mkdir() missing parent directories

--- src/lib/test-file-create-locked.c
+++ src/lib/test-file-create-locked.c
@@ -1,6 +1,7 @@
 /* Copyright (c) 2017 Dovecot authors, see the included COPYING file */
 
 #include "test-lib.h"
+#include "unlink-directory.h"
 #include "file-create-locked.h"
 
 #include <fcntl.h>
@@ -85,7 +86,51 @@ static void test_file_create_locked_basic(void)
 	test_end();
 }
 
+static void test_file_create_locked_mkdir(void)
+{
+	struct file_create_settings set = {
+		.lock_timeout_secs = 0,
+		.lock_method = FILE_LOCK_METHOD_FCNTL,
+	};
+	const char *path = ".test-file-create-locked";
+	struct file_lock *lock;
+	const char *error, *dir;
+	bool created;
+	int fd;
+
+	test_begin("file_create_locked() with mkdir");
+
+	dir = ".test-temp-file-create-locked-dir";
+	if (unlink_directory(dir, UNLINK_DIRECTORY_FLAG_RMDIR, &error) < 0)
+		i_fatal("unlink_directory(%s) failed: %s", dir, error);
+	path = t_strconcat(dir, "/lockfile", NULL);
+
+	/* try without mkdir enabled */
+	test_assert(file_create_locked(path, &set, &lock, &created, &error) == -1);
+	test_assert(errno == ENOENT);
+
+	/* try with mkdir enabled */
+	set.mkdir_mode = 0700;
+	fd = file_create_locked(path, &set, &lock, &created, &error);
+	test_assert(fd > 0);
+	test_assert(created);
+	i_close_fd(&fd);
+
+	struct stat st;
+	if (stat(dir, &st) < 0)
+		i_error("stat(%s) failed: %m", dir);
+	test_assert((st.st_mode & 0777) == 0700);
+	i_unlink(path);
+	file_lock_free(&lock);
+
+	if (unlink_directory(dir, UNLINK_DIRECTORY_FLAG_RMDIR, &error) < 0)
+		i_fatal("unlink_directory(%s) failed: %s", dir, error);
+
+	test_end();
+}
+
 void test_file_create_locked(void)
 {
 	test_file_create_locked_basic();
+	test_file_create_locked_mkdir();
 }
