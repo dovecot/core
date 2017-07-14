dovecot/core

commit 7071a02a8413fb409ade1625f6a1763c7493b66b

lib-storage: mailbox_create_missing_dir() - Handle mailbox delete race condition
Make sure the directory isn't created if the mail root directory no longer
exists. This might happen during mailbox deletion when another process is
opening the mailbox at the same time.

src/lib-storage/mail-storage.c
@@ -2732,6 +2732,15 @@ int mailbox_create_missing_dir(struct mailbox *box,
 	if (stat(dir, &st) == 0)
 		return 0;
 
+	if (null_strcmp(dir, mail_dir) != 0 &&
+	    stat(mail_dir, &st) < 0 && (errno == ENOENT || errno == ENOTDIR)) {
+		/* Race condition - mail root directory doesn't exist
+		   anymore either. We shouldn't create this directory
+		   anymore. */
+		mailbox_set_deleted(box);
+		return -1;
+	}
+
 	return mailbox_mkdir(box, dir, type);
 }
 

commit 66a4847c67f7fe2f076b7c11103ae045fa218bec

lib-storage: mailbox_create_missing_dir() - Fix handling NULL mailbox directory
It would have caused stat(NULL), which would have failed.
Broken by 7071a02a8413fb409ade1625f6a1763c7493b66b

src/lib-storage/mail-storage.c
@@ -2732,7 +2732,7 @@ int mailbox_create_missing_dir(struct mailbox *box,
 	if (stat(dir, &st) == 0)
 		return 0;
 
-	if (null_strcmp(dir, mail_dir) != 0 &&
+	if (null_strcmp(dir, mail_dir) != 0 && mail_dir != NULL &&
 	    stat(mail_dir, &st) < 0 && (errno == ENOENT || errno == ENOTDIR)) {
 		/* Race condition - mail root directory doesn't exist
 		   anymore either. We shouldn't create this directory