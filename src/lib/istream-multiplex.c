7d2f1e73e8425ffcd82bc5f96f81db59ae61aab8
Aki Tuomi <aki.tuomi@dovecot.fi>
2017-08-22 10:14:22 +0300

    lib: Add multiplex stream support
    This allows having multiple channels of data in single stream.

---
+++ src/lib/istream-multiplex.c
@@ -0,0 +1,270 @@
+/* Copyright (c) 2017 Dovecot authors, see the included COPYING file */
+
+#include "lib.h"
+#include "ioloop.h"
+#include "array.h"
+#include "istream-private.h"
+#include "istream-multiplex.h"
+
+/* all multiplex packets are [1 byte cid][4 byte length][data] */
+
+struct multiplex_istream;
+
+struct multiplex_ichannel {
+	struct istream_private istream;
+	struct multiplex_istream *mstream;
+	uint8_t cid;
+	size_t pending_pos;
+	bool closed:1;
+};
+
+struct multiplex_istream {
+	struct istream *parent;
+
+	/* channel 0 is main channel */
+	uint8_t cur_channel;
+	unsigned int remain;
+	size_t bufsize;
+	ARRAY(struct multiplex_ichannel *) channels;
+
+	bool blocking:1;
+};
+
+static ssize_t i_stream_multiplex_ichannel_read(struct istream_private *stream);
+
+static struct multiplex_ichannel *
+get_channel(struct multiplex_istream *mstream, uint8_t cid)
+{
+	struct multiplex_ichannel **channelp;
+	i_assert(mstream != NULL);
+	array_foreach_modifiable(&mstream->channels, channelp) {
+		if (*channelp != NULL && (*channelp)->cid == cid)
+			return *channelp;
+	}
+	return NULL;
+}
+
+static void propagate_error(struct multiplex_istream *mstream, int stream_errno)
+{
+	struct multiplex_ichannel **channelp;
+	array_foreach_modifiable(&mstream->channels, channelp)
+		if (*channelp != NULL)
+			(*channelp)->istream.istream.stream_errno = stream_errno;
+}
+
+static void propagate_eof(struct multiplex_istream *mstream)
+{
+	struct multiplex_ichannel **channelp;
+	array_foreach_modifiable(&mstream->channels, channelp) {
+		if (*channelp != NULL) {
+			(*channelp)->istream.istream.eof = TRUE;
+		}
+	}
+}
+
+static ssize_t
+i_stream_multiplex_read(struct multiplex_istream *mstream, uint8_t cid)
+{
+	struct multiplex_ichannel *req_channel = get_channel(mstream, cid);
+	const unsigned char *data;
+	size_t len = 0, used, wanted, avail;
+	ssize_t ret, got = 0;
+
+	if (mstream->parent == NULL) {
+		req_channel->istream.istream.eof = TRUE;
+		return -1;
+	}
+
+	data = i_stream_get_data(mstream->parent, &len);
+
+	if (len == 0 && mstream->parent->closed) {
+		req_channel->istream.istream.eof = TRUE;
+		return -1;
+	}
+
+	if (((mstream->remain > 0 && len == 0) ||
+	     (mstream->remain == 0 && len < 5)) &&
+	    (ret = i_stream_read(mstream->parent)) <= 0) {
+		propagate_error(mstream, mstream->parent->stream_errno);
+		if (mstream->parent->eof)
+			propagate_eof(mstream);
+		return ret;
+	}
+
+	for(;;) {
+		data = i_stream_get_data(mstream->parent, &len);
+		if (len == 0) {
+			if (got == 0 && mstream->blocking)
+				got += i_stream_multiplex_read(mstream, cid);
+			break;
+		}
+
+		if (mstream->remain > 0) {
+			struct multiplex_ichannel *channel =
+				get_channel(mstream, mstream->cur_channel);
+			wanted = I_MIN(len, mstream->remain);
+			/* is it open? */
+			if (channel != NULL && !channel->closed) {
+				struct istream_private *stream = &channel->istream;
+				stream->pos += channel->pending_pos;
+				bool alloc_ret = i_stream_try_alloc(stream, wanted, &avail);
+				stream->pos -= channel->pending_pos;
+				if (!alloc_ret) {
+					i_stream_set_input_pending(&stream->istream, TRUE);
+					if (channel->cid != cid)
+						return 0;
+					if (got > 0)
+						break;
+					return -2;
+				}
+
+				used = I_MIN(wanted, avail);
+
+				/* dump into buffer */
+				if (channel->cid != cid) {
+					i_assert(stream->pos + channel->pending_pos + used <= stream->buffer_size);
+					memcpy(stream->w_buffer + stream->pos + channel->pending_pos,
+					       data, used);
+					channel->pending_pos += used;
+					i_stream_set_input_pending(&stream->istream, TRUE);
+				} else {
+					i_assert(stream->pos + used <= stream->buffer_size);
+					memcpy(stream->w_buffer + stream->pos, data, used);
+					stream->pos += used;
+					got += used;
+				}
+			} else {
+				used = wanted;
+			}
+			mstream->remain -= used;
+			i_stream_skip(mstream->parent, used);
+			/* see if there is more to read */
+			continue;
+		}
+		if (mstream->remain == 0) {
+			/* need more data */
+			if (len < 5) {
+				ret = i_stream_multiplex_ichannel_read(&req_channel->istream);
+				if (ret > 0)
+					got += ret;
+				break;
+			}
+			/* channel ID */
+			mstream->cur_channel = data[0];
+			/* data length */
+			mstream->remain = be32_to_cpu_unaligned(data+1);
+			i_stream_skip(mstream->parent, 5);
+		}
+	}
+
+	propagate_error(mstream, mstream->parent->stream_errno);
+	if (mstream->parent->eof)
+		propagate_eof(mstream);
+
+	return got;
+}
+
+static ssize_t i_stream_multiplex_ichannel_read(struct istream_private *stream)
+{
+	struct multiplex_ichannel *channel = (struct multiplex_ichannel*)stream;
+	/* if previous multiplex read dumped data for us
+	   actually serve it here. */
+	if (channel->pending_pos > 0) {
+		ssize_t ret = channel->pending_pos;
+		stream->pos += channel->pending_pos;
+		channel->pending_pos = 0;
+		return ret;
+	}
+	return i_stream_multiplex_read(channel->mstream, channel->cid);
+}
+
+static void
+i_stream_multiplex_ichannel_close(struct iostream_private *stream, bool close_parent)
+{
+	struct multiplex_ichannel *const *channelp;
+	struct multiplex_ichannel *channel = (struct multiplex_ichannel*)stream;
+	channel->closed = TRUE;
+	if (close_parent) {
+		array_foreach(&channel->mstream->channels, channelp)
+			if (*channelp != NULL && !(*channelp)->closed)
+				return;
+		i_stream_close(channel->mstream->parent);
+	}
+}
+
+static void i_stream_multiplex_try_destroy(struct multiplex_istream *mstream)
+{
+	struct multiplex_ichannel **channelp;
+	/* can't do anything until they are all closed */
+	array_foreach_modifiable(&mstream->channels, channelp)
+		if (*channelp != NULL)
+			return;
+	i_stream_unref(&mstream->parent);
+	array_free(&mstream->channels);
+	i_free(mstream);
+}
+
+static void i_stream_multiplex_ichannel_destroy(struct iostream_private *stream)
+{
+	struct multiplex_ichannel **channelp;
+	struct multiplex_ichannel *channel = (struct multiplex_ichannel*)stream;
+	i_stream_multiplex_ichannel_close(stream, TRUE);
+	i_free(channel->istream.w_buffer);
+	array_foreach_modifiable(&channel->mstream->channels, channelp) {
+		if (*channelp == channel) {
+			*channelp = NULL;
+			break;
+		}
+	}
+	i_stream_multiplex_try_destroy(channel->mstream);
+}
+
+static struct istream *
+i_stream_add_channel_real(struct multiplex_istream *mstream, uint8_t cid)
+{
+	struct multiplex_ichannel *channel = i_new(struct multiplex_ichannel, 1);
+	channel->cid = cid;
+	channel->mstream = mstream;
+	channel->istream.read = i_stream_multiplex_ichannel_read;
+	channel->istream.iostream.close = i_stream_multiplex_ichannel_close;
+	channel->istream.iostream.destroy = i_stream_multiplex_ichannel_destroy;
+	channel->istream.max_buffer_size = mstream->bufsize;
+	channel->istream.istream.blocking = mstream->blocking;
+	if (cid == 0)
+		channel->istream.fd = i_stream_get_fd(mstream->parent);
+	else
+		channel->istream.fd = -1;
+	array_append(&channel->mstream->channels, &channel, 1);
+
+	return i_stream_create(&channel->istream, NULL, channel->istream.fd);
+}
+
+struct istream *i_stream_multiplex_add_channel(struct istream *stream, uint8_t cid)
+{
+	struct multiplex_ichannel *chan =
+		(struct multiplex_ichannel *)stream->real_stream;
+	i_assert(get_channel(chan->mstream, cid) == NULL);
+
+	return i_stream_add_channel_real(chan->mstream, cid);
+}
+
+struct istream *i_stream_create_multiplex(struct istream *parent, size_t bufsize)
+{
+	struct multiplex_istream *mstream;
+
+	mstream = i_new(struct multiplex_istream, 1);
+	mstream->parent = parent;
+	mstream->bufsize = bufsize;
+	mstream->blocking = parent->blocking;
+	i_array_init(&mstream->channels, 8);
+	i_stream_ref(parent);
+
+	return i_stream_add_channel_real(mstream, 0);
+}
+
+uint8_t i_stream_multiplex_get_channel_id(struct istream *stream)
+{
+	struct multiplex_ichannel *channel =
+		(struct multiplex_ichannel *)stream->real_stream;
+	return channel->cid;
+}
---
+++ src/lib/test-multiplex.c
@@ -0,0 +1,160 @@
+/* Copyright (c) 2017 Dovecot authors, see the included COPYING file */
+
+#include "test-lib.h"
+#include "ioloop.h"
+#include "str.h"
+#include "istream.h"
+#include "istream-multiplex.h"
+#include "ostream.h"
+#include "ostream-multiplex.h"
+#include "ostream.h"
+#include "randgen.h"
+
+#include <unistd.h>
+
+struct test_channel {
+	int fds[2];
+	unsigned int cid;
+
+	struct istream *in;
+	struct ostream *out;
+	struct io *io;
+
+	struct istream *in_alt;
+	struct ostream *out_alt;
+	struct io *io_alt;
+
+	buffer_t *received;
+	buffer_t *received_alt;
+
+	unsigned int counter;
+};
+
+static struct test_channel test_channel[2];
+
+static void test_multiplex_channel_write(struct test_channel *channel)
+{
+	unsigned char buf[128];
+	size_t len = rand() % sizeof(buf);
+	random_fill(buf, len);
+	o_stream_send(channel->out, buf, len);
+	o_stream_send(channel->out_alt, buf, len);
+}
+
+static void test_multiplex_stream_write(struct ostream *channel ATTR_UNUSED)
+{
+	if (test_channel[0].received->used > 1000 &&
+	    test_channel[1].received->used > 1000)
+		io_loop_stop(current_ioloop);
+	else
+		test_multiplex_channel_write(&test_channel[rand() % 2]);
+}
+
+static void test_istream_multiplex_stream_read(struct test_channel *channel)
+{
+	const unsigned char *data = NULL;
+	size_t siz = 0;
+
+	if (i_stream_read(channel->in) > 0) {
+		data = i_stream_get_data(channel->in, &siz);
+		buffer_append(channel->received, data, siz);
+		i_stream_skip(channel->in, siz);
+	}
+}
+
+static void test_istream_read_alt(struct test_channel *channel)
+{
+	const unsigned char *data = NULL;
+	size_t siz = 0;
+
+	if (i_stream_read(channel->in_alt) > 0) {
+		data = i_stream_get_data(channel->in_alt, &siz);
+		buffer_append(channel->received_alt, data, siz);
+		i_stream_skip(channel->in_alt, siz);
+	}
+}
+
+static void setup_channel(struct test_channel *channel,
+			  struct istream *is, struct ostream *os)
+{
+	/* setup first channel */
+	channel->in = is;
+	channel->out = os;
+	channel->io = io_add_istream(is, test_istream_multiplex_stream_read,
+				     channel);
+	test_assert(pipe(channel->fds) == 0);
+	channel->in_alt = i_stream_create_fd(channel->fds[0], (size_t)-1, FALSE);
+	channel->out_alt = o_stream_create_fd(channel->fds[1], IO_BLOCK_SIZE, FALSE);
+	channel->io_alt = io_add_istream(channel->in_alt, test_istream_read_alt,
+					 channel);
+	channel->received = buffer_create_dynamic(default_pool, 32768);
+	channel->received_alt = buffer_create_dynamic(default_pool, 32768);
+}
+
+static void teardown_channel(struct test_channel *channel)
+{
+	test_assert(memcmp(channel->received->data,
+			   channel->received_alt->data,
+			   channel->received->used) == 0);
+	test_assert(channel->received->used == channel->received_alt->used);
+
+	buffer_free(&channel->received);
+	buffer_free(&channel->received_alt);
+
+	io_remove(&channel->io);
+	io_remove(&channel->io_alt);
+	i_stream_unref(&channel->in);
+	o_stream_unref(&channel->out);
+	i_stream_unref(&channel->in_alt);
+	o_stream_unref(&channel->out_alt);
+	i_close_fd(&channel->fds[0]);
+	i_close_fd(&channel->fds[1]);
+}
+
+static void test_multiplex_stream(void) {
+	test_begin("test multiplex (stream)");
+
+	struct ioloop *ioloop = io_loop_create();
+	io_loop_set_current(ioloop);
+
+	int fds[2];
+	test_assert(pipe(fds) == 0);
+	struct ostream *os = o_stream_create_fd(fds[1], (size_t)-1, FALSE);
+	struct istream *is = i_stream_create_fd(fds[0], (size_t)-1, FALSE);
+
+	struct istream *ichan0 = i_stream_create_multiplex(is, (size_t)-1);
+	struct istream *ichan1 = i_stream_multiplex_add_channel(ichan0, 1);
+	i_stream_unref(&is);
+
+	struct ostream *ochan0 = o_stream_create_multiplex(os, 1024);
+	struct ostream *ochan1 = o_stream_multiplex_add_channel(ochan0, 1);
+	o_stream_unref(&os);
+
+	struct io *io = io_add(fds[1], IO_WRITE, test_multiplex_stream_write, os);
+
+	setup_channel(&test_channel[0], ichan0, ochan0);
+	setup_channel(&test_channel[1], ichan1, ochan1);
+
+	test_channel[0].cid = 0;
+	test_channel[1].cid = 1;
+
+	io_loop_run(current_ioloop);
+
+	io_remove(&io);
+
+	teardown_channel(&test_channel[0]);
+	teardown_channel(&test_channel[1]);
+
+	io_loop_destroy(&ioloop);
+
+	i_close_fd(&fds[0]);
+	i_close_fd(&fds[1]);
+
+	test_end();
+}
+
+void test_multiplex(void) {
+	random_init();
+	test_multiplex_stream();
+	random_deinit();
+}
