84af637240881b8c1cc4df472832f49de952e4c6
Timo Sirainen <timo.sirainen@dovecot.fi>
2017-08-25 18:40:23 +0300

    sdbox: Don't open mail files when expunging
    It was done when mail_attachment_fs was non-empty (and default is
    non-empty), even though mail_attachment_dir was empty.

--- src/lib-storage/index/dbox-common/dbox-attachment.c
+++ src/lib-storage/index/dbox-common/dbox-attachment.c
@@ -30,7 +30,7 @@ dbox_attachment_file_get_stream_from(struct dbox_file *file,
 	const char *path_suffix;
 	uoff_t msg_size;
 
-	if (*file->storage->attachment_dir == '\0') {
+	if (file->storage->attachment_dir == NULL) {
 		mail_storage_set_critical(&file->storage->storage,
 			"%s contains references to external attachments, "
 			"but mail_attachment_dir is unset", file->cur_path);
