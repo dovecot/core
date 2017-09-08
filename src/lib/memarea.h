4c7d704408fc77467143a945dc2d0a856f72e674
Timo Sirainen <timo.sirainen@dovecot.fi>
2017-09-08 13:33:27 +0300

    lib: Add memarea API
    This can be used to create reference counted memory areas where a callback
    is called once the refcount drops to zero.

---
+++ src/lib/memarea.h
@@ -0,0 +1,31 @@
+#ifndef MEMAREA_H
+#define MEMAREA_H
+
+typedef void memarea_free_callback_t(void *context);
+
+/* Create reference counted memory area. The callback is called when the
+   refcount drops to 0. */
+struct memarea *
+memarea_init(const void *data, size_t size,
+	     memarea_free_callback_t *callback, void *context);
+#define memarea_init(data, size, callback, context) \
+	memarea_init(data, size + \
+		CALLBACK_TYPECHECK(callback, void (*)(typeof(context))), \
+		(memarea_free_callback_t *)callback, context)
+/* Returns an empty memory area. */
+struct memarea *memarea_init_empty(void);
+
+void memarea_ref(struct memarea *area);
+void memarea_unref(struct memarea **area);
+/* Free the memory area without calling the callback.
+   This is allowed only when refcount==1. */
+void memarea_free_without_callback(struct memarea **area);
+
+unsigned int memarea_get_refcount(struct memarea *area);
+const void *memarea_get(struct memarea *area, size_t *size_r);
+size_t memarea_get_size(struct memarea *area);
+
+/* free-callback that does nothing */
+void memarea_free_callback_noop(void *context);
+
+#endif
---
+++ src/lib/test-memarea.c
@@ -0,0 +1,42 @@
+/* Copyright (c) 2017 Dovecot authors, see the included COPYING file */
+
+#include "test-lib.h"
+#include "buffer.h"
+#include "memarea.h"
+
+static bool test_callback_called = FALSE;
+
+static void test_callback(buffer_t *buf)
+{
+	test_assert(!test_callback_called);
+	test_callback_called = TRUE;
+	buffer_free(&buf);
+}
+
+void test_memarea(void)
+{
+	struct memarea *area, *area2;
+	buffer_t *buf;
+	size_t size;
+
+	test_begin("memarea");
+	buf = buffer_create_dynamic(default_pool, 128);
+	buffer_append(buf, "123", 3);
+
+	area = memarea_init(buf->data, buf->used, test_callback, buf);
+	test_assert(memarea_get_refcount(area) == 1);
+	test_assert(memarea_get(area, &size) == buf->data && size == buf->used);
+
+	area2 = area;
+	memarea_ref(area2);
+	test_assert(memarea_get_refcount(area2) == 2);
+	test_assert(memarea_get(area2, &size) == buf->data && size == buf->used);
+	memarea_unref(&area2);
+	test_assert(area2 == NULL);
+	test_assert(!test_callback_called);
+
+	memarea_unref(&area);
+	test_assert(test_callback_called);
+
+	test_end();
+}
