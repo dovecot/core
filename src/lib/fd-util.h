fa26ad32932ab2ce1b7e3b2b121bb2df886f5736
Josef 'Jeff' Sipek <jeff.sipek@dovecot.fi>
2017-10-04 08:28:38 -0400

    lib: move fd-close-on-exec.[ch] code into fd-util.[ch]

--- src/lib/fd-util.h
+++ src/lib/fd-util.h
@@ -1,7 +1,12 @@
 #ifndef FD_UTIL_H
 #define FD_UTIL_H
 
-#include "fd-close-on-exec.h"
 #include "fd-set-nonblock.h"
+
+/* Change close-on-exec flag of fd. */
+void fd_close_on_exec(int fd, bool set);
+
+/* Verify that fds in given range don't exist. */
+void fd_debug_verify_leaks(int first_fd, int last_fd);
 
 #endif
