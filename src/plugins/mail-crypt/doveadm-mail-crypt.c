dovecot/core

commit 446d7d9ddfe122e152b832c13fc28d164ae2c5e9

doveadm-mail-crypt: Print existing userkey hash when aborting generate

src/plugins/mail-crypt/doveadm-mail-crypt.c
@@ -325,9 +325,26 @@ static int mcp_keypair_generate_run(struct doveadm_mail_cmd_context *_ctx,
 		if (ret < 0) return ret;
 		ctx->matched_keys++;
 	}
-
-	if (ret == 1 && ctx->force &&
-	    ctx->userkey_only && !user_key_generated) {
+	if (ret == 1 && ctx->userkey_only && !user_key_generated) {
+		if (!ctx->force) {
+			i_info("userkey exists. Use -f to generate a new one");
+			buffer_t *key_id = t_str_new(MAIL_CRYPT_HASH_BUF_SIZE);
+			if (!dcrypt_key_id_public(user_key,
+						MAIL_CRYPT_KEY_ID_ALGORITHM,
+						key_id, &error)) {
+				i_error("dcrypt_key_id_public() failed: %s",
+					error);
+				return -1;
+			}
+			const char *hash = binary_to_hex(key_id->data,
+							 key_id->used);
+			res = array_append_space(result);
+			res->name = DOVEADM_MCP_USERKEY;
+			res->id = p_strdup(_ctx->pool, hash);
+			res->success = TRUE;
+			ctx->matched_keys++;
+			return 1;
+		}
 		struct dcrypt_keypair pair;
 		dcrypt_key_unref_public(&user_key);
 		/* regen user key */

commit edde8ef105eb385f6e0a7e551ac87aa0ccdfac98

doveadm-mail-crypt: Do not return prematurely in key generate
Broken by 446d7d9ddfe122e152b832c13fc28d164ae2c5e9

src/plugins/mail-crypt/doveadm-mail-crypt.c
@@ -260,12 +260,13 @@ static int mcp_keypair_generate(struct mcp_cmd_context *ctx,
 					key_id, &error)) {
 			i_error("dcrypt_key_id_public() failed: %s",
 				error);
-			return -1;
+			ret = -1;
+		} else {
+			*pubid_r = p_strdup(ctx->ctx.pool, binary_to_hex(key_id->data,
+									 key_id->used));
+			*pair_r = pair;
+			ret = 1;
 		}
-		*pubid_r = p_strdup(ctx->ctx.pool, binary_to_hex(key_id->data,
-								 key_id->used));
-		*pair_r = pair;
-		return 1;
 	} else if (ret == 1 && ctx->recrypt_box_keys) {
 		/* do nothing, because force isn't being used *OR*
 		   we are recrypting box keys and force refers to