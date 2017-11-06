c2ece29906877a82e6b78dac45cab0722b1b0c90
Timo Sirainen <timo.sirainen@dovecot.fi>
2017-11-07 01:33:57 +0200

    log: Fix log reopening on SIGUSR1

--- src/log/main.c
+++ src/log/main.c
@@ -4,7 +4,7 @@
 #include "lib-signals.h"
 #include "restrict-access.h"
 #include "master-interface.h"
-#include "master-service.h"
+#include "master-service-private.h"
 #include "master-service-settings.h"
 #include "log-error-buffer.h"
 #include "log-connection.h"
@@ -18,6 +18,7 @@ static struct log_error_buffer *errorbuf;
 static void
 sig_reopen_logs(const siginfo_t *si ATTR_UNUSED, void *context ATTR_UNUSED)
 {
+	master_service->log_initialized = FALSE;
 	master_service_init_log(master_service, "log: ");
 }
 
