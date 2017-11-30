aea4efba7510f1d9ea8e137e32432f8e64156483
Aki Tuomi <aki.tuomi@dovecot.fi>
2017-11-30 15:46:40 +0200

    config: Add config_filter_get_all
    Returns all filters

--- src/config/config-filter.c
+++ src/config/config-filter.c
@@ -252,6 +252,21 @@ config_filter_find_all(struct config_filter_context *ctx, pool_t pool,
 }
 
 struct config_filter_parser *const *
+config_filter_get_all(struct config_filter_context *ctx)
+{
+	ARRAY_TYPE(config_filter_parsers) filters;
+	unsigned int i;
+
+	t_array_init(&filters, 8);
+	for (i = 0; ctx->parsers[i] != NULL; i++) {
+		array_append(&filters, &ctx->parsers[i], 1);
+	}
+	array_sort(&filters, config_filter_parser_cmp_rev);
+	array_append_zero(&filters);
+	return array_idx(&filters, 0);
+}
+
+struct config_filter_parser *const *
 config_filter_find_subset(struct config_filter_context *ctx,
 			  const struct config_filter *filter)
 {
