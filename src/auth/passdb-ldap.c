80064363aa97af808ab813da46aef80632bf05d7
Martti Rannanjärvi <martti.rannanjarvi@dovecot.fi>
2017-12-06 17:41:14 +0200

    auth: passdb-ldap - Fix AUTH_LOG_MSG_PASSWORD_MISMATCH use in logging

--- src/auth/passdb-ldap.c
+++ src/auth/passdb-ldap.c
@@ -155,8 +155,8 @@ ldap_auth_bind_callback(struct ldap_connection *conn,
 			passdb_result = PASSDB_RESULT_OK;
 		else if (ret == LDAP_INVALID_CREDENTIALS) {
 		  	auth_request_log_login_failure(auth_request,
-						       AUTH_SUBSYS_DB,
-						       "AUTH_LOG_MSG_PASSWORD_MISMATCH (for LDAP bind)");
+				AUTH_SUBSYS_DB,
+				AUTH_LOG_MSG_PASSWORD_MISMATCH" (for LDAP bind)");
 			passdb_result = PASSDB_RESULT_PASSWORD_MISMATCH;
 		} else if (ret == LDAP_NO_SUCH_OBJECT) {
 			passdb_result = PASSDB_RESULT_USER_UNKNOWN;
