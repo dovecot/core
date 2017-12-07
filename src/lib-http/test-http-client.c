dovecot/core

commit c45da70c96800ce9d84a886b163ad913e3d8ddea

lib-http: Link test-http-client libssl_iostream_openssl.so directly.
This way it can be run without having to do make install first.

src/lib-http/test-http-client.c
@@ -9,6 +9,7 @@
 #include "http-client.h"
 #include "dns-lookup.h"
 #include "iostream-ssl.h"
+#include "iostream-openssl.h"
 
 struct http_test_request {
 	struct io *io;
@@ -342,6 +343,7 @@ int main(int argc, char *argv[])
 	struct ioloop *ioloop;
 
 	lib_init();
+	iostream_openssl_init();
 
 	ioloop = io_loop_create();
 	io_loop_set_running(ioloop);
@@ -398,5 +400,6 @@ int main(int argc, char *argv[])
 	dns_client_deinit(&dns_client);
 
 	io_loop_destroy(&ioloop);
+	iostream_openssl_deinit();
 	lib_deinit();
 }

src/lib-http/Makefile.am
@@ -158,7 +158,8 @@ test_http_payload_DEPENDENCIES = \
 test_http_client_SOURCES = test-http-client.c
 test_http_client_LDFLAGS = -export-dynamic
 test_http_client_LDADD = \
-	$(test_http_libs)
+	$(test_http_libs) \
+	../lib-ssl-iostream/libssl_iostream_openssl.la
 test_http_client_DEPENDENCIES = \
 	$(test_http_deps)
 

commit 7cc5fcc8555809289d88d31f70c155d3152aac49

lib-http: test-http-client - conditionally depend on openssl
Fixes compiling without openssl, broken by c45da70c

src/lib-http/test-http-client.c
@@ -9,7 +9,9 @@
 #include "http-client.h"
 #include "dns-lookup.h"
 #include "iostream-ssl.h"
+#ifdef HAVE_OPENSSL
 #include "iostream-openssl.h"
+#endif
 
 struct http_test_request {
 	struct io *io;
@@ -344,8 +346,9 @@ int main(int argc, char *argv[])
 	struct ioloop *ioloop;
 
 	lib_init();
+#ifdef HAVE_OPENSSL
 	ssl_iostream_openssl_init();
-
+#endif
 	ioloop = io_loop_create();
 	io_loop_set_running(ioloop);
 
@@ -401,6 +404,8 @@ int main(int argc, char *argv[])
 	dns_client_deinit(&dns_client);
 
 	io_loop_destroy(&ioloop);
+#ifdef HAVE_OPENSSL
 	ssl_iostream_openssl_deinit();
+#endif
 	lib_deinit();
 }