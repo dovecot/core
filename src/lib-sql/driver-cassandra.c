dovecot/core

commit cae3c6903c10682429f9849ab1a25093e3f538e4

driver-cassandra: Free statement pool on update
Otherwise the pool memory will leak. This was happening with non-prepared
statements when version was older than 4.

src/lib-sql/driver-cassandra.c
@@ -2018,6 +2018,7 @@ driver_cassandra_update_stmt(struct sql_transaction_context *_ctx,
 		ctx->stmt = stmt;
 	else
 		ctx->query = i_strdup(sql_statement_get_query(_stmt));
+	pool_unref(&_stmt->pool);
 }
 
 const struct sql_db driver_cassandra_db = {

commit 117ae14172071ab6334260dfd3cab3609234ef2a

cassandra: Fix crash when using prepared statements
Broken by cae3c6903c10682429f9849ab1a25093e3f538e4

src/lib-sql/driver-cassandra.c
@@ -2016,9 +2016,10 @@ driver_cassandra_update_stmt(struct sql_transaction_context *_ctx,
 	}
 	if (stmt->prep != NULL)
 		ctx->stmt = stmt;
-	else
+	else {
 		ctx->query = i_strdup(sql_statement_get_query(_stmt));
-	pool_unref(&_stmt->pool);
+		pool_unref(&_stmt->pool);
+	}
 }
 
 const struct sql_db driver_cassandra_db = {