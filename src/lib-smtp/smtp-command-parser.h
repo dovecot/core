4e4b3ae48b9ce1c397189c263cbd42df1b8c1d87
Timo Sirainen <timo.sirainen@dovecot.fi>
2017-12-18 12:05:59 +0200

    lib-smtp: server: Fix infinite loop when client disconnects in initial state

--- src/lib-smtp/smtp-command-parser.h
+++ src/lib-smtp/smtp-command-parser.h
@@ -22,6 +22,9 @@ void smtp_command_parser_deinit(struct smtp_command_parser **_parser);
 void smtp_command_parser_set_stream(struct smtp_command_parser *parser,
 	struct istream *input);
 
+/* Returns 1 if a command was returned, 0 if more data is needed, -1 on error,
+   -2 if disconnected in SMTP_COMMAND_PARSE_STATE_INIT state. -2 is mainly for
+   unit tests - it can normally be treated the same as -1. */
 int smtp_command_parse_next(struct smtp_command_parser *parser,
 			    const char **cmd_name_r, const char **cmd_params_r,
 			    enum smtp_command_parse_error *error_code_r, const char **error_r);
@@ -33,6 +36,7 @@ struct istream *
 smtp_command_parse_data_with_dot(struct smtp_command_parser *parser);
 bool smtp_command_parser_pending_data(struct smtp_command_parser *parser);
 
+/* Returns the same as smtp_command_parse_next() */
 int smtp_command_parse_auth_response(struct smtp_command_parser *parser,
 			    const char **line_r, enum smtp_command_parse_error *error_code_r,
 			    const char **error_r);
