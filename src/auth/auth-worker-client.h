ff17366f92ed936e05a48f9d20af6089dfb18037
Aki Tuomi <aki.tuomi@dovecot.fi>
2018-01-08 14:52:10 +0200

    auth-worker: Add auth_worker_auth_request_new
    Replaces worker_auth_request_new, moves in
    check for username and service. Simplifies code.

--- src/auth/auth-worker-client.h
+++ src/auth/auth-worker-client.h
@@ -8,6 +8,8 @@
 extern struct auth_worker_client *auth_worker_client;
 
 struct auth_worker_client *auth_worker_client_create(struct auth *auth, int fd);
+bool auth_worker_auth_request_new(struct auth_worker_client *client, unsigned int id,
+				  const char *const *args, struct auth_request **request_r);
 void auth_worker_client_destroy(struct auth_worker_client **client);
 void auth_worker_client_unref(struct auth_worker_client **client);
 
