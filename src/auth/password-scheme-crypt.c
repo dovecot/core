dovecot/core

commit 1ebb6094e5105ba7ef521a0177c42d3ea81243f0

libpassword: Add BLF-CRYPT support independent of crypt

src/auth/password-scheme-crypt.c
@@ -3,12 +3,17 @@
 #include "lib.h"
 #include "mycrypt.h"
 #include "password-scheme.h"
+#include "crypt-blowfish.h"
+#include "randgen.h"
 
 /* Lengths and limits for some crypt() algorithms. */
 #define CRYPT_BLF_ROUNDS_DEFAULT 5
 #define CRYPT_BLF_ROUNDS_MIN 4
 #define CRYPT_BLF_ROUNDS_MAX 31
-#define CRYPT_BLF_SALT_LEN 22
+#define CRYPT_BLF_SALT_LEN 16 /* raw salt */
+#define CRYPT_BLF_PREFIX_LEN (7+22+1) /* $2.$nn$ + salt */
+#define CRYPT_BLF_BUFFER_LEN 128
+#define CRYPT_BLF_PREFIX "$2y"
 #define CRYPT_SHA2_ROUNDS_DEFAULT 5000
 #define CRYPT_SHA2_ROUNDS_MIN 1000
 #define CRYPT_SHA2_ROUNDS_MAX 999999999
@@ -31,7 +36,9 @@ static void
 crypt_generate_blowfish(const char *plaintext, const struct password_generate_params *params,
 			 const unsigned char **raw_password_r, size_t *size_r)
 {
-	const char *password, *salt, *magic_salt;
+	char salt[CRYPT_BLF_SALT_LEN];
+	char password[CRYPT_BLF_BUFFER_LEN];
+	char magic_salt[CRYPT_BLF_PREFIX_LEN];
 	unsigned int rounds = params->rounds;
 
 	if (rounds == 0)
@@ -41,11 +48,51 @@ crypt_generate_blowfish(const char *plaintext, const struct password_generate_pa
 	else if (rounds > CRYPT_BLF_ROUNDS_MAX)
 		rounds = CRYPT_BLF_ROUNDS_MAX;
 
-	salt = password_generate_salt(CRYPT_BLF_SALT_LEN);
-	magic_salt = t_strdup_printf("$2a$%02u$%s", rounds, salt);
-	password = t_strdup(mycrypt(plaintext, magic_salt));
-	*raw_password_r = (const unsigned char *)password;
+	random_fill(salt, CRYPT_BLF_SALT_LEN);
+	if (crypt_gensalt_blowfish_rn(CRYPT_BLF_PREFIX, rounds,
+				      salt, CRYPT_BLF_SALT_LEN,
+				      magic_salt, CRYPT_BLF_PREFIX_LEN) == NULL)
+		i_fatal("crypt_gensalt_blowfish_rn failed: %m");
+
+	if (crypt_blowfish_rn(plaintext, magic_salt, password,
+			       CRYPT_BLF_BUFFER_LEN) == NULL)
+		i_fatal("crypt_blowfish_rn failed: %m");
+
+	*raw_password_r = (const unsigned char *)t_strdup(password);
 	*size_r = strlen(password);
+}
+
+static int
+crypt_verify_blowfish(const char *plaintext, const struct password_generate_params *params ATTR_UNUSED,
+		      const unsigned char *raw_password, size_t size,
+		      const char **error_r)
+{
+	const char *password;
+	const char *salt;
+	char crypted[CRYPT_BLF_BUFFER_LEN];
+
+	if (size == 0) {
+		/* the default mycrypt() handler would return match */
+		return 0;
+	}
+	password = t_strndup(raw_password, size);
+
+	if (size < CRYPT_BLF_PREFIX_LEN ||
+	    strncmp(password, "$2", 2) != 0 ||
+	    password[2] < 'a' || password[2] > 'z' ||
+	    password[3] != '$') {
+		*error_r = "Password is not blowfish password";
+		return -1;
+	}
+
+	salt = t_strndup(password, CRYPT_BLF_PREFIX_LEN);
+	if (crypt_blowfish_rn(plaintext, salt, crypted, CRYPT_BLF_BUFFER_LEN) == NULL) {
+		/* really shouldn't happen unless the system is broken */
+		*error_r = t_strdup_printf("crypt_blowfish_rn failed: %m");
+		return -1;
+	}
+
+	return strcmp(crypted, password) == 0 ? 1 : 0;
 }
 
 static void
@@ -117,12 +164,15 @@ static const struct {
 static const struct password_scheme crypt_schemes[] = {
 	{ "CRYPT", PW_ENCODING_NONE, 0, crypt_verify,
 	  crypt_generate_des },
-	{ "BLF-CRYPT", PW_ENCODING_NONE, 0, crypt_verify,
-	  crypt_generate_blowfish },
 	{ "SHA256-CRYPT", PW_ENCODING_NONE, 0, crypt_verify,
 	  crypt_generate_sha256 },
 	{ "SHA512-CRYPT", PW_ENCODING_NONE, 0, crypt_verify,
 	  crypt_generate_sha512 }
+};
+
+static const struct password_scheme blf_crypt_scheme = {
+	"BLF-CRYPT", PW_ENCODING_NONE, 0, crypt_verify_blowfish,
+		crypt_generate_blowfish
 };
 
 void password_scheme_register_crypt(void)
@@ -136,4 +186,5 @@ void password_scheme_register_crypt(void)
 		   (strcmp(crypted, sample[i].expected) == 0))
 			password_scheme_register(&crypt_schemes[i]);
 	}
+	password_scheme_register(&blf_crypt_scheme);
 }

AUTHORS
@@ -17,4 +17,6 @@ Vaclav Haisman <v.haisman@sh.cvut.cz> (src/lib/ioloop-kqueue.c,
 
 Portions Copyright (c) 2008 Apple Inc.  All rights reserved.
 
+Blowfish code by Solar Designer <solar at openwall.com>
+
 Grepping 'Patch by' from ChangeLog shows up more people.

src/auth/Makefile.am
@@ -44,6 +44,7 @@ AM_CPPFLAGS = \
 auth_LDFLAGS = -export-dynamic
 
 libpassword_la_SOURCES = \
+	crypt-blowfish.c \
 	mycrypt.c \
 	password-scheme.c \
 	password-scheme-crypt.c \
@@ -231,7 +232,7 @@ test_programs = \
 
 noinst_PROGRAMS = $(test_programs)
 
-noinst_HEADERS = test-auth.h
+noinst_HEADERS = test-auth.h crypt-blowfish.h
 
 test_libs = \
 	../lib-dovecot/libdovecot.la

src/auth/crypt-blowfish.h
@@ -1,0 +1,29 @@
+/*
+ * Written by Solar Designer <solar at openwall.com> in 2000-2011.
+ * No copyright is claimed, and the software is hereby placed in the public
+ * domain.  In case this attempt to disclaim copyright and place the software
+ * in the public domain is deemed null and void, then the software is
+ * Copyright (c) 2000-2011 Solar Designer and it is hereby released to the
+ * general public under the following terms:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted.
+ *
+ * There's ABSOLUTELY NO WARRANTY, express or implied.
+ *
+ * See crypt_blowfish.c for more information.
+ *
+ * 2017-10-10 - Adapted for dovecot code by Aki Tuomi <aki.tuomi@dovecot.fi>
+ */
+
+#ifndef CRYPT_BLOWFISH_H
+#define CRYPT_BLOWFISH_H
+
+extern int crypt_output_magic(const char *setting, char *output, size_t size);
+extern char *crypt_blowfish_rn(const char *key, const char *setting,
+	char *output, size_t size);
+extern char *crypt_gensalt_blowfish_rn(const char *prefix,
+	unsigned long count,
+	const char *input, size_t size, char *output, size_t output_size);
+
+#endif

src/auth/password-scheme.c
@@ -313,6 +313,11 @@ int crypt_verify(const char *plaintext, const struct password_generate_params *p
 {
 	const char *password, *crypted;
 
+	if (size > 4 && raw_password[0] == '$' && raw_password[1] == '2' &&
+	    raw_password[3] == '$')
+		return password_verify(plaintext, params, "BLF-CRYPT",
+				       raw_password, size, error_r);
+
 	if (size == 0) {
 		/* the default mycrypt() handler would return match */
 		return 0;

commit b0b349d5a55ddf4b4c20a2a5a295299e4dde304b

auth: Fix crypt scheme detection
Broken by 1ebb6094e5105ba7ef521a0177c42d3ea81243f0

src/auth/password-scheme-crypt.c
@@ -150,8 +150,6 @@ static const struct {
 	const char *expected;
 } sample[] = {
 	{ "08/15!test~4711", "JB", "JBOZ0DgmtucwE" },
-	{ "08/15!test~4711", "$2a$04$0123456789abcdefABCDEF",
-	  "$2a$04$0123456789abcdefABCDE.N.drYX5yIAL1LkTaaZotW3yI0hQhZru" },
 	{ "08/15!test~4711", "$5$rounds=1000$0123456789abcdef",
 	  "$5$rounds=1000$0123456789abcdef$K/DksR0DT01hGc8g/kt"
 	  "9McEgrbFMKi9qrb1jehe7hn4" },