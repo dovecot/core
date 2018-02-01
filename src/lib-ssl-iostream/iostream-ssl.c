dovecot/core

commit 4836d541b1c1354073e068aabe5cd92aa67fe61d

ssl-iostream: ssl_iostream_destroy(NULL) should be a no-op

src/lib-ssl-iostream/iostream-ssl.c
@@ -161,6 +161,10 @@ void ssl_iostream_destroy(struct ssl_iostream **_ssl_io)
 {
 	struct ssl_iostream *ssl_io = *_ssl_io;
 
+	if (_ssl_io == NULL || *_ssl_io == NULL)
+		return;
+
+	ssl_io = *_ssl_io;
 	*_ssl_io = NULL;
 	ssl_vfuncs->destroy(ssl_io);
 }

commit 421e784f235b52d7b4957055e186c518cf028929

lib-ssl-iostream: Fix premature NULL deref
Broken in 4836d541b1c1354073e068aabe5cd92aa67fe61d

Found by coverity

src/lib-ssl-iostream/iostream-ssl.c
@@ -159,7 +159,7 @@ void ssl_iostream_unref(struct ssl_iostream **_ssl_io)
 
 void ssl_iostream_destroy(struct ssl_iostream **_ssl_io)
 {
-	struct ssl_iostream *ssl_io = *_ssl_io;
+	struct ssl_iostream *ssl_io;
 
 	if (_ssl_io == NULL || *_ssl_io == NULL)
 		return;