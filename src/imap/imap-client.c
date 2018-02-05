dovecot/core

commit e0a42caee5a04d29cfa95ae7719aeaa708001255

pop3: Include mail user variables in logout format

src/pop3/pop3-client.c
@@ -540,22 +540,15 @@ static const char *client_stats(struct client *client)
 		{ 'd', !client->delete_success ? "0" :
 		       dec2str(client->deleted_size), "deleted_bytes" },
 	};
-	ARRAY(struct var_expand_table) tab;
 	const struct var_expand_table *user_tab =
 		mail_user_var_expand_table(client->user);
+	const struct var_expand_table *tab =
+		t_var_expand_merge_tables(logout_tab, user_tab);
 	string_t *str;
-	size_t n;
-
-	t_array_init(&tab, 32);
-	array_append(&tab, logout_tab, N_ELEMENTS(logout_tab));
-	/* count elements */
-	for(n = 0; user_tab[n].long_key != NULL; n++)
-		;
-	array_append(&tab, user_tab, n);
-	array_append_zero(&tab);
 
 	str = t_str_new(128);
-	var_expand(str, client->set->pop3_logout_format, array_idx(&tab, 0));
+	var_expand_with_funcs(str, client->set->pop3_logout_format, tab,
+			      mail_user_var_expand_func_table, client->user);
 	return str_c(str);
 }
 

commit 7378176061447e7ede9d239764326868ebba9368

imap,pop3: Properly terminate logout tab.
Fixes Fatal: master: service(imap): child killed with signal 11

Broken in e0a42caee5a04d29cfa95ae7719aeaa708001255 and
1ecc087ee90439787de7df9b9a60e0d35b5dda07

src/imap/imap-client.c
@@ -246,6 +246,7 @@ const char *client_stats(struct client *client)
 		{ '\0', dec2str(client->trashed_count), "trashed" },
 		{ '\0', dec2str(client->autoexpunged_count), "autoexpunged" },
 		{ '\0', dec2str(client->append_count), "appended" },
+		{ '\0', NULL, NULL }
 	};
 	const struct var_expand_table *user_tab =
 		mail_user_var_expand_table(client->user);

src/pop3/pop3-client.c
@@ -539,6 +539,7 @@ static const char *client_stats(struct client *client)
 		{ '\0', client->session_id, "session" },
 		{ 'd', !client->delete_success ? "0" :
 		       dec2str(client->deleted_size), "deleted_bytes" },
+		{ '\0', NULL, NULL }
 	};
 	const struct var_expand_table *user_tab =
 		mail_user_var_expand_table(client->user);