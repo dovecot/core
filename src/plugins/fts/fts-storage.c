4031a87e9c2852f5e0ea0613ffb7f971310fe653
Timo Sirainen <timo.sirainen@dovecot.fi>
2018-02-05 22:21:13 +0200

    fts: Don't reindex FTS mails if .cache file is deleted
    This means that if fts is enabled, "doveadm index" no longer adds mails
    to dovecot.index.cache if it's deleted. However, it was rarely used for
    that purpose. More likely due to a corrupted cache file all the mails were
    unnecessarily being opened and reindexed.

--- src/plugins/fts/fts-storage.c
+++ src/plugins/fts/fts-storage.c
@@ -105,9 +105,10 @@ fts_mailbox_get_status(struct mailbox *box, enum mailbox_status_items items,
 		if (fts_mailbox_get_last_cached_seq(box, &seq) < 0)
 			return -1;
 
-		/* use whichever is smaller */
-		if (status_r->last_cached_seq > seq)
-			status_r->last_cached_seq = seq;
+		/* Always use the FTS's last_cached_seq. This is because we
+		   don't want to reindex all mails to FTS if .cache file is
+		   deleted. */
+		status_r->last_cached_seq = seq;
 	}
 	return 0;
 }
