ea2dbaa29028719a3f9599502ffe732cf73dc544
Timo Sirainen <timo.sirainen@dovecot.fi>
2018-02-06 15:10:48 +0200

    lib-storage: Set mailbox_transaction_context.flags earlier
    Set it in index_transaction_init() so plugins' transaction_begin() methods
    see it after calling super.transaction_begin().

--- src/lib-storage/index/index-transaction.c
+++ src/lib-storage/index/index-transaction.c
@@ -162,6 +162,7 @@ void index_transaction_init(struct mailbox_transaction_context *t,
 	if ((flags & MAILBOX_TRANSACTION_FLAG_REFRESH) != 0)
 		mail_index_refresh(box->index);
 
+	t->flags = flags;
 	t->box = box;
 	t->reason = i_strdup(reason);
 	t->itrans = mail_index_transaction_begin(box->view, itrans_flags);
