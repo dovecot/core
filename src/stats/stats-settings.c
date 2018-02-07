49568744a836768dc6c76db837130b7966c5ff38
Timo Sirainen <timo.sirainen@dovecot.fi>
2018-02-07 13:05:09 +0200

    stats: Change stats-writer default socket permissions to allow default_internal_group
    It's important that all dovecot processes can send statistics to the stats
    process.

--- src/stats/stats-settings.c
+++ src/stats/stats-settings.c
@@ -11,7 +11,7 @@ static bool stats_metric_settings_check(void *_set, pool_t pool, const char **er
 /* <settings checks> */
 static struct file_listener_settings stats_unix_listeners_array[] = {
 	{ "stats-reader", 0600, "", "" },
-	{ "stats-writer", 0600, "", "" },
+	{ "stats-writer", 0660, "", "$default_internal_group" },
 };
 static struct file_listener_settings *stats_unix_listeners[] = {
 	&stats_unix_listeners_array[0],
