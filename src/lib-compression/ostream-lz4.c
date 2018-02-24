25c0f5136d1b85b377bf3aa65bbd4429fcb4ecea
Timo Sirainen <timo.sirainen@dovecot.fi>
2018-02-24 22:04:49 +0200

    lib-compression: Implement the new ostream.get_buffer_used/avail_size() APIs
    The avail_size() implementation isn't fully correct for bzlib/zlib/lzma.
    Fixing it requires larger changes though.

--- src/lib-compression/ostream-lz4.c
+++ src/lib-compression/ostream-lz4.c
@@ -146,6 +146,31 @@ static int o_stream_lz4_flush(struct ostream_private *stream)
 	return o_stream_flush_parent(stream);
 }
 
+static size_t
+o_stream_lz4_get_buffer_used_size(const struct ostream_private *stream)
+{
+	const struct lz4_ostream *zstream =
+		(const struct lz4_ostream *)stream;
+
+	/* outbuf has already compressed data that we're trying to send to the
+	   parent stream. compressbuf isn't included in the return value,
+	   because it needs to be filled up or flushed. */
+	return (zstream->outbuf_used - zstream->outbuf_offset) +
+		o_stream_get_buffer_used_size(stream->parent);
+}
+
+static size_t
+o_stream_lz4_get_buffer_avail_size(const struct ostream_private *stream)
+{
+	const struct lz4_ostream *zstream =
+		(const struct lz4_ostream *)stream;
+
+	/* We're only guaranteed to accept data to compressbuf. The parent
+	   stream might have space, but since compressed data gets written
+	   there it's not really known how much we can actually write there. */
+	return sizeof(zstream->compressbuf) - zstream->compressbuf_offset;
+}
+
 static ssize_t
 o_stream_lz4_sendv(struct ostream_private *stream,
 		    const struct const_iovec *iov, unsigned int iov_count)
@@ -183,6 +208,10 @@ struct ostream *o_stream_create_lz4(struct ostream *output, int level)
 	zstream = i_new(struct lz4_ostream, 1);
 	zstream->ostream.sendv = o_stream_lz4_sendv;
 	zstream->ostream.flush = o_stream_lz4_flush;
+	zstream->ostream.get_buffer_used_size =
+		o_stream_lz4_get_buffer_used_size;
+	zstream->ostream.get_buffer_avail_size =
+		o_stream_lz4_get_buffer_avail_size;
 	zstream->ostream.iostream.close = o_stream_lz4_close;
 
 	i_assert(sizeof(zstream->outbuf) >= sizeof(*hdr));
