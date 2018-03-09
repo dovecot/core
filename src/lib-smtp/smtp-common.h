2b03e47da3554f7b396f67ebeeba1f53af614b70
Stephan Bosch <stephan.bosch@dovecot.fi>
2018-03-09 15:47:06 +0100

    lib-smtp: common: Use the BIT() macro for defining the capability flags.

--- src/lib-smtp/smtp-common.h
+++ src/lib-smtp/smtp-common.h
@@ -29,19 +29,19 @@ smtp_protocol_name(enum smtp_protocol proto)
 /* SMTP capabilities */
 
 enum smtp_capability {
-	SMTP_CAPABILITY_AUTH                = 0x0001,
-	SMTP_CAPABILITY_STARTTLS            = 0x0002,
-	SMTP_CAPABILITY_PIPELINING          = 0x0004,
-	SMTP_CAPABILITY_SIZE                = 0x0008,
-	SMTP_CAPABILITY_ENHANCEDSTATUSCODES = 0x0010,
-	SMTP_CAPABILITY_8BITMIME            = 0x0020,
-	SMTP_CAPABILITY_CHUNKING            = 0x0040,
-	SMTP_CAPABILITY_BINARYMIME          = 0x0080,
-	SMTP_CAPABILITY_BURL                = 0x0100,
-	SMTP_CAPABILITY_DSN                 = 0x0200,
-	SMTP_CAPABILITY_VRFY                = 0x0400,
-	SMTP_CAPABILITY_ETRN                = 0x0800,
-	SMTP_CAPABILITY_XCLIENT             = 0x1000
+	SMTP_CAPABILITY_AUTH                = BIT(0),
+	SMTP_CAPABILITY_STARTTLS            = BIT(1),
+	SMTP_CAPABILITY_PIPELINING          = BIT(2),
+	SMTP_CAPABILITY_SIZE                = BIT(3),
+	SMTP_CAPABILITY_ENHANCEDSTATUSCODES = BIT(4),
+	SMTP_CAPABILITY_8BITMIME            = BIT(5),
+	SMTP_CAPABILITY_CHUNKING            = BIT(6),
+	SMTP_CAPABILITY_BINARYMIME          = BIT(7),
+	SMTP_CAPABILITY_BURL                = BIT(8),
+	SMTP_CAPABILITY_DSN                 = BIT(9),
+	SMTP_CAPABILITY_VRFY                = BIT(10),
+	SMTP_CAPABILITY_ETRN                = BIT(11),
+	SMTP_CAPABILITY_XCLIENT             = BIT(12),
 };
 struct smtp_capability_name {
 	const char *name;
