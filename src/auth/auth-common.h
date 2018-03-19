39f0972a30168aa666d9b568d3fe81bc04a85fe5
Sergey Kitov <sergey.kitov@open-xchange.com>
2018-03-19 11:14:22 +0200

    auth: Add global auth_event with category "auth".

--- src/auth/auth-common.h
+++ src/auth/auth-common.h
@@ -7,6 +7,8 @@
 extern bool worker, worker_restart_request;
 extern time_t process_start_time;
 extern struct auth_penalty *auth_penalty;
+extern struct event_category event_category_auth;
+extern struct event *auth_event;
 
 void auth_refresh_proctitle(void);
 void auth_worker_refresh_proctitle(const char *state);
