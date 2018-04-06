a9b8af221b2337cbca1d68773502db59f8752129
Sergey Kitov <sergey.kitov@open-xchange.com>
2018-04-06 15:48:02 +0300

    lib-mail: Fix include guards for istream-nonuls.h

--- src/lib-mail/istream-nonuls.h
+++ src/lib-mail/istream-nonuls.h
@@ -1,5 +1,5 @@
-#ifndef ISTREAM_DOT_H
-#define ISTREAM_DOT_H
+#ifndef ISTREAM_NONULS_H
+#define ISTREAM_NONULS_H
 
 /* Translate all NUL characters to the specified replace_chr. */
 struct istream *i_stream_create_nonuls(struct istream *input, char replace_chr);
