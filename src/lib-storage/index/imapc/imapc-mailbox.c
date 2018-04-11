dovecot/core

commit c05b4ac76c4c6012958f1cc37e2fccf7a7d31c33

imapc: Fix local state if mailbox is empty after reconnection
If local indexes had more than 0 mails, but after reconnection the remote
IMAP server replied with "* 0 EXISTS", the mails in local index weren't
expunged. This could have resulted in:

Panic: file imapc-sync.c: line 328 (imapc_initial_sync_check): assertion failed: (mail_index_is_expunged(view, lseq) || seq_range_exists(&ctx->mbox->delayed_expunged_uids, luid))

src/lib-storage/index/imapc/imapc-mailbox.c
@@ -220,8 +220,10 @@ imapc_mailbox_fetch_state(struct imapc_mailbox *mbox, uint32_t first_uid)
 	struct imapc_command *cmd;
 
 	if (mbox->exists_count == 0) {
-		/* empty mailbox - no point in fetching anything */
-		mbox->state_fetched_success = TRUE;
+		/* empty mailbox - no point in fetching anything.
+		   just make sure everything is expunged in local index. */
+		mbox->sync_next_lseq = 1;
+		imapc_mailbox_fetch_state_finish(mbox);
 		return;
 	}
 	if (mbox->state_fetching_uid1) {

commit 7a1c3ec3711ec8708d0999a4fdda70431dd28341

imapc: Fix crash when handling empty mailboxes.
Broken by c05b4ac76c4c6012958f1cc37e2fccf7a7d31c33

src/lib-storage/index/imapc/imapc-mailbox.c
@@ -223,6 +223,7 @@ imapc_mailbox_fetch_state(struct imapc_mailbox *mbox, uint32_t first_uid)
 		/* empty mailbox - no point in fetching anything.
 		   just make sure everything is expunged in local index. */
 		mbox->sync_next_lseq = 1;
+		imapc_mailbox_init_delayed_trans(mbox);
 		imapc_mailbox_fetch_state_finish(mbox);
 		return;
 	}