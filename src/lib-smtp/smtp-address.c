dovecot/core

commit 81c8955e3d1475c6ab1892b1d3edf91126683fda

lib-smtp: address parser: Do not allow source route for path with omitted brackets.
Parse it strictly as a Mailbox, which makes more sense.

src/lib-smtp/smtp-address.c
@@ -195,7 +195,7 @@ smtp_parse_path(struct smtp_address_parser *aparser,
 	}
 
 	/* [ A-d-l ":" ] */
-	if ((sret=smtp_parse_source_route(parser)) < 0)
+	if (aparser->path && (sret=smtp_parse_source_route(parser)) < 0)
 		return -1;
 
 	/* Mailbox */

src/lib-smtp/test-smtp-address.c
@@ -178,6 +178,11 @@ valid_path_parse_tests[] = {
 		.input = "<@otherdomain.tld,@yetanotherdomain.tld:user@domain.tld>",
 		.address = { .localpart = "user", .domain = "domain.tld" },
 		.output = "<user@domain.tld>"
+	},{
+		.input = "user@domain.tld",
+		.flags = SMTP_ADDRESS_PARSE_FLAG_BRACKETS_OPTIONAL,
+		.address = { .localpart = "user", .domain = "domain.tld" },
+		.output = "<user@domain.tld>"
 	}
 };
 
@@ -585,6 +590,9 @@ invalid_path_parse_tests[] = {
 		.flags = SMTP_ADDRESS_PARSE_FLAG_BRACKETS_OPTIONAL
 	}, {
 		.input = "email@example..com",
+		.flags = SMTP_ADDRESS_PARSE_FLAG_BRACKETS_OPTIONAL
+	}, {
+		.input = "@otherdomain.tld,@yetanotherdomain.tld:user@domain.tld",
 		.flags = SMTP_ADDRESS_PARSE_FLAG_BRACKETS_OPTIONAL
 	}, {
 		.input = "<>",

commit 483940da2d54dc156270676d1766a8319109515f

lib-smtp: Fix empty address parsing
sret wasn't initialized, which could have randomly caused "Path only
consists of source route" error with empty addresses. Broken by
81c8955e3d1475c6ab1892b1d3edf91126683fda

src/lib-smtp/smtp-address.c
@@ -181,7 +181,7 @@ smtp_parse_path(struct smtp_address_parser *aparser,
 	enum smtp_address_parse_flags flags)
 {
 	struct smtp_parser *parser = &aparser->parser;
-	int ret, sret;
+	int ret, sret = 0;
 
 	/* Path = "<" [ A-d-l ":" ] Mailbox ">"
 	 */