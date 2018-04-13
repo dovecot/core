a9e78999d2a86f1e9f014fa9ce2ec61a79b47ce1
Timo Sirainen <timo.sirainen@dovecot.fi>
2018-04-13 16:12:27 +0300

    lib-master: Add how long request took for "Internal auth failure" errors

--- src/lib-master/master-login-auth.c
+++ src/lib-master/master-login-auth.c
@@ -296,8 +296,10 @@ master_login_auth_input_fail(struct master_login_auth *auth,
 						 "Internal auth failure");
 		} else {
 			i_error("Internal auth failure: %s "
-				"(client-pid=%u client-id=%u)",
-				error, request->client_pid, request->auth_id);
+				"(Request took %ld secs, "
+				"client-pid=%u client-id=%u)",
+				error, (long)(ioloop_time - request->create_stamp),
+				request->client_pid, request->auth_id);
 			request->callback(NULL, error, request->context);
 		}
 		i_free(request);
