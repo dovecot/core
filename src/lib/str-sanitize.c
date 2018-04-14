b1b531b3720961b87cd94975341a941bc3406d58
Stephan Bosch <stephan.bosch@dovecot.fi>
2018-04-14 02:05:51 +0200

    lib: Implement str_sanitize_utf8().
    Unlike str_sanitize(), this function truncates strings based on a UTF8 code point
    limit rather than a maximum size in bytes. Also, the Unicode replacement
    character is used to mark invalid/control characters and an ellipsis character
    is used to indicate the string truncation. For the normal str_sanitize() this is
    done using a question mark and triple dots respectively.

--- src/lib/str-sanitize.c
+++ src/lib/str-sanitize.c
@@ -22,6 +22,27 @@ static size_t str_sanitize_skip_start(const char *src, size_t max_bytes)
 	return i;
 }
 
+
+static size_t
+str_sanitize_skip_start_utf8(const char *src, uintmax_t max_chars)
+{
+	unichar_t chr;
+	uintmax_t c;
+	size_t i;
+
+	for (i = 0, c = 0; c < max_chars && src[i] != '\0'; ) {
+		int len = uni_utf8_get_char(src+i, &chr);
+		if (len <= 0)
+			break;
+		if ((unsigned char)src[i] < 32)
+			break;
+		c++;
+		i += len;
+	}
+	i_assert(c <= max_chars);
+	return i;
+}
+
 static void str_sanitize_truncate_char(string_t *dest, unsigned int initial_pos)
 {
 	const unsigned char *data = str_data(dest);
@@ -77,6 +98,42 @@ void str_sanitize_append(string_t *dest, const char *src, size_t max_bytes)
 	}
 }
 
+void str_sanitize_append_utf8(string_t *dest, const char *src,
+			      uintmax_t max_cps)
+{
+	size_t last_pos = 0;
+	unichar_t chr;
+	uintmax_t c;
+	size_t i;
+
+	i_assert(max_cps > 0);
+
+	for (i = 0, c = 0; c < max_cps && src[i] != '\0'; ) {
+		int len = uni_utf8_get_char(src+i, &chr);
+		if (len == 0)
+			break; /* input ended too early */
+
+		last_pos = str_len(dest);
+		if (len < 0) {
+			/* invalid UTF-8 */
+			str_append(dest, UNICODE_REPLACEMENT_CHAR_UTF8);
+			i++;
+			continue;
+		}
+		if ((unsigned char)src[i] < 32)
+			str_append(dest, UNICODE_REPLACEMENT_CHAR_UTF8);
+		else
+			str_append_n(dest, src+i, len);
+		i += len;
+		c++;
+	}
+
+	if (src[i] != '\0') {
+		str_truncate(dest, last_pos);
+		str_append(dest, UNICODE_HORIZONTAL_ELLIPSIS_CHAR_UTF8);
+	}
+}
+
 const char *str_sanitize(const char *src, size_t max_bytes)
 {
 	string_t *str;
@@ -93,3 +150,21 @@ const char *str_sanitize(const char *src, size_t max_bytes)
 	str_sanitize_append(str, src, max_bytes);
 	return str_c(str);
 }
+
+const char *str_sanitize_utf8(const char *src, uintmax_t max_cps)
+{
+	string_t *str;
+	size_t i;
+
+	if (src == NULL)
+		return NULL;
+
+	i = str_sanitize_skip_start_utf8(src, max_cps);
+	if (src[i] == '\0')
+		return src;
+
+	str = t_str_new(I_MIN(max_cps, 256));
+	str_sanitize_append_utf8(str, src, max_cps);
+	return str_c(str);
+}
+
--- src/lib/str-sanitize.h
+++ src/lib/str-sanitize.h
@@ -6,8 +6,17 @@
    src is treated as UTF-8 input, but max_bytes is in bytes instead of
    UTF-8 characters. */
 void str_sanitize_append(string_t *dest, const char *src, size_t max_bytes);
+/* All control characters in src will be appended as the unicode replacement
+   character (U+FFFD). If src has more than max_cps unicode code points, it's
+   truncated with a horizontal ellipsis character (U+2026) appended to the end.
+ */
+void str_sanitize_append_utf8(string_t *dest, const char *src,
+			      uintmax_t max_cps);
 /* Return src sanitized. If there are no changes, src pointer is returned.
    If src is NULL, returns NULL. */
 const char *str_sanitize(const char *src, size_t max_bytes);
+/* The unicode version of str_sanitize() using str_sanitize_append_utf8()
+   internally. */
+const char *str_sanitize_utf8(const char *src, uintmax_t max_cps);
 
 #endif
