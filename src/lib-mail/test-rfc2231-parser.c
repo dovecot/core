ce7821c83d60a1c7c64249972edbe243c978eeb6
Timo Sirainen <timo.sirainen@dovecot.fi>
2018-04-21 16:47:16 +0300

    lib-mail: rfc2231_parse() - Replace NULs with 0x80
    Instead of truncating the strings at NULs.

--- src/lib-mail/test-rfc2231-parser.c
+++ src/lib-mail/test-rfc2231-parser.c
@@ -7,18 +7,18 @@
 
 static void test_rfc2231_parser(void)
 {
-	const char *input =
+	const unsigned char input[] =
 		"; key4*=us-ascii''foo"
 		"; key*2=ba%"
 		"; key2*0=a"
 		"; key3*0*=us-ascii'en'xyz"
-		"; key*0=\"foo\""
+		"; key*0=\"f\0oo\""
 		"; key2*1*=b%25"
 		"; key3*1=plop%"
 		"; key*1=baz";
 	const char *output[] = {
 		"key",
-		"foobazba%",
+		"f\x80oobazba%",
 		"key2*",
 		"''ab%25",
 		"key3*",
@@ -32,10 +32,10 @@ static void test_rfc2231_parser(void)
 	unsigned int i;
 
 	test_begin("rfc2231 parser");
-	rfc822_parser_init(&parser, (const void *)input, strlen(input), NULL);
+	rfc822_parser_init(&parser, input, sizeof(input)-1, NULL);
 	test_assert(rfc2231_parse(&parser, &result) == 0);
 	for (i = 0; output[i] != NULL && result[i] != NULL; i++)
-		test_assert(strcmp(output[i], result[i]) == 0);
+		test_assert_idx(strcmp(output[i], result[i]) == 0, i);
 	rfc822_parser_deinit(&parser);
 	test_assert(output[i] == NULL && result[i] == NULL);
 	test_end();
