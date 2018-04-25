cb716afd3377dc18e0cafd149fa28a0669b2732e
Timo Sirainen <timo.sirainen@dovecot.fi>
2018-04-25 14:17:34 +0300

    lib-mail: Change NUL -> 0x80 replacement to use unicode replacement char instead
    Using 0x80 produces invalid UTF-8 output, which we should avoid. Some places
    in Dovecot code already requires that input is valid UTF-8, so it's also
    safer and easier to use the unicode replacement character.
    
    Fixes: Panic: file fts-tokenizer-generic.c: line 210 (fts_tokenizer_generic_simple_next): assertion failed: (char_size > 0)

--- src/lib-mail/rfc2231-parser.h
+++ src/lib-mail/rfc2231-parser.h
@@ -3,9 +3,9 @@
 
 /* Parse all content parameters using rfc822_parse_content_param() and return
    them as a NULL-terminated [key, value] array. RFC 2231-style continuations
-   are merged to a single key. NULs are converted into 0x80. Returns -1 if some
-   of the input was invalid (but valid key/value pairs are still returned), 0
-   if everything looked ok. */
+   are merged to a single key. NULs are converted into unicode replacement
+   character (U+FFFD). Returns -1 if some of the input was invalid (but valid
+   key/value pairs are still returned), 0 if everything looked ok. */
 int ATTR_NOWARN_UNUSED_RESULT
 rfc2231_parse(struct rfc822_parser_context *ctx,
 	      const char *const **result_r);
