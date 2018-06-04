dovecot/core

commit 7f7e7c16d956de8e92376a0633ca6e4cb85e8d84

quota-clone: Contexts are now required or checked
Satisfies static analyzers

src/plugins/quota-clone/quota-clone-plugin.c
@@ -17,9 +17,9 @@
 #define DICT_QUOTA_CLONE_COUNT_PATH DICT_QUOTA_CLONE_PATH"messages"
 
 #define QUOTA_CLONE_USER_CONTEXT(obj) \
-	MODULE_CONTEXT(obj, quota_clone_user_module)
+	MODULE_CONTEXT_REQUIRE(obj, quota_clone_user_module)
 #define QUOTA_CLONE_CONTEXT(obj) \
-	MODULE_CONTEXT(obj, quota_clone_storage_module)
+	MODULE_CONTEXT_REQUIRE(obj, quota_clone_storage_module)
 
 static MODULE_CONTEXT_DEFINE_INIT(quota_clone_user_module,
 				  &mail_user_module_register);

commit d7b5be845ba6375da02fbb56aa05f70976cd9b62

quota-clone: Fix CONTEXT_REQUIRE use
Fixes Panic: Module context quota_clone_user_module missing

Broken in 7f7e7c16d956de8e92376a0633ca6e4cb85e8d84

src/plugins/quota-clone/quota-clone-plugin.c
@@ -16,8 +16,10 @@
 #define DICT_QUOTA_CLONE_BYTES_PATH DICT_QUOTA_CLONE_PATH"storage"
 #define DICT_QUOTA_CLONE_COUNT_PATH DICT_QUOTA_CLONE_PATH"messages"
 
-#define QUOTA_CLONE_USER_CONTEXT(obj) \
+#define QUOTA_CLONE_USER_CONTEXT_REQUIRE(obj) \
 	MODULE_CONTEXT_REQUIRE(obj, quota_clone_user_module)
+#define QUOTA_CLONE_USER_CONTEXT(obj) \
+	MODULE_CONTEXT(obj, quota_clone_user_module)
 #define QUOTA_CLONE_CONTEXT(obj) \
 	MODULE_CONTEXT_REQUIRE(obj, quota_clone_storage_module)
 
@@ -42,7 +44,7 @@ static void quota_clone_flush_real(struct mailbox *box)
 {
 	struct quota_clone_mailbox *qbox = QUOTA_CLONE_CONTEXT(box);
 	struct quota_clone_user *quser =
-		QUOTA_CLONE_USER_CONTEXT(box->storage->user);
+		QUOTA_CLONE_USER_CONTEXT_REQUIRE(box->storage->user);
 	struct dict_transaction_context *trans;
 	struct quota_root_iter *iter;
 	struct quota_root *root;
@@ -114,7 +116,7 @@ static void quota_clone_flush(struct mailbox *box)
 {
 	struct quota_clone_mailbox *qbox = QUOTA_CLONE_CONTEXT(box);
 	struct quota_clone_user *quser =
-		QUOTA_CLONE_USER_CONTEXT(box->storage->user);
+		QUOTA_CLONE_USER_CONTEXT_REQUIRE(box->storage->user);
 
 	timeout_remove(&qbox->to_quota_flush);
 
@@ -202,7 +204,7 @@ static void quota_clone_mailbox_allocated(struct mailbox *box)
 
 static void quota_clone_mail_user_deinit(struct mail_user *user)
 {
-	struct quota_clone_user *quser = QUOTA_CLONE_USER_CONTEXT(user);
+	struct quota_clone_user *quser = QUOTA_CLONE_USER_CONTEXT_REQUIRE(user);
 
 	dict_deinit(&quser->dict);
 	quser->module_ctx.super.deinit(user);