3d17d17ba779a3cb8a4f9c9d07f71ab4d26421aa
Martti Rannanjärvi <martti.rannanjarvi@dovecot.fi>
2018-06-27 17:27:15 +0300

    fts-solr: Fix default_ns parameter parsing
    This version makes it possible to specify a value to the setting as
    well.

--- src/plugins/fts-solr/fts-solr-plugin.c
+++ src/plugins/fts-solr/fts-solr-plugin.c
@@ -32,7 +32,7 @@ fts_solr_plugin_init_settings(struct mail_user *user,
 			set->debug = TRUE;
 		} else if (strcmp(*tmp, "use_libfts") == 0) {
 			set->use_libfts = TRUE;
-		} else if (strcmp(*tmp, "default_ns=") == 0) {
+		} else if (str_begins(*tmp, "default_ns=")) {
 			set->default_ns_prefix =
 				p_strdup(user->pool, *tmp + 11);
 		} else {
