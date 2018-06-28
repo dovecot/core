43f8de9a9fe40a43ad963f645905280c2871ace1
Martti Rannanjärvi <martti.rannanjarvi@dovecot.fi>
2018-06-28 14:36:51 +0300

    fts-solr: Remove obsolete break-imap-search setting

--- src/plugins/fts-solr/fts-solr-plugin.c
+++ src/plugins/fts-solr/fts-solr-plugin.c
@@ -36,8 +36,6 @@ fts_solr_plugin_init_settings(struct mail_user *user,
 			set->debug = TRUE;
 		} else if (strcmp(*tmp, "use_libfts") == 0) {
 			set->use_libfts = TRUE;
-		} else if (strcmp(*tmp, "break-imap-search") == 0) {
-			/* for backwards compatibility */
 		} else if (str_begins(*tmp, "default_ns=")) {
 			set->default_ns_prefix =
 				p_strdup(user->pool, *tmp + 11);
