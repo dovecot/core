2dbb36ce557c2b7540d3fb8f1d88983cfedb3314
Stephan Bosch <stephan.bosch@dovecot.fi>
2018-07-15 19:03:09 +0200

    submission: Move relay part of DATA command to submission-backend-relay.c.

--- src/submission/submission-backend-relay.h
+++ src/submission/submission-backend-relay.h
@@ -8,5 +8,8 @@ int cmd_mail_relay(struct client *client, struct smtp_server_cmd_ctx *cmd,
 int cmd_rcpt_relay(struct client *client, struct smtp_server_cmd_ctx *cmd,
 		   struct smtp_server_cmd_rcpt *data);
 int cmd_rset_relay(struct client *client, struct smtp_server_cmd_ctx *cmd);
+int cmd_data_relay(struct client *client, struct smtp_server_cmd_ctx *cmd,
+		   struct smtp_server_transaction *trans,
+		   struct istream *data_input);
 
 #endif
