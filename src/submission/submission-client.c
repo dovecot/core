a47cf1f11bdd98b3d3fc2a187b1e20160cd3bdf2
Stephan Bosch <stephan.bosch@dovecot.fi>
2018-07-15 19:52:41 +0200

    submission: Remove client_proxy_is_ready().
    It is not used anywhere.

--- src/submission/submission-client.c
+++ src/submission/submission-client.c
@@ -415,12 +415,6 @@ void client_disconnect(struct client *client, const char *enh_code,
 	}
 }
 
-bool client_proxy_is_ready(struct client *client)
-{
-	return (smtp_client_connection_get_state(client->proxy_conn) ==
-		SMTP_CLIENT_CONNECTION_STATE_READY);
-}
-
 bool client_proxy_is_disconnected(struct client *client)
 {
 	return (smtp_client_connection_get_state(client->proxy_conn) ==
