aaedb98d65fe490689b35161e48c9f512df02e4d
Stephan Bosch <stephan.bosch@dovecot.fi>
2018-07-15 18:57:26 +0200

    submission: Move relay part of MAIL command to submission-backend-relay.c.

--- src/submission/submission-backend-relay.h
+++ src/submission/submission-backend-relay.h
@@ -3,5 +3,7 @@
 
 int cmd_helo_relay(struct client *client, struct smtp_server_cmd_ctx *cmd,
 		   struct smtp_server_cmd_helo *data);
+int cmd_mail_relay(struct client *client, struct smtp_server_cmd_ctx *cmd,
+		   struct smtp_server_cmd_mail *data);
 
 #endif
