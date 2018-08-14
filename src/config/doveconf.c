dovecot/core

commit 1518e085d8d88b6221627203f5a906be7873b31a

config: Move sensitive value hiding to own function
Simplifies next change

src/config/doveconf.c
@@ -168,6 +168,22 @@ static bool value_need_quote(const char *value)
 	return FALSE;
 }
 
+static bool
+hide_secrets_from_value(struct ostream *output, const char *key,
+			const char *value)
+{
+	if (*value != '\0' &&
+	    ((value-key > 8 && strncmp(value-9, "_password", 8) == 0) ||
+	     (value-key > 7 && strncmp(value-8, "_api_key", 7) == 0) ||
+	     strncmp(key, "ssl_key",7) == 0 ||
+	     strncmp(key, "ssl_dh",6) == 0)) {
+		o_stream_nsend_str(output, "# hidden, use -P to show it");
+		return TRUE;
+	}
+
+	return FALSE;
+}
+
 static int ATTR_NULL(4)
 config_dump_human_output(struct config_dump_human_context *ctx,
 			 struct ostream *output, unsigned int indent,
@@ -306,13 +322,11 @@ config_dump_human_output(struct config_dump_human_context *ctx,
 		i_assert(value != NULL);
 		o_stream_nsend(output, key, value-key);
 		o_stream_nsend_str(output, " = ");
-		if (hide_passwords && value[1] != '\0' &&
-		    ((value-key > 9 && strncmp(value-9, "_password", 9) == 0) ||
-		     (value-key > 8 && strncmp(value-8, "_api_key", 8) == 0) ||
-		     strncmp(key, "ssl_key",7) == 0 ||
-		     strncmp(key, "ssl_dh",6) == 0)) {
-			o_stream_nsend_str(output, " # hidden, use -P to show it");
-		} else if (!value_need_quote(value+1))
+		if (hide_passwords &&
+		    hide_secrets_from_value(output, key, value+1))
+			/* sent */
+			;
+		else if (!value_need_quote(value+1))
 			o_stream_nsend_str(output, value+1);
 		else {
 			o_stream_nsend(output, "\"", 1);

commit 970d8291c7ab22cea01e50ffb342780b8fe2b5f0

doveconf: Use key_ends_with to compare suffixes
Fixes key hiding.

Broken in 1518e085

src/config/doveconf.c
@@ -239,6 +239,14 @@ hide_url_userpart_from_value(struct ostream *output, const char **_ptr,
 	return TRUE;
 }
 
+static inline bool key_ends_with(const char *key, const char *eptr,
+				 const char *suffix)
+{
+	/* take = into account */
+	size_t n = strlen(suffix)+1;
+	return (eptr-key > (ptrdiff_t)n && str_begins(eptr-n, suffix));
+}
+
 static bool
 hide_secrets_from_value(struct ostream *output, const char *key,
 			const char *value)
@@ -246,8 +254,8 @@ hide_secrets_from_value(struct ostream *output, const char *key,
 	bool ret = FALSE, quote = value_need_quote(value);
 	const char *ptr, *optr, *secret;
 	if (*value != '\0' &&
-	    ((value-key > 8 && str_begins(value-9, "_password")) ||
-	     (value-key > 7 && str_begins(value-8, "_api_key")) ||
+	    (key_ends_with(key, value, "_password") ||
+	     key_ends_with(key, value, "_api_key") ||
 	     str_begins(key, "ssl_key") ||
 	     str_begins(key, "ssl_dh"))) {
 		o_stream_nsend_str(output, "# hidden, use -P to show it");