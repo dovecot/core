ca4ab3eb5a75bf670e6f82aa091c5e655cfaf094
Timo Sirainen <timo.sirainen@dovecot.fi>
2018-08-17 11:33:17 +0300

    lib: ioloop-epoll/kqueue: Remove now unnecessary infinite wait check.
    The previous change adds this globally to io_loop_run_get_wait_time().

--- src/lib/ioloop-epoll.c
+++ src/lib/ioloop-epoll.c
@@ -185,8 +185,7 @@ void io_loop_handler_run_internal(struct ioloop *ioloop)
 	} else {
 		/* no I/Os, but we should have some timeouts.
 		   just wait for them. */
-		if (msecs < 0)
-			i_panic("BUG: No IOs or timeouts set. Not waiting for infinity.");
+		i_assert(msecs >= 0);
 		usleep(msecs*1000);
 		ret = 0;
 	}
