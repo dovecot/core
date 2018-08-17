14d0377b8ee993f02f0736e96b865d6686e04817
Aki Tuomi <aki.tuomi@dovecot.fi>
2018-08-17 14:27:52 +0300

    lib-http: Hook lib-http event to connection event

--- src/lib-http/http-client-connection.c
+++ src/lib-http/http-client-connection.c
@@ -1620,6 +1620,7 @@ http_client_connection_tunnel_response(const struct http_response *response,
 
 	http_client_request_start_tunnel(req, &tunnel);
 
+	conn->conn.event_parent = conn->event;
 	connection_init_from_streams(cctx->conn_list, &conn->conn,
 				     name, tunnel.input, tunnel.output);
 	connection_switch_ioloop_to(&conn->conn, cctx->ioloop);
@@ -1701,6 +1702,7 @@ http_client_connection_create(struct http_client_peer *peer)
 	conn->label = i_strdup_printf("%s [%d]",
 		http_client_peer_shared_label(pshared), conn->id);
 	conn->event = event_create(peer->client->event);
+	conn->conn.event_parent = conn->event;
 	event_set_append_log_prefix(conn->event,
 		t_strdup_printf("conn %s: ", conn->label));
 
