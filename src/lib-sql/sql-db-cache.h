95b6a4ee2819279fc8306c44fb783ce5ee2aa631
Aki Tuomi <aki.tuomi@dovecot.fi>
2018-09-03 14:12:05 +0300

    lib-sql: Add sql_db_cache_new2 that uses the new sql_init_full initializer

--- src/lib-sql/sql-db-cache.h
+++ src/lib-sql/sql-db-cache.h
@@ -7,6 +7,8 @@ struct sql_db_cache;
 struct sql_db *
 sql_db_cache_new(struct sql_db_cache *cache,
 		 const char *db_driver, const char *connect_string);
+int sql_db_cache_new2(struct sql_db_cache *cache, const struct sql_settings *set,
+		      struct sql_db **db_r, const char **error_r);
 
 struct sql_db_cache *sql_db_cache_init(unsigned int max_unused_connections);
 void sql_db_cache_deinit(struct sql_db_cache **cache);
