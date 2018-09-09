f53bb2ac1cccc893d6cf07b59873b9df9a13a1e1
Stephan Bosch <stephan.bosch@dovecot.fi>
2018-09-10 01:07:58 +0200

    submission: Move relay part of VRFY command to submission-backend-relay.c.

--- src/submission/submission-backend-relay.h
+++ src/submission/submission-backend-relay.h
@@ -11,5 +11,7 @@ int cmd_rset_relay(struct client *client, struct smtp_server_cmd_ctx *cmd);
 int cmd_data_relay(struct client *client, struct smtp_server_cmd_ctx *cmd,
 		   struct smtp_server_transaction *trans,
 		   struct istream *data_input);
+int cmd_vrfy_relay(struct client *client, struct smtp_server_cmd_ctx *cmd,
+		   const char *param);
 
 #endif
--- src/submission/submission-commands.h
+++ src/submission/submission-commands.h
@@ -4,8 +4,6 @@
 bool client_command_handle_proxy_reply(struct client *client,
 	const struct smtp_reply *reply, struct smtp_reply *reply_r);
 
-int cmd_vrfy_relay(struct client *client, struct smtp_server_cmd_ctx *cmd,
-		   const char *param);
 int cmd_noop_relay(struct client *client, struct smtp_server_cmd_ctx *cmd);
 int cmd_quit_relay(struct client *client, struct smtp_server_cmd_ctx *cmd);
 
