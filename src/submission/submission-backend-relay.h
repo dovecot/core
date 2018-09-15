54dafcf1b6a8d29b640054498126279f8cb15e6d
Stephan Bosch <stephan.bosch@dovecot.fi>
2018-09-15 13:53:07 +0200

    submission: relay backend: Turn SMTP command functions into relay backend vfuncs.

--- src/submission/submission-backend-relay.h
+++ src/submission/submission-backend-relay.h
@@ -5,21 +5,6 @@
 
 struct client;
 
-int cmd_helo_relay(struct client *client, struct smtp_server_cmd_ctx *cmd,
-		   struct smtp_server_cmd_helo *data);
-int cmd_mail_relay(struct client *client, struct smtp_server_cmd_ctx *cmd,
-		   struct smtp_server_cmd_mail *data);
-int cmd_rcpt_relay(struct client *client, struct smtp_server_cmd_ctx *cmd,
-		   struct smtp_server_cmd_rcpt *data);
-int cmd_rset_relay(struct client *client, struct smtp_server_cmd_ctx *cmd);
-int cmd_data_relay(struct client *client, struct smtp_server_cmd_ctx *cmd,
-		   struct smtp_server_transaction *trans,
-		   struct istream *data_input);
-int cmd_vrfy_relay(struct client *client, struct smtp_server_cmd_ctx *cmd,
-		   const char *param);
-int cmd_noop_relay(struct client *client, struct smtp_server_cmd_ctx *cmd);
-int cmd_quit_relay(struct client *client, struct smtp_server_cmd_ctx *cmd);
-
 struct submission_settings;
 
 struct submission_backend_relay {
