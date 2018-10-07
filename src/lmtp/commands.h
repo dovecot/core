0de66ec4ed154c704cb9ae753c688878c55feec6
Stephan Bosch <stephan.bosch@dovecot.fi>
2018-10-07 18:17:28 +0200

    lmtp: Reformat commands.h.

--- src/lmtp/commands.h
+++ src/lmtp/commands.h
@@ -6,13 +6,13 @@ struct smtp_server_cmd_ctx;
 struct smtp_server_cmd_helo;
 
 int cmd_mail(void *conn_ctx, struct smtp_server_cmd_ctx *cmd,
-	struct smtp_server_cmd_mail *data);
+	     struct smtp_server_cmd_mail *data);
 int cmd_rcpt(void *conn_ctx, struct smtp_server_cmd_ctx *cmd,
-	struct smtp_server_cmd_rcpt *data);
+	     struct smtp_server_cmd_rcpt *data);
 int cmd_data_continue(void *conn_ctx, struct smtp_server_cmd_ctx *cmd,
-	struct smtp_server_transaction *trans);
-int cmd_data_begin(void *conn_ctx,
-	struct smtp_server_cmd_ctx *cmd ATTR_UNUSED,
-	struct smtp_server_transaction *trans, struct istream *data_input);
+		      struct smtp_server_transaction *trans);
+int cmd_data_begin(void *conn_ctx, struct smtp_server_cmd_ctx *cmd ATTR_UNUSED,
+		   struct smtp_server_transaction *trans,
+		   struct istream *data_input);
 
 #endif
