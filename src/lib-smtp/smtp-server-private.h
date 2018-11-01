ce9373c8dca5b265fb030ff08328f5fb17cda9d9
Stephan Bosch <stephan.bosch@dovecot.fi>
2018-11-01 01:25:10 +0100

    lib-smtp: server: recipient: Add reference counting.
    Unlike the server command, this is not strictly necessary for the recipient
    object, but we add this anyway to prevent future problems when the recipient
    implementation becomes more complex.

--- src/lib-smtp/smtp-server-private.h
+++ src/lib-smtp/smtp-server-private.h
@@ -113,6 +113,7 @@ struct smtp_server_command {
 
 struct smtp_server_recipient_private {
 	struct smtp_server_recipient rcpt;
+	int refcount;
 
 	struct smtp_server_recipient_hook *hooks_head, *hooks_tail;
 };
@@ -374,6 +375,8 @@ void smtp_server_connection_set_proxy_data(struct smtp_server_connection *conn,
 struct smtp_server_recipient *
 smtp_server_recipient_create(struct smtp_server_cmd_ctx *cmd,
 			     const struct smtp_address *rcpt_to);
+void smtp_server_recipient_ref(struct smtp_server_recipient *rcpt);
+bool smtp_server_recipient_unref(struct smtp_server_recipient **_rcpt);
 void smtp_server_recipient_destroy(struct smtp_server_recipient **_rcpt);
 
 void smtp_server_recipient_approved(struct smtp_server_recipient *rcpt);
