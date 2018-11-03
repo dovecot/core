08cb9acf351ad6d52099a5522c9337910b12192e
Stephan Bosch <stephan.bosch@dovecot.fi>
2018-11-03 01:00:13 +0100

    lmtp: Add support for module contexts to recipient.

--- src/lmtp/lmtp-recipient.h
+++ src/lmtp/lmtp-recipient.h
@@ -5,6 +5,7 @@ struct smtp_address;
 struct smtp_server_cmd_ctx;
 struct smtp_server_cmd_rcpt;
 struct smtp_server_recipient;
+union lmtp_recipient_module_context;
 struct client;
 
 enum lmtp_recipient_type {
@@ -18,7 +19,19 @@ struct lmtp_recipient {
 
 	enum lmtp_recipient_type type;
 	void *backend_context;
+
+	/* Module-specific contexts. */
+	ARRAY(union lmtp_recipient_module_context *) module_contexts;
 };
+
+struct lmtp_recipient_module_register {
+	unsigned int id;
+};
+
+union lmtp_recipient_module_context {
+	struct lmtp_recipient_module_register *reg;
+};
+extern struct lmtp_recipient_module_register lmtp_recipient_module_register;
 
 struct lmtp_recipient *
 lmtp_recipient_create(struct client *client,
