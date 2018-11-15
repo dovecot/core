9c588586857fcacf27c8fb6a4d438632d074ed80
Aki Tuomi <aki.tuomi@dovecot.fi>
2018-11-15 14:42:28 +0200

    lib-storage: mail-user - Use mail_storage_settings_init_ssl_client_settings

--- src/lib-storage/mail-user.c
+++ src/lib-storage/mail-user.c
@@ -694,19 +694,7 @@ void mail_user_init_ssl_client_settings(struct mail_user *user,
 	const struct mail_storage_settings *mail_set =
 		mail_user_set_get_storage_set(user);
 
-	ssl_set->ca_dir = mail_set->ssl_client_ca_dir;
-	ssl_set->ca_file = mail_set->ssl_client_ca_file;
-	if (*mail_set->ssl_client_cert != '\0')
-		ssl_set->cert.cert = mail_set->ssl_client_cert;
-	if (*mail_set->ssl_client_key != '\0')
-		ssl_set->cert.key = mail_set->ssl_client_key;
-	ssl_set->cipher_list = mail_set->ssl_cipher_list;
-	ssl_set->curve_list = mail_set->ssl_curve_list;
-	ssl_set->min_protocol = mail_set->ssl_min_protocol;
-	ssl_set->crypto_device = mail_set->ssl_crypto_device;
-	ssl_set->verify_remote_cert = mail_set->ssl_client_require_valid_cert;
-	ssl_set->allow_invalid_cert = !ssl_set->verify_remote_cert;
-	ssl_set->verbose = mail_set->verbose_ssl;
+	mail_storage_settings_init_ssl_client_settings(mail_set, ssl_set);
 }
 
 void mail_user_init_fs_settings(struct mail_user *user,
