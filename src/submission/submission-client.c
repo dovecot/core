9f210003665652a5382e7e4eb4b2c314386f95b5
Stephan Bosch <stephan.bosch@dovecot.fi>
2018-12-27 22:14:01 +0100

    submission: submission-client - Use client.event for SMTP server connection.

--- src/submission/submission-client.c
+++ src/submission/submission-client.c
@@ -217,6 +217,7 @@ client_create(int fd_in, int fd_out, struct event *event,
 	smtp_set.rawlog_dir = set->rawlog_dir;
 	smtp_set.no_greeting = no_greeting;
 	smtp_set.debug = user->mail_debug;
+	smtp_set.event_parent = event;
 
 	if ((workarounds & SUBMISSION_WORKAROUND_WHITESPACE_BEFORE_PATH) != 0) {
 		smtp_set.workarounds |=
