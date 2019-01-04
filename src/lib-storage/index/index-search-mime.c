d07a16d87e97642c9bacbee71d04efc0ffe11c9d
Aki Tuomi <aki.tuomi@dovecot.fi>
2019-01-04 15:16:44 +0200

    global: Use array_last instead of array_idx
    @@
    expression E;
    @@
    
    -array_idx(E, array_count(E) - 1)
    +array_last(E)
    
    @@
    expression E;
    @@
    
    -array_idx_modifiable(E, array_count(E) - 1)
    +array_last_modifiable(E)

--- src/lib-storage/index/index-search-mime.c
+++ src/lib-storage/index/index-search-mime.c
@@ -514,8 +514,7 @@ static int seach_arg_mime_parts_match(struct search_mimepart_context *mpctx,
 				if (part->parent == NULL)
 					break;
 				array_pop_back(&mpctx->stack);
-				level = array_idx_modifiable
-					(&mpctx->stack, array_count(&mpctx->stack)-1);
+				level = array_last_modifiable(&mpctx->stack);
 				part = part->parent;
 			}
 			level->index++;
