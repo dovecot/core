9c1f26dcf1b7fec7646531baa677449cda6630f0
Stephan Bosch <stephan.bosch@dovecot.fi>
2019-01-27 17:13:12 +0100

    lib-auth: Move struct auth_server_connection from auth-server-connection.h to auth-client-private.h.

--- src/lib-auth/auth-client-private.h
+++ src/lib-auth/auth-client-private.h
@@ -5,6 +5,33 @@
 
 #define AUTH_CONNECT_TIMEOUT_MSECS (30*1000)
 
+struct auth_server_connection {
+	pool_t pool;
+
+	struct auth_client *client;
+	int fd;
+	time_t last_connect;
+
+	struct io *io;
+	struct timeout *to;
+	struct istream *input;
+	struct ostream *output;
+
+	unsigned int server_pid;
+	unsigned int connect_uid;
+	char *cookie;
+
+	ARRAY(struct auth_mech_desc) available_auth_mechs;
+
+	/* id => request */
+	HASH_TABLE(void *, struct auth_client_request *) requests;
+
+	bool version_received:1;
+	bool handshake_received:1;
+	bool has_plain_mech:1;
+	bool connected:1;
+};
+
 struct auth_client {
 	char *auth_socket_path;
 	unsigned int client_pid;
