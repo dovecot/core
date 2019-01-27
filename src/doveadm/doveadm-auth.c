27010f3315f56b61aa54562b3a41b13362cb6a16
Stephan Bosch <stephan.bosch@dovecot.fi>
2019-01-27 16:59:32 +0100

    doveadm: doveadm-auth - Remove useless include for auth-server-connection.h.

--- src/doveadm/doveadm-auth.c
+++ src/doveadm/doveadm-auth.c
@@ -14,7 +14,6 @@
 #include "master-service-settings.h"
 #include "auth-client.h"
 #include "auth-master.h"
-#include "auth-server-connection.h"
 #include "master-auth.h"
 #include "master-login-auth.h"
 #include "mail-storage-service.h"
