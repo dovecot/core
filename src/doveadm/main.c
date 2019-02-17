4adda9a677d2c10afe6c7f7b48b5c4abc09f5f41
Aki Tuomi <aki.tuomi@open-xchange.com>
2019-02-17 23:02:16 +0200

    doveadm-server: Load SSL settings
    Otherwise operations that might use ssl can lead
    into invalid memory access.

--- src/doveadm/main.c
+++ src/doveadm/main.c
@@ -110,7 +110,8 @@ int main(int argc, char *argv[])
 		NULL
 	};
 	enum master_service_flags service_flags =
-		MASTER_SERVICE_FLAG_KEEP_CONFIG_OPEN;
+		MASTER_SERVICE_FLAG_KEEP_CONFIG_OPEN |
+		MASTER_SERVICE_FLAG_USE_SSL_SETTINGS;
 	struct master_service_settings_input input;
 	struct master_service_settings_output output;
 	const char *error;
