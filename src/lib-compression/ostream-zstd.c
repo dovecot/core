a3551d120e72d94061fb0e25eabe712863058bfe
Odin Hultgren Van Der Horst <odin@digitalgarden.no>
2019-03-05 13:39:51 +0100

    Mistaken level comparsion

--- src/lib-compression/ostream-zstd.c
+++ src/lib-compression/ostream-zstd.c
@@ -143,7 +143,7 @@ struct ostream *o_stream_create_zstd(struct ostream *output, int level)
 	zstream->output.dst = i_malloc(ZSTD_CStreamOutSize());
 	zstream->output.size = ZSTD_CStreamOutSize();
 	zstream->output.pos = 0;
-	i_assert(1 >= 1 && level <= ZSTD_maxCLevel());
+	i_assert(level >= 1 && level <= ZSTD_maxCLevel());
 
 	ZSTD_initCStream(zstream->cstream, level);
 	return o_stream_create(&zstream->ostream, output,
