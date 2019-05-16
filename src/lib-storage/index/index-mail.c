dovecot/core

commit 057028e3f5567e21c964005ffccda1055fa86508

lib-storage: Attachment flag detection shouldn't cause mail body to be opened
Fetching cached mail headers with
mail_attachment_detection_options=add-flags-on-save caued IMAP BODYSTRUCTURE
to be fetched as well. If it wasn't already cached, this caused opening the
mail body to generate it. Also the result wasn't added to cache, so the
inefficient behavior continued.

src/lib-storage/index/index-mail.c
@@ -1903,10 +1903,6 @@ void index_mail_update_access_parts_pre(struct mail *_mail)
 			data->save_sent_date = TRUE;
 		}
 	}
-	if (mail_set->parsed_mail_attachment_detection_add_flags_on_save) {
-		data->save_bodystructure_header = TRUE;
-		data->save_bodystructure_body = TRUE;
-	}
 	if ((data->wanted_fields & MAIL_FETCH_BODY_SNIPPET) != 0 &&
 	    (storage->nonbody_access_fields & MAIL_FETCH_BODY_SNIPPET) == 0) {
 		const unsigned int cache_field =
@@ -1924,6 +1920,19 @@ void index_mail_update_access_parts_pre(struct mail *_mail)
 			data->access_part |= READ_HDR;
 		if ((data->wanted_fields & MAIL_FETCH_STREAM_BODY) != 0)
 			data->access_part |= READ_BODY;
+	}
+
+	/* NOTE: Keep this attachment detection the last, so that the
+	   access_part check works correctly.
+
+	   The attachment flag detection is done while parsing BODYSTRUCTURE.
+	   We want to do this for mails that are being saved, but also when
+	   we need to open the mail body anyway. */
+	if (mail_set->parsed_mail_attachment_detection_add_flags_on_save &&
+	    (_mail->saving || data->access_part != 0) &&
+	    !mail_has_attachment_keywords(&mail->mail.mail)) {
+		data->save_bodystructure_header = TRUE;
+		data->save_bodystructure_body = TRUE;
 	}
 }
 

commit 7ceccd68bb49130e73e290eae4292cb0c76bff22

lib-storage: Don't try to set attachment keywords if bodystructure wasn't parsed
This was broken in 057028e3f5567e21c964005ffccda1055fa86508 as there are
code paths leading here that omit bodystructure processing.

Fixes Panic: file index-mail.c: line 1150 (index_mail_parse_body_finish): assertion failed: (mail->data.parsed_bodystructure)

src/lib-storage/index/index-mail.c
@@ -1145,9 +1145,9 @@ index_mail_parse_body_finish(struct index_mail *mail,
 	index_mail_cache_sizes(mail);
 	index_mail_cache_dates(mail);
 	if (mail_set->parsed_mail_attachment_detection_add_flags_on_save &&
+	    mail->data.parsed_bodystructure &&
 	    !mail_has_attachment_keywords(&mail->mail.mail)) {
 		i_assert(mail->data.parts != NULL);
-		i_assert(mail->data.parsed_bodystructure);
 		(void)mail_set_attachment_keywords(&mail->mail.mail);
 	}
 	return 0;