5dd539f61c5b9eac959466675a9baf5260d27178
Timo Sirainen <timo.sirainen@open-xchange.com>
2019-05-17 16:13:17 +0300

    lib: ostream-file - Ignore ENOPROTOOPT error also when setting TCP_NODELAY
    This happens in some OSes.

--- src/lib/ostream-file.c
+++ src/lib/ostream-file.c
@@ -333,7 +333,8 @@ static int buffer_flush(struct file_ostream *fstream)
 static void o_stream_tcp_flush_via_nodelay(struct file_ostream *fstream)
 {
 	if (net_set_tcp_nodelay(fstream->fd, TRUE) < 0) {
-		if (errno != ENOTSUP && errno != ENOTSOCK) {
+		if (errno != ENOTSUP && errno != ENOTSOCK &&
+		    errno != ENOPROTOOPT) {
 			i_error("file_ostream.net_set_tcp_nodelay(%s, TRUE) failed: %m",
 				o_stream_get_name(&fstream->ostream.ostream));
 		}
