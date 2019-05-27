dovecot/core

commit 5c1267b97367b666bd24e6aadfd85ad3902a5b05

doveadm service status: Support different number of fields returned by master

src/doveadm/doveadm-master.c
@@ -152,6 +152,7 @@ static void cmd_service_stop(struct doveadm_cmd_context *cctx)
 static void cmd_service_status(struct doveadm_cmd_context *cctx)
 {
 	const char *line, *const *services;
+	unsigned int fields_count;
 
 	if (!doveadm_cmd_param_array(cctx, "service", &services))
 		services = NULL;
@@ -171,6 +172,7 @@ static void cmd_service_status(struct doveadm_cmd_context *cctx)
 	doveadm_print_header_simple("listen_pending");
 	doveadm_print_header_simple("listening");
 	doveadm_print_header_simple("doveadm_stop");
+	fields_count = doveadm_print_get_headers_count();
 
 	alarm(5);
 	while ((line = i_stream_read_next_line(input)) != NULL) {
@@ -178,11 +180,14 @@ static void cmd_service_status(struct doveadm_cmd_context *cctx)
 			break;
 		T_BEGIN {
 			const char *const *args = t_strsplit_tabescaped(line);
-			if (str_array_length(args) >= 12 &&
+			if (args[0] != NULL &&
 			    (services == NULL ||
 			     str_array_find(services, args[0]))) {
-				for (unsigned int i = 0; i < 12; i++)
+				unsigned int i;
+				for (i = 0; i < fields_count && args[i] != NULL; i++)
 					doveadm_print(args[i]);
+				for (; i < fields_count; i++)
+					doveadm_print("");
 			}
 		} T_END;
 	}

commit 2148805a75abd86d1769d9fd20652551cc5c5ac2

doveadm service status: Handle VERSION handshake line properly
It was thought to be part of the command response, which caused somewhat
broken replies. Broken by 5c1267b97367b666bd24e6aadfd85ad3902a5b05

src/doveadm/doveadm-master.c
@@ -5,6 +5,7 @@
 #include "strescape.h"
 #include "istream.h"
 #include "write-full.h"
+#include "master-service.h"
 #include "doveadm.h"
 #include "doveadm-print.h"
 
@@ -91,7 +92,8 @@ static void cmd_reload(int argc ATTR_UNUSED, char *argv[] ATTR_UNUSED)
 
 static struct istream *master_service_send_cmd(const char *cmd)
 {
-	const char *path;
+	struct istream *input;
+	const char *path, *line;
 
 	path = t_strconcat(doveadm_settings->base_dir, "/master", NULL);
 	int fd = net_connect_unix(path);
@@ -102,8 +104,18 @@ static struct istream *master_service_send_cmd(const char *cmd)
 	const char *str =
 		t_strdup_printf("VERSION\tmaster-client\t1\t0\n%s\n", cmd);
 	if (write_full(fd, str, strlen(str)) < 0)
-		i_error("write(%s) failed: %m", path);
-	return i_stream_create_fd_autoclose(&fd, IO_BLOCK_SIZE);
+		i_fatal("write(%s) failed: %m", path);
+
+	input = i_stream_create_fd_autoclose(&fd, IO_BLOCK_SIZE);
+	alarm(5);
+	if ((line = i_stream_read_next_line(input)) == NULL)
+		i_fatal("read(%s) failed: %m", path);
+	if (!version_string_verify(line, "master-server", 1)) {
+		i_fatal_status(EX_PROTOCOL,
+			"%s is not a compatible master socket", path);
+	}
+	alarm(0);
+	return input;
 }
 
 static struct istream *