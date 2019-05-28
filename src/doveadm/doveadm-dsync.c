dovecot/core

commit 7f0dcac9942910c2934ceab1230e539043167601

doveadm: Add NOREPLICATE error when "noreplicate" user flag is used
It will be only understood if -U flag has been used

src/doveadm/doveadm-dsync.c
@@ -1154,6 +1154,11 @@ cmd_dsync_server_run(struct doveadm_mail_cmd_context *_ctx,
 	enum mail_error mail_error;
 
 	if (!cli) {
+		if (ctx->replicator_notify &&
+		    mail_user_plugin_getenv_bool(_ctx->cur_mail_user, "noreplicate")) {
+			return DOVEADM_EX_NOREPLICATE;
+		}
+
 		/* doveadm-server connection. start with a success reply.
 		   after that follows the regular dsync protocol. */
 		ctx->fd_in = ctx->fd_out = -1;

src/doveadm/client-connection-tcp.c
@@ -290,6 +290,8 @@ doveadm_mail_cmd_server_run(struct client_connection_tcp *conn,
 		o_stream_nsend(conn->output, "\n-\n", 3);
 	} else if (ret == 0) {
 		o_stream_nsend_str(conn->output, "\n-NOUSER\n");
+	} else if (mctx->exit_code == DOVEADM_EX_NOREPLICATE) {
+		o_stream_nsend_str(conn->output, "\n-NOREPLICATE\n");
 	} else if (mctx->exit_code != 0) {
 		/* maybe not an error, but not a full success either */
 		o_stream_nsend_str(conn->output,

src/doveadm/doveadm.h
@@ -11,6 +11,8 @@
 #define DOVEADM_EX_NOTPOSSIBLE EX_DATAERR
 #define DOVEADM_EX_UNKNOWN -1
 
+#define DOVEADM_EX_NOREPLICATE 1001
+
 enum doveadm_client_type {
 	DOVEADM_CONNECTION_TYPE_CLI = 0,
 	DOVEADM_CONNECTION_TYPE_TCP,

commit 6d5b4b51cdfaec39c9680ee8d96105f798e5e80c

doveadm-dsync: Set NOREPLICATE error code instead of returning it
This is how the error handling is supposed to be done.

Broken in 7f0dcac9942910c2934ceab1230e539043167601

src/doveadm/doveadm-dsync.c
@@ -1170,7 +1170,8 @@ cmd_dsync_server_run(struct doveadm_mail_cmd_context *_ctx,
 	if (!cli) {
 		if (ctx->replicator_notify &&
 		    mail_user_plugin_getenv_bool(_ctx->cur_mail_user, "noreplicate")) {
-			return DOVEADM_EX_NOREPLICATE;
+			_ctx->exit_code = DOVEADM_EX_NOREPLICATE;
+			return -1;
 		}
 
 		/* doveadm-server connection. start with a success reply.