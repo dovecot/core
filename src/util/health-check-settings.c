31361b44e5d079ab7e968a2ac86faefb5d42bdc1
Markus Valentin <markus.valentin@open-xchange.com>
2019-06-19 16:29:49 +0200

    script: health-check: add health-check.sh default script
    As a example for an simple tcp health check the health-check.sh script
    is added. It closes the connection after 10 seconds and only answer to
    "PING\n" with a "PONG\n". It is installed by default next to the script
    executable.

---
+++ src/util/health-check-settings.c
@@ -0,0 +1,31 @@
+/* Copyright (c) 2019 Dovecot authors, see the included COPYING file */
+
+#include "lib.h"
+#include "buffer.h"
+#include "settings-parser.h"
+#include "service-settings.h"
+
+struct service_settings health_check_service_settings = {
+	.name = "health-check",
+	.protocol = "",
+	.type = "",
+	.executable = "script -p health-check.sh",
+	.user = "$default_internal_user",
+	.group = "",
+	.privileged_group = "",
+	.extra_groups = "",
+	.chroot = "",
+
+	.drop_priv_before_exec = TRUE,
+
+	.process_min_avail = 0,
+	.process_limit = 0,
+	.client_limit = 1,
+	.service_count = 0,
+	.idle_kill = 0,
+	.vsz_limit = (uoff_t)-1,
+
+	.unix_listeners = ARRAY_INIT,
+	.fifo_listeners = ARRAY_INIT,
+	.inet_listeners = ARRAY_INIT
+};
