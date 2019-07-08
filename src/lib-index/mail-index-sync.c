dovecot/core

commit e31b0637d8788885a71db2def5743ebf14c698f3

lib-index: Compress cache immediately after enough mails have been expunged
This fixes a bug where cache may never become compressed in certain mailbox
access patterns. Especially for autoexpunging in lazy_expunge mailboxes.
The cache compression happened only when:

a) After the mailbox_sync() that finishes expunging there was another
mailbox_sync(). If mailbox was immediately closed after expunging, this
didn't happen.

b) Fields are fetched from cache

If neither of these happened, the cache just kept growing. This happened
especially with lazy_expunge mailboxes.

sdbox format was always doing b) during expunging, because it looked up
GUIDs from cache. However, this helped only with regular expunges, not
with autoexpunges, because autoexpunging didn't finish with any
mailbox_sync() (which is a bug on its own).

mdbox also did GUID lookups from cache, but it wasn't doing cache
compressions due the bug fixed by the previous commit.

src/lib-index/mail-index-sync.c
@@ -879,13 +879,6 @@ int mail_index_sync_commit(struct mail_index_sync_ctx **_ctx)
 	}
 
 	mail_index_sync_update_mailbox_offset(ctx);
-	if (mail_cache_need_compress(index->cache)) {
-		/* if cache compression fails, we don't really care.
-		   the cache offsets are updated only if the compression was
-		   successful. */
-		(void)mail_cache_compress(index->cache, ctx->ext_trans,
-					  &cache_lock);
-	}
 
 	if ((ctx->flags & MAIL_INDEX_SYNC_FLAG_DROP_RECENT) != 0) {
 		next_uid = mail_index_transaction_get_next_uid(ctx->ext_trans);
@@ -906,8 +899,6 @@ int mail_index_sync_commit(struct mail_index_sync_ctx **_ctx)
 	}
 
 	ret2 = mail_index_transaction_commit(&ctx->ext_trans);
-	if (cache_lock != NULL)
-		mail_cache_compress_unlock(&cache_lock);
 	if (ret2 < 0) {
 		mail_index_sync_end(&ctx);
 		return -1;
@@ -927,6 +918,27 @@ int mail_index_sync_commit(struct mail_index_sync_ctx **_ctx)
 	if (mail_index_map(ctx->index, MAIL_INDEX_SYNC_HANDLER_FILE) <= 0)
 		ret = -1;
 	index->sync_commit_result = NULL;
+
+	/* The previously called expunged handlers will update cache's
+	   record_count and deleted_record_count. That also has a side effect
+	   of updating whether cache needs to be compressed. */
+	if (ret == 0 && mail_cache_need_compress(index->cache)) {
+		struct mail_index_transaction *cache_trans;
+		enum mail_index_transaction_flags trans_flags;
+
+		trans_flags = MAIL_INDEX_TRANSACTION_FLAG_EXTERNAL;
+		if ((ctx->flags & MAIL_INDEX_SYNC_FLAG_FSYNC) != 0)
+			trans_flags |= MAIL_INDEX_TRANSACTION_FLAG_FSYNC;
+		cache_trans = mail_index_transaction_begin(ctx->view, trans_flags);
+		if (mail_cache_compress(index->cache, cache_trans,
+					&cache_lock) < 0)
+			mail_index_transaction_rollback(&cache_trans);
+		else {
+			/* can't really do anything if index commit fails */
+			(void)mail_index_transaction_commit(&cache_trans);
+			mail_cache_compress_unlock(&cache_lock);
+		}
+	}
 
 	want_rotate = mail_transaction_log_want_rotate(index->log);
 	if (ret == 0 &&

commit 46e93d9f13141877948ee6d3325147add3ad46b1

lib-index: After recreating cache, make sure offsets are immediately updated to map
They were most likely refreshed anyway before the next cache usage, but this
caused an assert-crash if the dovecot.index was also recreated in the same
sync.

Broken by e31b0637d8788885a71db2def5743ebf14c698f3

Fixes:
Panic: file mail-index-write.c: line 138 (mail_index_write): assertion failed: (file->hdr.prev_file_offset == hdr->log_file_head_offset)

src/lib-index/mail-index-sync.c
@@ -937,6 +937,15 @@ int mail_index_sync_commit(struct mail_index_sync_ctx **_ctx)
 			/* can't really do anything if index commit fails */
 			(void)mail_index_transaction_commit(&cache_trans);
 			mail_cache_compress_unlock(&cache_lock);
+			/* Make sure the newly committed cache record offsets
+			   are updated to the current index. This is important
+			   if the dovecot.index gets recreated below, because
+			   rotation of dovecot.index.log also re-maps the index
+			   to make sure everything is up-to-date. But if it
+			   wasn't, mail_index_write() will just assert-crash
+			   because log_file_head_offset changed. */
+			if (mail_index_map(ctx->index, MAIL_INDEX_SYNC_HANDLER_FILE) <= 0)
+				ret = -1;
 		}
 	}
 