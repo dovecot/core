dovecot/core

commit fc8c54c48fec6a89d9cda755756c233191c62af1

lib-dns: dns-lookup - Perform connection-related logging using the connection event.

src/lib-dns/dns-lookup.c
@@ -108,7 +108,7 @@ static void dns_client_disconnect(struct dns_client *client, const char *error)
 		dns_lookup_free(&lookup);
 		lookup = next;
 	}
-	e_debug(client->event, "Disconnect: %s", error);
+	e_debug(client->conn.event, "Disconnect: %s", error);
 }
 
 static void dns_client_destroy(struct connection *conn)

commit 3b7bb089d3cb82f9dbaeeb60f32e2e89620e1850

lib-dns: Log "Disconnect" event before calling DNS callbacks and freeing lookups
dns_lookup_free() could be freeing the dns_client, which causes a segfault
in the e_debug() log call.

Broken by fc8c54c48fec6a89d9cda755756c233191c62af1

src/lib-dns/dns-lookup.c
@@ -99,6 +99,7 @@ static void dns_client_disconnect(struct dns_client *client, const char *error)
 	i_zero(&result);
 	result.ret = EAI_FAIL;
 	result.error = error;
+	e_debug(client->conn.event, "Disconnect: %s", error);
 
 	lookup = client->head;
 	client->head = NULL;
@@ -108,7 +109,6 @@ static void dns_client_disconnect(struct dns_client *client, const char *error)
 		dns_lookup_free(&lookup);
 		lookup = next;
 	}
-	e_debug(client->conn.event, "Disconnect: %s", error);
 }
 
 static void dns_client_destroy(struct connection *conn)