dovecot/core

commit 90c2995737cc1f3fe042993beb7b0b32e5375795

lib-storage: mailbox_verify_name() - Move prefix check to its own function
No functional changes.

src/lib-storage/mail-storage.c
@@ -1032,6 +1032,44 @@ mailbox_name_verify_extra_separators(const char *vname, char sep,
 	return TRUE;
 }
 
+static bool mailbox_verify_name_prefix(struct mailbox *box)
+{
+	const char *vname = box->vname;
+	struct mail_namespace *ns = box->list->ns;
+
+	if (ns->prefix_len == 0)
+		return TRUE;
+
+	/* vname is either "namespace/box" or "namespace" */
+	if (strncmp(vname, ns->prefix, ns->prefix_len-1) != 0 ||
+	    (vname[ns->prefix_len-1] != '\0' &&
+	     vname[ns->prefix_len-1] != ns->prefix[ns->prefix_len-1])) {
+		/* User input shouldn't normally be able to get us in
+		   here. The main reason this isn't an assert is to
+		   allow any input at all to mailbox_verify_*_name()
+		   without crashing. */
+		mail_storage_set_error(box->storage, MAIL_ERROR_PARAMS,
+			t_strdup_printf("Invalid mailbox name '%s': "
+					"Missing namespace prefix '%s'",
+					str_sanitize(vname, 80), ns->prefix));
+		return FALSE;
+	}
+	vname += ns->prefix_len - 1;
+	if (vname[0] != '\0') {
+		i_assert(vname[0] == ns->prefix[ns->prefix_len-1]);
+		vname++;
+
+		if (vname[0] == '\0') {
+			/* "namespace/" isn't a valid mailbox name. */
+			mail_storage_set_error(box->storage,
+					       MAIL_ERROR_PARAMS,
+					       "Invalid mailbox name");
+			return FALSE;
+		}
+	}
+	return TRUE;
+}
+
 static int mailbox_verify_name(struct mailbox *box)
 {
 	struct mail_namespace *ns = box->list->ns;
@@ -1043,38 +1081,11 @@ static int mailbox_verify_name(struct mailbox *box)
 		return 0;
 	}
 
+	if (!mailbox_verify_name_prefix(box))
+		return -1;
+
 	list_sep = mailbox_list_get_hierarchy_sep(box->list);
 	ns_sep = mail_namespace_get_sep(ns);
-
-	if (ns->prefix_len > 0) {
-		/* vname is either "namespace/box" or "namespace" */
-		if (strncmp(vname, ns->prefix, ns->prefix_len-1) != 0 ||
-		    (vname[ns->prefix_len-1] != '\0' &&
-		     vname[ns->prefix_len-1] != ns->prefix[ns->prefix_len-1])) {
-			/* User input shouldn't normally be able to get us in
-			   here. The main reason this isn't an assert is to
-			   allow any input at all to mailbox_verify_*_name()
-			   without crashing. */
-			mail_storage_set_error(box->storage, MAIL_ERROR_PARAMS,
-				t_strdup_printf("Invalid mailbox name '%s': "
-					"Missing namespace prefix '%s'",
-					str_sanitize(vname, 80), ns->prefix));
-			return -1;
-		}
-		vname += ns->prefix_len - 1;
-		if (vname[0] != '\0') {
-			i_assert(vname[0] == ns->prefix[ns->prefix_len-1]);
-			vname++;
-
-			if (vname[0] == '\0') {
-				/* "namespace/" isn't a valid mailbox name. */
-				mail_storage_set_error(box->storage,
-						       MAIL_ERROR_PARAMS,
-						       "Invalid mailbox name");
-				return -1;
-			}
-		}
-	}
 
 	/* If namespace { separator } differs from the mailbox_list separator,
 	   the list separator can't actually be used in the mailbox name

commit 62f3b738efd4a6444a4bde4a80bea208c5b39ccd

lib-storage: Namespace prefix shouldn't be included in all mailbox name validity checks
Broken by 90c2995737cc1f3fe042993beb7b0b32e5375795

src/lib-storage/mail-storage.c
@@ -1040,10 +1040,10 @@ mailbox_name_verify_extra_separators(const char *vname, char sep,
 }
 
 static bool
-mailbox_verify_name_prefix(struct mailbox *box, const char **error_r)
+mailbox_verify_name_prefix(struct mail_namespace *ns, const char **vnamep,
+			   const char **error_r)
 {
-	const char *vname = box->vname;
-	struct mail_namespace *ns = box->list->ns;
+	const char *vname = *vnamep;
 
 	if (ns->prefix_len == 0)
 		return TRUE;
@@ -1071,6 +1071,7 @@ mailbox_verify_name_prefix(struct mailbox *box, const char **error_r)
 			return FALSE;
 		}
 	}
+	*vnamep = vname;
 	return TRUE;
 }
 
@@ -1085,7 +1086,9 @@ int mailbox_verify_name(struct mailbox *box)
 		return 0;
 	}
 
-	if (!mailbox_verify_name_prefix(box, &error)) {
+	/* Verify the namespace prefix here. Change vname to skip the prefix
+	   for the following checks. */
+	if (!mailbox_verify_name_prefix(box->list->ns, &vname, &error)) {
 		mail_storage_set_error(box->storage, MAIL_ERROR_PARAMS,
 			t_strdup_printf("Invalid mailbox name '%s': %s",
 					str_sanitize(vname, 80), error));