dovecot/core

commit 883a3022992d0f832f0aff21572caeb7c63b7668

lib-index: Fix calling expunge handlers with mdbox
This fixes updating deleted_record_count in dovecot.index.cache files.
Because they were wrong, the cache wasn't always compressed as early as
it should have been.

mdbox uses a separate transaction to commit expunges while mailbox is being
synced. When syncing was finished, tail_offset was updated too early so
mail_index_map(MAIL_INDEX_SYNC_HANDLER_FILE) was always a no-op and expunge
handlers were never called.

There doesn't seem to be any downside to not updating tail_offset early.
sdbox saving also uses such a transaction, but there is no difference in
the resulting dovecot.index.log file. The main worry I had was that
tail_offset wouldn't be updated to point to the end of the log file.
However, this doesn't happen with the old code either. This is because the
extra transaction is external, and tail_offset updating skips over all
external transactions anyway.

src/lib-index/mail-index-sync.c
@@ -789,9 +789,27 @@ mail_index_sync_update_mailbox_offset(struct mail_index_sync_ctx *ctx)
 		   tail offset. */
 		return;
 	}
-	/* synced everything, but we might also have committed new
-	   transactions. include them also here. */
-	mail_transaction_log_get_head(ctx->index->log, &seq, &offset);
+	/* All changes were synced. During the syncing other transactions may
+	   have been created and committed as well. They're expected to be
+	   external transactions. These could be at least:
+	    - mdbox finishing expunges
+	    - mdbox writing to dovecot.map.index (requires tail offset updates)
+	    - sdbox appending messages
+
+	   If any expunges were committed, tail_offset must not be updated
+	   before mail_index_map(MAIL_INDEX_SYNC_HANDLER_FILE) is called.
+	   Otherwise expunge handlers won't be called for them.
+
+	   We'll require MAIL_INDEX_SYNC_FLAG_UPDATE_TAIL_OFFSET flag for the
+	   few places that actually require tail_offset to include the
+	   externally committed transactions. Otherwise tail_offset is updated
+	   only up to what was just synced. */
+	if ((ctx->flags & MAIL_INDEX_SYNC_FLAG_UPDATE_TAIL_OFFSET) != 0)
+		mail_transaction_log_get_head(ctx->index->log, &seq, &offset);
+	else {
+		mail_transaction_log_view_get_prev_pos(ctx->view->log_view,
+						       &seq, &offset);
+	}
 	mail_transaction_log_set_mailbox_sync_pos(ctx->index->log, seq, offset);
 
 	/* If tail offset has changed, make sure it gets written to

commit 5e9e09a041b318025fd52db2df25052b60d0fc98

lib-index: Fix checking if log file can be rotated
This fixes a bug caused by 883a3022992d0f832f0aff21572caeb7c63b7668 where
dovecot.index.log file wasn't rotated as often as it was supposed to be. It
also could have caused dovecot.index to be rewritten much more often than
it was supposed to be.

The buggy commit was correct that the tail_offset wasn't updated previously
to the dovecot.index.log file after changes. However, I didn't realize that
the tail_offset was internally still updated and required by this rotation
check to work correctly.

The fix works by relaxing the tail_offset==head_offset requirement for
rotation. It's enough that all the changes are synced, which can be checked
from mail_index_sync_ctx.fully_synced. Any changes after tail_offset are
then supposed to be external transactions, which aren't synced anyway, so
rotation is allowed.

src/lib-index/mail-index-sync.c
@@ -949,7 +949,14 @@ int mail_index_sync_commit(struct mail_index_sync_ctx **_ctx)
 		}
 	}
 
-	want_rotate = mail_transaction_log_want_rotate(index->log);
+	/* Log rotation is allowed only if everything was synced. Note that
+	   tail_offset might not equal head_offset here, because
+	   mail_index_sync_update_mailbox_offset() doesn't always update
+	   tail_offset to skip over other committed external transactions.
+	   However, it's still safe to do the rotation because external
+	   transactions don't require syncing. */
+	want_rotate = ctx->fully_synced &&
+		mail_transaction_log_want_rotate(index->log);
 	if (ret == 0 &&
 	    (want_rotate || mail_index_sync_want_index_write(index))) {
 		index->need_recreate = FALSE;

src/lib-index/mail-index-write.c
@@ -126,10 +126,10 @@ void mail_index_write(struct mail_index *index, bool want_rotate)
 		return;
 
 	/* rotate the .log before writing index, so the index will point to
-	   the latest log. */
-	if (want_rotate &&
-	    hdr->log_file_seq == index->log->head->hdr.file_seq &&
-	    hdr->log_file_tail_offset == hdr->log_file_head_offset) {
+	   the latest log. Note that it's the caller's responsibility to make
+	   sure that the .log can be safely rotated (i.e. everything has been
+	   synced). */
+	if (want_rotate) {
 		if (mail_transaction_log_rotate(index->log, FALSE) == 0) {
 			struct mail_transaction_log_file *file =
 				index->log->head;