de800898419a993fb565ee7b4340026619156d53
Timo Sirainen <timo.sirainen@open-xchange.com>
2019-10-31 10:49:52 +0200

    lib-http: Add comments to http_client_request.*_time

--- src/lib-http/http-client-private.h
+++ src/lib-http/http-client-private.h
@@ -108,11 +108,22 @@ struct http_client_request {
 	uoff_t payload_size, payload_offset;
 	struct ostream *payload_output;
 
+	/* Time when request can be sent the next time. This is set by
+	   http_client_request_delay*(). Default is 0 = immediately. Retries
+	   can update this. */
 	struct timeval release_time;
+	/* Time when http_client_request_submit() was called. */
 	struct timeval submit_time;
+	/* Time when the request was first sent to the server. The HTTP
+	   connection already exists at this time. */
 	struct timeval first_sent_time;
+	/* Time when the request was last sent to the server (if it was
+	   retried). */
 	struct timeval sent_time;
+	/* Time when the HTTP response header was last received. */
 	struct timeval response_time;
+	/* Time when the request will be aborted. Set by
+	   http_client_request_set_timeout(). */
 	struct timeval timeout_time;
 	unsigned int timeout_msecs;
 	unsigned int attempt_timeout_msecs;
