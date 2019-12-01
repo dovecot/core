faf88a632b23258fd460c38a75722724dc9abbeb
Stephan Bosch <stephan.bosch@open-xchange.com>
2019-12-01 11:30:14 +0100

    plugins: push-notification: Reformat push-notification-events-rfc5423.h.

--- src/plugins/push-notification/push-notification-events-rfc5423.h
+++ src/plugins/push-notification/push-notification-events-rfc5423.h
@@ -3,10 +3,8 @@
 #ifndef PUSH_NOTIFICATION_EVENTS_RFC5423_H
 #define PUSH_NOTIFICATION_EVENTS_RFC5423_H
 
-
 void push_notification_event_register_rfc5423_events(void);
 void push_notification_event_unregister_rfc5423_events(void);
 
-
-#endif	/* PUSH_NOTIFICATION_EVENTS_H */
+#endif
 
