bea0b5e077f258c4cb98fab3c0b9f8f593007bdf
Stephan Bosch <stephan.bosch@open-xchange.com>
2019-12-01 11:28:27 +0100

    plugins: push-notification: Reformat push-notification-event-messageappend.h.

--- src/plugins/push-notification/push-notification-event-messageappend.h
+++ src/plugins/push-notification/push-notification-event-messageappend.h
@@ -3,30 +3,28 @@
 #ifndef PUSH_NOTIFICATION_EVENT_MESSAGEAPPEND_H
 #define PUSH_NOTIFICATION_EVENT_MESSAGEAPPEND_H
 
-
 struct push_notification_event_messageappend_config {
-    enum push_notification_event_message_flags flags;
+	enum push_notification_event_message_flags flags;
 };
 
 struct push_notification_event_messageappend_data {
-    const char *from;
-    const char *to;
-    const char *subject;
-    const char *snippet;
-    /* PUSH_NOTIFICATION_MESSAGE_HDR_DATE */
-    time_t date;
-    int date_tz;
-    /* PUSH_NOTIFICATION_MESSAGE_FLAGS */
-    bool flags_set;
-    enum mail_flags flags;
-    /* PUSH_NOTIFICATION_MESSAGE_KEYWORDS */
-    const char *const *keywords;
-    /* PUSH_NOTIFICATION_MESSAGE_HDR_MESSAGE_ID */
-    const char *message_id;
+	const char *from;
+	const char *to;
+	const char *subject;
+	const char *snippet;
+	/* PUSH_NOTIFICATION_MESSAGE_HDR_DATE */
+	time_t date;
+	int date_tz;
+	/* PUSH_NOTIFICATION_MESSAGE_FLAGS */
+	bool flags_set;
+	enum mail_flags flags;
+	/* PUSH_NOTIFICATION_MESSAGE_KEYWORDS */
+	const char *const *keywords;
+	/* PUSH_NOTIFICATION_MESSAGE_HDR_MESSAGE_ID */
+	const char *message_id;
 
-    struct push_notification_message_ext ext;
+	struct push_notification_message_ext ext;
 };
 
-
-#endif	/* PUSH_NOTIFICATION_EVENT_MESSAGEAPPEND_H */
+#endif
 
