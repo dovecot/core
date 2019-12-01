1b2a982ccedd410bdb8db75b19b62299a092d091
Stephan Bosch <stephan.bosch@open-xchange.com>
2019-12-01 11:25:31 +0100

    plugins: push-notification: Reformat push-notification-event-flagsset.h.

--- src/plugins/push-notification/push-notification-event-flagsset.h
+++ src/plugins/push-notification/push-notification-event-flagsset.h
@@ -3,24 +3,20 @@
 #ifndef PUSH_NOTIFICATION_EVENT_FLAGSSET_H
 #define PUSH_NOTIFICATION_EVENT_FLAGSSET_H
 
-
 #include "mail-types.h"
 
-
 struct push_notification_event_flagsset_config {
-    /* RFC 5423[4.2] - allow configuration whether FlagsSet event returns
-     * Deleted and/or Seen flags, since these flags are also settable
-     * via MessageRead/MessageTrash events. By default, include them
-     * here. */
-    bool hide_deleted;
-    bool hide_seen;
+	/* RFC 5423[4.2] - allow configuration whether FlagsSet event returns
+	   Deleted and/or Seen flags, since these flags are also settable via
+	   MessageRead/MessageTrash events. By default, include them here. */
+	bool hide_deleted;
+	bool hide_seen;
 };
 
 struct push_notification_event_flagsset_data {
-    enum mail_flags flags_set;
-    ARRAY_TYPE(keywords) keywords_set;
+	enum mail_flags flags_set;
+	ARRAY_TYPE(keywords) keywords_set;
 };
 
-
-#endif /* PUSH_NOTIFICATION_EVENT_FLAGSSET_H */
+#endif
 
