227aab7fbd4d47c1fd679ee482374673473b8628
Stephan Bosch <stephan.bosch@open-xchange.com>
2019-12-01 11:29:44 +0100

    plugins: push-notification: Reformat push-notification-event-messagetrash.h.

--- src/plugins/push-notification/push-notification-event-messagetrash.h
+++ src/plugins/push-notification/push-notification-event-messagetrash.h
@@ -3,12 +3,10 @@
 #ifndef PUSH_NOTIFICATION_EVENT_MESSAGETRASH_H
 #define PUSH_NOTIFICATION_EVENT_MESSAGETRASH_H
 
-
 struct push_notification_event_messagetrash_data {
-    /* Can only be true. */
-    bool trash;
+	/* Can only be TRUE. */
+	bool trash;
 };
 
-
-#endif /* PUSH_NOTIFICATION_EVENT_MESSAGETRASH_H */
+#endif
 
