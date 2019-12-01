5ac04c59503c5c2574066dcad6a3ae657d0d93ea
Stephan Bosch <stephan.bosch@open-xchange.com>
2019-12-01 11:25:55 +0100

    plugins: push-notification: Reformat push-notification-event-mailboxcreate.h.

--- src/plugins/push-notification/push-notification-event-mailboxcreate.h
+++ src/plugins/push-notification/push-notification-event-mailboxcreate.h
@@ -3,12 +3,10 @@
 #ifndef PUSH_NOTIFICATION_EVENT_MAILBOXCREATE_H
 #define PUSH_NOTIFICATION_EVENT_MAILBOXCREATE_H
 
-
 struct push_notification_event_mailboxcreate_data {
-    /* RFC 5423 [4.4]: UIDVALIDITY required for create event. */
-    uint32_t uid_validity;
+	/* RFC 5423 [4.4]: UIDVALIDITY required for create event. */
+	uint32_t uid_validity;
 };
 
-
-#endif /* PUSH_NOTIFICATION_EVENT_MAILBOXCREATE_H */
+#endif
 
