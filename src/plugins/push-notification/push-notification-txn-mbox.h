8f3f47510456a33339b3ff42ac7482a3ff8a215d
Stephan Bosch <stephan.bosch@open-xchange.com>
2019-12-01 11:32:09 +0100

    plugins: push-notification: Reformat push-notification-txn-mbox.h.

--- src/plugins/push-notification/push-notification-txn-mbox.h
+++ src/plugins/push-notification/push-notification-txn-mbox.h
@@ -3,32 +3,27 @@
 #ifndef PUSH_NOTIFICATION_TXN_MBOX_H
 #define PUSH_NOTIFICATION_TXN_MBOX_H
 
-
 struct push_notification_txn_event;
 
 struct push_notification_txn_mbox {
-    const char *mailbox;
+	const char *mailbox;
 
-    ARRAY(struct push_notification_txn_event *) eventdata;
+	ARRAY(struct push_notification_txn_event *) eventdata;
 };
-
 
 struct push_notification_txn_mbox *
 push_notification_txn_mbox_create(struct push_notification_txn *txn,
-                                  struct mailbox *box);
-void
-push_notification_txn_mbox_end(struct push_notification_txn *ptxn);
+				  struct mailbox *box);
+void push_notification_txn_mbox_end(struct push_notification_txn *ptxn);
 
 void *
-push_notification_txn_mbox_get_eventdata(struct push_notification_txn_mbox *mbox,
-                                         const char *event_name);
-void
-push_notification_txn_mbox_set_eventdata(struct push_notification_txn *txn,
-                                         struct push_notification_txn_mbox *mbox,
-                                         struct push_notification_event_config *event,
-                                         void *data);
-void
-push_notification_txn_mbox_deinit_eventdata(struct push_notification_txn_mbox *mbox);
+push_notification_txn_mbox_get_eventdata(
+	struct push_notification_txn_mbox *mbox, const char *event_name);
+void push_notification_txn_mbox_set_eventdata(
+	struct push_notification_txn *txn,
+	struct push_notification_txn_mbox *mbox,
+	struct push_notification_event_config *event, void *data);
+void push_notification_txn_mbox_deinit_eventdata(
+	struct push_notification_txn_mbox *mbox);
 
-
-#endif	/* PUSH_NOTIFICATION_TXN_MBOX_H */
+#endif
