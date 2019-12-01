568b0958f6e6a30698b3693e03bfba2bc0c9b2c4
Stephan Bosch <stephan.bosch@open-xchange.com>
2019-12-01 11:26:35 +0100

    plugins: push-notification: Reformat push-notification-event-mailboxrename.c.

--- src/plugins/push-notification/push-notification-event-mailboxrename.c
+++ src/plugins/push-notification/push-notification-event-mailboxrename.c
@@ -8,45 +8,43 @@
 #include "push-notification-event-mailboxrename.h"
 #include "push-notification-txn-mbox.h"
 
-
 #define EVENT_NAME "MailboxRename"
 
-
-static void push_notification_event_mailboxrename_debug_mbox
-(struct push_notification_txn_event *event)
+static void
+push_notification_event_mailboxrename_debug_mbox(
+	struct push_notification_txn_event *event)
 {
-    struct push_notification_event_mailboxrename_data *data = event->data;
+	struct push_notification_event_mailboxrename_data *data = event->data;
 
-    i_debug("%s: Mailbox was renamed (old name: %s)",
-            EVENT_NAME, data->old_mbox);
+	i_debug("%s: Mailbox was renamed (old name: %s)",
+		EVENT_NAME, data->old_mbox);
 }
 
-static void push_notification_event_mailboxrename_event(
-    struct push_notification_txn *ptxn,
-    struct push_notification_event_config *ec,
-    struct push_notification_txn_mbox *mbox,
-    struct mailbox *old)
+static void
+push_notification_event_mailboxrename_event(
+	struct push_notification_txn *ptxn,
+	struct push_notification_event_config *ec,
+	struct push_notification_txn_mbox *mbox, struct mailbox *old)
 {
-    struct push_notification_event_mailboxrename_data *data;
+	struct push_notification_event_mailboxrename_data *data;
 
-    data = p_new(ptxn->pool,
-                 struct push_notification_event_mailboxrename_data, 1);
-    data->old_mbox = mailbox_get_vname(old);
+	data = p_new(ptxn->pool,
+		     struct push_notification_event_mailboxrename_data, 1);
+	data->old_mbox = mailbox_get_vname(old);
 
-    push_notification_txn_mbox_set_eventdata(ptxn, mbox, ec, data);
+	push_notification_txn_mbox_set_eventdata(ptxn, mbox, ec, data);
 }
-
 
 /* Event definition */
 
 extern struct push_notification_event push_notification_event_mailboxrename;
 
 struct push_notification_event push_notification_event_mailboxrename = {
-    .name = EVENT_NAME,
-    .mbox = {
-        .debug_mbox = push_notification_event_mailboxrename_debug_mbox
-    },
-    .mbox_triggers = {
-        .rename = push_notification_event_mailboxrename_event
-    }
+	.name = EVENT_NAME,
+	.mbox = {
+		.debug_mbox = push_notification_event_mailboxrename_debug_mbox,
+	},
+	.mbox_triggers = {
+		.rename = push_notification_event_mailboxrename_event,
+	},
 };
