50e2ca4c2edd41097d997a1845302165de79120f
Stephan Bosch <stephan.bosch@open-xchange.com>
2019-12-01 11:29:10 +0100

    plugins: push-notification: Reformat push-notification-event-messagenew.h.

--- src/plugins/push-notification/push-notification-event-messagenew.h
+++ src/plugins/push-notification/push-notification-event-messagenew.h
@@ -3,37 +3,34 @@
 #ifndef PUSH_NOTIFICATION_EVENT_MESSAGENEW_H
 #define PUSH_NOTIFICATION_EVENT_MESSAGENEW_H
 
-
 #include "push-notification-event-message-common.h"
 
-
 struct push_notification_event_messagenew_config {
-    enum push_notification_event_message_flags flags;
+	enum push_notification_event_message_flags flags;
 };
 
 struct push_notification_event_messagenew_data {
-    /* PUSH_NOTIFICATION_MESSAGE_HDR_FROM */
-    const char *from;
-    /* PUSH_NOTIFICATION_MESSAGE_HDR_TO */
-    const char *to;
-    /* PUSH_NOTIFICATION_MESSAGE_HDR_SUBJECT */
-    const char *subject;
-    /* PUSH_NOTIFICATION_MESSAGE_HDR_DATE */
-    time_t date;
-    int date_tz;
-    /* PUSH_NOTIFICATION_MESSAGE_BODY_SNIPPET */
-    const char *snippet;
-    /* PUSH_NOTIFICATION_MESSAGE_FLAGS */
-    bool flags_set;
-    enum mail_flags flags;
-    /* PUSH_NOTIFICATION_MESSAGE_KEYWORDS */
-    const char *const *keywords;
-    /* PUSH_NOTIFICATION_MESSAGE_HDR_MESSAGE_ID */
-    const char *message_id;
+	/* PUSH_NOTIFICATION_MESSAGE_HDR_FROM */
+	const char *from;
+	/* PUSH_NOTIFICATION_MESSAGE_HDR_TO */
+	const char *to;
+	/* PUSH_NOTIFICATION_MESSAGE_HDR_SUBJECT */
+	const char *subject;
+	/* PUSH_NOTIFICATION_MESSAGE_HDR_DATE */
+	time_t date;
+	int date_tz;
+	/* PUSH_NOTIFICATION_MESSAGE_BODY_SNIPPET */
+	const char *snippet;
+	/* PUSH_NOTIFICATION_MESSAGE_FLAGS */
+	bool flags_set;
+	enum mail_flags flags;
+	/* PUSH_NOTIFICATION_MESSAGE_KEYWORDS */
+	const char *const *keywords;
+	/* PUSH_NOTIFICATION_MESSAGE_HDR_MESSAGE_ID */
+	const char *message_id;
 
-    struct push_notification_message_ext ext;
+	struct push_notification_message_ext ext;
 };
 
-
-#endif	/* PUSH_NOTIFICATION_EVENT_MESSAGENEW_H */
+#endif
 
