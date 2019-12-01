f479b6dbf9d8756d351e7e8fb82cc8371741947a
Stephan Bosch <stephan.bosch@open-xchange.com>
2019-12-01 11:28:07 +0100

    plugins: push-notification: Reformat push-notification-event-message-common.h.

--- src/plugins/push-notification/push-notification-event-message-common.h
+++ src/plugins/push-notification/push-notification-event-message-common.h
@@ -3,24 +3,23 @@
 #ifndef PUSH_NOTIFICATION_EVENT_MESSAGE_COMMON_H
 #define PUSH_NOTIFICATION_EVENT_MESSAGE_COMMON_H
 
-
 enum push_notification_event_message_flags {
-    /* Header: From */
-    PUSH_NOTIFICATION_MESSAGE_HDR_FROM = 0x01,
-    /* Header: To */
-    PUSH_NOTIFICATION_MESSAGE_HDR_TO = 0x02,
-    /* Header: Subject */
-    PUSH_NOTIFICATION_MESSAGE_HDR_SUBJECT = 0x04,
-    /* Header: Date */
-    PUSH_NOTIFICATION_MESSAGE_HDR_DATE = 0x08,
-    /* Body: Snippet */
-    PUSH_NOTIFICATION_MESSAGE_BODY_SNIPPET = 0x10,
-    /* Meta: Flags */
-    PUSH_NOTIFICATION_MESSAGE_FLAGS = 0x20,
-    /* Meta: Keywords */
-    PUSH_NOTIFICATION_MESSAGE_KEYWORDS = 0x40,
-    /* Header: Message-ID */
-    PUSH_NOTIFICATION_MESSAGE_HDR_MESSAGE_ID = 0x80,
+	/* Header: From */
+	PUSH_NOTIFICATION_MESSAGE_HDR_FROM = 0x01,
+	/* Header: To */
+	PUSH_NOTIFICATION_MESSAGE_HDR_TO = 0x02,
+	/* Header: Subject */
+	PUSH_NOTIFICATION_MESSAGE_HDR_SUBJECT = 0x04,
+	/* Header: Date */
+	PUSH_NOTIFICATION_MESSAGE_HDR_DATE = 0x08,
+	/* Body: Snippet */
+	PUSH_NOTIFICATION_MESSAGE_BODY_SNIPPET = 0x10,
+	/* Meta: Flags */
+	PUSH_NOTIFICATION_MESSAGE_FLAGS = 0x20,
+	/* Meta: Keywords */
+	PUSH_NOTIFICATION_MESSAGE_KEYWORDS = 0x40,
+	/* Header: Message-ID */
+	PUSH_NOTIFICATION_MESSAGE_HDR_MESSAGE_ID = 0x80,
 };
 
 struct push_notification_message_ext {
@@ -29,16 +28,13 @@ struct push_notification_message_ext {
 	const char *subject_utf8;
 };
 
-void push_notification_message_fill(struct mail *mail, pool_t pool,
-				    enum push_notification_event_message_flags event_flags,
-				    const char **from, const char **to,
-				    const char **subject,
-				    time_t *date, int *date_tz,
-				    const char **message_id,
-				    enum mail_flags *flags, bool *flags_set,
-				    const char *const **keywords,
-				    const char **snippet,
-				    struct push_notification_message_ext *ext);
+void push_notification_message_fill(
+	struct mail *mail, pool_t pool,
+	enum push_notification_event_message_flags event_flags,
+	const char **from, const char **to, const char **subject, time_t *date,
+	int *date_tz, const char **message_id, enum mail_flags *flags,
+	bool *flags_set, const char *const **keywords, const char **snippet,
+	struct push_notification_message_ext *ext);
 
-#endif	/* PUSH_NOTIFICATION_EVENT_MESSAGE_COMMON_H */
+#endif
 
