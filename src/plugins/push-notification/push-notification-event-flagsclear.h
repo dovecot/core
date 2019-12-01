1fa04f6f62d115f3000d12ec79281093eec54cae
Stephan Bosch <stephan.bosch@open-xchange.com>
2019-12-01 11:25:09 +0100

    plugins: push-notification: Reformat push-notification-event-flagsclear.h.

--- src/plugins/push-notification/push-notification-event-flagsclear.h
+++ src/plugins/push-notification/push-notification-event-flagsclear.h
@@ -3,23 +3,20 @@
 #ifndef PUSH_NOTIFICATION_EVENT_FLAGSCLEAR_H
 #define PUSH_NOTIFICATION_EVENT_FLAGSCLEAR_H
 
-
 #include "mail-types.h"
 
-
 struct push_notification_event_flagsclear_config {
-    /* Store the old flags/keywords? */
-    bool store_old;
+	/* Store the old flags/keywords? */
+	bool store_old;
 };
 
 struct push_notification_event_flagsclear_data {
-    enum mail_flags flags_clear;
-    ARRAY_TYPE(keywords) keywords_clear;
+	enum mail_flags flags_clear;
+	ARRAY_TYPE(keywords) keywords_clear;
 
-    enum mail_flags flags_old;
-    ARRAY_TYPE(keywords) keywords_old;
+	enum mail_flags flags_old;
+	ARRAY_TYPE(keywords) keywords_old;
 };
 
-
-#endif /* PUSH_NOTIFICATION_EVENT_FLAGSCLEAR_H */
+#endif
 
