24d2643140a7a7e285c6b6b81009ffd7be4cb737
Stephan Bosch <stephan.bosch@open-xchange.com>
2019-12-01 11:28:42 +0100

    plugins: push-notification: Reformat push-notification-event-messageexpunge.c.

--- src/plugins/push-notification/push-notification-event-messageexpunge.c
+++ src/plugins/push-notification/push-notification-event-messageexpunge.c
@@ -9,47 +9,45 @@
 #include "push-notification-events.h"
 #include "push-notification-txn-msg.h"
 
-
 #define EVENT_NAME "MessageExpunge"
 
-
-static void push_notification_event_messageexpunge_debug_msg
-(struct push_notification_txn_event *event)
+static void
+push_notification_event_messageexpunge_debug_msg(
+	struct push_notification_txn_event *event)
 {
-    struct push_notification_event_messageexpunge_data *data = event->data;
+	struct push_notification_event_messageexpunge_data *data = event->data;
 
-    if (data != NULL) {
-        i_debug("%s: Message was expunged", EVENT_NAME);
-    }
+	if (data != NULL)
+		i_debug("%s: Message was expunged", EVENT_NAME);
 }
 
-static void push_notification_event_messageexpunge_event(
-    struct push_notification_txn *ptxn,
-    struct push_notification_event_config *ec,
-    struct push_notification_txn_msg *msg)
+static void
+push_notification_event_messageexpunge_event(
+	struct push_notification_txn *ptxn,
+	struct push_notification_event_config *ec,
+	struct push_notification_txn_msg *msg)
 {
-    struct push_notification_event_messageexpunge_data *data;
+	struct push_notification_event_messageexpunge_data *data;
 
-    data = push_notification_txn_msg_get_eventdata(msg, EVENT_NAME);
-    if (data == NULL) {
-        data = p_new(ptxn->pool,
-                     struct push_notification_event_messageexpunge_data, 1);
-        data->expunge = TRUE;
-        push_notification_txn_msg_set_eventdata(ptxn, msg, ec, data);
-    }
+	data = push_notification_txn_msg_get_eventdata(msg, EVENT_NAME);
+	if (data == NULL) {
+		data = p_new(ptxn->pool,
+			     struct push_notification_event_messageexpunge_data, 1);
+		data->expunge = TRUE;
+		push_notification_txn_msg_set_eventdata(ptxn, msg, ec, data);
+	}
 }
-
 
 /* Event definition */
 
 extern struct push_notification_event push_notification_event_messageexpunge;
 
 struct push_notification_event push_notification_event_messageexpunge = {
-    .name = EVENT_NAME,
-    .msg = {
-        .debug_msg = push_notification_event_messageexpunge_debug_msg
-    },
-    .msg_triggers = {
-        .expunge = push_notification_event_messageexpunge_event
-    }
+	.name = EVENT_NAME,
+	.msg = {
+		.debug_msg = push_notification_event_messageexpunge_debug_msg,
+	},
+	.msg_triggers = {
+		.expunge = push_notification_event_messageexpunge_event,
+	},
 };
