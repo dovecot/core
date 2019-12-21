ede279a11165049004f4b52fc6cfba576e3e773f
Nick <nick@kousu.ca>
2019-12-20 21:33:47 -0500

    Let bsdauth choose peoples' names.
    This is key when using the name:style partial hack that authenticate(3)
    supports: a user can login as 'johhny:yubikey' and pass their yubikey
    one time password in the password field and it will Just Work on OpenBSD,
    assuming johhny has a yubikey configured in their account. That's cool,
    but dovecot would then go looking for a user named 'johhny:yubikey'
    and fail, even though auth_userokay() passed.

--- src/auth/passdb-bsdauth.c
+++ src/auth/passdb-bsdauth.c
@@ -18,20 +18,38 @@ bsdauth_verify_plain(struct auth_request *request, const char *password,
 		    verify_plain_callback_t *callback)
 {
 	const char *type;
-	int result;
+	auth_session_t *result;
 
 	e_debug(authdb_event(request), "lookup");
 
 	/* check if the auth is valid */
 	type = t_strdup_printf("auth-%s", request->service);
-	result = auth_userokay(request->user, NULL, t_strdup_noconst(type),
+	result = auth_usercheck(request->user, NULL, t_strdup_noconst(type),
 			       t_strdup_noconst(password));
 
-	if (result == 0) {
+	if (auth_getstate(result) == 0) {
 		auth_request_log_password_mismatch(request, AUTH_SUBSYS_DB);
 		callback(PASSDB_RESULT_PASSWORD_MISMATCH, request);
 		return;
 	}
+
+	/* make sure we're using the username as defined by the database.
+	 * On OpenBSD, auth "styles" will translate a request->user of "name:style"
+	 * to "name" authenticated via "style"; or the default auth style on a system
+	 * might do something unusual to map any input to any username it wants.
+	 * See authenticate(3).
+	 */
+	if(auth_getpwd(result) == NULL) {
+		/* FIXME: internal error?
+		 * It would be strange but not impossible to
+		 * accept a user with missing account details.
+		 */
+		auth_request_set_field(request, "user", NULL, NULL);
+	} else {
+		auth_request_set_field(request, "user", auth_getpwd(result)->pw_name, NULL);
+	}
+
+	auth_close(result); // should equal the earlier auth_getstate()
 
 	callback(PASSDB_RESULT_OK, request);
 }
