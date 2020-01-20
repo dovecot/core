dovecot/core

commit f7aafd832151dd34e28cb1bba214accf9b15d908

lib-fs: Don't log "Asynchronous operation in progress" debug messages

src/lib-fs/fs-api.c
@@ -560,7 +560,10 @@ fs_set_verror(struct event *event, const char *fmt, va_list args)
 	}
 
 	char *new_error = i_strdup_vprintf(fmt, args);
-	e_debug(event, "%s", new_error);
+	/* Don't flood the debug log with "Asynchronous operation in progress"
+	   messages. They tell nothing useful. */
+	if (errno != EAGAIN)
+		e_debug(event, "%s", new_error);
 
 	/* free old error after strdup in case args point to the old error */
 	if (file != NULL) {

commit a333079a815583c478c943a68ccb32d27b350797

lib-fs: Fix event leak when EAGAIN error is logged via fs_set_error()
Broken by f7aafd832151dd34e28cb1bba214accf9b15d908

src/lib-fs/fs-api.c
@@ -564,6 +564,8 @@ fs_set_verror(struct event *event, const char *fmt, va_list args)
 	   messages. They tell nothing useful. */
 	if (errno != EAGAIN)
 		e_debug(event, "%s", new_error);
+	else
+		event_send_abort(event);
 
 	/* free old error after strdup in case args point to the old error */
 	if (file != NULL) {