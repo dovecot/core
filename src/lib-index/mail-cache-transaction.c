dovecot/core

commit a4671d9c5744d728e030370a39df914cebefbefe

lib-index: Try to compress cache when it's about to become too large

src/lib-index/mail-cache-transaction.c
@@ -254,9 +254,20 @@ mail_cache_transaction_open_if_needed(struct mail_cache_transaction_ctx *ctx)
 static int mail_cache_transaction_lock(struct mail_cache_transaction_ctx *ctx)
 {
 	struct mail_cache *cache = ctx->cache;
+	const uoff_t cache_max_size =
+		cache->index->optimization_set.cache.max_size;
 	int ret;
 
 	mail_cache_transaction_open_if_needed(ctx);
+
+	if (!ctx->tried_compression && ctx->cache_data != NULL &&
+	    cache->last_stat_size + ctx->cache_data->used >= cache_max_size) {
+		/* Looks like cache file is becoming too large. Try to compress
+		   it to free up some space. */
+		if (cache->hdr->continued_record_count > 0 ||
+		    cache->hdr->deleted_record_count > 0)
+			(void)mail_cache_transaction_compress(ctx);
+	}
 
 	if ((ret = mail_cache_lock(cache)) <= 0) {
 		if (ret < 0)

commit eaec3d6eb86d84a1b51b4ac7a33618f6c7cad68e

lib-index: Fix potential crash when writing to unusable dovecot.index.cache
Broken by a4671d9c5744d728e030370a39df914cebefbefe

src/lib-index/mail-cache-transaction.c
@@ -264,8 +264,9 @@ static int mail_cache_transaction_lock(struct mail_cache_transaction_ctx *ctx)
 	    cache->last_stat_size + ctx->cache_data->used >= cache_max_size) {
 		/* Looks like cache file is becoming too large. Try to compress
 		   it to free up some space. */
-		if (cache->hdr->continued_record_count > 0 ||
-		    cache->hdr->deleted_record_count > 0)
+		if (!MAIL_CACHE_IS_UNUSABLE(cache) &&
+		    (cache->hdr->continued_record_count > 0 ||
+		     cache->hdr->deleted_record_count > 0))
 			(void)mail_cache_transaction_compress(ctx);
 	}
 