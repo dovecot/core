9f42630b22092de7f3aee7eb04bca1064f32c870
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-03-17 09:41:10 +0100

    lib-smtp: smtp-server-command - Change log label for invalid/unknown command to "[unknown]".

--- src/lib-smtp/smtp-server-command.c
+++ src/lib-smtp/smtp-server-command.c
@@ -131,7 +131,7 @@ smtp_server_command_update_event(struct smtp_server_command *cmd)
 {
 	struct event *event = cmd->context.event;
 	const char *label = (cmd->context.name == NULL ?
-			     "[INVALID]" :
+			     "[unknown]" :
 			     t_str_ucase(cmd->context.name));
 
 	if (cmd->reg != NULL)
