e9b76d9065715cc344b70bfcbe03b238b2e121f6
Timo Sirainen <timo.sirainen@open-xchange.com>
2020-03-24 22:23:19 +0200

    lib-index: unit tests: Split off test_mail_index_close/delete()

--- src/lib-index/test-mail-index.h
+++ src/lib-index/test-mail-index.h
@@ -29,13 +29,23 @@ static inline struct mail_index *test_mail_index_init(void)
 	return test_mail_index_open();
 }
 
-static inline void test_mail_index_deinit(struct mail_index **index)
+static inline void test_mail_index_close(struct mail_index **index)
+{
+	mail_index_close(*index);
+	mail_index_free(index);
+}
+
+static inline void test_mail_index_delete(void)
 {
 	const char *error;
 
-	mail_index_close(*index);
-	mail_index_free(index);
 	(void)unlink_directory(TESTDIR_NAME, UNLINK_DIRECTORY_FLAG_RMDIR, &error);
+}
+
+static inline void test_mail_index_deinit(struct mail_index **index)
+{
+	test_mail_index_close(index);
+	test_mail_index_delete();
 }
 
 #endif
