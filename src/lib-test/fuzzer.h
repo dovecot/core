fb70a8197f109f6c84bf17b78210884e5c4ffc2d
Timo Sirainen <timo.sirainen@open-xchange.com>
2020-03-26 10:55:56 +0200

    lib-test: Fix FUZZ_BEGIN_DATA() to actually work

--- src/lib-test/fuzzer.h
+++ src/lib-test/fuzzer.h
@@ -1,9 +1,10 @@
 #ifndef FUZZER_H
 #define FUZZER_H
 
-#define FUZZ_BEGIN_DATA(args) \
-	int LLVMFuzzerTestOneInput(args); \
-	int LLVMFuzzerTestOneInput(args) {
+#define FUZZ_BEGIN_DATA(data_arg, size_arg) \
+	int LLVMFuzzerTestOneInput(data_arg, size_arg); \
+	int LLVMFuzzerTestOneInput(data_arg, size_arg) { \
+		T_BEGIN {
 
 #define FUZZ_BEGIN_STR(str_arg) \
 	int LLVMFuzzerTestOneInput(const uint8_t *_param_data, size_t _param_size); \
