dovecot/core

commit 23cfbf2451f7191dee1d9995011c898a7bf32bd4

lib-program-client: Use event logging.
This also makes the overall log messages better match similar messages produced
by other parts of Dovecot.

src/lib-program-client/program-client-private.h
@@ -32,9 +32,10 @@ struct program_client {
 	pool_t pool;
 	struct program_client_settings set;
 
-	char *path;
 	const char **args;
 	ARRAY_TYPE(const_string) envs;
+
+	struct event *event;
 
 	int fd_in, fd_out;
 	struct io *io;
@@ -67,8 +68,11 @@ struct program_client {
 	bool destroying:1;
 };
 
+void program_client_set_label(struct program_client *pclient,
+			      const char *label);
+
 void program_client_init(struct program_client *pclient, pool_t pool,
-			 const char *path,
+			 const char *initial_label,
 			 const char *const *args,
 			 const struct program_client_settings *set);
 

src/lib-program-client/program-client.c
@@ -20,6 +20,13 @@
 #define MAX_OUTPUT_BUFFER_SIZE 16384
 #define MAX_OUTPUT_MEMORY_BUFFER (1024*128)
 
+void program_client_set_label(struct program_client *pclient,
+			      const char *label)
+{
+	event_set_append_log_prefix(pclient->event,
+		t_strconcat("program ", label, ": ", NULL));
+}
+
 static void
 program_client_callback(struct program_client *pclient, int result,
 			void *context)
@@ -35,16 +42,18 @@ program_client_callback(struct program_client *pclient, int result,
 static void
 program_client_timeout(struct program_client *pclient)
 {
-	i_error("program `%s' execution timed out (> %u msecs)",
-		pclient->path, pclient->set.input_idle_timeout_msecs);
+	e_error(pclient->event,
+		"Execution timed out (> %u msecs)",
+		pclient->set.input_idle_timeout_msecs);
 	program_client_fail(pclient, PROGRAM_CLIENT_ERROR_RUN_TIMEOUT);
 }
 
 static void
 program_client_connect_timeout(struct program_client *pclient)
 {
-	i_error("program `%s' socket connection timed out (> %u msecs)",
-		pclient->path, pclient->set.client_connect_timeout_msecs);
+	e_error(pclient->event,
+		"Connection timed out (> %u msecs)",
+		pclient->set.client_connect_timeout_msecs);
 	program_client_fail(pclient, PROGRAM_CLIENT_ERROR_CONNECT_TIMEOUT);
 }
 
@@ -207,7 +216,8 @@ program_client_output_finish(struct program_client *pclient)
 
 	/* flush the output */
 	if ((ret=o_stream_finish(output)) < 0) {
-		i_error("write(%s) failed: %s",
+		e_error(pclient->event,
+			"write(%s) failed: %s",
 			o_stream_get_name(output),
 			o_stream_get_error(output));
 		program_client_fail(pclient, PROGRAM_CLIENT_ERROR_IO);
@@ -232,13 +242,15 @@ program_client_output_pump_finished(enum iostream_pump_status status,
 	case IOSTREAM_PUMP_STATUS_INPUT_EOF:
 		break;
 	case IOSTREAM_PUMP_STATUS_INPUT_ERROR:
-		i_error("read(%s) failed: %s",
+		e_error(pclient->event,
+			"read(%s) failed: %s",
 			i_stream_get_name(input),
 			i_stream_get_error(input));
 		program_client_fail(pclient, PROGRAM_CLIENT_ERROR_IO);
 		return;
 	case IOSTREAM_PUMP_STATUS_OUTPUT_ERROR:
-		i_error("write(%s) failed: %s",
+		e_error(pclient->event,
+			"write(%s) failed: %s",
 			o_stream_get_name(output),
 			o_stream_get_error(output));
 		program_client_fail(pclient, PROGRAM_CLIENT_ERROR_IO);
@@ -278,7 +290,8 @@ program_client_input_finish(struct program_client *pclient)
 		return;
 	if (ret < 0) {
 		if (input->stream_errno != 0) {
-			i_error("read(%s) failed: %s",
+			e_error(pclient->event,
+				"read(%s) failed: %s",
 				i_stream_get_name(input),
 				i_stream_get_error(input));
 			program_client_fail(pclient,
@@ -317,13 +330,15 @@ program_client_input_pump_finished(enum iostream_pump_status status,
 	case IOSTREAM_PUMP_STATUS_INPUT_EOF:
 		break;
 	case IOSTREAM_PUMP_STATUS_INPUT_ERROR:
-		i_error("read(%s) failed: %s",
+		e_error(pclient->event,
+			"read(%s) failed: %s",
 			i_stream_get_name(input),
 			i_stream_get_error(input));
 		program_client_fail(pclient, PROGRAM_CLIENT_ERROR_IO);
 		return;
 	case IOSTREAM_PUMP_STATUS_OUTPUT_ERROR:
-		i_error("write(%s) failed: %s",
+		e_error(pclient->event,
+			"write(%s) failed: %s",
 			o_stream_get_name(output),
 			o_stream_get_error(output));
 		program_client_fail(pclient, PROGRAM_CLIENT_ERROR_IO);
@@ -428,18 +443,21 @@ void program_client_connected(struct program_client *pclient)
 }
 
 void program_client_init(struct program_client *pclient, pool_t pool,
-			 const char *path,
-			 const char *const *args,
+			 const char *initial_label, const char *const *args,
 			 const struct program_client_settings *set)
 {
 	pclient->pool = pool;
-	pclient->path = p_strdup(pool, path);
 	if (args != NULL)
 		pclient->args = p_strarray_dup(pool, args);
 	pclient->set = *set;
 	pclient->debug = set->debug;
 	pclient->fd_in = -1;
 	pclient->fd_out = -1;
+
+	pclient->event = event_create(set->event);
+	if ((set != NULL && set->debug))
+		event_set_forced_debug(pclient->event, TRUE);
+	program_client_set_label(pclient, initial_label);
 }
 
 void program_client_set_input(struct program_client *pclient,
@@ -584,6 +602,8 @@ void program_client_destroy(struct program_client **_pclient)
 
 	if (pclient->destroy != NULL)
 		pclient->destroy(pclient);
+
+	event_unref(&pclient->event);
 
 	pool_unref(&pclient->pool);
 }

src/lib-program-client/program-client.h
@@ -16,6 +16,9 @@ struct program_client_settings {
 	const char *dns_client_socket_path;
 	const char *home;
 
+	/* Event to use for the program client. */
+	struct event *event;
+
 	bool allow_root:1;
 	bool debug:1;
 	bool drop_stderr:1;

src/lib-program-client/program-client-local.c
@@ -29,6 +29,8 @@ struct program_client_local {
 	struct child_wait *child_wait;
 	struct timeout *to_kill;
 
+	char *bin_path;
+
 	pid_t pid;
 	int status;
 	bool exited:1;
@@ -59,20 +61,22 @@ exec_child(const char *bin_path, const char *const *args,
 		out_fd = dev_null_fd;
 
 	if (in_fd != STDIN_FILENO && dup2(in_fd, STDIN_FILENO) < 0)
-		i_fatal("dup2(stdin) failed: %m");
+		i_fatal("program %s: dup2(stdin) failed: %m", bin_path);
 	if (out_fd != STDOUT_FILENO && dup2(out_fd, STDOUT_FILENO) < 0)
-		i_fatal("dup2(stdout) failed: %m");
+		i_fatal("program %s: dup2(stdout) failed: %m", bin_path);
 
 	if (in_fd != STDIN_FILENO && in_fd != dev_null_fd && close(in_fd) < 0)
-		i_error("close(in_fd) failed: %m");
+		i_error("program %s: close(in_fd) failed: %m", bin_path);
 	if (out_fd != STDOUT_FILENO && out_fd != dev_null_fd &&
 	    (out_fd != in_fd) && close(out_fd) < 0)
-		i_error("close(out_fd) failed: %m");
+		i_error("program %s: close(out_fd) failed: %m", bin_path);
 
 	/* Drop stderr if requested */
 	if (drop_stderr) {
-		if (dup2(dev_null_fd, STDERR_FILENO) < 0)
-			i_fatal("dup2(stderr) failed: %m");
+		if (dup2(dev_null_fd, STDERR_FILENO) < 0) {
+			i_fatal("program %s: "
+				"dup2(stderr) failed: %m", bin_path);
+		}
 	}
 
 	/* Setup extra fds */
@@ -83,18 +87,22 @@ exec_child(const char *bin_path, const char *const *args,
 			i_assert(efd[1] != STDOUT_FILENO);
 			i_assert(efd[1] != STDERR_FILENO);
 			if (efd[0] != efd[1]) {
-				if (dup2(efd[0], efd[1]) < 0)
-					i_fatal("dup2(extra_fd=%d) failed: %m",
-						efd[1]);
+				if (dup2(efd[0], efd[1]) < 0) {
+					i_fatal("program %s"
+						"dup2(extra_fd=%d) failed: %m",
+						bin_path, efd[1]);
+				}
 			}
 		}
 		for(efd = extra_fds; *efd != -1; efd += 2) {
 			if (efd[0] != efd[1] && efd[0] != STDIN_FILENO &&
 			    efd[0] != STDOUT_FILENO &&
 			    efd[0] != STDERR_FILENO) {
-				if (close(efd[0]) < 0)
-					i_error("close(extra_fd=%d) failed: %m",
-						efd[1]);
+				if (close(efd[0]) < 0) {
+					i_error("program %s"
+						"close(extra_fd=%d) failed: %m",
+						bin_path, efd[1]);
+				}
 			}
 		}
 	}
@@ -156,13 +164,13 @@ program_client_local_connect(struct program_client *pclient)
 	/* create normal I/O fds */
 	if (pclient->input != NULL) {
 		if (pipe(fd_in) < 0) {
-			i_error("pipe(in) failed: %m");
+			e_error(pclient->event, "pipe(in) failed: %m");
 			return -1;
 		}
 	}
 	if (pclient->output != NULL) {
 		if (pipe(fd_out) < 0) {
-			i_error("pipe(out) failed: %m");
+			e_error(pclient->event, "pipe(out) failed: %m");
 			return -1;
 		}
 	}
@@ -178,7 +186,8 @@ program_client_local_connect(struct program_client *pclient)
 			child_extra_fds = t_new(int, xfd_count * 2 + 1);
 			for(i = 0; i < xfd_count; i++) {
 				if (pipe(extra_fd) < 0) {
-					i_error("pipe(extra=%d) failed: %m",
+					e_error(pclient->event,
+						"pipe(extra=%d) failed: %m",
 						extra_fd[1]);
 					return -1;
 				}
@@ -192,28 +201,34 @@ program_client_local_connect(struct program_client *pclient)
 
 	/* fork child */
 	if ((plclient->pid = fork()) == (pid_t)-1) {
-		i_error("fork() failed: %m");
+		e_error(pclient->event, "fork() failed: %m");
 
 		/* clean up */
 		if (fd_in[0] >= 0 && close(fd_in[0]) < 0) {
-			i_error("close(pipe:in:rd) failed: %m");
+			e_error(pclient->event,
+				"close(pipe:in:rd) failed: %m");
 		}
 		if (fd_in[1] >= 0 && close(fd_in[1]) < 0) {
-			i_error("close(pipe:in:wr) failed: %m");
+			e_error(pclient->event,
+				"close(pipe:in:wr) failed: %m");
 		}
 		if (fd_out[0] >= 0 && close(fd_out[0]) < 0) {
-			i_error("close(pipe:out:rd) failed: %m");
+			e_error(pclient->event,
+				"close(pipe:out:rd) failed: %m");
 		}
 		if (fd_out[1] >= 0 && close(fd_out[1]) < 0) {
-			i_error("close(pipe:out:wr) failed: %m");
+			e_error(pclient->event,
+				"close(pipe:out:wr) failed: %m");
 		}
 		for(i = 0; i < xfd_count; i++) {
 			if (close(child_extra_fds[i * 2]) < 0) {
-				i_error("close(pipe:extra=%d:wr) failed: %m",
+				e_error(pclient->event,
+					"close(pipe:extra=%d:wr) failed: %m",
 					child_extra_fds[i * 2 + 1]);
 			}
 			if (close(parent_extra_fds[i]) < 0) {
-				i_error("close(pipe:extra=%d:rd) failed: %m",
+				e_error(pclient->event,
+					"close(pipe:extra=%d:rd) failed: %m",
 					child_extra_fds[i * 2 + 1]);
 			}
 		}
@@ -222,13 +237,18 @@ program_client_local_connect(struct program_client *pclient)
 
 	if (plclient->pid == 0) {
 		/* child */
-		if (fd_in[1] >= 0 && close(fd_in[1]) < 0)
-			i_error("close(pipe:in:wr) failed: %m");
-		if (fd_out[0] >= 0 && close(fd_out[0]) < 0)
-			i_error("close(pipe:out:rd) failed: %m");
+		if (fd_in[1] >= 0 && close(fd_in[1]) < 0) {
+			e_error(pclient->event,
+				"close(pipe:in:wr) failed: %m");
+		}
+		if (fd_out[0] >= 0 && close(fd_out[0]) < 0) {
+			e_error(pclient->event,
+				"close(pipe:out:rd) failed: %m");
+		}
 		for(i = 0; i < xfd_count; i++) {
 			if (close(parent_extra_fds[i]) < 0) {
-				i_error("close(pipe:extra=%d:rd) failed: %m",
+				e_error(pclient->event,
+					"close(pipe:extra=%d:rd) failed: %m",
 					child_extra_fds[i * 2 + 1]);
 			}
 		}
@@ -240,17 +260,23 @@ program_client_local_connect(struct program_client *pclient)
 					RESTRICT_ACCESS_FLAG_ALLOW_ROOT : 0),
 				pclient->set.home);
 
-		exec_child(pclient->path, pclient->args, &pclient->envs,
+		exec_child(plclient->bin_path, pclient->args, &pclient->envs,
 			   fd_in[0], fd_out[1], child_extra_fds,
 			   pclient->set.drop_stderr);
 		i_unreached();
 	}
 
 	/* parent */
-	if (fd_in[0] >= 0 && close(fd_in[0]) < 0)
-		i_error("close(pipe:in:rd) failed: %m");
-	if (fd_out[1] >= 0 && close(fd_out[1]) < 0)
-		i_error("close(pipe:out:wr) failed: %m");
+	program_client_set_label(pclient,
+		t_strdup_printf("exec:%s (%d)", plclient->bin_path,
+				plclient->pid));
+
+	if (fd_in[0] >= 0 && close(fd_in[0]) < 0) {
+		e_error(pclient->event, "close(pipe:in:rd) failed: %m");
+	}
+	if (fd_out[1] >= 0 && close(fd_out[1]) < 0) {
+		e_error(pclient->event, "close(pipe:out:wr) failed: %m");
+	}
 	if (fd_in[1] >= 0) {
 		net_set_nonblock(fd_in[1], TRUE);
 		pclient->fd_out = fd_in[1];
@@ -261,7 +287,8 @@ program_client_local_connect(struct program_client *pclient)
 	}
 	for(i = 0; i < xfd_count; i++) {
 		if (close(child_extra_fds[i * 2]) < 0) {
-			i_error("close(pipe:extra=%d:wr) failed: %m",
+			e_error(pclient->event,
+				"close(pipe:extra=%d:wr) failed: %m",
 				child_extra_fds[i * 2 + 1]);
 		}
 		net_set_nonblock(parent_extra_fds[i], TRUE);
@@ -287,7 +314,8 @@ program_client_local_close_output(struct program_client *pclient)
 
 	/* Shutdown output; program stdin will get EOF */
 	if (fd_out >= 0 && close(fd_out) < 0) {
-		i_error("close(%s) failed: %m", pclient->path);
+		e_error(pclient->event,
+			"close(fd_out) failed: %m");
 		return -1;
 	}
 	return 1;
@@ -312,8 +340,9 @@ program_client_local_exited(struct program_client_local *plclient)
 		int exit_code = WEXITSTATUS(plclient->status);
 
 		if (exit_code != 0) {
-			i_info("program `%s' terminated with non-zero exit code %d",
-			       pclient->path, exit_code);
+			e_info(pclient->event,
+			       "Terminated with non-zero exit code %d",
+			       exit_code);
 			pclient->exit_code = PROGRAM_CLIENT_EXIT_FAILURE;
 		} else {
 			pclient->exit_code = PROGRAM_CLIENT_EXIT_SUCCESS;
@@ -321,20 +350,24 @@ program_client_local_exited(struct program_client_local *plclient)
 	} else if (WIFSIGNALED(plclient->status)) {
 		/* Killed with a signal */
 		if (plclient->sent_term) {
-			i_error("program `%s' was forcibly terminated with signal %d",
-				pclient->path, WTERMSIG(plclient->status));
+			e_error(pclient->event,
+				"Forcibly terminated with signal %d",
+				WTERMSIG(plclient->status));
 		} else {
-			i_error("program `%s' terminated abnormally, signal %d",
-				pclient->path, WTERMSIG(plclient->status));
+			e_error(pclient->event,
+				"Terminated abnormally with signal %d",
+				WTERMSIG(plclient->status));
 		}
 	} else if (WIFSTOPPED(plclient->status)) {
 		/* Stopped */
-		i_error("program `%s' stopped, signal %d",
-			pclient->path, WSTOPSIG(plclient->status));
+		e_error(pclient->event,
+			"Stopped with signal %d",
+			WSTOPSIG(plclient->status));
 	} else {
 		/* Something else */
-		i_error("program `%s' terminated abnormally, return status %d",
-			pclient->path, plclient->status);
+		e_error(pclient->event,
+			"Terminated abnormally with status %d",
+			plclient->status);
 	}
 
 	program_client_disconnected(pclient);
@@ -343,6 +376,8 @@ program_client_local_exited(struct program_client_local *plclient)
 static void
 program_client_local_kill_now(struct program_client_local *plclient)
 {
+	struct program_client *pclient = &plclient->client;
+
 	if (plclient->child_wait != NULL) {
 		/* no need for this anymore */
 		child_wait_free(&plclient->child_wait);
@@ -353,17 +388,19 @@ program_client_local_kill_now(struct program_client_local *plclient)
 
 	/* kill it brutally now: it should die right away */
 	if (kill(plclient->pid, SIGKILL) < 0) {
-		i_error("failed to send SIGKILL signal to program `%s'",
-			plclient->client.path);
+		e_error(pclient->event,
+			"Failed to send SIGKILL signal to program");
 	} else if (waitpid(plclient->pid, &plclient->status, 0) < 0) {
-		i_error("waitpid(%s) failed: %m",
-			plclient->client.path);
+		e_error(pclient->event, "waitpid(%d) failed: %m",
+			plclient->pid);
 	}
 }
 
 static void
 program_client_local_kill(struct program_client_local *plclient)
 {
+	struct program_client *pclient = &plclient->client;
+
 	/* time to die */
 	timeout_remove(&plclient->to_kill);
 
@@ -374,29 +411,27 @@ program_client_local_kill(struct program_client_local *plclient)
 
 	if (plclient->sent_term) {
 		/* Timed out again */
-		if (plclient->client.debug) {
-			i_debug("program `%s' (%d) did not die after %d milliseconds: "
-				"sending KILL signal",
-				plclient->client.path, plclient->pid, KILL_TIMEOUT);
-		}
+		e_debug(pclient->event,
+			"Program did not die after %d milliseconds",
+			KILL_TIMEOUT);
 
 		program_client_local_kill_now(plclient);
 		program_client_local_exited(plclient);
 		return;
 	}
 
-	if (plclient->client.debug)
-		i_debug("program `%s'(%d) execution timed out after %u milliseconds: "
-			"sending TERM signal", plclient->client.path, plclient->pid,
-			plclient->client.set.input_idle_timeout_msecs);
+	e_debug(pclient->event,
+		"Execution timed out after %u milliseconds: "
+		"Sending TERM signal",
+		pclient->set.input_idle_timeout_msecs);
 
 	/* send sigterm, keep on waiting */
 	plclient->sent_term = TRUE;
 
 	/* Kill child gently first */
 	if (kill(plclient->pid, SIGTERM) < 0) {
-		i_error("failed to send SIGTERM signal to program `%s'",
-			plclient->client.path);
+		e_error(pclient->event,
+			"Failed to send SIGTERM signal to program");
 		(void)kill(plclient->pid, SIGKILL);
 		program_client_local_exited(plclient);
 		return;
@@ -443,10 +478,8 @@ program_client_local_disconnect(struct program_client *pclient, bool force)
 	    runtime < pclient->set.input_idle_timeout_msecs)
 		timeout = pclient->set.input_idle_timeout_msecs - runtime;
 
-	if (pclient->debug) {
-		i_debug("waiting for program `%s' to finish after %lu msecs",
-			pclient->path, runtime);
-	}
+	e_debug(pclient->event,
+		"Waiting for program to finish after %lu msecs", runtime);
 
 	force = force ||
 		(timeout == 0 && pclient->set.input_idle_timeout_msecs > 0);
@@ -492,16 +525,20 @@ program_client_local_create(const char *bin_path,
 			    const struct program_client_settings *set)
 {
 	struct program_client_local *plclient;
+	const char *label;
 	pool_t pool;
+
+	label = t_strconcat("exec:", bin_path, NULL);
 
 	pool = pool_alloconly_create("program client local", 1024);
 	plclient = p_new(pool, struct program_client_local, 1);
-	program_client_init(&plclient->client, pool, bin_path, args, set);
+	program_client_init(&plclient->client, pool, label, args, set);
 	plclient->client.connect = program_client_local_connect;
 	plclient->client.close_output = program_client_local_close_output;
 	plclient->client.switch_ioloop = program_client_local_switch_ioloop;
 	plclient->client.disconnect = program_client_local_disconnect;
 	plclient->client.destroy = program_client_local_destroy;
+	plclient->bin_path = p_strdup(pool, bin_path);
 	plclient->pid = -1;
 
 	child_wait_init();

commit 63741ac523771a0819b7b694b700861ace22ef08

lib-program-client: Do not derefence set when it is NULL.
Found by coverity. Broken in 23cfbf2451f7191dee1d9995011c898a7bf32bd4

src/lib-program-client/program-client-private.h
@@ -74,7 +74,8 @@ void program_client_set_label(struct program_client *pclient,
 void program_client_init(struct program_client *pclient, pool_t pool,
 			 const char *initial_label,
 			 const char *const *args,
-			 const struct program_client_settings *set);
+			 const struct program_client_settings *set)
+			 ATTR_NULL(5);
 
 void program_client_init_streams(struct program_client *pclient);
 

src/lib-program-client/program-client.c
@@ -463,20 +463,25 @@ void program_client_init(struct program_client *pclient, pool_t pool,
 			 const char *initial_label, const char *const *args,
 			 const struct program_client_settings *set)
 {
-	i_assert(set != NULL);
 	pclient->pool = pool;
 	if (args != NULL)
 		pclient->args = p_strarray_dup(pool, args);
-	pclient->set = *set;
-	pclient->set.dns_client_socket_path =
-		p_strdup(pool, set->dns_client_socket_path);
-	pclient->set.home = p_strdup(pool, set->home);
-	pclient->debug = set->debug;
 	pclient->fd_in = -1;
 	pclient->fd_out = -1;
 
-	pclient->event = event_create(set->event);
-	event_set_forced_debug(pclient->event, set->debug);
+	if (set == NULL)
+		pclient->event = event_create(NULL);
+	else {
+		pclient->set = *set;
+		pclient->debug = set->debug;
+		pclient->set.dns_client_socket_path =
+			p_strdup(pool, set->dns_client_socket_path);
+		pclient->set.home = p_strdup(pool, set->home);
+
+		pclient->event = event_create(set->event);
+		event_set_forced_debug(pclient->event, set->debug);
+	}
+
 	program_client_set_label(pclient, initial_label);
 
 	e_debug(pclient->event, "Created");

src/lib-program-client/program-client.h
@@ -33,24 +33,28 @@ struct program_client_settings {
 typedef void program_client_fd_callback_t(void *context, struct istream *input);
 typedef void program_client_callback_t(int, void *);
 
-struct program_client *program_client_local_create(const char *bin_path,
-	const char *const *args,
-	const struct program_client_settings *set);
-struct program_client *program_client_unix_create(const char *socket_path,
-	const char *const *args,
-	const struct program_client_settings *set, bool noreply);
-struct program_client *program_client_net_create(const char *host,
-	in_port_t port, const char *const *args,
-	const struct program_client_settings *set, bool noreply);
+struct program_client *
+program_client_local_create(const char *bin_path, const char *const *args,
+			    const struct program_client_settings *set)
+			    ATTR_NULL(3);
+struct program_client *
+program_client_unix_create(const char *socket_path, const char *const *args,
+			   const struct program_client_settings *set,
+			   bool noreply) ATTR_NULL(3);
+struct program_client *
+program_client_net_create(const char *host, in_port_t port,
+			  const char *const *args,
+			  const struct program_client_settings *set,
+			  bool noreply) ATTR_NULL(4);
 struct program_client *
 program_client_net_create_ips(const struct ip_addr *ips, size_t ips_count,
 			      in_port_t port, const char *const *args,
 			      const struct program_client_settings *set,
-			      bool noreply);
+			      bool noreply) ATTR_NULL(5);
 int program_client_create(const char *uri, const char *const *args,
 			  const struct program_client_settings *set,
 			  bool noreply, struct program_client **pc_r,
-			  const char **error_r);
+			  const char **error_r) ATTR_NULL(3);
 
 void program_client_destroy(struct program_client **_pclient);
 