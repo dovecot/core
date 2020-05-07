faebf72dfa664db2c809f5ef7f9c5046dd4f94e6
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-05-07 12:31:53 +0200

    auth: Reformat auth-policy.h

--- src/auth/auth-policy.h
+++ src/auth/auth-policy.h
@@ -3,7 +3,8 @@
 
 typedef void (*auth_policy_callback_t)(int, void *);
 
-void auth_policy_check(struct auth_request *request, const char *password, auth_policy_callback_t cb, void *context);
+void auth_policy_check(struct auth_request *request, const char *password,
+		       auth_policy_callback_t cb, void *context);
 void auth_policy_report(struct auth_request *request);
 void auth_policy_init(void);
 void auth_policy_deinit(void);
