b196135db141a4c4614dbc1ad354b63eb73edaa4
Aki Tuomi <aki.tuomi@open-xchange.com>
2020-06-03 15:40:40 +0300

    lib-oauth2: Use azp to find token
    This validates that the token is actually for us and also allows
    having multiple tokens with same ID but different issuer.

--- src/lib-oauth2/test-oauth2-jwt.c
+++ src/lib-oauth2/test-oauth2-jwt.c
@@ -211,7 +211,7 @@ static void save_key_to(const char *algo, const char *name, const char *keydata)
 {
 	const char *error;
 	struct dict_transaction_context *ctx = dict_transaction_begin(keys_dict);
-	dict_set(ctx, t_strconcat(DICT_PATH_SHARED, algo, "/", name, NULL), keydata);
+	dict_set(ctx, t_strconcat(DICT_PATH_SHARED, "default/", algo, "/", name, NULL), keydata);
 	if (dict_transaction_commit(&ctx, &error) < 0)
 		i_error("dict_set(%s) failed: %s", name, error);
 }
