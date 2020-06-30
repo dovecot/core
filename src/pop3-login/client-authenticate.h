d8c3cfca79fdcb044d3760f0b09f463598f63deb
Aki Tuomi <aki.tuomi@open-xchange.com>
2020-06-30 14:02:34 +0300

    pop3-login: Read SASL-IR properly
    This fixes issue where login would fail if SASL-IR message would
    be too long.

--- src/pop3-login/client-authenticate.h
+++ src/pop3-login/client-authenticate.h
@@ -9,7 +9,7 @@ void pop3_client_auth_result(struct client *client,
 bool cmd_capa(struct pop3_client *client, const char *args);
 bool cmd_user(struct pop3_client *client, const char *args);
 bool cmd_pass(struct pop3_client *client, const char *args);
-bool cmd_auth(struct pop3_client *client, const char *args);
+int cmd_auth(struct pop3_client *client, bool *parsed_r);
 bool cmd_apop(struct pop3_client *client, const char *args);
 
 #endif
