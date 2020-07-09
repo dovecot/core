dovecot/core

commit 6c55437036b3de11804eb68f66d84cb164c33d63

pop3-login: Read SASL-IR properly
This fixes issue where login would fail if SASL-IR message would
be too long.

src/pop3-login/client-authenticate.c
@@ -76,46 +76,61 @@ void pop3_client_auth_result(struct client *client,
 	}
 }
 
-bool cmd_auth(struct pop3_client *pop3_client, const char *args)
+int cmd_auth(struct pop3_client *pop3_client, bool *parsed_r)
 {
+	/* NOTE: This command's input is handled specially because the
+	   SASL-IR can be large. */
 	struct client *client = &pop3_client->common;
-	const struct auth_mech_desc *mech;
-	const char *mech_name, *init_resp, *p;
+	const unsigned char *data;
+	size_t i, size;
+	int ret;
 
-	if (*args == '\0') {
-		/* Old-style SASL discovery, used by MS Outlook */
-		unsigned int i, count;
+	*parsed_r = FALSE;
 
-		client_send_raw(client, "+OK\r\n");
-		mech = sasl_server_get_advertised_mechs(client, &count);
-		for (i = 0; i < count; i++) {
-			client_send_raw(client, mech[i].name);
-			client_send_raw(client, "\r\n");
+	/* <auth mechanism name> [<initial SASL response>] */
+	if (!pop3_client->auth_mech_name_parsed) {
+		data = i_stream_get_data(client->input, &size);
+		for (i = 0; i < size; i++) {
+			if (data[i] == ' ' ||
+			    data[i] == '\r' || data[i] == '\n')
+				break;
 		}
- 		client_send_raw(client, ".\r\n");
- 		return TRUE;
+		if (i == size)
+			return 0;
+		if (i == 0) {
+			/* Old-style SASL discovery, used by MS Outlook */
+			unsigned int i, count;
+			const struct auth_mech_desc *mech;
+
+			client_send_raw(client, "+OK\r\n");
+			mech = sasl_server_get_advertised_mechs(client, &count);
+			for (i = 0; i < count; i++) {
+				client_send_raw(client, mech[i].name);
+				client_send_raw(client, "\r\n");
+			}
+			client_send_raw(client, ".\r\n");
+			*parsed_r = TRUE;
+			return 1;
+		}
+		i_free(client->auth_mech_name);
+		client->auth_mech_name = i_strndup(data, i);
+		pop3_client->auth_mech_name_parsed = TRUE;
+		if (data[i] == ' ')
+			i++;
+		i_stream_skip(client->input, i);
 	}
 
-	/* <mechanism name> <initial response> */
-	p = strchr(args, ' ');
-	if (p == NULL) {
-		mech_name = args;
-		/* no initial response */
-		init_resp = NULL;
-	} else {
-		mech_name = t_strdup_until(args, p);
-		init_resp = p + 1;
-		if (*init_resp == '\0') {
-			/* no initial response */
-			init_resp = NULL;
-		} else if (strcmp(init_resp, "=") == 0) {
-			/* empty initial response */
-			init_resp = "";
-		}
-	}
+	/* get SASL-IR, if any */
+	if ((ret = client_auth_read_line(client)) <= 0)
+		return ret;
 
-	(void)client_auth_begin(client, mech_name, init_resp);
-	return TRUE;
+	const char *ir = NULL;
+	if (client->auth_response->used > 0)
+		ir = t_strdup(str_c(client->auth_response));
+
+	*parsed_r = TRUE;
+	pop3_client->auth_mech_name_parsed = FALSE;
+	return client_auth_begin(client, t_strdup(client->auth_mech_name), ir);
 }
 
 bool cmd_user(struct pop3_client *pop3_client, const char *args)

src/pop3-login/client.c
@@ -95,8 +95,6 @@ static bool client_command_execute(struct pop3_client *client, const char *cmd,
 		return cmd_user(client, args);
 	if (strcmp(cmd, "PASS") == 0)
 		return cmd_pass(client, args);
-	if (strcmp(cmd, "AUTH") == 0)
-		return cmd_auth(client, args);
 	if (strcmp(cmd, "APOP") == 0)
 		return cmd_apop(client, args);
 	if (strcmp(cmd, "STLS") == 0)
@@ -164,8 +162,19 @@ static bool pop3_client_input_next_cmd(struct client *client)
 	const char *cmd, *args;
 	bool parsed;
 
-	if (!client_read_cmd_name(client, &cmd))
+	if (!pop3_client->authenticating && !client_read_cmd_name(client, &cmd))
 		return FALSE;
+
+	if (pop3_client->authenticating ||
+	    strcmp(cmd, "AUTH") == 0) {
+		pop3_client->authenticating = TRUE;
+		int ret = cmd_auth(pop3_client, &parsed);
+		if (ret == 0 || !parsed)
+			return FALSE;
+		pop3_client->authenticating = !parsed;
+		return parsed;
+	}
+
 	if ((args = i_stream_next_line(client->input)) == NULL)
 		return FALSE;
 

src/pop3-login/client.h
@@ -23,6 +23,8 @@ struct pop3_client {
 	unsigned int apop_server_pid, apop_connect_uid;
 	enum pop3_proxy_state proxy_state;
 	bool proxy_xclient;
+	bool auth_mech_name_parsed;
+	bool authenticating;
 };
 
 enum pop3_cmd_reply {

src/pop3-login/client-authenticate.h
@@ -9,7 +9,7 @@ void pop3_client_auth_result(struct client *client,
 bool cmd_capa(struct pop3_client *client, const char *args);
 bool cmd_user(struct pop3_client *client, const char *args);
 bool cmd_pass(struct pop3_client *client, const char *args);
-bool cmd_auth(struct pop3_client *client, const char *args);
+int cmd_auth(struct pop3_client *client, bool *parsed_r);
 bool cmd_apop(struct pop3_client *client, const char *args);
 
 #endif

commit f19d3ad349a287fa62737945976debb0a94e59a0

pop3-login: Use struct client authenticating member
This is what should be used instead of introducing our own.

Broken in 6c55437036b3de11804eb68f66d84cb164c33d63

src/pop3-login/client-authenticate.c
@@ -121,6 +121,7 @@ int cmd_auth(struct pop3_client *pop3_client, bool *parsed_r)
 		i_stream_skip(client->input, i);
 	}
 
+	client->authenticating = TRUE;
 	/* get SASL-IR, if any */
 	if ((ret = client_auth_read_line(client)) <= 0)
 		return ret;

src/pop3-login/client.c
@@ -117,7 +117,16 @@ static bool client_command_execute(struct pop3_client *client, const char *cmd,
 
 static void pop3_client_input(struct client *client)
 {
-	i_assert(!client->authenticating);
+	if (client->authenticating) {
+		struct pop3_client *pop3_client =
+			container_of(client, struct pop3_client, common);
+		bool parsed;
+		cmd_auth(pop3_client, &parsed);
+		if (!parsed)
+			client->authenticating = FALSE;
+		else
+			return;
+	}
 
 	if (!client_read(client))
 		return;
@@ -169,16 +178,13 @@ static bool pop3_client_input_next_cmd(struct client *client)
 	const char *cmd, *args;
 	bool parsed;
 
-	if (!pop3_client->authenticating && !client_read_cmd_name(client, &cmd))
+	if (!client_read_cmd_name(client, &cmd))
 		return FALSE;
 
-	if (pop3_client->authenticating ||
-	    strcmp(cmd, "AUTH") == 0) {
-		pop3_client->authenticating = TRUE;
+	if (strcmp(cmd, "AUTH") == 0) {
 		int ret = cmd_auth(pop3_client, &parsed);
 		if (ret == 0 || !parsed)
 			return FALSE;
-		pop3_client->authenticating = !parsed;
 		return parsed;
 	}
 

src/pop3-login/client.h
@@ -24,7 +24,6 @@ struct pop3_client {
 	enum pop3_proxy_state proxy_state;
 	bool proxy_xclient;
 	bool auth_mech_name_parsed;
-	bool authenticating;
 };
 
 enum pop3_cmd_reply {