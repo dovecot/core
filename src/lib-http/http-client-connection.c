7e849b84ac70e86caa5cb0edb8589444fbeab5b8
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-08-12 22:24:22 +0200

    lib-http: http-client-connection - Explicitly handle an infinite connection limit for setting idle timeout.
    This can just use the maximum idle timeout directly.

--- src/lib-http/http-client-connection.c
+++ src/lib-http/http-client-connection.c
@@ -444,8 +444,9 @@ http_client_connection_start_idle_timeout(struct http_client_connection *conn)
 	i_assert(max > 0);
 
 	/* Set timeout for this connection */
-	if (idle_count == 0) {
-		/* No idle connections yet; use the maximum idle time. */
+	if (idle_count == 0 || max == UINT_MAX) {
+		/* No idle connections yet or infinite connections allowed;
+		   use the maximum idle time. */
 		timeout = set->max_idle_time_msecs;
 	} else if (count > max || idle_count >= max) {
 		/* Instant death for (urgent) connections above limit */
