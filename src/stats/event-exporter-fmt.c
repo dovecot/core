eb4c8a9a5ac3c958d06507d7c01b5308b5153e6a
Josef 'Jeff' Sipek <jeff.sipek@open-xchange.com>
2020-08-21 14:23:14 -0400

    stats: Add helper to append category names without duplicates

--- src/stats/event-exporter-fmt.c
+++ src/stats/event-exporter-fmt.c
@@ -2,6 +2,7 @@
 
 #include "lib.h"
 #include "str.h"
+#include "hash.h"
 #include "ioloop.h"
 #include "event-exporter.h"
 
@@ -23,4 +24,55 @@ void event_export_helper_fmt_rfc3339_time(string_t *dest,
 		    tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday,
 		    tm->tm_hour, tm->tm_min, tm->tm_sec,
 		    time->tv_usec);
+}
+
+HASH_TABLE_DEFINE_TYPE(category_set, void *, const struct event_category *);
+
+static void insert_category(HASH_TABLE_TYPE(category_set) hash,
+			    const struct event_category * const cat)
+{
+	/* insert this category (key == the unique internal pointer) */
+	hash_table_update(hash, cat->internal, cat);
+
+	/* insert parent's categories */
+	if (cat->parent != NULL)
+		insert_category(hash, cat->parent);
+}
+
+void event_export_helper_fmt_categories(string_t *dest,
+					struct event_category * const *cats,
+					unsigned int count,
+					void (*append)(string_t *, const char *),
+					const char *separator)
+{
+	HASH_TABLE_TYPE(category_set) hash;
+	struct hash_iterate_context *iter;
+	const struct event_category *cat;
+	void *key ATTR_UNUSED;
+	unsigned int i;
+	bool first = TRUE;
+
+	if (count == 0)
+		return;
+
+	hash_table_create_direct(&hash, pool_datastack_create(),
+				 3 * count /* estimate */);
+
+	/* insert all the categories into the hash table */
+	for (i = 0; i < count; i++)
+		insert_category(hash, cats[i]);
+
+	/* output each category from hash table */
+	iter = hash_table_iterate_init(hash);
+	while (hash_table_iterate(iter, hash, &key, &cat)) {
+		if (!first)
+			str_append(dest, separator);
+
+		append(dest, cat->name);
+
+		first = FALSE;
+	}
+	hash_table_iterate_deinit(&iter);
+
+	hash_table_destroy(&hash);
 }
