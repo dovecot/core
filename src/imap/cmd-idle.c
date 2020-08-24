1a27cfa8e337b7e3298ba230059e766cdbc1123d
Timo Sirainen <timo.sirainen@open-xchange.com>
2020-08-24 19:10:43 +0300

    imap: imap_client_hibernate() - Return reason string on failure
    This helps writing a unit test for it.

--- src/imap/cmd-idle.c
+++ src/imap/cmd-idle.c
@@ -175,11 +175,12 @@ static void idle_add_keepalive_timeout(struct cmd_idle_context *ctx)
 static void idle_hibernate_timeout(struct cmd_idle_context *ctx)
 {
 	struct client *client = ctx->client;
+	const char *reason;
 
 	i_assert(ctx->sync_ctx == NULL);
 	i_assert(!ctx->sync_pending);
 
-	if (imap_client_hibernate(&client)) {
+	if (imap_client_hibernate(&client, &reason)) {
 		/* client may be destroyed now */
 	} else {
 		/* failed - don't bother retrying */
