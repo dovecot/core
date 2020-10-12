7f1738a772fad3de5126d0307c0f34b3f30ec288
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-10-12 23:29:02 +0200

    doveadm-server: Fix STARTTLS support.

--- src/doveadm/main.c
+++ src/doveadm/main.c
@@ -115,6 +115,7 @@ int main(int argc, char *argv[])
 	};
 	enum master_service_flags service_flags =
 		MASTER_SERVICE_FLAG_KEEP_CONFIG_OPEN |
+		MASTER_SERVICE_FLAG_HAVE_STARTTLS |
 		MASTER_SERVICE_FLAG_USE_SSL_SETTINGS;
 	struct master_service_settings_input input;
 	struct master_service_settings_output output;
