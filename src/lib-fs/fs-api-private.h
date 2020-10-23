dovecot/core

commit eaaf40ca4e8994824abff89fc617c3cc737d83a5

lib-fs: Include file's path when logging pending error messages
Otherwise it may not be known which file the error is about.

This happens when a file is freed without the last error being logged.
Also when an unlogged error is being replaced with a new error.

src/lib-fs/fs-api.c
@@ -321,7 +321,8 @@ void fs_file_free(struct fs_file *file)
 		   fs_file_last_error(). Log it to make sure it's not lost.
 		   Note that the errors are always set only to the file at
 		   the root of the parent hierarchy. */
-		e_error(file->event, "%s (in file deinit)", file->last_error);
+		e_error(file->event, "%s (in file %s deinit)",
+			file->last_error, fs_file_path(file));
 	}
 
 	fs_file_deinit(&file->parent);
@@ -608,7 +609,8 @@ fs_set_verror(struct event *event, const char *fmt, va_list args)
 		} else if (file->last_error_changed) {
 			/* multiple fs_set_error() calls used without
 			   fs_file_last_error() in the middle. */
-			e_error(file->event, "%s (overwriting error)", old_error);
+			e_error(file->event, "%s (overwriting error for file %s)",
+				old_error, fs_file_path(file));
 		}
 		if (errno == EAGAIN || errno == ENOENT || errno == EEXIST ||
 		    errno == ENOTEMPTY) {
@@ -630,8 +632,8 @@ fs_set_verror(struct event *event, const char *fmt, va_list args)
 		} else if (iter->last_error != NULL) {
 			/* multiple fs_set_error() calls before the iter
 			   finishes */
-			e_error(iter->fs->event, "%s (overwriting error)",
-				iter->last_error);
+			e_error(iter->fs->event, "%s (overwriting error for file %s)",
+				iter->last_error, fs_file_path(file));
 		}
 		i_free(iter->last_error);
 		iter->last_error = new_error;

commit 005a3d3b52cf0a560d2b0058610566c70d33fb1d

lib-fs: iteration - Fix crash when overwriting iteration error
Can't use fs_file_path() because file is NULL. Instead, add the path to
struct fs_iter directly so it can be used in the error.

Broken by eaaf40ca4e8994824abff89fc617c3cc737d83a5

src/lib-fs/fs-api-private.h
@@ -155,6 +155,7 @@ struct fs_iter {
 
 	struct fs *fs;
 	struct event *event;
+	char *path;
 	enum fs_iter_flags flags;
 	struct timeval start_time;
 	char *last_error;

src/lib-fs/fs-api.c
@@ -635,7 +635,7 @@ fs_set_verror(struct event *event, const char *fmt, va_list args)
 			/* multiple fs_set_error() calls before the iter
 			   finishes */
 			e_error(iter->fs->event, "%s (overwriting error for file %s)",
-				iter->last_error, fs_file_path(file));
+				iter->last_error, iter->path);
 		}
 		i_free(iter->last_error);
 		iter->last_error = new_error;
@@ -1246,6 +1246,7 @@ fs_iter_init_with_event(struct fs *fs, struct event *event,
 		iter = fs->v.iter_alloc();
 		iter->fs = fs;
 		iter->flags = flags;
+		iter->path = i_strdup(path);
 		iter->event = fs_create_event(fs, event);
 		event_set_ptr(iter->event, FS_EVENT_FIELD_FS, fs);
 		event_set_ptr(iter->event, FS_EVENT_FIELD_ITER, iter);
@@ -1281,6 +1282,7 @@ int fs_iter_deinit(struct fs_iter **_iter, const char **error_r)
 	if (ret < 0)
 		*error_r = t_strdup(iter->last_error);
 	i_free(iter->last_error);
+	i_free(iter->path);
 	i_free(iter);
 	event_unref(&event);
 	return ret;