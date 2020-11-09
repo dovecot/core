e416d0c6f95838b21a438451773d1a53167f2689
Timo Sirainen <timo.sirainen@open-xchange.com>
2020-11-09 17:38:26 +0200

    lib: istream-concat - Avoid NULL pointer arithmetic
    Even though it was only doing +0. Fixes:
    runtime error: applying zero offset to null pointer

--- src/lib/istream-concat.c
+++ src/lib/istream-concat.c
@@ -128,9 +128,12 @@ static void i_stream_concat_skip(struct concat_istream *cstream)
 		bytes_skipped -= cstream->prev_stream_left;
 		cstream->prev_stream_left = 0;
 	}
-	stream->pos -= bytes_skipped;
-	stream->skip -= bytes_skipped;
-	stream->buffer += bytes_skipped;
+	if (bytes_skipped > 0) {
+		i_assert(stream->buffer != NULL);
+		stream->pos -= bytes_skipped;
+		stream->skip -= bytes_skipped;
+		stream->buffer += bytes_skipped;
+	}
 	cstream->prev_skip = stream->skip;
 	i_stream_skip(cstream->cur_input, bytes_skipped);
 }
