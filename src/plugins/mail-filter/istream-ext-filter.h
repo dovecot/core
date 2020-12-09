26a6a90baf14c2f6671fbcf07a42fd5ed43046f5
Josef 'Jeff' Sipek <jeff.sipek@open-xchange.com>
2020-12-09 09:20:54 -0500

    plugins: Remove mail-filter plugin

--- src/plugins/mail-filter/istream-ext-filter.h
+++
@@ -1,8 +0,0 @@
-#ifndef ISTREAM_MAIL_FILTER_H
-#define ISTREAM_MAIL_FILTER_H
-
-struct istream *
-i_stream_create_ext_filter(struct istream *input, const char *socket_path,
-			   const char *args);
-
-#endif
