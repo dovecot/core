84da97fd4ebaad2f758484c05b4c4805c1e76385
Timo Sirainen <timo.sirainen@open-xchange.com>
2021-02-04 01:36:52 +0200

    lib-dict: Set connections' event_parent correctly
    dict->dict.event is NULL at this point.

--- src/lib-dict/dict-client.c
+++ src/lib-dict/dict-client.c
@@ -748,6 +748,7 @@ client_dict_init(struct dict *driver, const char *uri,
 	dict = i_new(struct client_dict, 1);
 	dict->dict = *driver;
 	dict->conn.dict = dict;
+	dict->conn.conn.event_parent = set->event_parent;
 	dict->value_type = set->value_type;
 	dict->username = i_strdup(set->username);
 	dict->idle_msecs = idle_msecs;
