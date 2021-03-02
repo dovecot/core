170505ce06da02d9f1664cbf8ccca1969389ca1d
Timo Sirainen <timo.sirainen@open-xchange.com>
2021-03-02 15:06:50 +0200

    lib: Clarify i[o]_stream_get_disconnect_reason() comment

--- src/lib/iostream.h
+++ src/lib/iostream.h
@@ -1,7 +1,9 @@
 #ifndef IOSTREAM_H
 #define IOSTREAM_H
 
-/* Returns human-readable reason for why iostream was disconnected. */
+/* Returns human-readable reason for why iostream was disconnected.
+   The output is either "Connection closed" for clean disconnections or
+   "Connection closed: <error>" for unclean disconnections. */
 const char *io_stream_get_disconnect_reason(struct istream *input,
 					    struct ostream *output);
 
