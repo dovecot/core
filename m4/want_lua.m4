dovecot/core

commit af819cd2f43b8896d949a33f8099234ac57df6e5

m4: want_lua - Check for lua_isinteger

m4/want_lua.m4
@@ -39,6 +39,7 @@ AC_DEFUN([DOVECOT_WANT_LUA],[
    old_CFLAGS="$CFLAGS"
    CFLAGS="$CFLAGS $LUA_CFLAGS $LUA_LIBS"
 
+   AC_CHECK_FUNCS([lua_isinteger])
    AC_CHECK_FUNCS([lua_tointegerx])
 
    CFLAGS="$old_CFLAGS"

commit b99dea5b81f086be3d013257012415a151e67759

m4: want_lua - Fix Lua function checks
One has to use $LIBS variable to ensure libraries
come after conftest.c in configure.

Broken in af819cd2f43b8896d949a33f8099234ac57df6e5

m4/want_lua.m4
@@ -37,7 +37,9 @@ AC_DEFUN([DOVECOT_WANT_LUA],[
    )
    dnl Check if various lua functions are present
    old_CFLAGS="$CFLAGS"
-   CFLAGS="$CFLAGS $LUA_CFLAGS $LUA_LIBS"
+   CFLAGS="$CFLAGS $LUA_CFLAGS"
+   old_LIBS="$LIBS"
+   LIBS="$LIBS $LUA_LIBS"
 
    AC_CHECK_FUNCS([luaL_setfuncs])
    AC_CHECK_FUNCS([luaL_setmetatable])
@@ -45,5 +47,6 @@ AC_DEFUN([DOVECOT_WANT_LUA],[
    AC_CHECK_FUNCS([lua_tointegerx])
 
    CFLAGS="$old_CFLAGS"
+   LIBS="$old_LIBS"
   )
 ])