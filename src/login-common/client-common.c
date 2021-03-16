dovecot/core

commit f32daf5ac9eaa886e28659965fc0156b52b60bda

login-common: Fix assert-crash if client is destroyed during anvil lookup
This could happen if the client gets disconnected due to
"Connection queue full".

Fixes:
Panic: file client-common.c: line 323 (client_destroy): assertion failed: (!client->authenticating)

src/login-common/client-common.c
@@ -24,6 +24,7 @@
 #include "master-service.h"
 #include "master-service-ssl-settings.h"
 #include "master-auth.h"
+#include "anvil-client.h"
 #include "auth-client.h"
 #include "dsasl-client.h"
 #include "login-proxy.h"
@@ -323,6 +324,8 @@ void client_destroy(struct client *client, const char *reason)
 	} else if (client->auth_request != NULL) {
 		i_assert(client->authenticating);
 		sasl_server_auth_abort(client);
+	} else if (client->anvil_query != NULL) {
+		anvil_client_query_abort(anvil, &client->anvil_query);
 	} else {
 		i_assert(!client->authenticating);
 	}

src/login-common/sasl-server.c
@@ -221,6 +221,8 @@ anvil_lookup_callback(const char *reply, void *context)
 	unsigned int conn_count;
 	int ret;
 
+	client->anvil_query = NULL;
+
 	conn_count = 0;
 	if (reply != NULL && str_to_uint(reply, &conn_count) < 0)
 		i_fatal("Received invalid reply from anvil: %s", reply);
@@ -270,7 +272,8 @@ anvil_check_too_many_connections(struct client *client,
 	query = t_strconcat("LOOKUP\t", login_binary->protocol, "/",
 			    net_ip2addr(&client->ip), "/",
 			    str_tabescape(client->virtual_user), NULL);
-	anvil_client_query(anvil, query, anvil_lookup_callback, req);
+	client->anvil_query =
+		anvil_client_query(anvil, query, anvil_lookup_callback, req);
 }
 
 static bool

src/login-common/client-common.h
@@ -193,6 +193,7 @@ struct client {
 	time_t auth_first_started, auth_finished;
 	const char *sasl_final_resp;
 	const char *const *auth_passdb_args;
+	struct anvil_query *anvil_query;
 
 	unsigned int master_auth_id;
 	unsigned int master_tag;

commit e1dd685f1ab902caddd1c9d4698f3522bc2da526

login-common: Finish freeing the client when anvil lookup is pending at destroy
Broken by f32daf5ac9eaa886e28659965fc0156b52b60bda

src/login-common/client-common.c
@@ -323,14 +323,14 @@ void client_destroy(struct client *client, const char *reason)
 		client->authenticating = FALSE;
 		master_auth_request_abort(master_auth, client->master_tag);
 		client->refcount--;
-	} else if (client->auth_request != NULL) {
+	} else if (client->auth_request != NULL ||
+		   client->anvil_query != NULL) {
 		i_assert(client->authenticating);
 		sasl_server_auth_abort(client);
-	} else if (client->anvil_query != NULL) {
-		anvil_client_query_abort(anvil, &client->anvil_query);
-	} else {
-		i_assert(!client->authenticating);
 	}
+	i_assert(!client->authenticating);
+	i_assert(client->auth_request == NULL);
+	i_assert(client->anvil_query == NULL);
 
 	timeout_remove(&client->to_disconnect);
 	timeout_remove(&client->to_auth_waiting);

src/login-common/sasl-server.c
@@ -552,5 +552,7 @@ void sasl_server_auth_failed(struct client *client, const char *reason,
 void sasl_server_auth_abort(struct client *client)
 {
 	client->auth_try_aborted = TRUE;
+	if (client->anvil_query != NULL)
+		anvil_client_query_abort(anvil, &client->anvil_query);
 	sasl_server_auth_cancel(client, NULL, NULL, SASL_SERVER_REPLY_AUTH_ABORTED);
 }