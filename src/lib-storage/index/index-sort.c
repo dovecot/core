d4b3fcee59dd677ef5db683a8d1ba80d0667718e
Timo Sirainen <timo.sirainen@open-xchange.com>
2021-03-17 17:48:33 +0200

    lib-storage: Add reason_code=mailbox:sort

--- src/lib-storage/index/index-sort.c
+++ src/lib-storage/index/index-sort.c
@@ -286,7 +286,9 @@ void index_sort_list_finish(struct mail_search_sort_program *program)
 	static_node_cmp_context.reverse =
 		(program->sort_program[0] & MAIL_SORT_FLAG_REVERSE) != 0;
 
+	struct event_reason *reason = event_reason_begin("mailbox:sort");
 	program->sort_list_finish(program);
+	event_reason_end(&reason);
 }
 
 bool index_sort_list_next(struct mail_search_sort_program *program,
