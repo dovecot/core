dovecot/core

commit 5fd5ad3a2be6708e81f8dcfcbb06a3957dddd276

lib-storage: mailbox_list_default_get_storage_name() - Cleanup shared namespace root handling

src/lib-storage/mailbox-list.c
@@ -604,6 +604,17 @@ const char *mailbox_list_default_get_storage_name(struct mailbox_list *list,
 	char list_sep, ns_sep, *ret;
 
 	mailbox_list_vname_prepare(list, &storage_name);
+	if (ns->type == MAIL_NAMESPACE_TYPE_SHARED &&
+	    (ns->flags & NAMESPACE_FLAG_AUTOCREATED) == 0) {
+		/* Accessing shared namespace root. This is just the initial
+		   lookup that ends up as parameter to
+		   shared_storage_get_namespace(). That then finds/creates the
+		   actual shared namespace, which gets used to generate the
+		   proper storage_name. So the only thing that's really
+		   necessary here is to just skip over the shared namespace
+		   prefix and leave the rest of the name untouched. */
+		return storage_name;
+	}
 	if (list->set.storage_name_escape_char != '\0') {
 		storage_name = mailbox_list_escape_name_params(storage_name,
 				ns->prefix,
@@ -625,14 +636,6 @@ const char *mailbox_list_default_get_storage_name(struct mailbox_list *list,
 	ns_sep = mail_namespace_get_sep(ns);
 
 	if (list_sep != ns_sep && list->set.storage_name_escape_char == '\0') {
-		if (ns->type == MAIL_NAMESPACE_TYPE_SHARED &&
-		    (ns->flags & NAMESPACE_FLAG_AUTOCREATED) == 0) {
-			/* shared namespace root. the backend storage's
-			   hierarchy separator isn't known yet, so do
-			   nothing. */
-			return storage_name;
-		}
-
 		ret = mailbox_list_convert_sep(storage_name, ns_sep, list_sep);
 	} else if (list->set.vname_escape_char == '\0' ||
 		   strchr(storage_name, list->set.vname_escape_char) == NULL) {

commit c3fc5c9060b13c9f891e3f08869d5e236cb1a3d9

lib-storage: Make sure listescape can work correctly with shared namespaces
This makes sure that if storage_name_escape_char is set, shared mailboxes
do not return the invalid/unescaped mailbox storage_name internally which
could result in failures like "Invalid mailbox name: Name must not have '/'
character" even when using the listescape plugin.

Broken by 5fd5ad3a2be6708e81f8dcfcbb06a3957dddd276

src/lib-storage/mailbox-list.c
@@ -619,23 +619,28 @@ const char *mailbox_list_default_get_storage_name(struct mailbox_list *list,
 						  const char *vname)
 {
 	const char *prepared_name = vname;
+	const char list_sep = mailbox_list_get_hierarchy_sep(list);
+	const char ns_sep = mail_namespace_get_sep(list->ns);
 
 	if (mailbox_list_vname_prepare(list, &prepared_name))
 		return prepared_name;
 	if (list->ns->type == MAIL_NAMESPACE_TYPE_SHARED &&
-	    (list->ns->flags & NAMESPACE_FLAG_AUTOCREATED) == 0) {
+	    (list->ns->flags & NAMESPACE_FLAG_AUTOCREATED) == 0 &&
+	    list_sep != ns_sep &&
+	    list->set.storage_name_escape_char == '\0') {
 		/* Accessing shared namespace root. This is just the initial
 		   lookup that ends up as parameter to
 		   shared_storage_get_namespace(). That then finds/creates the
 		   actual shared namespace, which gets used to generate the
 		   proper storage_name. So the only thing that's really
 		   necessary here is to just skip over the shared namespace
-		   prefix and leave the rest of the name untouched. */
+		   prefix and leave the rest of the name untouched. The only
+		   exception is if there is a storage_name_escape_char set, in
+		   this case the storage name must be handled. */
 		return prepared_name;
 	}
 
-	char list_sep = mailbox_list_get_hierarchy_sep(list);
-	char sep[] = { mail_namespace_get_sep(list->ns), '\0' };
+	const char sep[] = { ns_sep, '\0' };
 	const char *const *parts = t_strsplit(prepared_name, sep);
 	string_t *storage_name = t_str_new(128);
 	for (unsigned int i = 0; parts[i] != NULL; i++) {