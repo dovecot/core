dovecot/core

commit 923362d27d9b2428e301614673cb0efba3bf928f

imap: SETMETADATA - Explicitly support using atoms for values
Required to keep atoms working (and especially not assert-crashing) after
the following imap_arg_get_nstring() change.

src/imap/cmd-setmetadata.c
@@ -177,7 +177,12 @@ cmd_setmetadata_entry(struct imap_setmetadata_context *ctx,
 		if (ctx->failed)
 			return 1;
 		i_zero(&value);
-		value.value = imap_arg_as_nstring(entry_value);
+		/* NOTE: The RFC doesn't allow atoms as value, but since
+		   Dovecot has traditionally supported it this is kept for
+		   backwards compatibility just in case some client is
+		   using it. */
+		if (!imap_arg_get_atom(entry_value, &value.value))
+			value.value = imap_arg_as_nstring(entry_value);
 		ret = imap_metadata_set(ctx->trans, entry_name, &value);
 		if (ret < 0) {
 			/* delay reporting the failure so we'll finish

commit 7031569672459fbd0af82f14e56bb5b3f9fb0375

imap: Fix using SETMETADATA NIL to unset value
Broken by 923362d27d9b2428e301614673cb0efba3bf928f

src/imap/cmd-setmetadata.c
@@ -181,7 +181,9 @@ cmd_setmetadata_entry(struct imap_setmetadata_context *ctx,
 		   Dovecot has traditionally supported it this is kept for
 		   backwards compatibility just in case some client is
 		   using it. */
-		if (!imap_arg_get_atom(entry_value, &value.value))
+		if (entry_value->type == IMAP_ARG_NIL)
+			;
+		else if (!imap_arg_get_atom(entry_value, &value.value))
 			value.value = imap_arg_as_nstring(entry_value);
 		ret = imap_metadata_set(ctx->trans, entry_name, &value);
 		if (ret < 0) {