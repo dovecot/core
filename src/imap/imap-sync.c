dovecot/core

commit 4eb3f6f273d913041caa5b1818c2d05c7f284091

imap: imap-sync - Avoid unsigned integer overflow
Error: imap-sync.c:433:17: runtime error: unsigned integer overflow:
4294967295 + 1 cannot be represented in type 'unsigned int'

src/imap/imap-sync.c
@@ -427,10 +427,10 @@ static void imap_sync_vanished(struct imap_sync_context *ctx)
 	line = t_str_new(256);
 	str_append(line, "* VANISHED ");
 	for (i = 0; i < count; i++) {
-		start_uid = 0; prev_uid = (uint32_t)-1;
+		start_uid = 0; prev_uid = 0;
 		for (seq = seqs[i].seq1; seq <= seqs[i].seq2; seq++) {
 			mail_set_seq(ctx->mail, seq);
-			if (prev_uid + 1 != ctx->mail->uid) {
+			if (prev_uid != ctx->mail->uid - 1) {
 				if (start_uid != 0) {
 					if (!comma)
 						comma = TRUE;

commit 62a11f952714de7df8e3cfae2d422b0a46bde68d

imap: imap-sync - Fix VANISHED response with UID=1
Fixes a bug introduced in 4eb3f6f27, where a UID EXPUNGE 1 command
resulted in a VANSIHED 0:1 response, which is invalid IMAP syntax
(sequence sets must have nonzero values).

src/imap/imap-sync.c
@@ -430,7 +430,7 @@ static void imap_sync_vanished(struct imap_sync_context *ctx)
 		start_uid = 0; prev_uid = 0;
 		for (seq = seqs[i].seq1; seq <= seqs[i].seq2; seq++) {
 			mail_set_seq(ctx->mail, seq);
-			if (prev_uid != ctx->mail->uid - 1) {
+			if (prev_uid == 0 || prev_uid + 1 != ctx->mail->uid) {
 				if (start_uid != 0) {
 					if (!comma)
 						comma = TRUE;