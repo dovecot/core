c963be712fff0371a8a4df283b4b2471e2b566e3
Markus Valentin <markus.valentin@open-xchange.com>
2021-05-20 23:21:10 +0200

    imap-acl: Add imap_acl_storage with module context

--- src/plugins/imap-acl/imap-acl-plugin.h
+++ src/plugins/imap-acl/imap-acl-plugin.h
@@ -4,6 +4,8 @@
 extern const char *imap_acl_plugin_dependencies[];
 extern const char imap_acl_plugin_binary_dependency[];
 
+extern MODULE_CONTEXT_DEFINE(imap_acl_storage_module, &mail_storage_module_register);
+
 void imap_acl_plugin_init(struct module *module);
 void imap_acl_plugin_deinit(void);
 
