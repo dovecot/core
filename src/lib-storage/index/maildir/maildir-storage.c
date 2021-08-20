18afa46644badced53715df7e56f8ef73bab74dc
Aki Tuomi <aki.tuomi@open-xchange.com>
2021-08-20 11:43:27 +0300

    lib-storage: maildir - Allow creating existing folder when rebuilding list index

--- src/lib-storage/index/maildir/maildir-storage.c
+++ src/lib-storage/index/maildir/maildir-storage.c
@@ -521,6 +521,9 @@ maildir_mailbox_create(struct mailbox *box, const struct mailbox_update *update,
 		       bool directory)
 {
 	const char *root_dir, *shared_path;
+	/* allow physical location to exist when we rebuild list index, this
+	   happens with LAYOUT=INDEX only. */
+	bool verify = box->storage->rebuilding_list_index;
 	struct stat st;
 	int ret;
 
@@ -528,7 +531,7 @@ maildir_mailbox_create(struct mailbox *box, const struct mailbox_update *update,
 		return ret;
 	ret = 0;
 	/* the maildir is created now. finish the creation as best as we can */
-	if (create_maildir_subdirs(box, FALSE) < 0)
+	if (create_maildir_subdirs(box, verify) < 0)
 		ret = -1;
 	if (maildir_create_maildirfolder_file(box) < 0)
 		ret = -1;
