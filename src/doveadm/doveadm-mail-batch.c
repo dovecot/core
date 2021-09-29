dovecot/core

commit fd4360e30b695e596a5081a6080152188a12852a

doveadm: Add reason_code=doveadm:cmd_<command name> to events

src/doveadm/doveadm-mail-batch.c
@@ -38,7 +38,13 @@ static int cmd_batch_run(struct doveadm_mail_cmd_context *_ctx,
 
 	array_foreach_elem(&ctx->commands, cmd) {
 		cmd->cur_mail_user = user;
-		if (cmd->v.run(cmd, user) < 0) {
+		const char *reason_code =
+			event_reason_code_prefix("doveadm", "cmd_",
+						 cmd->cmd->name);
+		struct event_reason *reason = event_reason_begin(reason_code);
+		int ret = cmd->v.run(cmd, user);
+		event_reason_end(&reason);
+		if (ret < 0) {
 			i_assert(cmd->exit_code != 0);
 			_ctx->exit_code = cmd->exit_code;
 			ret = -1;

src/doveadm/doveadm-mail.c
@@ -458,12 +458,18 @@ doveadm_mail_next_user(struct doveadm_mail_cmd_context *ctx,
 		return ret;
 	}
 
+	struct event_reason *reason =
+		event_reason_begin(event_reason_code_prefix("doveadm", "cmd_",
+							    ctx->cmd->name));
 	T_BEGIN {
 		if (ctx->v.run(ctx, ctx->cur_mail_user) < 0) {
 			i_assert(ctx->exit_code != 0);
 		}
 	} T_END;
 	mail_user_deinit(&ctx->cur_mail_user);
+	/* user deinit may still do some work, so finish the reason after it */
+	event_reason_end(&reason);
+
 	mail_storage_service_user_unref(&ctx->cur_service_user);
 	return 1;
 }

commit f318731eacd4e1090511c52bc4697c57cb9c4a07

doveadm mail batch: Fix run() return value on errors
The function always returned 0, even when there was an error. This practically
doesn't fix anything, but makes it work the way it was intended. It also makes
scan-build happier.

Broken by fd4360e30b695e596a5081a6080152188a12852a

src/doveadm/doveadm-mail-batch.c
@@ -42,12 +42,11 @@ static int cmd_batch_run(struct doveadm_mail_cmd_context *_ctx,
 			event_reason_code_prefix("doveadm", "cmd_",
 						 cmd->cmd->name);
 		struct event_reason *reason = event_reason_begin(reason_code);
-		int ret = cmd->v.run(cmd, user);
+		ret = cmd->v.run(cmd, user);
 		event_reason_end(&reason);
 		if (ret < 0) {
 			i_assert(cmd->exit_code != 0);
 			_ctx->exit_code = cmd->exit_code;
-			ret = -1;
 			break;
 		}
 		cmd->cur_mail_user = NULL;