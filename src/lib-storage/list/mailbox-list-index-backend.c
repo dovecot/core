dovecot/core

commit f5328d6f7e4a8e460c736fa0336f5766aa58abda

lib-storage: mailbox-list: index_list_delete_mailbox() - Support NO-NOSELECT
When NO-NOSELECT is configured delete parent mailboxes which are not
selectable or not existant when deleting child mailboxes.

src/lib-storage/list/mailbox-list-index-backend.c
@@ -2,6 +2,7 @@
 
 #include "lib.h"
 #include "hostpid.h"
+#include "str.h"
 #include "mail-index.h"
 #include "subscription-file.h"
 #include "mailbox-list-delete.h"
@@ -690,6 +691,50 @@ index_list_delete_entry(struct index_mailbox_list *list, const char *name,
 }
 
 static int
+index_list_try_delete_nonexistent_parent(struct mailbox_list *_list,
+					 const char *name)
+{
+	struct index_mailbox_list *list =
+		container_of(_list, struct index_mailbox_list, list);
+	struct mailbox_list_index_node *node, *parent = NULL;
+	string_t *full_name;
+	const char *p;
+	char sep = mailbox_list_get_hierarchy_sep(_list);
+
+	if ((p = strrchr(name, sep)) == NULL) {
+		/* No occurrences of the hierarchy separator could be found
+		   in the name, so the mailbox has no parents. */
+               return 0;
+	}
+
+	/* Lookup parent node of of given "name" */
+	node = mailbox_list_index_lookup(_list, t_strdup_until(name, p));
+	full_name = t_str_new(32);
+
+	while (node != NULL) {
+		/* Attempt to delete all parent nodes that are NOSELECT or
+		   NONEXISTENT */
+		if (node->children != NULL)
+			break;
+		if ((node->flags & MAILBOX_LIST_INDEX_FLAG_NOSELECT) != 0 ||
+		    (node->flags & MAILBOX_LIST_INDEX_FLAG_NONEXISTENT) != 0) {
+			/* The parent mailbox has no other children and is not
+			   existant or not selectable, delete it */
+			str_truncate(full_name, 0);
+			mailbox_list_index_node_get_path(node, sep, full_name);
+			parent = node->parent;
+			if (index_list_delete_entry(list, str_c(full_name), FALSE) < 0)
+				return -1;
+		} else
+			break;
+
+		/* If there is another parent attempt to delete it as well */
+		node = parent;
+	}
+	return 0;
+}
+
+static int
 index_list_delete_mailbox(struct mailbox_list *_list, const char *name)
 {
 	struct index_mailbox_list *list = (struct index_mailbox_list *)_list;
@@ -719,6 +764,9 @@ index_list_delete_mailbox(struct mailbox_list *_list, const char *name)
 		if (index_list_delete_entry(list, name, TRUE) < 0)
 			return -1;
 	}
+	if (_list->set.no_noselect && ret == 0)
+		(void)index_list_try_delete_nonexistent_parent(_list, name);
+
 	return ret;
 }
 

commit 453f7fe515df3638aaa51c6efb5a7cb0f3b033d2

lib-storage: LAYOUT=index: Fix accessing freed memory when deleting \Noselect parents
Broken by f5328d6f7e4a8e460c736fa0336f5766aa58abda

src/lib-storage/list/mailbox-list-index-backend.c
@@ -696,7 +696,7 @@ index_list_try_delete_nonexistent_parent(struct mailbox_list *_list,
 {
 	struct index_mailbox_list *list =
 		container_of(_list, struct index_mailbox_list, list);
-	struct mailbox_list_index_node *node, *parent = NULL;
+	struct mailbox_list_index_node *node;
 	string_t *full_name;
 	const char *p;
 	char sep = mailbox_list_get_hierarchy_sep(_list);
@@ -722,14 +722,22 @@ index_list_try_delete_nonexistent_parent(struct mailbox_list *_list,
 			   existant or not selectable, delete it */
 			str_truncate(full_name, 0);
 			mailbox_list_index_node_get_path(node, sep, full_name);
-			parent = node->parent;
 			if (index_list_delete_entry(list, str_c(full_name), FALSE) < 0)
 				return -1;
+
+			if ((p = strrchr(str_c(full_name), sep)) == NULL) {
+				/* No occurrences of the hierarchy separator
+				   could be found in the mailbox that was
+				   just deleted. */
+				node = NULL;
+			} else {
+				/* lookup parent node of the node just deleted */
+				str_truncate(full_name, p - str_c(full_name));
+				node = mailbox_list_index_lookup(_list, str_c(full_name));
+			}
 		} else
 			break;
 
-		/* If there is another parent attempt to delete it as well */
-		node = parent;
 	}
 	return 0;
 }