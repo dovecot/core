dovecot/core

commit 4453ca75c600ed35643d0775b16848cafae9eaea

lib-dict: Change dict_vfuncs.lookup() to support multiple values

src/lib-dict-backend/dict-ldap.c
@@ -355,7 +355,7 @@ ldap_dict_lookup_callback(struct ldap_result *result, struct dict_ldap_op *op)
 static int
 ldap_dict_lookup(struct dict *dict, const struct dict_op_settings *set,
 		 pool_t pool, const char *key,
-		 const char **value_r, const char **error_r)
+		 const char *const **values_r, const char **error_r)
 {
 	struct dict_lookup_result res;
 
@@ -367,7 +367,7 @@ ldap_dict_lookup(struct dict *dict, const struct dict_op_settings *set,
 		return -1;
 	}
 	if (res.ret > 0)
-		*value_r = p_strdup(pool, res.value);
+		*values_r = p_strarray_dup(pool, res.values);
 	return res.ret;
 }
 

src/lib-dict-backend/dict-cdb.c
@@ -87,7 +87,7 @@ static int
 cdb_dict_lookup(struct dict *_dict,
 		const struct dict_op_settings *set ATTR_UNUSED,
 		pool_t pool,
-	        const char *key, const char **value_r,
+	        const char *key, const char *const **values_r,
 	        const char **error_r)
 {
 	struct cdb_dict *dict = (struct cdb_dict *)_dict;
@@ -110,7 +110,6 @@ cdb_dict_lookup(struct dict *_dict,
 	}
 
 	if (ret <= 0) {
-		*value_r = NULL;
 		/* something bad with db */
 		if (ret < 0) {
 			*error_r = t_strdup_printf("cdb_find(%s) failed: %m", dict->path);
@@ -126,7 +125,9 @@ cdb_dict_lookup(struct dict *_dict,
 		*error_r = t_strdup_printf("cdb_read(%s) failed: %m", dict->path);
 		return -1;
 	}
-	*value_r = data;
+	const char **values = p_new(pool, const char *, 2);
+	values[0] = data;
+	*values_r = values;
 	return 1;
 }
 

src/lib-dict-backend/dict-sql.c
@@ -492,13 +492,6 @@ sql_dict_result_unescape(enum dict_sql_type type, pool_t pool,
 	return str_c(str);
 }
 
-static const char *
-sql_dict_result_unescape_value(const struct dict_sql_map *map, pool_t pool,
-			       struct sql_result *result)
-{
-	return sql_dict_result_unescape(map->value_types[0], pool, result, 0);
-}
-
 static const char *const *
 sql_dict_result_unescape_values(const struct dict_sql_map *map, pool_t pool,
 				struct sql_result *result)
@@ -528,15 +521,13 @@ sql_dict_result_unescape_field(const struct dict_sql_map *map, pool_t pool,
 
 static int sql_dict_lookup(struct dict *_dict, const struct dict_op_settings *set,
 			   pool_t pool, const char *key,
-			   const char **value_r, const char **error_r)
+			   const char *const **values_r, const char **error_r)
 {
 	struct sql_dict *dict = (struct sql_dict *)_dict;
 	const struct dict_sql_map *map;
 	struct sql_statement *stmt;
 	struct sql_result *result = NULL;
 	int ret;
-
-	*value_r = NULL;
 
 	if (sql_lookup_get_query(dict, set, key, &map, &stmt, error_r) < 0)
 		return -1;
@@ -547,7 +538,7 @@ static int sql_dict_lookup(struct dict *_dict, const struct dict_op_settings *se
 		*error_r = t_strdup_printf("dict sql lookup failed: %s",
 					   sql_result_get_error(result));
 	} else if (ret > 0) {
-		*value_r = sql_dict_result_unescape_value(map, pool, result);
+		*values_r = sql_dict_result_unescape_values(map, pool, result);
 	}
 
 	sql_result_unref(result);

src/lib-dict-extra/dict-fs.c
@@ -19,7 +19,7 @@ struct fs_dict_iterate_context {
 	enum dict_iterate_flags flags;
 	pool_t value_pool;
 	struct fs_iter *fs_iter;
-	const char *values[2];
+	const char *const *values;
 	char *error;
 };
 
@@ -106,7 +106,7 @@ static const char *fs_dict_get_full_key(const char *username, const char *key)
 
 static int fs_dict_lookup(struct dict *_dict, const struct dict_op_settings *set,
 			  pool_t pool, const char *key,
-			  const char **value_r, const char **error_r)
+			  const char *const **values_r, const char **error_r)
 {
 	struct fs_dict *dict = (struct fs_dict *)_dict;
 	struct fs_file *file;
@@ -130,10 +130,11 @@ static int fs_dict_lookup(struct dict *_dict, const struct dict_op_settings *set
 	i_assert(ret == -1);
 
 	if (input->stream_errno == 0) {
-		*value_r = str_c(str);
+		const char **values = p_new(pool, const char *, 2);
+		values[0] = str_c(str);
+		*values_r = values;
 		ret = 1;
 	} else {
-		*value_r = NULL;
 		if (input->stream_errno == ENOENT)
 			ret = 0;
 		else {
@@ -196,7 +197,6 @@ static bool fs_dict_iterate(struct dict_iterate_context *ctx,
 	}
 	path = t_strconcat(iter->path, *key_r, NULL);
 	if ((iter->flags & DICT_ITERATE_FLAG_NO_VALUE) != 0) {
-		iter->values[0] = NULL;
 		*key_r = path;
 		return TRUE;
 	}
@@ -205,7 +205,7 @@ static bool fs_dict_iterate(struct dict_iterate_context *ctx,
 		.username = ctx->set.username,
 	};
 	ret = fs_dict_lookup(ctx->dict, &set, iter->value_pool, path,
-			     &iter->values[0], &error);
+			     &iter->values, &error);
 	if (ret < 0) {
 		/* I/O error */
 		iter->error = i_strdup(error);

src/lib-dict/dict-client.c
@@ -988,7 +988,7 @@ client_dict_lookup_async(struct dict *_dict, const struct dict_op_settings *set,
 
 struct client_dict_sync_lookup {
 	char *error;
-	char *value;
+	const char **values;
 	int ret;
 };
 
@@ -998,14 +998,19 @@ static void client_dict_lookup_callback(const struct dict_lookup_result *result,
 	lookup->ret = result->ret;
 	if (result->ret == -1)
 		lookup->error = i_strdup(result->error);
-	else if (result->ret == 1)
-		lookup->value = i_strdup(result->value);
+	else if (result->ret == 1) {
+		/* The caller's pool could point to data stack. We can't
+		   allocate from there, since we're in a different data stack
+		   frame. */
+		lookup->values = p_strarray_dup(default_pool, result->values);
+	}
 }
 
 static int client_dict_lookup(struct dict *_dict,
 			      const struct dict_op_settings *set,
 			      pool_t pool, const char *key,
-			      const char **value_r, const char **error_r)
+			      const char *const **values_r,
+			      const char **error_r)
 {
 	struct client_dict_sync_lookup lookup;
 
@@ -1022,12 +1027,11 @@ static int client_dict_lookup(struct dict *_dict,
 		i_free(lookup.error);
 		return -1;
 	case 0:
-		i_assert(lookup.value == NULL);
-		*value_r = NULL;
+		i_assert(lookup.values == NULL);
 		return 0;
 	case 1:
-		*value_r = p_strdup(pool, lookup.value);
-		i_free(lookup.value);
+		*values_r = p_strarray_dup(pool, lookup.values);
+		i_free(lookup.values);
 		return 1;
 	}
 	i_unreached();

src/lib-dict/dict-fail.c
@@ -34,7 +34,8 @@ static void dict_fail_wait(struct dict *dict ATTR_UNUSED)
 static int dict_fail_lookup(struct dict *dict ATTR_UNUSED,
 			    const struct dict_op_settings *set ATTR_UNUSED,
 			    pool_t pool ATTR_UNUSED,
-			    const char *key ATTR_UNUSED, const char **value_r ATTR_UNUSED,
+			    const char *key ATTR_UNUSED,
+			    const char *const **values_r ATTR_UNUSED,
 			    const char **error_r)
 {
 	*error_r = "Unsupported operation (dict does not support this feature)";

src/lib-dict/dict-file.c
@@ -224,9 +224,10 @@ static int file_dict_refresh(struct file_dict *dict, const char **error_r)
 static int file_dict_lookup(struct dict *_dict,
 			    const struct dict_op_settings *set,
 			    pool_t pool, const char *key,
-			    const char **value_r, const char **error_r)
+			    const char *const **values_r, const char **error_r)
 {
 	struct file_dict *dict = (struct file_dict *)_dict;
+	const char *value;
 
 	if (file_dict_ensure_path_home_dir(dict, set->home_dir, error_r) < 0)
 		return -1;
@@ -234,8 +235,14 @@ static int file_dict_lookup(struct dict *_dict,
 	if (file_dict_refresh(dict, error_r) < 0)
 		return -1;
 
-	*value_r = p_strdup(pool, hash_table_lookup(dict->hash, key));
-	return *value_r == NULL ? 0 : 1;
+	value = hash_table_lookup(dict->hash, key);
+	if (value == NULL)
+		return 0;
+
+	const char **values = p_new(pool, const char *, 2);
+	values[0] = p_strdup(pool, value);
+	*values_r = values;
+	return 1;
 }
 
 static struct dict_iterate_context *

src/lib-dict/dict-memcached-ascii.c
@@ -520,7 +520,8 @@ memcached_ascii_dict_get_full_key(struct memcached_ascii_dict *dict,
 static int
 memcached_ascii_dict_lookup(struct dict *_dict,
 			    const struct dict_op_settings *set,
-			    pool_t pool, const char *key, const char **value_r,
+			    pool_t pool, const char *key,
+			    const char *const **values_r,
 			    const char **error_r)
 {
 	struct memcached_ascii_dict *dict = (struct memcached_ascii_dict *)_dict;
@@ -541,7 +542,9 @@ memcached_ascii_dict_lookup(struct dict *_dict,
 	if (memcached_ascii_wait(dict, error_r) < 0)
 		return -1;
 
-	*value_r = p_strdup(pool, str_c(dict->conn.reply_str));
+	const char **values = p_new(pool, const char *, 2);
+	values[0] = p_strdup(pool, str_c(dict->conn.reply_str));
+	*values_r = values;
 	return dict->conn.value_received ? 1 : 0;
 }
 

commit 1270f38994d0ef9148ca51a5d4e5cdc64ab15834

dict-ldap: Fix crash in synchronous dict_lookup()
However, this code could never actually be called. All dict-ldap lookups
go through dict process, which uses only dict_lookup_async().

Broken by 4453ca75c600ed35643d0775b16848cafae9eaea

src/lib-dict-backend/dict-ldap.c
@@ -300,7 +300,10 @@ void ldap_dict_lookup_done(const struct dict_lookup_result *result, void *ctx)
 {
 	struct dict_lookup_result *res = ctx;
 	res->ret = result->ret;
-	res->value = t_strdup(result->value);
+	if (result->ret > 0) {
+		res->values = p_strarray_dup(pool_datastack_create(),
+					     result->values);
+	}
 	res->error = t_strdup(result->error);
 }
 