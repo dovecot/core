dovecot/core

commit a9683d7b3d7b8d044d8d39d98a3273610419b838

indexer: Fix memory leak on indexer timeout

src/indexer/indexer-client.c
@@ -139,8 +139,11 @@ void indexer_client_status_callback(int percentage, void *context)
 	struct indexer_client_request *ctx = context;
 
 	/* we are in deinit already, or the client has disconnected */
-	if (ctx->client->conn.output == NULL)
+	if (ctx->client->conn.output == NULL) {
+		indexer_client_unref(ctx->client);
+		i_free(ctx);
 		return;
+	}
 
 	T_BEGIN {
 		o_stream_nsend_str(ctx->client->conn.output,

commit c17ac4ba0af952b432052fe4146a889da7ac6cab

indexer: indexer_client_status_callback() - Fix accessing freed memory
Broken by a9683d7b3

src/indexer/indexer-client.c
@@ -138,14 +138,7 @@ void indexer_client_status_callback(int percentage, void *context)
 {
 	struct indexer_client_request *ctx = context;
 
-	/* we are in deinit already, or the client has disconnected */
-	if (ctx->client->conn.output == NULL) {
-		indexer_client_unref(ctx->client);
-		i_free(ctx);
-		return;
-	}
-
-	T_BEGIN {
+	if (ctx->client->conn.output != NULL) T_BEGIN {
 		o_stream_nsend_str(ctx->client->conn.output,
 			t_strdup_printf("%u\t%d\n", ctx->tag, percentage));
 	} T_END;