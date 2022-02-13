dovecot/core

commit b4827bc2f2c99029e8524e0c57228124dd22de52

plugins/fts: Restore fts_indexer_cmd
It was removed in cf114f90e0ba25c18db846ee582e3a130bd52949 and that
broke some FTS plugins.

src/plugins/fts/fts-indexer.c
@@ -3,6 +3,7 @@
 #include "lib.h"
 #include "ioloop.h"
 #include "connection.h"
+#include "write-full.h"
 #include "istream.h"
 #include "ostream.h"
 #include "strescape.h"
@@ -266,4 +267,30 @@ int fts_indexer_init(struct fts_backend *backend, struct mailbox *box,
 	io_loop_set_current(prev_ioloop);
 	*ctx_r = ctx;
 	return ctx->failed || ret < 0 ? -1 : 1;
+}
+
+#define INDEXER_HANDSHAKE "1\t0\tindexer\tindexer\n"
+
+int fts_indexer_cmd(struct mail_user *user, const char *cmd,
+	            const char **path_r)
+{
+	const char *path;
+	int fd;
+
+	path = t_strconcat(user->set->base_dir,
+	                   "/"INDEXER_SOCKET_NAME, NULL);
+	fd = net_connect_unix_with_retries(path, 1000);
+	if (fd == -1) {
+	        i_error("net_connect_unix(%s) failed: %m", path);
+	        return -1;
+	}
+
+	cmd = t_strconcat(INDEXER_HANDSHAKE, cmd, NULL);
+	if (write_full(fd, cmd, strlen(cmd)) < 0) {
+	        i_error("write(%s) failed: %m", path);
+	        i_close_fd(&fd);
+	        return -1;
+	}
+	*path_r = path;
+	return fd;
 }

commit 5fadf3d48c6ef1747325163a1d58d69fb7e252eb

fts: fts_indexer_cmd() - Fix wrong handshake
This was used only by fts-lucene to send OPTIMIZE command.

Broken by b4827bc2f2c99029e8524e0c57228124dd22de52

src/plugins/fts/fts-indexer.c
@@ -272,7 +272,7 @@ int fts_indexer_init(struct fts_backend *backend, struct mailbox *box,
 	return ctx->failed || ret < 0 ? -1 : 1;
 }
 
-#define INDEXER_HANDSHAKE "1\t0\tindexer\tindexer\n"
+#define INDEXER_HANDSHAKE "VERSION\tindexer\t1\t0\n"
 
 int fts_indexer_cmd(struct mail_user *user, const char *cmd,
 	            const char **path_r)