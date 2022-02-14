dovecot/core

commit d2ab26be6038bd53b13a3ff18c403d6c192c1d91

pop3: Add reason_code=pop3:cmd_<command name> to events

src/pop3/pop3-client.c
@@ -744,8 +744,13 @@ bool client_handle_input(struct client *client)
 			*args++ = '\0';
 
 		T_BEGIN {
+			const char *reason_code =
+				event_reason_code_prefix("pop3", "cmd_", line);
+			struct event_reason *reason =
+				event_reason_begin(reason_code);
 			ret = client_command_execute(client, line,
 						     args != NULL ? args : "");
+			event_reason_end(&reason);
 		} T_END;
 		if (ret >= 0) {
 			client->bad_counter = 0;

commit 1309137812424c80e63d3c1052795b43d6e19803

pop3: Fix empty command handling
If empty command is given, error out instead of trying to create event.
This fixes a crash caused by d2ab26be6038bd53b13a3ff18c403d6c192c1d91.

src/pop3/pop3-client.c
@@ -741,8 +741,10 @@ bool client_handle_input(struct client *client)
 		args = strchr(line, ' ');
 		if (args != NULL)
 			*args++ = '\0';
-
-		T_BEGIN {
+		if (*line == '\0') {
+			client_send_line(client, "-ERR Unknown command.");
+			ret = -1;
+		} else T_BEGIN {
 			const char *reason_code =
 				event_reason_code_prefix("pop3", "cmd_", line);
 			struct event_reason *reason =