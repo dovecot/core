dovecot/core

commit f9627ea62b5cb471d9adde0cc188ee8dea93c172

indexer-worker: Register indexing session to anvil

src/indexer/master-connection.c
@@ -273,6 +273,14 @@ master_connection_cmd_index(struct master_connection *conn,
 		return -1;
 	}
 
+	struct master_service_anvil_session anvil_session;
+	guid_128_t anvil_conn_guid;
+	bool anvil_sent = FALSE;
+	mail_user_get_anvil_session(user, &anvil_session);
+	if (master_service_anvil_connect(master_service, &anvil_session,
+					 TRUE, anvil_conn_guid))
+		anvil_sent = TRUE;
+
 	indexer_worker_refresh_proctitle(user->username, mailbox, 0, 0);
 	struct event_reason *reason =
 		event_reason_begin("indexer:index_mailbox");
@@ -283,6 +291,11 @@ master_connection_cmd_index(struct master_connection *conn,
 	indexer_worker_refresh_proctitle(user->username, "(deinit)", 0, 0);
 	mail_user_deinit(&user);
 	mail_storage_service_user_unref(&service_user);
+
+	if (anvil_sent) {
+		master_service_anvil_disconnect(master_service, &anvil_session,
+						anvil_conn_guid);
+	}
 	indexer_worker_refresh_proctitle(NULL, NULL, 0, 0);
 	return ret;
 }

commit 902029cfd725fb22a681f8cc61f8cd8abda542e4

indexer: Free user after anvil disconnect
Fixes invalid use after free. Broken by f9627ea62b5cb471d9adde0cc188ee8dea93c172

src/indexer/master-connection.c
@@ -289,13 +289,14 @@ master_connection_cmd_index(struct master_connection *conn,
 	/* refresh proctitle before a potentially long-running
 	   user unref */
 	indexer_worker_refresh_proctitle(user->username, "(deinit)", 0, 0);
-	mail_user_deinit(&user);
-	mail_storage_service_user_unref(&service_user);
 
 	if (anvil_sent) {
 		master_service_anvil_disconnect(master_service, &anvil_session,
 						anvil_conn_guid);
 	}
+
+	mail_user_deinit(&user);
+	mail_storage_service_user_unref(&service_user);
 	indexer_worker_refresh_proctitle(NULL, NULL, 0, 0);
 	return ret;
 }