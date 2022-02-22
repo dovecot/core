dovecot/core

commit 9372e48b702a3af5705785e08fbf47b0e37f2047

lib: If log writing to log process is blocking, show it in process title
This change also makes the log pipe non-blocking.

src/lib/failures.c
@@ -5,6 +5,8 @@
 #include "str.h"
 #include "hostpid.h"
 #include "net.h"
+#include "fd-set-nonblock.h"
+#include "process-title.h"
 #include "lib-signals.h"
 #include "backtrace-string.h"
 #include "printf-format-fix.h"
@@ -123,16 +125,31 @@ static int log_fd_write(int fd, const unsigned char *data, size_t len)
 			return -1;
 		}
 		switch (errno) {
-		case EAGAIN:
+		case EAGAIN: {
 			/* wait until we can write more. this can happen at
 			   least when writing to terminal, even if fd is
-			   blocking. */
+			   blocking. Internal logging fd is also now
+			   non-blocking, so we can show warnings about blocking
+			   on a log write. */
+			const char *title, *old_title =
+				t_strdup(process_title_get());
+
+			if (old_title == NULL)
+				title = "[blocking on log write]";
+			else
+				title = t_strdup_printf("%s - [blocking on log write]",
+							old_title);
+			process_title_set(title);
+
 			ioloop = io_loop_create();
 			io = io_add(fd, IO_WRITE, log_fd_flush_stop, ioloop);
 			io_loop_run(ioloop);
 			io_remove(&io);
 			io_loop_destroy(&ioloop);
+
+			process_title_set(old_title);
 			break;
+		}
 		case EINTR:
 			if (prev_signal_term_counter == signal_term_counter) {
 				/* non-terminal signal. ignore. */
@@ -680,6 +697,7 @@ i_internal_error_handler(const struct failure_context *ctx,
 
 void i_set_failure_internal(void)
 {
+	fd_set_nonblock(STDERR_FILENO, TRUE);
 	i_set_fatal_handler(i_internal_fatal_handler);
 	i_set_error_handler(i_internal_error_handler);
 	i_set_info_handler(i_internal_error_handler);

commit 6cd6732f3f0c4b149c7174decd46a40c4183a91d

lib: Fix losing log prefix or IP change when log process is busy
The fd for writing to log process is non-blocking. When sending options
to log process, it was done with write_full(), which stopped whenever
EAGAIN was returned. This error was simply ignored, and the logging code
thought that the prefix was successfully changed (or the IP was sent).

This mainly caused a problem when processes were reused and log process
was busy. The prefix update could have failed, and the following logging
would be using the previous session's log prefix, i.e. log lines could have
been logged for the wrong user/session.

Broken by 9372e48b702a3af5705785e08fbf47b0e37f2047

src/lib/failures.c
@@ -724,7 +724,8 @@ static void i_failure_send_option_forced(const char *key, const char *value)
 
 	str = t_strdup_printf("\001%c%s %s=%s\n", LOG_TYPE_OPTION+1,
 			      my_pid, key, value);
-	(void)write_full(STDERR_FILENO, str, strlen(str));
+	(void)log_fd_write(STDERR_FILENO, (const unsigned char *)str,
+			   strlen(str));
 }
 
 static void i_failure_send_option(const char *key, const char *value)