dovecot/core

commit 6d16e82fa2c352974558e1415d06f07d8962fe09

imapc: imapc_connection_disconnect_full() - Prevent leaking cmd->pool
If the imapc connection was already disconnected when calling
imapc_connection_disconnect() it didn't clean up the commands and
especially the command pools.

src/lib-imap-client/imapc-connection.c
@@ -446,6 +446,7 @@ void imapc_connection_disconnect_full(struct imapc_connection *conn,
 
 	if (conn->state == IMAPC_CONNECTION_STATE_DISCONNECTED) {
 		i_assert(array_count(&conn->cmd_wait_list) == 0);
+		imapc_connection_abort_commands(conn, NULL, reconnecting);
 		return;
 	}
 

commit 581443c4f136240843641b6a11da683c1f200847

imapc: imapc_connection_disconnect_full() - Only conditionally abort
Only abort commands if there are no reconnect commands. Otherwise these
still will be handled and the commands memory will be freed after handling
them.

Broken by 6d16e82fa2c352974558e1415d06f07d8962fe09

src/lib-imap-client/imapc-connection.c
@@ -446,7 +446,9 @@ void imapc_connection_disconnect_full(struct imapc_connection *conn,
 
 	if (conn->state == IMAPC_CONNECTION_STATE_DISCONNECTED) {
 		i_assert(array_count(&conn->cmd_wait_list) == 0);
-		imapc_connection_abort_commands(conn, NULL, reconnecting);
+		if (conn->reconnect_command_count == 0)
+			imapc_connection_abort_commands(conn, NULL,
+						reconnecting);
 		return;
 	}
 