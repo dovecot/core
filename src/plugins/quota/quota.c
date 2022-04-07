dovecot/core

commit 9926b729694421daeb97769a925f0f273b83b0a9

quota: Remove plugin/quota_vsizes setting

src/plugins/quota/quota.c
@@ -311,7 +311,6 @@ int quota_user_read_settings(struct mail_user *user,
 		mail_user_plugin_getenv(user, "quota_exceeded_message");
 	if (quota_set->quota_exceeded_msg == NULL)
 		quota_set->quota_exceeded_msg = DEFAULT_QUOTA_EXCEEDED_MSG;
-	quota_set->vsizes = mail_user_plugin_getenv_bool(user, "quota_vsizes");
 
 	const char *max_size = mail_user_plugin_getenv(user,
 						       "quota_max_mail_size");
@@ -463,6 +462,11 @@ int quota_init(struct quota_settings *quota_set, struct mail_user *user,
 		}
 		if (ret > 0)
 			array_push_back(&quota->roots, &root);
+		/* If a quota backend needs virtual size instead of physical
+		   size, use this for all backends. This is not ideal, but
+		   works. */
+		if (root->set->backend->use_vsize)
+			quota->set->vsizes = TRUE;
 	}
 	*quota_r = quota;
 	return 0;

src/config/old-set-parser.c
@@ -652,6 +652,10 @@ old_settings_handle_path(struct config_parser_context *ctx,
 				t_strdup_printf("plugin/0/imap_compress_deflate_level=%s", value), NULL);
 			return TRUE;
 		}
+		if (strcmp(key, "quota_vsizes") == 0) {
+			obsolete(ctx, "%s is no longer needed", key);
+			return TRUE;
+		}
 	}
 	return FALSE;
 }

src/plugins/quota/quota-count.c
@@ -240,10 +240,6 @@ static struct quota_root *count_quota_alloc(void)
 static int count_quota_init(struct quota_root *root, const char *args,
 			    const char **error_r)
 {
-	if (!root->quota->set->vsizes) {
-		*error_r = "quota count backend requires quota_vsizes=yes";
-		return -1;
-	}
 	event_set_append_log_prefix(root->backend.event, "quota-count: ");
 
 	root->auto_updating = TRUE;

commit 33214f6dcadc780f56f6c8143762dbd9653c2c46

quota: Only check vsize flag if quota driver succesfully initialized
Broken by 9926b729694421daeb97769a925f0f273b83b0a9

src/plugins/quota/quota.c
@@ -460,13 +460,14 @@ int quota_init(struct quota_settings *quota_set, struct mail_user *user,
 			quota_deinit(&quota);
 			return -1;
 		}
-		if (ret > 0)
+		if (ret > 0) {
 			array_push_back(&quota->roots, &root);
-		/* If a quota backend needs virtual size instead of physical
-		   size, use this for all backends. This is not ideal, but
-		   works. */
-		if (root->set->backend->use_vsize)
-			quota->set->vsizes = TRUE;
+			/* If a quota backend needs virtual size instead of physical
+			   size, use this for all backends. This is not ideal, but
+			   works. */
+			if (root->set->backend->use_vsize)
+				quota->set->vsizes = TRUE;
+		}
 	}
 	*quota_r = quota;
 	return 0;