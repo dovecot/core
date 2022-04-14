dovecot/core

commit 95c63f3e9efdb7ba916dac3e6b013d131392c48a

login-common: Replace i_error() with e_error() where possible

src/login-common/client-common.c
@@ -154,7 +154,7 @@ static bool client_is_trusted(struct client *client)
 	net = t_strsplit_spaces(client->set->login_trusted_networks, ", ");
 	for (; *net != NULL; net++) {
 		if (net_parse_range(*net, &net_ip, &bits) < 0) {
-			i_error("login_trusted_networks: "
+			e_error(client->event, "login_trusted_networks: "
 				"Invalid network '%s'", *net);
 			break;
 		}

src/login-common/client-common-auth.c
@@ -165,7 +165,8 @@ static void client_auth_parse_args(struct client *client, bool success,
 			reply_r->source_ip = value;
 		else if (strcmp(key, "port") == 0) {
 			if (net_str2port(value, &reply_r->port) < 0) {
-				i_error("Auth service returned invalid "
+				e_error(client->event,
+					"Auth service returned invalid "
 					"port number: %s", value);
 			}
 		} else if (strcmp(key, "destuser") == 0)
@@ -174,13 +175,15 @@ static void client_auth_parse_args(struct client *client, bool success,
 			reply_r->password = value;
 		else if (strcmp(key, "proxy_timeout") == 0) {
 			if (str_to_uint(value, &reply_r->proxy_timeout_msecs) < 0) {
-				i_error("BUG: Auth service returned invalid "
+				e_error(client->event,
+					"BUG: Auth service returned invalid "
 					"proxy_timeout value: %s", value);
 			}
 			reply_r->proxy_timeout_msecs *= 1000;
 		} else if (strcmp(key, "proxy_refresh") == 0) {
 			if (str_to_uint(value, &reply_r->proxy_refresh_secs) < 0) {
-				i_error("BUG: Auth service returned invalid "
+				e_error(client->event,
+					"BUG: Auth service returned invalid "
 					"proxy_refresh value: %s", value);
 			}
 		} else if (strcmp(key, "proxy_mech") == 0)
@@ -734,7 +737,7 @@ sasl_callback(struct client *client, enum sasl_server_reply sasl_reply,
 		/* the fd may still be hanging somewhere in kernel or another
 		   process. make sure the client gets disconnected. */
 		if (shutdown(client->fd, SHUT_RDWR) < 0 && errno != ENOTCONN)
-			i_error("shutdown() failed: %m");
+			e_error(client->event, "shutdown() failed: %m");
 
 		if (data != NULL) {
 			/* e.g. mail_max_userip_connections is reached */

src/login-common/login-proxy.c
@@ -850,7 +850,8 @@ static bool director_username_hash(struct client *client, unsigned int *hash_r)
 				   client->set->director_username_hash,
 				   &client->director_username_hash_cache,
 				   &error)) {
-		i_error("Failed to expand director_username_hash=%s: %s",
+		e_error(client->event,
+			"Failed to expand director_username_hash=%s: %s",
 			client->set->director_username_hash, error);
 		return FALSE;
 	}

commit b13f915dd3656d4611d8a419ef5734cf71ceba7b

login-common: Fix crash with invalid login_trusted_networks setting
client_is_trusted() uses the event to log an error.

Broken in 95c63f3e9efdb7ba916dac3e6b013d131392c48a

src/login-common/client-common.c
@@ -204,6 +204,16 @@ client_alloc(int fd, pool_t pool,
 	client->real_remote_ip = conn->real_remote_ip;
 	client->real_remote_port = conn->real_remote_port;
 	client->listener_name = p_strdup(client->pool, conn->name);
+	/* This event must exist before client_is_trusted() is called */
+	client->event = event_create(NULL);
+	event_add_category(client->event, &login_binary->event_category);
+	event_add_str(client->event, "local_ip", net_ip2addr(&conn->local_ip));
+	event_add_int(client->event, "local_port", conn->local_port);
+	event_add_str(client->event, "remote_ip", net_ip2addr(&conn->remote_ip));
+	event_add_int(client->event, "remote_port", conn->remote_port);
+	event_add_str(client->event, "service", login_binary->protocol);
+	event_set_log_message_callback(client->event, client_log_msg_callback,
+				       client);
 	client->trusted = client_is_trusted(client);
 
 	if (conn->proxied) {
@@ -218,15 +228,6 @@ client_alloc(int fd, pool_t pool,
 	}
 	client->proxy_ttl = LOGIN_PROXY_TTL;
 
-	client->event = event_create(NULL);
-	event_add_category(client->event, &login_binary->event_category);
-	event_add_str(client->event, "local_ip", net_ip2addr(&conn->local_ip));
-	event_add_int(client->event, "local_port", conn->local_port);
-	event_add_str(client->event, "remote_ip", net_ip2addr(&conn->remote_ip));
-	event_add_int(client->event, "remote_port", conn->remote_port);
-	event_add_str(client->event, "service", login_binary->protocol);
-	event_set_log_message_callback(client->event, client_log_msg_callback,
-				       client);
 
 	client_open_streams(client);
 	return client;