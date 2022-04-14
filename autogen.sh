4e4816f1c032a2e3652e9941ffcd5e9e3195c487
Karl Fleischmann <karl.fleischmann@open-xchange.com>
2022-04-14 14:21:01 +0200

    global: Remove wiki from packaging

--- autogen.sh
+++ autogen.sh
@@ -12,24 +12,6 @@ if ! test -f build-aux/config.rpath; then
   done
 fi
 
-if test ! -f doc/wiki/Authentication.txt; then
-  cd doc
-  wget https://www.dovecot.org/tmp/wiki2-export.tar.gz
-  tar xzf wiki2-export.tar.gz
-  if [ $? != 0 ]; then
-    echo "Failed to uncompress wiki docs"
-    exit
-  fi
-  mv wiki2-export/*.txt wiki/
-  rm -rf wiki2-export wiki2-export.tar.gz
-  cd ..
-fi
-
-cd doc/wiki
-cp -f Makefile.am.in Makefile.am
-echo *.txt | sed 's, , \\/	,g' | tr '/' '\n' >> Makefile.am
-cd ../..
-
 autoreconf -i
 
 rm -f ChangeLog
