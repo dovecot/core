c451f1fcab050fb5b31522d9a898ea4c99ce6229
Timo Sirainen <timo.sirainen@open-xchange.com>
2022-06-20 22:27:15 +0300

    lib-imap: fuzz-imap-bodystructure - Fix parsing to be used correctly
    imap_bodystructure_parse_full() should be used to generate the message_parts
    correctly based on the input string. This fixes wrongly triggered panics.

--- src/lib-imap/fuzz-imap-bodystructure.c
+++ src/lib-imap/fuzz-imap-bodystructure.c
@@ -22,23 +22,22 @@ FUZZ_BEGIN_STR(const char *str)
 {
 	pool_t pool =
 		pool_alloconly_create(MEMPOOL_GROWING"fuzz bodystructure", 1024);
-	struct message_part parts;
+	struct message_part *parts = NULL;
 	string_t *dest = str_new(pool, 32);
 	const char *error ATTR_UNUSED;
-	i_zero(&parts);
 
-	if (imap_bodystructure_parse(str, pool, &parts, &error) == 0) {
-		if (imap_bodystructure_write(&parts, dest, TRUE, &error) < 0)
+	if (imap_bodystructure_parse_full(str, pool, &parts, &error) == 0) {
+		if (imap_bodystructure_write(parts, dest, TRUE, &error) < 0)
 			i_panic("Failed to write bodystructure: %s", error);
 		/* The written bodystructure must be parseable *and*
 		   it must come out exactly the same again */
-		if (imap_bodystructure_parse(str_c(dest), pool, &parts, &error) != 0) {
+		if (imap_bodystructure_parse(str_c(dest), pool, parts, &error) != 0) {
 			i_panic("Failed to reparse bodystructure '%s'",
 				str_sanitize_binary(str_c(dest)));
 		} else {
 			const char *new_str = t_strdup(str_c(dest));
 			str_truncate(dest, 0);
-			if (imap_bodystructure_write(&parts, dest, TRUE, &error) < 0)
+			if (imap_bodystructure_write(parts, dest, TRUE, &error) < 0)
 				i_panic("Failed to write reparsed bodystructure: %s", error);
 			if (strcmp(str_c(dest), new_str) != 0) {
 				i_panic("Parsed bodystructure '%s' does not match '%s'",
