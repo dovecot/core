dovecot/core

commit 838e65f96678a4a390c94f0a59e1ec489ee532d2

auth/db-ldap.h: Define LDAP_DEPRECATED in m4/want_ldap.m4 if enabled
This will supply this macro to all places where db-ldap.h is not
included. This is necessary to make the project compile on FreeBSD.

m4/want_ldap.m4
@@ -40,6 +40,10 @@ AC_DEFUN([DOVECOT_WANT_LDAP], [
       AUTH_LIBS="$AUTH_LIBS $LDAP_LIBS"
       DICT_LIBS="$DICT_LIBS $LDAP_LIBS"
       AC_DEFINE(BUILTIN_LDAP,, [LDAP support is built in])
+      AC_DEFINE(LDAP_DEPRECATED, [1], [
+        Functions like ldap_bind() have been deprecated in OpenLDAP 2.3
+        This define enables them until the code here can be refactored
+      ])
     ])
 
     AC_CHECK_LIB(ldap, ldap_initialize, [

src/auth/db-ldap.h
@@ -2,8 +2,9 @@
 #define DB_LDAP_H
 
 /* Functions like ldap_bind() have been deprecated in OpenLDAP 2.3
-   This define enables them until the code here can be refactored */
-#define LDAP_DEPRECATED 1
+   This define enables them until the code here can be refactored
+   It is now set in m4/want_ldap.m4 if ldap is enabled. */
+/* #define LDAP_DEPRECATED 1 */
 
 /* Maximum number of pending requests before delaying new requests. */
 #define DB_LDAP_MAX_PENDING_REQUESTS 8

commit 355e4a1951bdbc06948a5b33ca1261c39c60da1f

want_ldap.m4: Define LDAP_DEPRECATED unconditionally when ldap is enabled
Regression was introduced in 838e65f96678a4a390c94f0a59e1ec489ee532d2.

m4/want_ldap.m4
@@ -40,10 +40,11 @@ AC_DEFUN([DOVECOT_WANT_LDAP], [
       AUTH_LIBS="$AUTH_LIBS $LDAP_LIBS"
       DICT_LIBS="$DICT_LIBS $LDAP_LIBS"
       AC_DEFINE(BUILTIN_LDAP,, [LDAP support is built in])
-      AC_DEFINE(LDAP_DEPRECATED, [1], [
-        Functions like ldap_bind() have been deprecated in OpenLDAP 2.3
-        This define enables them until the code here can be refactored
-      ])
+    ])
+
+    AC_DEFINE(LDAP_DEPRECATED, [1], [
+      Functions like ldap_bind() have been deprecated in OpenLDAP 2.3
+      This define enables them until the code here can be refactored
     ])
 
     AC_CHECK_LIB(ldap, ldap_initialize, [