669b5b3fc810600b812bba8a594be7ac27f7add0
Marco Bettini <marco.bettini@open-xchange.com>
2022-07-07 12:36:56 +0000

    login-common: login_settings_check() - Stop cascading auth_debug_passwords -> auth_debug -> auth_verbose

--- src/login-common/login-settings.c
+++ src/login-common/login-settings.c
@@ -106,10 +106,6 @@ static bool login_settings_check(void *_set, pool_t pool,
 	set->log_format_elements_split =
 		p_strsplit(pool, set->login_log_format_elements, " ");
 
-	if (set->auth_debug_passwords)
-		set->auth_debug = TRUE;
-	if (set->auth_debug)
-		set->auth_verbose = TRUE;
 	return TRUE;
 }
 /* </settings checks> */
