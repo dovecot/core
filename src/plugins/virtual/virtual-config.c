dovecot/core

commit 8536cd46be13bc18aa538b279d7fdba7d54e268f

virtual: If virtual mailbox has no config file, assume it's just nonexistent. Most importantly if the virtual namespace root doesn't have the config, it should fail with MAIL_ERROR_NOTFOUND rather than MAIL_ERROR_NOTPOSSIBLE.

src/plugins/virtual/virtual-config.c
@@ -356,7 +356,6 @@ int virtual_config_read(struct virtual_mailbox *mbox)
 {
 	struct mail_storage *storage = mbox->box.storage;
 	struct virtual_parse_context ctx;
-	struct stat st;
 	const char *box_path, *path, *line, *error;
 	unsigned int linenum = 0;
 	int fd, ret = 0;
@@ -374,9 +373,6 @@ int virtual_config_read(struct virtual_mailbox *mbox)
 		} else if (errno != ENOENT) {
 			mail_storage_set_critical(storage,
 						  "open(%s) failed: %m", path);
-		} else if (stat(box_path, &st) == 0) {
-			mail_storage_set_error(storage, MAIL_ERROR_NOTPOSSIBLE,
-				"Virtual mailbox missing configuration file");
 		} else if (errno == ENOENT) {
 			mail_storage_set_error(storage, MAIL_ERROR_NOTFOUND,
 				T_MAIL_ERR_MAILBOX_NOT_FOUND(mbox->box.vname));

commit b4f07d48c577d4d6beae5f3e28c1409888e155d0

virtual: Remove dead code
Caused by 8536cd46be13bc18aa538b279d7fdba7d54e268f

src/plugins/virtual/virtual-config.c
@@ -485,12 +485,9 @@ int virtual_config_read(struct virtual_mailbox *mbox)
 		} else if (errno != ENOENT) {
 			mailbox_set_critical(&mbox->box,
 					     "open(%s) failed: %m", path);
-		} else if (errno == ENOENT) {
+		} else {
 			mail_storage_set_error(storage, MAIL_ERROR_NOTFOUND,
 				T_MAIL_ERR_MAILBOX_NOT_FOUND(mbox->box.vname));
-		} else {
-			mailbox_set_critical(&mbox->box,
-				"stat(%s) failed: %m", box_path);
 		}
 		return -1;
 	}