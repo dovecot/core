dovecot/core

commit 24c1c355bb46a3a824573467f887972ad13a33cb

lib-doveadm, doveadm: Add and require doveadm_client_unref()
The only way to destroy a doveadm_client before was to send a command.

src/doveadm/doveadm-mail-server.c
@@ -357,6 +357,7 @@ doveadm_cmd_redirect_finish(struct doveadm_mail_server_cmd *servercmd,
 	cmd_set.forward_fields = doveadm_mail_get_outgoing_forward_fields(cmd_ctx);
 	doveadm_client_cmd(conn, &cmd_set, servercmd->cmdline, servercmd->input,
 			   doveadm_cmd_callback, servercmd);
+	doveadm_client_unref(&conn);
 	return 1;
 }
 
@@ -602,6 +603,7 @@ static int doveadm_mail_server_request_queue_handle_next(const char **error_r)
 	doveadm_mail_server_handle(request_copy.server, conn,
 				   request_copy.username);
 	doveadm_server_request_free(&request_copy);
+	doveadm_client_unref(&conn);
 	return 0;
 }
 
@@ -802,6 +804,7 @@ int doveadm_mail_server_user(struct doveadm_mail_cmd_context *ctx,
 		} else {
 			doveadm_mail_server_handle(server, conn,
 						   proxy_set.username);
+			doveadm_client_unref(&conn);
 		}
 	} else {
 		request = array_append_space(&doveadm_server_request_queue);

src/doveadm/doveadm-dsync.c
@@ -988,6 +988,7 @@ dsync_connect_tcp(struct dsync_cmd_context *ctx,
 		}
 
 		dsync_server_run_command(ctx, conn);
+		doveadm_client_unref(&conn);
 	}
 
 	doveadm_clients_destroy_all();

src/lib-doveadm/doveadm-client.c
@@ -28,6 +28,7 @@ enum doveadm_client_reply_state {
 struct doveadm_client {
 	struct connection conn;
 
+	int refcount;
 	struct doveadm_client_settings set;
 
 	pool_t pool;
@@ -48,6 +49,7 @@ struct doveadm_client {
 
 	enum doveadm_client_reply_state state;
 
+	bool destroyed:1;
 	bool authenticate_sent:1;
 	bool authenticated:1;
 	bool ssl_done:1;
@@ -656,6 +658,7 @@ int doveadm_client_create(const struct doveadm_client_settings *set,
 	pool = pool_alloconly_create("doveadm server connection", 1024*16);
 	conn = p_new(pool, struct doveadm_client, 1);
 	conn->pool = pool;
+	conn->refcount = 1;
 	doveadm_client_settings_dup(set, &conn->set, pool);
 
 	if (set->socket_path != NULL) {
@@ -701,12 +704,9 @@ void doveadm_client_set_print(struct doveadm_client *conn,
 	conn->print_context = context;
 }
 
-static void doveadm_client_destroy(struct doveadm_client **_conn)
+static void doveadm_client_destroy_int(struct doveadm_client *conn)
 {
-	struct doveadm_client *conn = *_conn;
 	const char *error;
-
-	*_conn = NULL;
 
 	if (conn->callback != NULL) {
 		error = conn->ssl_iostream == NULL ? NULL :
@@ -737,6 +737,30 @@ static void doveadm_client_destroy(struct doveadm_client **_conn)
 
 	connection_deinit(&conn->conn);
 	ssl_iostream_context_unref(&conn->set.ssl_ctx);
+}
+
+static void doveadm_client_destroy(struct doveadm_client **_conn)
+{
+	struct doveadm_client *conn = *_conn;
+
+	*_conn = NULL;
+
+	conn->destroyed = TRUE;
+	doveadm_client_destroy_int(conn);
+	doveadm_client_unref(&conn);
+}
+
+void doveadm_client_unref(struct doveadm_client **_conn)
+{
+	struct doveadm_client *conn = *_conn;
+
+	*_conn = NULL;
+
+	i_assert(conn->refcount > 0);
+	if (--conn->refcount > 0)
+		return;
+	if (!conn->destroyed)
+		doveadm_client_destroy_int(conn);
 	pool_unref(&conn->pool);
 }
 
@@ -790,6 +814,8 @@ void doveadm_client_cmd(struct doveadm_client *conn,
 	}
 	conn->callback = callback;
 	conn->context = context;
+	/* doveadm_client_destroy() will be called to unreference */
+	conn->refcount++;
 }
 
 void doveadm_client_extract(struct doveadm_client *conn,

src/lib-doveadm/doveadm-client.h
@@ -68,6 +68,7 @@ void doveadm_client_settings_dup(const struct doveadm_client_settings *src,
 int doveadm_client_create(const struct doveadm_client_settings *set,
 			  struct doveadm_client **conn_r,
 			  const char **error_r);
+void doveadm_client_unref(struct doveadm_client **conn);
 
 void doveadm_client_get_dest(struct doveadm_client *conn,
 			     struct ip_addr *ip_r, in_port_t *port_r);

commit 93f156904ef53964a29eca39a27aad4ce4837cb7

doveadm: Add missing doveadm_client_unref()
doveadm_client_unref() got required to be called, but got forgotten to
be added in doveadm_cmd_callback.
Broken in 24c1c355bb46a3a824573467f887972ad13a33cb

src/doveadm/doveadm-mail-server.c
@@ -535,6 +535,7 @@ static void doveadm_cmd_callback(const struct doveadm_server_reply *reply,
 			doveadm_mail_server_handle(server, conn,
 						   request_copy.username);
 			doveadm_server_request_free(&request_copy);
+			doveadm_client_unref(&conn);
 			break;
 		}
 	}