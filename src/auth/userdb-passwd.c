dovecot/core

commit 0f5dc4da3982053036be65190e44bf28a67b1ca2

lib: API change - var_expand*() now returns error string.
This allows callers to fail properly if the format string is invalid.

src/auth/userdb-passwd.c
@@ -84,6 +84,7 @@ static void passwd_lookup(struct auth_request *auth_request,
 		(struct passwd_userdb_module *)_module;
 	struct passwd pw;
 	struct timeval start_tv;
+	const char *error;
 	int ret;
 
 	auth_request_log_debug(auth_request, AUTH_SUBSYS_DB, "lookup");
@@ -114,7 +115,11 @@ static void passwd_lookup(struct auth_request *auth_request,
 	auth_request_set_userdb_field(auth_request, "gid", dec2str(pw.pw_gid));
 	auth_request_set_userdb_field(auth_request, "home", pw.pw_dir);
 
-	userdb_template_export(module->tmpl, auth_request);
+	if (userdb_template_export(module->tmpl, auth_request, &error) < 0) {
+		auth_request_log_error(auth_request, AUTH_SUBSYS_DB,
+				       "Failed to expand template: %s", error);
+		callback(USERDB_RESULT_INTERNAL_FAILURE, auth_request);
+	}
 
 	callback(USERDB_RESULT_OK, auth_request);
 }

src/auth/userdb-static.c
@@ -27,8 +27,13 @@ static void static_lookup_real(struct auth_request *auth_request,
 	struct userdb_module *_module = auth_request->userdb->userdb;
 	struct static_userdb_module *module =
 		(struct static_userdb_module *)_module;
+	const char *error;
 
-	userdb_template_export(module->tmpl, auth_request);
+	if (userdb_template_export(module->tmpl, auth_request, &error) < 0) {
+		auth_request_log_error(auth_request, AUTH_SUBSYS_DB,
+				       "Failed to expand template: %s", error);
+		callback(USERDB_RESULT_INTERNAL_FAILURE, auth_request);
+	}
 	callback(USERDB_RESULT_OK, auth_request);
 }
 

commit 02e1d09e416584dec94266c59df334fdf5b64fde

auth: userdb - Stop calling USERDB_RESULT_OK after template expansion failure
Call only USERDB_RESULT_INTERNAL_FAILURE. This was broken by
0f5dc4da3982053036be65190e44bf28a67b1ca2.

src/auth/userdb-passwd.c
@@ -118,6 +118,7 @@ static void passwd_lookup(struct auth_request *auth_request,
 		e_error(authdb_event(auth_request),
 			"Failed to expand template: %s", error);
 		callback(USERDB_RESULT_INTERNAL_FAILURE, auth_request);
+		return;
 	}
 
 	callback(USERDB_RESULT_OK, auth_request);

src/auth/userdb-static.c
@@ -33,6 +33,7 @@ static void static_lookup_real(struct auth_request *auth_request,
 		e_error(authdb_event(auth_request),
 			"Failed to expand template: %s", error);
 		callback(USERDB_RESULT_INTERNAL_FAILURE, auth_request);
+		return;
 	}
 	callback(USERDB_RESULT_OK, auth_request);
 }