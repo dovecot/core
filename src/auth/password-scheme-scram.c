39ff0e1ab2aa96dfa4b88981b290bb6a0b0bbd60
Stephan Bosch <stephan.bosch@open-xchange.com>
2022-09-26 22:16:35 +0200

    auth: password-scheme-scram - Move Hi() to auth-scram.c.

--- src/auth/password-scheme-scram.c
+++ src/auth/password-scheme-scram.c
@@ -25,32 +25,7 @@
 
 #define SCRAM_DEFAULT_ITERATE_COUNT 4096
 
-static void
-Hi(const struct hash_method *hmethod, const unsigned char *str, size_t str_size,
-   const unsigned char *salt, size_t salt_size, unsigned int i,
-   unsigned char *result)
-{
-	struct hmac_context ctx;
-	unsigned char U[hmethod->digest_size];
-	unsigned int j, k;
-
-	/* Calculate U1 */
-	hmac_init(&ctx, str, str_size, hmethod);
-	hmac_update(&ctx, salt, salt_size);
-	hmac_update(&ctx, "\0\0\0\1", 4);
-	hmac_final(&ctx, U);
-
-	memcpy(result, U, hmethod->digest_size);
-
-	/* Calculate U2 to Ui and Hi */
-	for (j = 2; j <= i; j++) {
-		hmac_init(&ctx, str, str_size, hmethod);
-		hmac_update(&ctx, U, sizeof(U));
-		hmac_final(&ctx, U);
-		for (k = 0; k < hmethod->digest_size; k++)
-			result[k] ^= U[k];
-	}
-}
+#include "auth-scram.c"
 
 int scram_scheme_parse(const struct hash_method *hmethod, const char *name,
 		       const unsigned char *credentials, size_t size,
