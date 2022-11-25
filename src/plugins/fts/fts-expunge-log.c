dovecot/core

commit 87b5c1fc03945708726c175d540fd307f7f78480

plugins/fts: expung-log - add the concept of not unlinking at the end No behavioural differences, the default is to unlink.
Signed-off-by: Phil Carmody <phil@dovecot.fi>

src/plugins/fts/fts-expunge-log.c
@@ -61,6 +61,7 @@ struct fts_expunge_log_read_ctx {
 
 	bool failed;
 	bool corrupted;
+	bool unlink;
 };
 
 struct fts_expunge_log *fts_expunge_log_init(const char *path)
@@ -348,6 +349,7 @@ fts_expunge_log_read_begin(struct fts_expunge_log *log)
 		ctx->failed = TRUE;
 	else if (log->fd != -1)
 		ctx->input = i_stream_create_fd(log->fd, (size_t)-1, FALSE);
+	ctx->unlink = TRUE;
 	return ctx;
 }
 
@@ -395,7 +397,8 @@ fts_expunge_log_read_next(struct fts_expunge_log_read_ctx *ctx)
 	(void)i_stream_read_data(ctx->input, &data, &size, IO_BLOCK_SIZE);
 	if (size == 0 && ctx->input->stream_errno == 0) {
 		/* expected EOF - mark the file as read by unlinking it */
-		if (unlink(ctx->log->path) < 0 && errno != ENOENT)
+		if (ctx->unlink &&
+		    unlink(ctx->log->path) < 0 && errno != ENOENT)
 			i_error("unlink(%s) failed: %m", ctx->log->path);
 
 		/* try reading again, in case something new was written */
@@ -462,7 +465,8 @@ int fts_expunge_log_read_end(struct fts_expunge_log_read_ctx **_ctx)
 	*_ctx = NULL;
 
 	if (ctx->corrupted) {
-		if (unlink(ctx->log->path) < 0 && errno != ENOENT)
+		if (ctx->unlink &&
+		    unlink(ctx->log->path) < 0 && errno != ENOENT)
 			i_error("unlink(%s) failed: %m", ctx->log->path);
 	}
 

commit 42e29be8c2ad6b045e832f91e4191b9bd689604f

fts: fts_expunge_log_read_end() - Always unlink corrupted log files
Broken by 87b5c1fc03945708726c175d540fd307f7f78480

src/plugins/fts/fts-expunge-log.c
@@ -524,10 +524,8 @@ int fts_expunge_log_read_end(struct fts_expunge_log_read_ctx **_ctx)
 
 	*_ctx = NULL;
 
-	if (ctx->corrupted) {
-		if (ctx->unlink)
-			i_unlink_if_exists(ctx->log->path);
-	}
+	if (ctx->corrupted)
+		i_unlink_if_exists(ctx->log->path);
 
 	i_stream_unref(&ctx->input);
 	i_free(ctx);