dovecot/core

commit 0a3997e1c64f48529338a9f93d7c39381dfe4f72

login-common: Replace i_<log>() with e_<log>()

src/login-common/client-common.c
@@ -1005,7 +1005,8 @@ client_get_log_str(struct client *client, const char *msg)
 		    !expand_error_logged) {
 			/* NOTE: Don't log via client->event - it would cause
 			   recursion */
-			i_error("Failed to expand log_format_elements=%s: %s",
+			e_error(client->event,
+				"Failed to expand log_format_elements=%s: %s",
 				*e, error);
 			expand_error_logged = TRUE;
 		}
@@ -1042,7 +1043,8 @@ client_get_log_str(struct client *client, const char *msg)
 	if (var_expand(str, client->set->login_log_format, tab, &error) <= 0) {
 		/* NOTE: Don't log via client->event - it would cause
 		   recursion */
-		i_error("Failed to expand login_log_format=%s: %s",
+		e_error(client->event,
+			"Failed to expand login_log_format=%s: %s",
 			client->set->login_log_format, error);
 		expand_error_logged = TRUE;
 	}

src/login-common/login-proxy-state.c
@@ -16,6 +16,7 @@ struct login_proxy_state {
 	HASH_TABLE(struct login_proxy_record *,
 		   struct login_proxy_record *) hash;
 	pool_t pool;
+	struct event *event;
 
 	const char *notify_path;
 	int notify_fd;
@@ -46,6 +47,7 @@ struct login_proxy_state *login_proxy_state_init(const char *notify_path)
 
 	state = i_new(struct login_proxy_state, 1);
 	state->pool = pool_alloconly_create("login proxy state", 1024);
+	state->event = event_create(NULL);
 	hash_table_create(&state->hash, state->pool, 0,
 			  login_proxy_record_hash, login_proxy_record_cmp);
 	state->notify_path = p_strdup(state->pool, notify_path);
@@ -75,6 +77,7 @@ void login_proxy_state_deinit(struct login_proxy_state **_state)
 	timeout_remove(&state->to_reopen);
 	login_proxy_state_close(state);
 	hash_table_destroy(&state->hash);
+	event_unref(&state->event);
 	pool_unref(&state->pool);
 	i_free(state);
 }
@@ -114,7 +117,7 @@ static int login_proxy_state_notify_open(struct login_proxy_state *state)
 
 	state->notify_fd = open(state->notify_path, O_WRONLY);
 	if (state->notify_fd == -1) {
-		i_error("open(%s) failed: %m", state->notify_path);
+		e_error(state->event, "open(%s) failed: %m", state->notify_path);
 		state->to_reopen = timeout_add(NOTIFY_RETRY_REOPEN_MSECS,
 					       login_proxy_state_reopen, state);
 		return -1;
@@ -145,9 +148,10 @@ static bool login_proxy_state_try_notify(struct login_proxy_state *state,
 
 	if (ret != (ssize_t)len) {
 		if (ret < 0)
-			i_error("write(%s) failed: %m", state->notify_path);
+			e_error(state->event, "write(%s) failed: %m",
+				state->notify_path);
 		else {
-			i_error("write(%s) wrote partial update",
+			e_error(state->event, "write(%s) wrote partial update",
 				state->notify_path);
 		}
 		login_proxy_state_close(state);

commit bb855e2094d14548fcbbae3be45e12924a4ec195

login-common: Fix for infinite recursion on invalid var expansion in login_log_format_elements and/or login_log_format
Broken by 0a3997e1c64f48529338a9f93d7c39381dfe4f72

src/login-common/client-common.c
@@ -1027,8 +1027,7 @@ client_get_log_str(struct client *client, const char *msg)
 		    !expand_error_logged) {
 			/* NOTE: Don't log via client->event - it would cause
 			   recursion */
-			e_error(client->event,
-				"Failed to expand log_format_elements=%s: %s",
+			i_error("Failed to expand log_format_elements=%s: %s",
 				*e, error);
 			expand_error_logged = TRUE;
 		}
@@ -1065,8 +1064,7 @@ client_get_log_str(struct client *client, const char *msg)
 	if (var_expand(str, client->set->login_log_format, tab, &error) <= 0) {
 		/* NOTE: Don't log via client->event - it would cause
 		   recursion */
-		e_error(client->event,
-			"Failed to expand login_log_format=%s: %s",
+		i_error("Failed to expand login_log_format=%s: %s",
 			client->set->login_log_format, error);
 		expand_error_logged = TRUE;
 	}