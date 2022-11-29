dovecot/core

commit a995ca1185afe80dd6819a856ca079ca4aae5126

lib-smtp: Add XCLIENT CLIENT-TRANSPORT field

src/lib-smtp/smtp-server-connection.c
@@ -1528,6 +1528,7 @@ void smtp_server_connection_get_proxy_data(struct smtp_server_connection *conn,
 		proxy_data->helo = conn->helo.domain;
 	proxy_data->login = conn->username;
 	proxy_data->session = conn->session_id;
+	proxy_data->client_transport = conn->client_transport;
 
 	if (conn->proxy_proto != SMTP_PROXY_PROTOCOL_UNKNOWN)
 		proxy_data->proto = conn->proxy_proto;
@@ -1572,6 +1573,10 @@ void smtp_server_connection_set_proxy_data(
 		        conn->session_id, proxy_data->session);
 		i_free(conn->session_id);
 		conn->session_id = i_strdup(proxy_data->session);
+	}
+	if (proxy_data->client_transport != NULL) {
+		i_free(conn->client_transport);
+		conn->client_transport = i_strdup(proxy_data->client_transport);
 	}
 
 	if (proxy_data->ttl_plus_1 > 0)

src/lib-smtp/smtp-client-connection.c
@@ -657,6 +657,13 @@ void smtp_client_connection_send_xclient(struct smtp_client_connection *conn)
 						   "SESSION", xclient->session);
 	}
 
+	/* CLIENT-TRANSPORT */
+	if (xclient->client_transport != NULL &&
+	    str_array_icase_find(xclient_args, "CLIENT-TRANSPORT")) {
+		smtp_client_connection_xclient_add(conn, str, offset,
+			"CLIENT-TRANSPORT", xclient->client_transport);
+	}
+
 	/* TTL */
 	if (xclient->ttl_plus_1 > 0 &&
 	    str_array_icase_find(xclient_args, "TTL")) {

src/lib-smtp/smtp-common.c
@@ -82,6 +82,8 @@ void smtp_proxy_data_merge(pool_t pool, struct smtp_proxy_data *dst,
 		dst->login = p_strdup(pool, src->login);
 	if (src->session != NULL && *src->session != '\0')
 		dst->session = p_strdup(pool, src->session);
+	if (src->client_transport != NULL && *src->client_transport != '\0')
+		dst->client_transport = p_strdup(pool, src->client_transport);
 	if (src->ttl_plus_1 > 0)
 		dst->ttl_plus_1 = src->ttl_plus_1;
 	if (src->timeout_secs > 0)

src/lib-smtp/smtp-common.h
@@ -96,6 +96,8 @@ struct smtp_proxy_data {
 	const char *helo, *login;
 	/* SESSION */
 	const char *session;
+	/* CLIENT-TRANSPORT */
+	const char *client_transport;
 
 	/* TTL: send as this -1, so the default 0 means "don't send it" */
 	unsigned int ttl_plus_1;

src/lib-smtp/smtp-server-cmd-xclient.c
@@ -185,6 +185,11 @@ void smtp_server_cmd_xclient(struct smtp_server_cmd_ctx *cmd,
 			if (strcasecmp(param.value, "[UNAVAILABLE]") == 0)
 				continue;
 			proxy_data->session = p_strdup(cmd->pool, param.value);
+		} else if (strcmp(param.keyword, "CLIENT-TRANSPORT") == 0) {
+			if (strcasecmp(param.value, "[UNAVAILABLE]") == 0)
+				continue;
+			proxy_data->client_transport =
+				p_strdup(cmd->pool, param.value);
 		} else if (strcmp(param.keyword, "TIMEOUT") == 0) {
 			if (str_to_uint(param.value,
 				&proxy_data->timeout_secs) < 0) {

src/lib-smtp/smtp-server-private.h
@@ -157,6 +157,7 @@ struct smtp_server_connection {
 	unsigned int proxy_ttl_plus_1;
 	unsigned int proxy_timeout_secs;
 	char *proxy_helo;
+	char *client_transport;
 
 	struct smtp_server_helo_data helo, *pending_helo;
 	char *helo_domain, *username;

src/lib-smtp/smtp-server-reply.c
@@ -856,7 +856,7 @@ void smtp_server_reply_ehlo_add_vrfy(struct smtp_server_reply *reply)
 void smtp_server_reply_ehlo_add_xclient(struct smtp_server_reply *reply)
 {
 	static const char *base_fields =
-		"ADDR PORT PROTO HELO LOGIN SESSION TTL TIMEOUT";
+		"ADDR PORT PROTO HELO LOGIN SESSION CLIENT-TRANSPORT TTL TIMEOUT";
 	struct smtp_server_cmd_ctx *cmd = &reply->command->context;
 	struct smtp_server_connection *conn = cmd->conn;
 

commit b64421faaaf7755de3a4aa38a1dbf9f91ffea903

lib-smtp: Free client_transport on destroy
Plug a memory leak introduced by
a995ca1185afe80dd6819a856ca079ca4aae5126

src/lib-smtp/smtp-server-connection.c
@@ -1123,6 +1123,7 @@ bool smtp_server_connection_unref(struct smtp_server_connection **_conn)
 	i_free(conn->helo_domain);
 	i_free(conn->username);
 	i_free(conn->session_id);
+	i_free(conn->client_transport);
 	event_unref(&conn->next_trans_event);
 	pool_unref(&conn->pool);
 	return FALSE;