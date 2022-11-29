b45872b1f5a7d771db20c1713c904a7307b14d38
Timo Sirainen <timo.sirainen@open-xchange.com>
2022-11-29 19:02:47 +0200

    config: Remove unused FILTERS command

--- src/config/config-filter.c
+++ src/config/config-filter.c
@@ -254,21 +254,6 @@ config_filter_find_all(struct config_filter_context *ctx, pool_t pool,
 }
 
 struct config_filter_parser *const *
-config_filter_get_all(struct config_filter_context *ctx)
-{
-	ARRAY_TYPE(config_filter_parsers) filters;
-	unsigned int i;
-
-	t_array_init(&filters, 8);
-	for (i = 0; ctx->parsers[i] != NULL; i++) {
-		array_push_back(&filters, &ctx->parsers[i]);
-	}
-	array_sort(&filters, config_filter_parser_cmp_rev);
-	array_append_zero(&filters);
-	return array_front(&filters);
-}
-
-struct config_filter_parser *const *
 config_filter_find_subset(struct config_filter_context *ctx,
 			  const struct config_filter *filter)
 {
