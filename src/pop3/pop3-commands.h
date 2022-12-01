795f36cf5339d9d7a7b5bf415296f42c837dafbc
Timo Sirainen <timo.sirainen@open-xchange.com>
2022-12-01 12:32:28 +0200

    pop3: Move pop3 commands to array and add pop3_command_find()

--- src/pop3/pop3-commands.h
+++ src/pop3/pop3-commands.h
@@ -1,6 +1,12 @@
 #ifndef POP3_COMMANDS_H
 #define POP3_COMMANDS_H
 
+struct pop3_command {
+	const char *name;
+	int (*func)(struct client *client, const char *args);
+};
+
+const struct pop3_command *pop3_command_find(const char *name);
 int client_command_execute(struct client *client,
 			   const char *name, const char *args);
 
