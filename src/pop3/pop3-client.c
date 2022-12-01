dovecot/core

commit d2ab26be6038bd53b13a3ff18c403d6c192c1d91

pop3: Add reason_code=pop3:cmd_<command name> to events

src/pop3/pop3-client.c
@@ -744,8 +744,13 @@ bool client_handle_input(struct client *client)
 			*args++ = '\0';
 
 		T_BEGIN {
+			const char *reason_code =
+				event_reason_code_prefix("pop3", "cmd_", line);
+			struct event_reason *reason =
+				event_reason_begin(reason_code);
 			ret = client_command_execute(client, line,
 						     args != NULL ? args : "");
+			event_reason_end(&reason);
 		} T_END;
 		if (ret >= 0) {
 			client->bad_counter = 0;

commit 972d6aee011703c40ed8d75833ef8f3c8e981e55

pop3: Fix assert-crash when POP3 command name contained ':'
The cmd_<name> reason code now only uses valid command names.

Broken by d2ab26be6038bd53b13a3ff18c403d6c192c1d91 with incomplete fix in
1309137812424c80e63d3c1052795b43d6e19803.

Fixes:
Panic: event_reason_code_prefix(): name has ':'

src/pop3/pop3-client.c
@@ -750,15 +750,18 @@ bool client_handle_input(struct client *client)
 		args = strchr(line, ' ');
 		if (args != NULL)
 			*args++ = '\0';
-		if (*line == '\0') {
-			client_send_line(client, "-ERR Unknown command.");
+
+		const struct pop3_command *cmd = pop3_command_find(line);
+		if (cmd == NULL) {
+			client_send_line(client, "-ERR Unknown command: %s", line);
 			ret = -1;
 		} else T_BEGIN {
 			const char *reason_code =
-				event_reason_code_prefix("pop3", "cmd_", line);
+				event_reason_code_prefix("pop3", "cmd_",
+							 cmd->name);
 			struct event_reason *reason =
 				event_reason_begin(reason_code);
-			ret = client_command_execute(client, line,
+			ret = client_command_execute(client, cmd,
 						     args != NULL ? args : "");
 			event_reason_end(&reason);
 		} T_END;

src/pop3/pop3-commands.c
@@ -956,14 +956,9 @@ const struct pop3_command *pop3_command_find(const char *name)
 }
 
 int client_command_execute(struct client *client,
-			   const char *name, const char *args)
+			   const struct pop3_command *cmd, const char *args)
 {
 	while (*args == ' ') args++;
 
-	const struct pop3_command *cmd = pop3_command_find(name);
-	if (cmd != NULL)
-		return cmd->func(client, args);
-
-	client_send_line(client, "-ERR Unknown command: %s", name);
-	return -1;
+	return cmd->func(client, args);
 }

src/pop3/pop3-commands.h
@@ -8,6 +8,6 @@ struct pop3_command {
 
 const struct pop3_command *pop3_command_find(const char *name);
 int client_command_execute(struct client *client,
-			   const char *name, const char *args);
+			   const struct pop3_command *cmd, const char *args);
 
 #endif