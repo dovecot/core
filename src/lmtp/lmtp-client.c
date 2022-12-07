dovecot/core

commit 8807ff0df48c5a19fec2a1f01dc0dac6d864b7b2

lmtp: Duplicate the whole settings parser rather than individual settings structs
This will be needed later on.

src/lmtp/lmtp-client.c
@@ -130,7 +130,8 @@ static void client_read_settings(struct client *client, bool ssl)
 					       &set_parser, &error) < 0)
 		i_fatal("%s", error);
 
-	lmtp_settings_dup(set_parser, client->pool, &lmtp_set, &lda_set);
+	set_parser = settings_parser_dup(set_parser, client->pool);
+	lmtp_settings_get(set_parser, client->pool, &lmtp_set, &lda_set);
 	const struct var_expand_table *tab =
 		mail_storage_service_get_var_expand_table(storage_service, &input);
 	if (settings_var_expand(&lmtp_setting_parser_info, lmtp_set,

src/lmtp/lmtp-settings.c
@@ -186,7 +186,7 @@ static bool lmtp_settings_check(void *_set, pool_t pool ATTR_UNUSED,
 }
 /* </settings checks> */
 
-void lmtp_settings_dup(const struct setting_parser_context *set_parser,
+void lmtp_settings_get(const struct setting_parser_context *set_parser,
 		       pool_t pool,
 		       struct lmtp_settings **lmtp_set_r,
 		       struct lda_settings **lda_set_r)
@@ -195,12 +195,8 @@ void lmtp_settings_dup(const struct setting_parser_context *set_parser,
 
 	*lda_set_r = settings_parser_get_root_set(set_parser,
 				&lda_setting_parser_info);
-	*lda_set_r = settings_dup(&lda_setting_parser_info,
-				  *lda_set_r, pool);
 	*lmtp_set_r = settings_parser_get_root_set(set_parser,
 				&lmtp_setting_parser_info);
-	*lmtp_set_r = settings_dup(&lmtp_setting_parser_info,
-				   *lmtp_set_r, pool);
 	if (!lmtp_settings_check(*lmtp_set_r, pool, &error))
 		i_unreached();
 }

src/lmtp/lmtp-settings.h
@@ -44,7 +44,7 @@ struct lmtp_settings {
 
 extern const struct setting_parser_info lmtp_setting_parser_info;
 
-void lmtp_settings_dup(const struct setting_parser_context *set_parser,
+void lmtp_settings_get(const struct setting_parser_context *set_parser,
 		       pool_t pool,
 		       struct lmtp_settings **lmtp_set_r,
 		       struct lda_settings **lda_set_r);

commit 3cb91e1c071666aa8dc3f4a1a07f41829bad0bbf

lmtp: Fix memory leak by unreferencing duplicated settings parser
Broken by 8807ff0df48c5a19fec2a1f01dc0dac6d864b7b2

src/lmtp/lmtp-client.c
@@ -123,6 +123,7 @@ static void client_read_settings(struct client *client, bool ssl)
 
 	set_parser = settings_parser_dup(set_parser, client->pool);
 	lmtp_settings_get(set_parser, client->pool, &lmtp_set, &lda_set);
+	settings_parser_unref(&set_parser);
 	const struct var_expand_table *tab =
 		mail_storage_service_get_var_expand_table(storage_service, &input);
 	if (settings_var_expand(&lmtp_setting_parser_info, lmtp_set,