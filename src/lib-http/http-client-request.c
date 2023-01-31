dovecot/core

commit 987fab8f4e143ab851c3318c8614e81bb0f2b360

lib-http: client - Don't unlock connection output before request is fully sent
This could happen at least when the payload is empty and the final LF was
buffered.

src/lib-http/http-client-request.c
@@ -1073,6 +1073,13 @@ http_client_request_finish_payload_out(struct http_client_request *req)
 		o_stream_unref(&req->payload_output);
 		req->payload_output = NULL;
 	}
+	if (conn->conn.output != NULL &&
+	    o_stream_get_buffer_used_size(conn->conn.output) > 0) {
+		e_debug(req->event,
+			"Not quite finished sending request");
+		conn->output_locked = TRUE;
+		return 0;
+	}
 
 	i_assert(req->request_offset < conn->conn.output->offset);
 	req->bytes_out = conn->conn.output->offset - req->request_offset;

commit 537f5c77a9f55dad3bbb26c0896c4deb6b89b975

lib-http: Remove unnecessary NULL check
This is confusing static analyzers.

Broken by 987fab8f4e143ab851c3318c8614e81bb0f2b360

src/lib-http/http-client-request.c
@@ -1073,8 +1073,7 @@ http_client_request_finish_payload_out(struct http_client_request *req)
 		o_stream_unref(&req->payload_output);
 		req->payload_output = NULL;
 	}
-	if (conn->conn.output != NULL &&
-	    o_stream_get_buffer_used_size(conn->conn.output) > 0) {
+	if (o_stream_get_buffer_used_size(conn->conn.output) > 0) {
 		e_debug(req->event,
 			"Not quite finished sending request");
 		conn->output_locked = TRUE;