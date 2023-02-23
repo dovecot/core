dovecot/core

commit 38dbeec767ad10021020e3833b1dbc3d04442f85

lib: Add memcpy() macro that assert-crashes if either parameter is NULL

src/lib/lib.h
@@ -66,6 +66,14 @@ typedef void lib_atexit_callback_t(void);
 #define static_assert_array_size(arr, count) \
 	static_assert(N_ELEMENTS(arr) == (count), "array/enum size mismatch")
 
+/* Using memcpy() with NULL pointers is undefined behavior. Make sure we don't
+   do that. */
+static inline void *i_memcpy(void *dest, const void *src, size_t n) {
+	i_assert(dest != NULL && src != NULL);
+	return memcpy(dest, src, n);
+}
+#define memcpy(dest, src, n) i_memcpy(dest, src, n)
+
 /* /dev/null opened as O_WRONLY. Opened at lib_init(), so it can be accessed
    also inside chroots. */
 extern int dev_null_fd;

src/lib/buffer.c
@@ -6,6 +6,10 @@
 #include "safe-memset.h"
 #include "buffer.h"
 
+/* Disable our memcpy() safety wrapper. This file is very performance sensitive
+   and it's been checked to work correctly with memcpy(). */
+#undef memcpy
+
 struct real_buffer {
 	union {
 		struct buffer buf;

src/lib/printf-format-fix.c
@@ -3,6 +3,10 @@
 #include "lib.h"
 #include "printf-format-fix.h"
 
+/* Disable our memcpy() safety wrapper. This file is very performance sensitive
+   and it's been checked to work correctly with memcpy(). */
+#undef memcpy
+
 static const char *
 fix_format_real(const char *fmt, const char *p, size_t *len_r)
 {

src/lib/strfuncs.c
@@ -12,6 +12,10 @@
 #include <limits.h>
 #include <ctype.h>
 
+/* Disable our memcpy() safety wrapper. This file is very performance sensitive
+   and it's been checked to work correctly with memcpy(). */
+#undef memcpy
+
 #define STRCONCAT_BUFSIZE 512
 
 enum _str_trim_sides {

commit 67939a5b9ac9b71140a1f5dfe24b9b45555a42b8

lib: lib - Remove memcpy macro in C++
This breaks C++ compilation due to too wide impact.

Broken in 38dbeec767ad10021020e3833b1dbc3d04442f85

src/lib/lib.h
@@ -72,7 +72,9 @@ static inline void *i_memcpy(void *dest, const void *src, size_t n) {
 	i_assert(dest != NULL && src != NULL);
 	return memcpy(dest, src, n);
 }
-#define memcpy(dest, src, n) i_memcpy(dest, src, n)
+#ifndef __cplusplus
+#  define memcpy(dest, src, n) i_memcpy(dest, src, n)
+#endif
 
 /* /dev/null opened as O_WRONLY. Opened at lib_init(), so it can be accessed
    also inside chroots. */