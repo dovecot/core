dovecot/core

commit e6f2682b32ff5b366ee7242841c3194425202880

lib/compat.h: Define a base compilation system
Add macros to setup the compiler environment for a specific baseline,
this allows us to drop manual re-implementations of system-provided
functionality.

src/lib/lib.h
@@ -1,6 +1,16 @@
 #ifndef LIB_H
 #define LIB_H
 
+#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
+#  define __BSD_VISIBLE
+#elif defined(__APPLE__)
+#  define _DARWIN_C_SOURCE
+#endif
+#define _BSD_SOURCE 1
+#define _DEFAULT_SOURCE 1
+#define _POSIX_C_SOURCE 200809L
+#define _XOPEN_SOURCE 700
+
 /* default lib includes */
 #ifdef HAVE_CONFIG_H
 #  include "config.h"

configure.ac
@@ -298,7 +298,6 @@ DC_DOVECOT_FUZZER
 DC_DOVECOT_CFLAGS
 
 DOVECOT_NSL
-DOVECOT_FDATASYNC
 DOVECOT_LIBCAP
 
 DOVECOT_RANDOM
@@ -306,10 +305,7 @@ DOVECOT_ARC4RANDOM
 
 AC_DEFINE(PACKAGE_WEBPAGE, "http://www.dovecot.org/", [Support URL])
 
-dnl * after -lsocket and -lnsl tests, inet_aton() may be in them
-AC_CHECK_FUNCS(fcntl flock lockf inet_aton sigaction getpagesize madvise \
-               vsyslog writev pread uname \
-	       setrlimit setproctitle seteuid setreuid setegid setresgid \
+AC_CHECK_FUNCS(flock madvise pread setproctitle setresgid \
 	       getmntinfo setpriority quotactl getmntent kqueue kevent \
 	       backtrace_symbols walkcontext dirfd clearenv \
 	       malloc_usable_size glob fallocate posix_fadvise \
@@ -402,11 +398,6 @@ AC_DEFINE_UNQUOTED(SSIZE_T_MAX, $ssizet_max, [Maximum value of ssize_t])
 
 dnl we require inttypes.h for PRIu{8,16,32,64} macros
 AC_CHECK_HEADER(inttypes.h,,AC_MSG_ERROR([inttypes.h missing but required]))
-
-AC_CHECKTYPE2(socklen_t, [#include <sys/socket.h>])
-if test $i_cv_type_socklen_t = yes; then
-  AC_DEFINE(HAVE_SOCKLEN_T,, [Define to 'int' if you don't have socklen_t])
-fi
 
 AC_DEFINE_UNQUOTED(MEM_ALIGN_SIZE, $mem_align, [Required memory alignment])
 

m4/fdatasync.m4
@@ -1,5 +1,0 @@
-AC_DEFUN([DOVECOT_FDATASYNC], [
-  AC_SEARCH_LIBS(fdatasync, rt, [
-    AC_DEFINE(HAVE_FDATASYNC,, [Define if you have fdatasync()])
-  ])
-])

src/lib-imap/imap-id.c
@@ -13,7 +13,6 @@
 #  include <sys/utsname.h>
 #endif
 
-#ifdef HAVE_UNAME
 static struct utsname utsname_result;
 static bool utsname_set = FALSE;
 
@@ -33,7 +32,6 @@ static const char *imap_id_get_uname(const char *key)
 		return utsname_result.release;
 	return NULL;
 }
-#endif
 
 static const char *imap_id_get_default(const char *key)
 {
@@ -47,9 +45,7 @@ static const char *imap_id_get_default(const char *key)
 		return PACKAGE_WEBPAGE;
 	if (strcasecmp(key, "support-email") == 0)
 		return PACKAGE_BUGREPORT;
-#ifdef HAVE_UNAME
 	return imap_id_get_uname(key);
-#endif
 }
 
 static const char *

src/lib-storage/index/mbox/mbox-lock.c
@@ -69,12 +69,8 @@ static int mbox_lock_flock(struct mbox_lock_context *ctx, int lock_type,
 #else
 #  define mbox_lock_flock NULL
 #endif
-#ifdef HAVE_LOCKF
 static int mbox_lock_lockf(struct mbox_lock_context *ctx, int lock_type,
 			   time_t max_wait_time);
-#else
-#  define mbox_lock_lockf NULL
-#endif
 
 static struct mbox_lock_data lock_data[] = {
 	{ MBOX_LOCK_DOTLOCK, "dotlock", mbox_lock_dotlock },
@@ -527,7 +523,6 @@ static int mbox_lock_flock(struct mbox_lock_context *ctx, int lock_type,
 }
 #endif
 
-#ifdef HAVE_LOCKF
 static int mbox_lock_lockf(struct mbox_lock_context *ctx, int lock_type,
 			   time_t max_wait_time)
 {
@@ -588,7 +583,6 @@ static int mbox_lock_lockf(struct mbox_lock_context *ctx, int lock_type,
 	alarm(0);
 	return 1;
 }
-#endif
 
 static int mbox_lock_fcntl(struct mbox_lock_context *ctx, int lock_type,
 			   time_t max_wait_time)

src/lib/compat.c
@@ -3,12 +3,6 @@
 #include "config.h"
 #undef HAVE_CONFIG_H
 
-/* Linux needs the _XOPEN_SOURCE define, but others don't. It needs to be
-   defined before unistd.h, so we need the above config.h include hack.. */
-#ifdef PREAD_WRAPPERS
-#  define _XOPEN_SOURCE 500 /* Linux */
-#endif
-
 #define IN_COMPAT_C
 #include "lib.h"
 
@@ -18,74 +12,6 @@
 #include <syslog.h>
 #include <time.h>
 #include <sys/time.h>
-
-#ifndef INADDR_NONE
-#  define INADDR_NONE INADDR_BROADCAST
-#endif
-
-#ifndef HAVE_INET_ATON
-int i_my_inet_aton(const char *cp, struct in_addr *inp)
-{
-	in_addr_t addr;
-
-	addr = inet_addr(cp);
-	if (addr == INADDR_NONE)
-		return 0;
-
-	inp->s_addr = addr;
-        return 1;
-}
-#endif
-
-#ifndef HAVE_VSYSLOG
-void i_my_vsyslog(int priority, const char *format, va_list args)
-{
-	T_BEGIN {
-		syslog(priority, "%s", t_strdup_vprintf(format, args));
-	} T_END;
-}
-#endif
-
-#ifndef HAVE_GETPAGESIZE
-int i_my_getpagesize(void)
-{
-#ifdef _SC_PAGESIZE
-	return sysconf(_SC_PAGESIZE);
-#else
-#  ifdef __GNUC__
-#    warning Guessing page size to be 4096
-#  endif
-	return 4096;
-#endif
-}
-#endif
-
-#ifndef HAVE_WRITEV
-ssize_t i_my_writev(int fd, const struct iovec *iov, int iov_len)
-{
-	size_t written;
-	ssize_t ret;
-	int i;
-
-	written = 0;
-	for (i = 0; i < iov_len; i++, iov++) {
-		ret = write(fd, iov->iov_base, iov->iov_len);
-		if (ret < 0)
-			return -1;
-
-		written += ret;
-		if ((size_t)ret != iov->iov_len)
-			break;
-	}
-
-	if (written > SSIZE_T_MAX) {
-		errno = ERANGE;
-		return -1;
-	}
-
-	return (ssize_t)written;
-}
-#endif
 
 #if !defined(HAVE_PREAD) || defined(PREAD_BROKEN)
 ssize_t i_my_pread(int fd, void *buf, size_t count, off_t offset)
@@ -142,45 +68,5 @@ ssize_t i_my_pread(int fd, void *buf, size_t count, off_t offset)
 ssize_t i_my_pwrite(int fd, const void *buf, size_t count, off_t offset)
 {
 	return pwrite(fd, buf, count, offset);
-}
-#endif
-
-#ifndef HAVE_SETEUID
-int i_my_seteuid(uid_t euid)
-{
-#ifdef HAVE_SETREUID
-	/* HP-UX at least doesn't have seteuid() but has setreuid() */
-	return setreuid(-1, euid);
-#else
-#  error Missing seteuid functionality
-#endif
-}
-#endif
-
-#ifndef HAVE_SETEGID
-int i_my_setegid(gid_t egid)
-{
-#ifdef HAVE_SETRESGID
-	/* HP-UX at least doesn't have setegid() but has setresgid() */
-	return setresgid(-1, egid, -1);
-#else
-#  error Missing setegid functionality
-#endif
-}
-#endif
-
-#ifndef HAVE_CLOCK_GETTIME
-int i_my_clock_gettime(int clk_id, struct timespec *tp)
-{
-	struct timeval tv;
-
-	i_assert(clk_id == CLOCK_REALTIME);
-
-	/* fallback to using microseconds */
-	if (gettimeofday(&tv, NULL) < 0)
-		return -1;
-	tp->tv_sec = tv.tv_sec;
-	tp->tv_nsec = tv.tv_usec * 1000;
-	return 0;
 }
 #endif

src/lib/compat.h
@@ -66,10 +66,6 @@ typedef unsigned long long uoff_t;
 #  error uoff_t size not set
 #endif
 
-#ifndef HAVE_SOCKLEN_T
-typedef int socklen_t;
-#endif
-
 /* WORDS_BIGENDIAN needs to be undefined if not enabled */
 #if defined(WORDS_BIGENDIAN) && WORDS_BIGENDIAN == 0
 #  undef WORDS_BIGENDIAN
@@ -128,39 +124,10 @@ typedef int socklen_t;
 	((st1)->st_ctime == (st2)->st_ctime && \
 	 ST_NTIMES_EQUAL(ST_CTIME_NSEC(*(st1)), ST_CTIME_NSEC(*(st2))))
 
-#ifndef HAVE_INET_ATON
-#  include <sys/socket.h>
-#  include <netinet/in.h>
-#  include <arpa/inet.h>
-#  define inet_aton i_my_inet_aton
-int i_my_inet_aton(const char *cp, struct in_addr *inp);
-#endif
-
-#ifndef HAVE_VSYSLOG
-#  define vsyslog i_my_vsyslog
-void i_my_vsyslog(int priority, const char *format, va_list args);
-#endif
-
-#ifndef HAVE_GETPAGESIZE
-#  define getpagesize i_my_getpagesize
-int i_my_getpagesize(void);
-#endif
-
-#ifndef HAVE_FDATASYNC
-#  define fdatasync fsync
-#endif
-
 struct const_iovec {
 	const void *iov_base;
 	size_t iov_len;
 };
-
-#ifndef HAVE_STRUCT_IOVEC
-struct iovec {
-	void *iov_base;
-	size_t iov_len;
-};
-#endif
 
 /*
  *
@@ -183,12 +150,6 @@ struct iovec {
 #  endif
 #endif
 
-#ifndef HAVE_WRITEV
-#  define writev i_my_writev
-struct iovec;
-ssize_t i_my_writev(int fd, const struct iovec *iov, int iov_len);
-#endif
-
 /*
  *
  *
@@ -205,35 +166,6 @@ ssize_t i_my_writev(int fd, const struct iovec *iov, int iov_len);
 #  endif
 ssize_t i_my_pread(int fd, void *buf, size_t count, off_t offset);
 ssize_t i_my_pwrite(int fd, const void *buf, size_t count, off_t offset);
-#endif
-
-#ifndef HAVE_SETEUID
-#  define seteuid i_my_seteuid
-int i_my_seteuid(uid_t euid);
-#endif
-
-#ifndef HAVE_SETEGID
-#  define setegid i_my_setegid
-int i_my_setegid(gid_t egid);
-#endif
-
-/*
- *
- *
- *
- * this searches the rt-library for a clock_gettime() function
- * gnu99/gnu11 do not specific linking with -lrt
- * compiling this with strict c99/c11 requires linking with -lrt
- *
- *
- *
- */
-#ifndef HAVE_CLOCK_GETTIME
-#  include <time.h>
-#  undef CLOCK_REALTIME
-#  define CLOCK_REALTIME 1
-#  define clock_gettime i_my_clock_gettime
-int i_my_clock_gettime(int clk_id, struct timespec *tp);
 #endif
 
 /* ctype.h isn't safe with signed chars,

src/lib/file-lock.c
@@ -178,11 +178,6 @@ static int file_lock_do(int fd, const char *path, int lock_type,
 
 	switch (set->lock_method) {
 	case FILE_LOCK_METHOD_FCNTL: {
-#ifndef HAVE_FCNTL
-		*error_r = t_strdup_printf(
-			"Can't lock file %s: fcntl() locks not supported", path);
-		return -1;
-#else
 		struct flock fl;
 
 		fl.l_type = lock_type;
@@ -226,7 +221,6 @@ static int file_lock_do(int fd, const char *path, int lock_type,
 					       lock_type));
 		}
 		return -1;
-#endif
 	}
 	case FILE_LOCK_METHOD_FLOCK: {
 #ifndef HAVE_FLOCK

commit 5181dd05e63292f2b251017d1f281a53dd233be2

lib: lib - Add value for defines that don't have it
Fixes compilation with FreeBSD that requires a value for
defines.

Broken in e6f2682b32ff5b366ee7242841c3194425202880

src/lib/lib.h
@@ -2,9 +2,9 @@
 #define LIB_H
 
 #if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
-#  define __BSD_VISIBLE
+#  define __BSD_VISIBLE 1
 #elif defined(__APPLE__)
-#  define _DARWIN_C_SOURCE
+#  define _DARWIN_C_SOURCE 1
 #endif
 #define _BSD_SOURCE 1
 #define _DEFAULT_SOURCE 1