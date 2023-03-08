1d1d368f4547b35082e53d684e5ebefcba4cc364
Timo Sirainen <timo.sirainen@open-xchange.com>
2023-03-08 16:48:30 +0200

    doveadm: Add local_ip and remote_ip fields to connection event
    These will be required to preserve support for local {) and remote {}
    config filters after the following change.

--- src/doveadm/client-connection.c
+++ src/doveadm/client-connection.c
@@ -62,8 +62,10 @@ int client_connection_init(struct client_connection *conn,
 	conn->type = type;
 	conn->pool = pool;
 
-	(void)net_getsockname(fd, &conn->local_ip, &conn->local_port);
-	(void)net_getpeername(fd, &conn->remote_ip, &conn->remote_port);
+	if (net_getsockname(fd, &conn->local_ip, &conn->local_port) == 0)
+		event_add_ip(conn->event, "local_ip", &conn->local_ip);
+	if (net_getpeername(fd, &conn->remote_ip, &conn->remote_port) == 0)
+		event_add_ip(conn->event, "remote_ip", &conn->local_ip);
 
 	ip = net_ip2addr(&conn->remote_ip);
 	if (ip[0] != '\0')
