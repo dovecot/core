df03060653da1a02a0d69f6d7a1efdb0912d5950
Timo Sirainen <timo.sirainen@open-xchange.com>
2023-04-01 00:03:57 +0300

    doveadm: Remove explicitly set master_service_settings_input.protocol
    It is now unnecessary.

--- src/doveadm/doveadm-settings.c
+++ src/doveadm/doveadm-settings.c
@@ -224,7 +224,6 @@ void doveadm_read_settings(void)
 	const char *error;
 
 	i_zero(&input);
-	input.protocol = "doveadm";
 	input.preserve_user = TRUE;
 	input.preserve_home = TRUE;
 	input.return_config_fd = TRUE; /* for doveadm exec */
