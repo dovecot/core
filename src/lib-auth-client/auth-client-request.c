8ca95c8c70159dbe3853d44e08907b1ec71069b1
Aki Tuomi <aki.tuomi@open-xchange.com>
2023-05-15 12:28:53 +0300

    lib-auth: Move auth_client_request_continue() lower
    Simplifies next commit

--- src/lib-auth-client/auth-client-request.c
+++ src/lib-auth-client/auth-client-request.c
@@ -198,32 +198,6 @@ auth_client_request_new(struct auth_client *client,
 	return request;
 }
 
-void auth_client_request_continue(struct auth_client_request *request,
-                                  const char *data_base64)
-{
-	struct const_iovec iov[3];
-	const char *prefix;
-
-	prefix = t_strdup_printf("CONT\t%u\t", request->id);
-
-	iov[0].iov_base = prefix;
-	iov[0].iov_len = strlen(prefix);
-	iov[1].iov_base = data_base64;
-	iov[1].iov_len = strlen(data_base64);
-	iov[2].iov_base = "\n";
-	iov[2].iov_len = 1;
-
-	struct event_passthrough *e =
-		event_create_passthrough(request->event)->
-		set_name("auth_client_request_continued");
-	e_debug(e->event(), "Continue request");
-
-	if (o_stream_sendv(request->conn->conn.output, iov, 3) < 0) {
-		e_error(request->event,
-			"Error sending continue request to auth server: %m");
-	}
-}
-
 static void ATTR_NULL(3, 4)
 call_callback(struct auth_client_request *request,
 	      enum auth_request_status status,
@@ -302,6 +276,32 @@ static void args_parse_user(struct auth_client_request *request, const char *key
 		event_add_str(request->event, "original_user", value);
 	else if (strcmp(key, "auth_user") == 0)
 		event_add_str(request->event, "auth_user", value);
+}
+
+void auth_client_request_continue(struct auth_client_request *request,
+                                  const char *data_base64)
+{
+	struct const_iovec iov[3];
+	const char *prefix;
+
+	prefix = t_strdup_printf("CONT\t%u\t", request->id);
+
+	iov[0].iov_base = prefix;
+	iov[0].iov_len = strlen(prefix);
+	iov[1].iov_base = data_base64;
+	iov[1].iov_len = strlen(data_base64);
+	iov[2].iov_base = "\n";
+	iov[2].iov_len = 1;
+
+	struct event_passthrough *e =
+		event_create_passthrough(request->event)->
+		set_name("auth_client_request_continued");
+	e_debug(e->event(), "Continue request");
+
+	if (o_stream_sendv(request->conn->conn.output, iov, 3) < 0) {
+		e_error(request->event,
+			"Error sending continue request to auth server: %m");
+	}
 }
 
 void auth_client_request_server_input(struct auth_client_request *request,
