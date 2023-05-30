dovecot/core

commit 5f27e25c64555dcaae6cb00c479cd05bc2638081

imap: Move imap-zlib plugin functionality into imap core
This commit auto-enables the imap's COMPRESS functionality by copying
the imap-zlib plugin functionality into the imap directory and making it
available as the COMPRESS command.

It also disables compilation of the still existing imap-zlib plugin.

src/imap/cmd-compress.c
@@ -1,0 +1,99 @@
+/* Copyright (c) 2010-2022 Dovecot authors, see the included COPYING file */
+
+#include "imap-common.h"
+#include "imap-commands.h"
+#include "istream.h"
+#include "ostream.h"
+#include "str.h"
+#include "compression.h"
+
+static void client_skip_line(struct client *client)
+{
+	const unsigned char *data;
+	size_t data_size;
+
+	data = i_stream_get_data(client->input, &data_size);
+	i_assert(data_size > 0);
+	if (data[0] == '\n')
+		i_stream_skip(client->input, 1);
+	else if (data[0] == '\r' && data_size > 1 && data[1] == '\n')
+		i_stream_skip(client->input, 2);
+	else
+		i_unreached();
+	client->input_skip_line = FALSE;
+}
+
+static void client_update_imap_parser_streams(struct client *client)
+{
+	struct client_command_context *cmd;
+
+	if (client->free_parser != NULL) {
+		imap_parser_set_streams(client->free_parser,
+					client->input, client->output);
+	}
+
+	for (cmd = client->command_queue; cmd != NULL; cmd = cmd->next) {
+		imap_parser_set_streams(cmd->parser,
+					client->input, client->output);
+	}
+}
+
+bool cmd_compress(struct client_command_context *cmd)
+{
+	struct client *client = cmd->client;
+	const struct compression_handler *handler;
+	const struct imap_arg *args;
+	struct istream *old_input;
+	struct ostream *old_output;
+	const char *mechanism, *value;
+	int level;
+
+	/* <mechanism> */
+	if (!client_read_args(cmd, 0, 0, &args))
+		return FALSE;
+
+	if (!imap_arg_get_atom(args, &mechanism) ||
+	    !IMAP_ARG_IS_EOL(&args[1])) {
+		client_send_command_error(cmd, "Invalid arguments.");
+		return TRUE;
+	}
+
+	int ret = compression_lookup_handler(t_str_lcase(mechanism), &handler);
+	if (ret <= 0) {
+		const char * tagline =
+			t_strdup_printf("NO %s compression mechanism",
+					ret == 0 ? "Unsupported" : "Unknown");
+		client_send_tagline(cmd, tagline);
+		return TRUE;
+	}
+
+	client_skip_line(client);
+	client_send_tagline(cmd, "OK Begin compression.");
+
+	const char *setting = t_strdup_printf("imap_compress_%s_level",
+					      handler->name);
+	value = mail_user_plugin_getenv(client->user, setting);
+	if (value == NULL) {
+		level = handler->get_default_level();
+	} else if (str_to_int(value, &level) < 0 ||
+		   level < handler->get_min_level() ||
+		   level > handler->get_max_level()) {
+		i_error("%s: Level must be between %d..%d",
+			setting,
+			handler->get_min_level(),
+			handler->get_max_level());
+		level = handler->get_default_level();
+	}
+	old_input = client->input;
+	old_output = client->output;
+	client->input = handler->create_istream(old_input);
+	client->output = handler->create_ostream(old_output, level);
+	/* preserve output offset so that the bytes out counter in logout
+	   message doesn't get reset here */
+	client->output->offset = old_output->offset;
+	i_stream_unref(&old_input);
+	o_stream_unref(&old_output);
+
+	client_update_imap_parser_streams(client);
+	return TRUE;
+}

configure.ac
@@ -699,7 +699,7 @@ dnl **
 dnl IDLE doesn't really belong to banner. It's there just to make Blackberries
 dnl happy, because otherwise BIS server disables push email.
 capability_banner="IMAP4rev1 SASL-IR LOGIN-REFERRALS ID ENABLE IDLE"
-capability="$capability_banner SORT SORT=DISPLAY THREAD=REFERENCES THREAD=REFS THREAD=ORDEREDSUBJECT MULTIAPPEND URL-PARTIAL CATENATE UNSELECT CHILDREN NAMESPACE UIDPLUS LIST-EXTENDED I18NLEVEL=1 CONDSTORE QRESYNC ESEARCH ESORT SEARCHRES WITHIN CONTEXT=SEARCH LIST-STATUS BINARY MOVE SNIPPET=FUZZY PREVIEW=FUZZY PREVIEW STATUS=SIZE SAVEDATE"
+capability="$capability_banner SORT SORT=DISPLAY THREAD=REFERENCES THREAD=REFS THREAD=ORDEREDSUBJECT MULTIAPPEND URL-PARTIAL CATENATE UNSELECT CHILDREN NAMESPACE UIDPLUS LIST-EXTENDED I18NLEVEL=1 CONDSTORE QRESYNC ESEARCH ESORT SEARCHRES WITHIN CONTEXT=SEARCH LIST-STATUS BINARY MOVE SNIPPET=FUZZY PREVIEW=FUZZY PREVIEW STATUS=SIZE SAVEDATE COMPRESS=DEFLATE"
 AC_DEFINE_UNQUOTED(CAPABILITY_STRING, "$capability", [IMAP capabilities])
 AC_DEFINE_UNQUOTED(CAPABILITY_BANNER_STRING, "$capability_banner", [IMAP capabilities advertised in banner])
 
@@ -838,7 +838,6 @@ src/plugins/trash/Makefile
 src/plugins/virtual/Makefile
 src/plugins/welcome/Makefile
 src/plugins/zlib/Makefile
-src/plugins/imap-zlib/Makefile
 src/plugins/mail-crypt/Makefile
 src/plugins/var-expand-crypt/Makefile
 src/plugins/apparmor/Makefile

src/imap/Makefile.am
@@ -16,6 +16,7 @@ AM_CPPFLAGS = \
 	-I$(top_srcdir)/src/lib-imap-storage \
 	-I$(top_srcdir)/src/lib-index \
 	-I$(top_srcdir)/src/lib-storage \
+	-I$(top_srcdir)/src/lib-compression \
 	$(BINARY_CFLAGS)
 
 imap_LDFLAGS = -export-dynamic \
@@ -23,6 +24,7 @@ imap_LDFLAGS = -export-dynamic \
 
 imap_LDADD = \
 	../lib-imap-urlauth/libimap-urlauth.la \
+	../lib-compression/libcompression.la \
 	$(LIBDOVECOT_STORAGE) \
 	$(LIBDOVECOT)
 imap_DEPENDENCIES = \
@@ -65,7 +67,8 @@ cmds = \
 	cmd-thread.c \
 	cmd-unselect.c \
 	cmd-unsubscribe.c \
-	cmd-urlfetch.c
+	cmd-urlfetch.c \
+	cmd-compress.c
 
 common_sources = \
 	$(cmds) \

src/imap/imap-commands.c
@@ -77,6 +77,7 @@ static const struct command imap_ext_commands[] = {
 	{ "UID THREAD",		cmd_thread,      COMMAND_FLAG_BREAKS_SEQS },
 	{ "UNSELECT",		cmd_unselect,    COMMAND_FLAG_BREAKS_MAILBOX },
 	{ "XLIST",		cmd_list,        0 },
+	{ "COMPRESS",           cmd_compress,    0 },
 	/* IMAP URLAUTH (RFC4467): */
 	{ "GENURLAUTH",		cmd_genurlauth,  0 },
 	{ "RESETKEY",		cmd_resetkey,    0 },

src/imap/imap-commands.h
@@ -121,6 +121,7 @@ bool cmd_thread(struct client_command_context *cmd);
 bool cmd_uid_expunge(struct client_command_context *cmd);
 bool cmd_move(struct client_command_context *cmd);
 bool cmd_unselect(struct client_command_context *cmd);
+bool cmd_compress(struct client_command_context *cmd);
 
 /* IMAP URLAUTH (RFC4467): */
 bool cmd_genurlauth(struct client_command_context *cmd);

src/plugins/Makefile.am
@@ -36,7 +36,6 @@ SUBDIRS = \
 	virtual \
 	welcome \
 	zlib \
-	imap-zlib \
 	$(FTS_SOLR) \
 	$(FTS_FLATCURVE) \
 	$(DICT_LDAP) \

commit f9128b0757b2a73dd76aea8cde82a12f82c46cec

imap: Fix crash when trying to use COMPRESS multiple times
Broken by 5f27e25c64555dcaae6cb00c479cd05bc2638081

Fixes:
Panic: file cmd-compress.c: line 104 (cmd_compress): assertion failed: (client->input->v_offset == prev_in_offset)

src/imap/cmd-compress.c
@@ -59,6 +59,12 @@ bool cmd_compress(struct client_command_context *cmd)
 		return TRUE;
 	}
 
+	if (client->compress_handler != NULL) {
+		client_send_tagline(cmd, t_strdup_printf(
+			"NO [COMPRESSIONACTIVE] COMPRESSION=%s already enabled.",
+			t_str_ucase(client->compress_handler->name)));
+		return TRUE;
+	}
 	int ret = compression_lookup_handler(t_str_lcase(mechanism), &handler);
 	if (ret <= 0) {
 		const char * tagline =
@@ -121,5 +127,6 @@ bool cmd_compress(struct client_command_context *cmd)
 	}
 
 	client_update_imap_parser_streams(client);
+	client->compress_handler = handler;
 	return TRUE;
 }

src/imap/imap-client.h
@@ -212,6 +212,8 @@ struct client {
 	/* NOTIFY extension */
 	struct imap_notify_context *notify_ctx;
 	uint32_t notify_uidnext;
+	/* COMPRESS extension */
+	const struct compression_handler *compress_handler;
 
 	/* client input/output is locked by this command */
 	struct client_command_context *input_lock;