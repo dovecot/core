46970de1a0849232e890905759a8b594cd87d939
Timo Sirainen <timo.sirainen@open-xchange.com>
2023-06-01 01:01:43 +0300

    lazy-expunge: Convert plugin settings to regular settings
    lazy_expunge_exclude wasn't converted yet, since it's a bit more complex and
    also it's going to be removed by a following commit.

--- src/plugins/lazy-expunge/lazy-expunge-plugin.h
+++ src/plugins/lazy-expunge/lazy-expunge-plugin.h
@@ -1,6 +1,17 @@
 #ifndef LAZY_EXPUNGE_PLUGIN_H
 #define LAZY_EXPUNGE_PLUGIN_H
 
+struct module;
+
+struct lazy_expunge_settings {
+	pool_t pool;
+
+	bool lazy_expunge_only_last_instance;
+	const char *lazy_expunge;
+};
+
+extern const struct setting_parser_info lazy_expunge_setting_parser_info;
+
 void lazy_expunge_plugin_init(struct module *module);
 void lazy_expunge_plugin_deinit(void);
 
---
+++ src/plugins/lazy-expunge/lazy-expunge-settings.c
@@ -0,0 +1,31 @@
+/* Copyright (c) 2023 Dovecot authors, see the included COPYING file */
+
+#include "lib.h"
+#include "settings-parser.h"
+#include "lazy-expunge-plugin.h"
+
+#undef DEF
+#define DEF(type, name) \
+	SETTING_DEFINE_STRUCT_##type(#name, name, struct lazy_expunge_settings)
+
+static struct setting_define lazy_expunge_setting_defines[] = {
+	DEF(BOOL, lazy_expunge_only_last_instance),
+	DEF(STR, lazy_expunge),
+
+	SETTING_DEFINE_LIST_END
+};
+
+static struct lazy_expunge_settings lazy_expunge_default_settings = {
+	.lazy_expunge_only_last_instance = FALSE,
+	.lazy_expunge = "",
+};
+
+const struct setting_parser_info lazy_expunge_setting_parser_info = {
+	.name = "lazy_expunge",
+
+	.defines = lazy_expunge_setting_defines,
+	.defaults = &lazy_expunge_default_settings,
+
+	.struct_size = sizeof(struct lazy_expunge_settings),
+	.pool_offset1 = 1 + offsetof(struct lazy_expunge_settings, pool),
+};
