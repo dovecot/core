47f760ce76dc6eced2cc13140f1f8253532d9457
Timo Sirainen <timo.sirainen@open-xchange.com>
2023-06-16 17:08:18 +0300

    lib-master: Use ssl_server_settings_to_iostream_set() to get server context settings

--- src/lib-master/master-service-ssl.c
+++ src/lib-master/master-service-ssl.c
@@ -15,7 +15,7 @@ void master_service_ssl_ctx_init(struct master_service *service)
 {
 	const struct ssl_settings *set = NULL;
 	const struct ssl_server_settings *server_set;
-	struct ssl_iostream_settings ssl_set;
+	const struct ssl_iostream_settings *ssl_set;
 	const char *error;
 
 	if (service->ssl_ctx_initialized)
@@ -42,32 +42,8 @@ void master_service_ssl_ctx_init(struct master_service *service)
 		return;
 	}
 
-	i_zero(&ssl_set);
-	ssl_set.pool = set->pool;
-	pool_add_external_ref(ssl_set.pool, server_set->pool);
-	ssl_set.min_protocol = set->ssl_min_protocol;
-	ssl_set.cipher_list = set->ssl_cipher_list;
-	ssl_set.curve_list = set->ssl_curve_list;
-	ssl_set.ca = server_set->ssl_ca;
-	ssl_set.cert.cert = server_set->ssl_cert;
-	ssl_set.cert.key = server_set->ssl_key;
-	ssl_set.dh = server_set->ssl_dh;
-	ssl_set.cert.key_password = server_set->ssl_key_password;
-	ssl_set.cert_username_field = server_set->ssl_cert_username_field;
-	if (server_set->ssl_alt_cert != NULL &&
-	    *server_set->ssl_alt_cert != '\0') {
-		ssl_set.alt_cert.cert = server_set->ssl_alt_cert;
-		ssl_set.alt_cert.key = server_set->ssl_alt_key;
-		ssl_set.alt_cert.key_password = server_set->ssl_key_password;
-	}
-	ssl_set.crypto_device = set->ssl_crypto_device;
-	ssl_set.skip_crl_check = !server_set->ssl_require_crl;
-
-	ssl_set.verify_remote_cert = server_set->ssl_request_client_cert;
-	ssl_set.prefer_server_ciphers = server_set->ssl_prefer_server_ciphers;
-	ssl_set.compression = set->parsed_opts.compression;
-
-	if (ssl_iostream_server_context_cache_get(&ssl_set, &service->ssl_ctx,
+	ssl_server_settings_to_iostream_set(set, server_set, &ssl_set);
+	if (ssl_iostream_server_context_cache_get(ssl_set, &service->ssl_ctx,
 						  &error) < 0) {
 		e_error(service->event,
 			"SSL context initialization failed, disabling SSL: %s",
@@ -76,6 +52,7 @@ void master_service_ssl_ctx_init(struct master_service *service)
 	}
 	settings_free(set);
 	settings_free(server_set);
+	settings_free(ssl_set);
 }
 
 void master_service_ssl_ctx_deinit(struct master_service *service)
