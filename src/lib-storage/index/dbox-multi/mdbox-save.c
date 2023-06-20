8bffe291fe5142a2e385de8f4fc7b69caa816522
Timo Sirainen <timo.sirainen@open-xchange.com>
2023-06-20 18:21:38 +0300

    mdbox: Fix crash if fdatasync_path() fails
    There is no dest_mail anymore at this stage of transaction.

--- src/lib-storage/index/dbox-multi/mdbox-save.c
+++ src/lib-storage/index/dbox-multi/mdbox-save.c
@@ -401,7 +401,7 @@ void mdbox_transaction_save_commit_post(struct mail_save_context *_ctx,
 		const char *box_path = mailbox_get_path(&ctx->mbox->box);
 
 		if (fdatasync_path(box_path) < 0) {
-			mail_set_critical(_ctx->dest_mail,
+			mailbox_set_critical(_ctx->transaction->box,
 				"fdatasync_path(%s) failed: %m", box_path);
 		}
 	}
