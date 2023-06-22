dovecot/core

commit 8bffe291fe5142a2e385de8f4fc7b69caa816522

mdbox: Fix crash if fdatasync_path() fails
There is no dest_mail anymore at this stage of transaction.

src/lib-storage/index/dbox-multi/mdbox-save.c
@@ -401,7 +401,7 @@ void mdbox_transaction_save_commit_post(struct mail_save_context *_ctx,
 		const char *box_path = mailbox_get_path(&ctx->mbox->box);
 
 		if (fdatasync_path(box_path) < 0) {
-			mail_set_critical(_ctx->dest_mail,
+			mailbox_set_critical(_ctx->transaction->box,
 				"fdatasync_path(%s) failed: %m", box_path);
 		}
 	}

commit 1a0732aabac301aa144d1503ea46433da31067ae

mdbox: Fix crash still if fdatasync_path() fails
Broken still in 8bffe291fe5142a2e385de8f4fc7b69caa816522

src/lib-storage/index/dbox-multi/mdbox-save.c
@@ -374,7 +374,8 @@ void mdbox_transaction_save_commit_post(struct mail_save_context *_ctx,
 					struct mail_index_transaction_commit_result *result)
 {
 	struct mdbox_save_context *ctx = MDBOX_SAVECTX(_ctx);
-	struct mail_storage *_storage = _ctx->transaction->box->storage;
+	struct mailbox *box = _ctx->transaction->box;
+	struct mail_storage *_storage = box->storage;
 	struct mdbox_storage *storage =
 		container_of(_storage, struct mdbox_storage, storage.storage);
 
@@ -401,7 +402,7 @@ void mdbox_transaction_save_commit_post(struct mail_save_context *_ctx,
 
 	if (_storage->set->parsed_fsync_mode != FSYNC_MODE_NEVER) {
 		if (fdatasync_path(storage->storage_dir) < 0) {
-			mailbox_set_critical(_ctx->transaction->box,
+			mailbox_set_critical(box,
 				"fdatasync_path(%s) failed: %m",
 				storage->storage_dir);
 		}