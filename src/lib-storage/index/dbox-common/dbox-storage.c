dovecot/core

commit 88eee28fd86d619b26ce6aebadca5b18e748dc6f

mdbox: Optimize mail_temp_scan_interval storage/ scanning
Use the generic dbox scanning code, which avoids stat()ing the directory by
keeping the timestamp in index header. Also do the scanning while at
session deinit instead of startup, so the latency isn't visible to clients.

src/lib-storage/index/dbox-multi/mdbox-map.c
@@ -80,12 +80,25 @@ mdbox_map_init(struct mdbox_storage *storage, struct mailbox_list *root_list)
 	return map;
 }
 
+static void mdbox_map_deinit_cleanup(struct mdbox_map *map)
+{
+	if (map->view == NULL)
+		return;
+
+	const struct mail_index_header *hdr =
+		mail_index_get_header(map->view);
+	if (dbox_mailbox_list_cleanup(map->root_list, map->path,
+				      hdr->last_temp_file_scan) > 0)
+		index_mailbox_view_update_last_temp_file_scan(map->view);
+}
+
 void mdbox_map_deinit(struct mdbox_map **_map)
 {
 	struct mdbox_map *map = *_map;
 
 	*_map = NULL;
 
+	mdbox_map_deinit_cleanup(map);
 	if (map->view != NULL) {
 		mail_index_view_close(&map->view);
 		mail_index_close(map->index);
@@ -112,28 +125,6 @@ static int mdbox_map_mkdir_storage(struct mdbox_map *map)
 		return -1;
 	}
 	return 0;
-}
-
-static void mdbox_map_cleanup(struct mdbox_map *map)
-{
-	unsigned int interval =
-		MAP_STORAGE(map)->set->mail_temp_scan_interval;
-	struct stat st;
-
-	if (stat(map->path, &st) < 0)
-		return;
-
-	/* check once in a while if there are temp files to clean up */
-	if (interval == 0) {
-		/* disabled */
-	} else if (st.st_atime > st.st_ctime + DBOX_TMP_DELETE_SECS) {
-		/* there haven't been any changes to this directory since we
-		   last checked it. */
-	} else if (st.st_atime < ioloop_time - (time_t)interval) {
-		/* time to scan */
-		(void)unlink_old_files(map->path, DBOX_TEMP_FILE_PREFIX,
-				       ioloop_time - DBOX_TMP_DELETE_SECS);
-	}
 }
 
 static int mdbox_map_open_internal(struct mdbox_map *map, bool create_missing)
@@ -184,7 +175,6 @@ static int mdbox_map_open_internal(struct mdbox_map *map, bool create_missing)
 	}
 
 	map->view = mail_index_view_open(map->index);
-	mdbox_map_cleanup(map);
 
 	if (mail_index_get_header(map->view)->uid_validity == 0) {
 		if (mdbox_map_generate_uid_validity(map) < 0) {

commit 8ce9a9b1c80dd829c580e584a276a55b941f3005

dbox: Fix for dangling pointer being passed to cleanup_interval()
Broken in 88eee28fd86d619b26ce6aebadca5b18e748dc6f

src/lib-storage/index/dbox-common/dbox-storage.c
@@ -174,13 +174,14 @@ void dbox_notify_changes(struct mailbox *box)
 	}
 }
 
-static time_t cleanup_interval(struct mailbox_list *list)
+static time_t cleanup_interval(struct mail_user *user)
 {
-	time_t interval = list->mail_set->mail_temp_scan_interval;
+	const struct mail_storage_settings *set =
+		mail_user_set_get_storage_set(user);
+	time_t interval = set->mail_temp_scan_interval;
 
-	const char *username = list->ns->user->username;
 	/* No need for a cryptographic-quality hash here. */
-	unsigned int hash = crc32_str(username);
+	unsigned int hash = crc32_str(user->username);
 
 	/* spread from 0.00 to to 30.00% more than the base interval */
 	unsigned int spread_factor = 100000 + hash % 30001;
@@ -188,11 +189,11 @@ static time_t cleanup_interval(struct mailbox_list *list)
 }
 
 static bool
-dbox_cleanup_temp_files(struct mailbox_list *list, const char *path,
+dbox_cleanup_temp_files(struct mail_user *user, const char *path,
 			time_t last_scan_time, time_t last_change_time)
 {
 	/* check once in a while if there are temp files to clean up */
-	time_t interval = cleanup_interval(list);
+	time_t interval = cleanup_interval(user);
 	if (interval == 0) {
 		/* disabled */
 		return FALSE;
@@ -210,8 +211,7 @@ dbox_cleanup_temp_files(struct mailbox_list *list, const char *path,
 		struct stat st;
 		if (stat(path, &st) < 0) {
 			if (errno != ENOENT)
-				e_error(list->ns->user->event,
-					"stat(%s) failed: %m", path);
+				e_error(user->event, "stat(%s) failed: %m", path);
 			return FALSE;
 		}
 		last_change_time = st.st_ctime;
@@ -285,7 +285,7 @@ int dbox_mailbox_open(struct mailbox *box)
 	return 0;
 }
 
-int dbox_mailbox_list_cleanup(struct mailbox_list *list, const char *path,
+int dbox_mailbox_list_cleanup(struct mail_user *user, const char *path,
 			      time_t last_temp_file_scan)
 {
 	time_t change_time = -1;
@@ -298,15 +298,13 @@ int dbox_mailbox_list_cleanup(struct mailbox_list *list, const char *path,
 			last_temp_file_scan = stats.st_atim.tv_sec;
 			change_time = stats.st_ctim.tv_sec;
 		} else {
-			if (errno != ENOENT) {
-				e_error(list->ns->user->event,
-					"stat(%s) failed: %m", path);
-			}
+			if (errno != ENOENT)
+				e_error(user->event, "stat(%s) failed: %m", path);
 			return -1;
 		}
 	}
 
-	if (dbox_cleanup_temp_files(list, path, last_temp_file_scan, change_time) ||
+	if (dbox_cleanup_temp_files(user, path, last_temp_file_scan, change_time) ||
 	    last_temp_file_scan == 0) {
 		/* temp files were scanned. update the last scan timestamp. */
 		return 1;
@@ -321,7 +319,8 @@ void dbox_mailbox_close_cleanup(struct mailbox *box)
 
 	const struct mail_index_header *hdr =
 		mail_index_get_header(box->view);
-	if (dbox_mailbox_list_cleanup(box->list, mailbox_get_path(box),
+	if (dbox_mailbox_list_cleanup(box->storage->user,
+				      mailbox_get_path(box),
 				      hdr->last_temp_file_scan) > 0)
 		index_mailbox_update_last_temp_file_scan(box);
 }

src/lib-storage/index/dbox-common/dbox-storage.h
@@ -77,7 +77,7 @@ int dbox_mailbox_check_existence(struct mailbox *box);
 int dbox_mailbox_open(struct mailbox *box);
 void dbox_mailbox_close(struct mailbox *box);
 void dbox_mailbox_close_cleanup(struct mailbox *box);
-int dbox_mailbox_list_cleanup(struct mailbox_list *list, const char *path,
+int dbox_mailbox_list_cleanup(struct mail_user *user, const char *path,
 			      time_t last_temp_file_scan);
 int dbox_mailbox_create(struct mailbox *box,
 			const struct mailbox_update *update, bool directory);

src/lib-storage/index/dbox-multi/mdbox-map.c
@@ -87,8 +87,8 @@ static void mdbox_map_deinit_cleanup(struct mdbox_map *map)
 
 	const struct mail_index_header *hdr =
 		mail_index_get_header(map->view);
-	if (dbox_mailbox_list_cleanup(map->root_list, map->path,
-				      hdr->last_temp_file_scan) > 0)
+	if (dbox_mailbox_list_cleanup(map->storage->storage.storage.user,
+				      map->path, hdr->last_temp_file_scan) > 0)
 		index_mailbox_view_update_last_temp_file_scan(map->view);
 }
 