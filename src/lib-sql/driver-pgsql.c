dovecot/core

commit 8981a973987c237552e46b68075b64caa8c1f5bb

driver-pgsql: Add wait() implementation

src/lib-sql/driver-pgsql.c
@@ -1239,6 +1239,30 @@ driver_pgsql_escape_blob(struct sql_db *_db ATTR_UNUSED,
 	return str_c(str);
 }
 
+static bool driver_pgsql_have_work(struct pgsql_db *db)
+{
+	return db->next_callback != NULL || db->pending_results != NULL;
+}
+
+static void driver_pgsql_wait(struct sql_db *_db)
+{
+	struct pgsql_db *db = (struct pgsql_db *)_db;
+
+	if (!driver_pgsql_have_work(db))
+		return;
+
+	struct ioloop *prev_ioloop = current_ioloop;
+	db->ioloop = io_loop_create();
+	db->io = io_loop_move_io(&db->io);
+	while (driver_pgsql_have_work(db))
+		io_loop_run(db->ioloop);
+
+	io_loop_set_current(prev_ioloop);
+	db->io = io_loop_move_io(&db->io);
+	io_loop_set_current(db->ioloop);
+	io_loop_destroy(&db->ioloop);
+}
+
 const struct sql_db driver_pgsql_db = {
 	.name = "pgsql",
 	.flags = SQL_DB_FLAG_POOLED,
@@ -1252,6 +1276,7 @@ const struct sql_db driver_pgsql_db = {
 		.exec = driver_pgsql_exec,
 		.query = driver_pgsql_query,
 		.query_s = driver_pgsql_query_s,
+		.wait = driver_pgsql_wait,
 
 		.transaction_begin = driver_pgsql_transaction_begin,
 		.transaction_commit = driver_pgsql_transaction_commit,

commit 2340275fade3af92792cd4303b8fc5945ed9d7c7

lib-sql: pgsql - Use orig_ioloop instead of local variable
orig_ioloop is used later, leaving it NULL here causes
problems when timeout is added by sqlpool.

Broken in 8981a973987c237552e46b68075b64caa8c1f5bb

src/lib-sql/driver-pgsql.c
@@ -1272,13 +1272,13 @@ static void driver_pgsql_wait(struct sql_db *_db)
 	if (!driver_pgsql_have_work(db))
 		return;
 
-	struct ioloop *prev_ioloop = current_ioloop;
+	db->orig_ioloop = current_ioloop;
 	db->ioloop = io_loop_create();
 	db->io = io_loop_move_io(&db->io);
 	while (driver_pgsql_have_work(db))
 		io_loop_run(db->ioloop);
 
-	io_loop_set_current(prev_ioloop);
+	io_loop_set_current(db->orig_ioloop);
 	db->io = io_loop_move_io(&db->io);
 	io_loop_set_current(db->ioloop);
 	io_loop_destroy(&db->ioloop);