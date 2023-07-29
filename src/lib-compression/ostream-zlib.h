ede60dec2bc2724d1f7c7b5bfe16e04e24484bd9
Timo Sirainen <timo.sirainen@open-xchange.com>
2023-07-29 20:49:34 +0300

    lib-compression: ostream-zlib - Add support for create_ostream_auto() API

--- src/lib-compression/ostream-zlib.h
+++ src/lib-compression/ostream-zlib.h
@@ -7,6 +7,9 @@ struct ostream *o_stream_create_bz2(struct ostream *output, int level);
 struct ostream *o_stream_create_lz4(struct ostream *output, int level);
 struct ostream *o_stream_create_zstd(struct ostream *output, int level);
 
+struct ostream *o_stream_create_gz_auto(struct ostream *output, struct event *event);
+struct ostream *o_stream_create_deflate_auto(struct ostream *output, struct event *event);
+
 int compression_get_min_level_gz(void);
 int compression_get_default_level_gz(void);
 int compression_get_max_level_gz(void);
