e85a2a6bfff4936eb373d6793494107e8ff86140
Timo Sirainen <timo.sirainen@open-xchange.com>
2023-07-29 21:22:28 +0300

    mail-compress: Use create_ostream_auto() API
    mail_compress_save plugin setting is replaced by mail_compress_write_method.
    mail_compress_save_level plugin setting is replaced by compression method
    specific settings.

--- src/plugins/mail-compress/mail-compress-plugin.h
+++ src/plugins/mail-compress/mail-compress-plugin.h
@@ -1,6 +1,13 @@
 #ifndef MAIL_COMPRESS_PLUGIN_H
 #define MAIL_COMPRESS_PLUGIN_H
 
+struct mail_compress_settings {
+	pool_t pool;
+	const char *mail_compress_write_method;
+};
+
+extern const struct setting_parser_info mail_compress_setting_parser_info;
+
 void mail_compress_plugin_init(struct module *module);
 void mail_compress_plugin_deinit(void);
 
