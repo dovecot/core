563055826e69a246c2271cabd4a5be4cf241ab6d
Timo Sirainen <timo.sirainen@open-xchange.com>
2023-08-07 11:17:34 +0300

    doveadm: Add "mail fs" commands
    These are similar to "doveadm fs" commands, but they're run in the mail user
    context. This way all user-specific settings are applied and process
    uid/gid is also set properly.

--- src/doveadm/doveadm-mail.h
+++ src/doveadm/doveadm-mail.h
@@ -210,6 +210,14 @@ extern struct doveadm_cmd_ver2 doveadm_cmd_mailbox_cache_decision;
 extern struct doveadm_cmd_ver2 doveadm_cmd_mailbox_cache_remove;
 extern struct doveadm_cmd_ver2 doveadm_cmd_mailbox_cache_purge;
 extern struct doveadm_cmd_ver2 doveadm_cmd_rebuild_attachments;
+extern struct doveadm_cmd_ver2 doveadm_cmd_mail_fs_get;
+extern struct doveadm_cmd_ver2 doveadm_cmd_mail_fs_put;
+extern struct doveadm_cmd_ver2 doveadm_cmd_mail_fs_copy;
+extern struct doveadm_cmd_ver2 doveadm_cmd_mail_fs_stat;
+extern struct doveadm_cmd_ver2 doveadm_cmd_mail_fs_metadata;
+extern struct doveadm_cmd_ver2 doveadm_cmd_mail_fs_delete;
+extern struct doveadm_cmd_ver2 doveadm_cmd_mail_fs_iter;
+extern struct doveadm_cmd_ver2 doveadm_cmd_mail_fs_iter_dirs;
 
 #define DOVEADM_CMD_MAIL_COMMON \
 DOVEADM_CMD_PARAM('A', "all-users", CMD_PARAM_BOOL, 0) \
