dovecot/core

commit 542877c08eb8130733567e5b718933bc3cdc8426

lib-sql: driver-mysql - Initialize MYSQL struct once
Calling initialize more than once will leak memory.

src/lib-sql/driver-mysql.c
@@ -91,9 +91,6 @@ static int driver_mysql_connect(struct sql_db *_db)
 
 	sql_db_set_state(&db->api, SQL_DB_STATE_CONNECTING);
 
-	if (mysql_init(db->mysql) == NULL)
-		i_fatal("mysql_init() failed");
-
 	if (db->host == NULL) {
 		/* assume option_file overrides the host, or if not we'll just
 		   connect to localhost */
@@ -177,7 +174,6 @@ static void driver_mysql_disconnect(struct sql_db *_db)
 	struct mysql_db *db = container_of(_db, struct mysql_db, api);
 	if (db->mysql != NULL)
 		mysql_close(db->mysql);
-	db->mysql = NULL;
 }
 
 static int driver_mysql_parse_connect_string(struct mysql_db *db,
@@ -274,7 +270,12 @@ static int driver_mysql_parse_connect_string(struct mysql_db *db,
 		*error_r = "No hosts given in connect string";
 		return -1;
 	}
-	db->mysql = p_new(db->pool, MYSQL, 1);
+	if (db->mysql == NULL) {
+		db->mysql = p_new(db->pool, MYSQL, 1);
+		MYSQL *ptr = mysql_init(db->mysql);
+		if (ptr == NULL)
+			i_fatal_status(FATAL_OUTOFMEM, "mysql_init() failed");
+	}
 	return 0;
 }
 

commit bafdc8321134ebe7b9444831689a020217588020

lib-sql: driver-mysql - Use API state to check if there is connection
db->mysql cannot be NULL.

Broken in 542877c08eb8130733567e5b718933bc3cdc8426

src/lib-sql/driver-mysql.c
@@ -369,7 +369,7 @@ driver_mysql_escape_string(struct sql_db *_db, const char *string)
 		(void)sql_connect(&db->api);
 	}
 
-	if (db->mysql == NULL) {
+	if (_db->state == SQL_DB_STATE_DISCONNECTED) {
 		/* FIXME: we don't have a valid connection, so fallback
 		   to using default escaping. the next query will most
 		   likely fail anyway so it shouldn't matter that much