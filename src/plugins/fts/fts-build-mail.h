6fbca9e2ca6d89a2bc7e5004a65c8f30f3a5fcf0
Marco Bettini <marco.bettini@open-xchange.com>
2023-09-29 09:25:07 +0000

    fts: fts_build_mail() - Update comment in header file

--- src/plugins/fts/fts-build-mail.h
+++ src/plugins/fts/fts-build-mail.h
@@ -1,8 +1,11 @@
 #ifndef FTS_BUILD_MAIL_H
 #define FTS_BUILD_MAIL_H
 
-/* Build indexes for the given mail. Returns 0 on success, -1 on error.
-   The error is set to mail's storage. */
+/* Build indexes for the given mail.
+   Returns -1 on error, The error is set to mail's storage.
+   Returns  0 on ignored error (retry limit reached and mail not built,
+                                see comments in function implementation)
+   Returns  1 on success */
 int fts_build_mail(struct fts_backend_update_context *update_ctx,
 		   struct mail *mail);
 
