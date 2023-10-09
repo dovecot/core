dovecot/core

commit 32e8bd5b5a9d5bc04e5ac6c65ac132ba5c386d64

dict backends: Remove multi-path handling code in dict iteration
With mutli-path iteration support being dropped from dict protocol, these
can be cleaned up.

src/lib-dict-extra/dict-fs.c
@@ -16,8 +16,7 @@ struct fs_dict {
 
 struct fs_dict_iterate_context {
 	struct dict_iterate_context ctx;
-	const char **paths;
-	unsigned int path_idx;
+	char *path;
 	enum dict_iterate_flags flags;
 	pool_t value_pool;
 	struct fs_iter *fs_iter;
@@ -152,7 +151,7 @@ static int fs_dict_lookup(struct dict *_dict, pool_t pool, const char *key,
 }
 
 static struct dict_iterate_context *
-fs_dict_iterate_init(struct dict *_dict, const char *const *paths,
+fs_dict_iterate_init(struct dict *_dict, const char *path,
 		     enum dict_iterate_flags flags)
 {
 	struct fs_dict *dict = (struct fs_dict *)_dict;
@@ -166,11 +165,11 @@ fs_dict_iterate_init(struct dict *_dict, const char *const *paths,
 
 	iter = i_new(struct fs_dict_iterate_context, 1);
 	iter->ctx.dict = _dict;
-	iter->paths = p_strarray_dup(default_pool, paths);
+	iter->path = i_strdup(path);
 	iter->flags = flags;
 	iter->value_pool = pool_alloconly_create("iterate value pool", 128);
 	iter->fs_iter = fs_iter_init(dict->fs,
-				     fs_dict_get_full_key(dict, paths[0]), 0);
+				     fs_dict_get_full_key(dict, path), 0);
 	return &iter->ctx;
 }
 
@@ -192,13 +191,13 @@ static bool fs_dict_iterate(struct dict_iterate_context *ctx,
 			iter->error = i_strdup(error);
 			return FALSE;
 		}
-		if (iter->paths[++iter->path_idx] == NULL)
+		if (iter->path == NULL)
 			return FALSE;
-		path = fs_dict_get_full_key(dict, iter->paths[iter->path_idx]);
+		path = fs_dict_get_full_key(dict, iter->path);
 		iter->fs_iter = fs_iter_init(dict->fs, path, 0);
 		return fs_dict_iterate(ctx, key_r, values_r);
 	}
-	path = t_strconcat(iter->paths[iter->path_idx], *key_r, NULL);
+	path = t_strconcat(iter->path, *key_r, NULL);
 	if ((iter->flags & DICT_ITERATE_FLAG_NO_VALUE) != 0) {
 		iter->values[0] = NULL;
 		*key_r = path;
@@ -235,7 +234,7 @@ static int fs_dict_iterate_deinit(struct dict_iterate_context *ctx,
 	*error_r = t_strdup(iter->error);
 
 	pool_unref(&iter->value_pool);
-	i_free(iter->paths);
+	i_free(iter->path);
 	i_free(iter->error);
 	i_free(iter);
 	return ret;

src/lib-dict-backend/dict-cdb.c
@@ -26,7 +26,8 @@ struct cdb_dict_iterate_context {
 
 	enum dict_iterate_flags flags;
 	buffer_t *buffer;
-	const char **paths, *values[2];
+	const char *values[2];
+	char *path;
 	unsigned cptr;
 	char *error;
 };
@@ -128,7 +129,7 @@ cdb_dict_lookup(struct dict *_dict, pool_t pool,
 }
 
 static struct dict_iterate_context *
-cdb_dict_iterate_init(struct dict *_dict, const char *const *paths,
+cdb_dict_iterate_init(struct dict *_dict, const char *path,
 		      enum dict_iterate_flags flags)
 {
 	struct cdb_dict_iterate_context *ctx =
@@ -136,7 +137,7 @@ cdb_dict_iterate_init(struct dict *_dict, const char *const *paths,
 	struct cdb_dict *dict = (struct cdb_dict *)_dict;
 
 	ctx->ctx.dict = &dict->dict;
-	ctx->paths = p_strarray_dup(default_pool, paths);
+	ctx->path = i_strdup(path);
 	ctx->flags = flags;
 	ctx->buffer = buffer_create_dynamic(default_pool, 256);
 
@@ -183,7 +184,7 @@ static bool cdb_dict_iterate(struct dict_iterate_context *_ctx,
 	struct cdb_dict_iterate_context *ctx =
 		(struct cdb_dict_iterate_context *)_ctx;
 	struct cdb_dict *dict = (struct cdb_dict *)_ctx->dict;
-	const char *key, **ptr;
+	const char *key;
 	bool match = FALSE;
 	char *data;
 	unsigned datalen;
@@ -192,18 +193,15 @@ static bool cdb_dict_iterate(struct dict_iterate_context *_ctx,
 		return FALSE;
 
 	while(!match && cdb_dict_next(ctx, &key)) {
-		/* if it matches any of the paths */
-		for(ptr = ctx->paths; *ptr != NULL; ptr++) {
-			if (((ctx->flags & DICT_ITERATE_FLAG_EXACT_KEY) != 0 &&
-			     strcmp(key, *ptr) == 0) ||
-			    ((ctx->flags & DICT_ITERATE_FLAG_RECURSE) != 0 &&
-			     str_begins(key, *ptr)) ||
-			    ((ctx->flags & DICT_ITERATE_FLAG_RECURSE) == 0 &&
-			     str_begins(key, *ptr) &&
-			     strchr(key + strlen(*ptr), '/') == NULL)) {
-				match = TRUE;
-				break;
-			}
+		if (((ctx->flags & DICT_ITERATE_FLAG_EXACT_KEY) != 0 &&
+		     strcmp(key, ctx->path) == 0) ||
+		    ((ctx->flags & DICT_ITERATE_FLAG_RECURSE) != 0 &&
+		     str_begins(key, ctx->path)) ||
+		    ((ctx->flags & DICT_ITERATE_FLAG_RECURSE) == 0 &&
+		     str_begins(key, ctx->path) &&
+		     strchr(key + strlen(ctx->path), '/') == NULL)) {
+			match = TRUE;
+			break;
 		}
 	}
 
@@ -244,7 +242,7 @@ static int cdb_dict_iterate_deinit(struct dict_iterate_context *_ctx,
 
 	buffer_free(&ctx->buffer);
 	i_free(ctx->error);
-	i_free(ctx->paths);
+	i_free(ctx->path);
 	i_free(ctx);
 
 	return ret;

src/lib-dict-backend/dict-sql.c
@@ -39,13 +39,13 @@ struct sql_dict_iterate_context {
 	pool_t pool;
 
 	enum dict_iterate_flags flags;
-	const char **paths;
+	const char *path;
 
 	struct sql_result *result;
 	string_t *key;
 	const struct dict_sql_map *map;
 	size_t key_prefix_len, pattern_prefix_len;
-	unsigned int path_idx, sql_fields_start_idx, next_map_idx;
+	unsigned int sql_fields_start_idx, next_map_idx;
 	bool destroyed;
 	bool synchronous_result;
 	bool iter_query_sent;
@@ -617,7 +617,7 @@ sql_dict_iterate_find_next_map(struct sql_dict_iterate_context *ctx,
 	t_array_init(pattern_values, dict->set->max_pattern_fields_count);
 	maps = array_get(&dict->set->maps, &count);
 	for (i = ctx->next_map_idx; i < count; i++) {
-		if (dict_sql_map_match(&maps[i], ctx->paths[ctx->path_idx],
+		if (dict_sql_map_match(&maps[i], ctx->path,
 				       pattern_values, &pat_len, &path_len,
 				       TRUE, recurse) &&
 		    (recurse ||
@@ -627,15 +627,10 @@ sql_dict_iterate_find_next_map(struct sql_dict_iterate_context *ctx,
 			ctx->next_map_idx = i + 1;
 
 			str_truncate(ctx->key, 0);
-			str_append(ctx->key, ctx->paths[ctx->path_idx]);
+			str_append(ctx->key, ctx->path);
 			return &maps[i];
 		}
 	}
-
-	/* try the next path, if there is any */
-	ctx->path_idx++;
-	if (ctx->paths[ctx->path_idx] != NULL)
-		return sql_dict_iterate_find_next_map(ctx, pattern_values);
 	return NULL;
 }
 
@@ -696,7 +691,7 @@ sql_dict_iterate_build_next_query(struct sql_dict_iterate_context *ctx,
 
 	ARRAY_TYPE(sql_dict_param) params;
 	t_array_init(&params, 4);
-	bool add_username = (ctx->paths[ctx->path_idx][0] == DICT_PATH_PRIVATE[0]);
+	bool add_username = (ctx->path[0] == DICT_PATH_PRIVATE[0]);
 	if (sql_dict_where_build(dict, map, &pattern_values, add_username,
 				 recurse_type, query, &params, error_r) < 0)
 		return -1;
@@ -740,7 +735,6 @@ static int sql_dict_iterate_next_query(struct sql_dict_iterate_context *ctx)
 {
 	struct sql_statement *stmt;
 	const char *error;
-	unsigned int path_idx = ctx->path_idx;
 	int ret;
 
 	ret = sql_dict_iterate_build_next_query(ctx, &stmt, &error);
@@ -751,7 +745,7 @@ static int sql_dict_iterate_next_query(struct sql_dict_iterate_context *ctx)
 		/* failed */
 		ctx->error = p_strdup_printf(ctx->pool,
 			"sql dict iterate failed for %s: %s",
-			ctx->paths[path_idx], error);
+			ctx->path, error);
 		return -1;
 	}
 
@@ -768,11 +762,10 @@ static int sql_dict_iterate_next_query(struct sql_dict_iterate_context *ctx)
 }
 
 static struct dict_iterate_context *
-sql_dict_iterate_init(struct dict *_dict, const char *const *paths,
+sql_dict_iterate_init(struct dict *_dict, const char *path,
 		      enum dict_iterate_flags flags)
 {
 	struct sql_dict_iterate_context *ctx;
-	unsigned int i, path_count;
 	pool_t pool;
 
 	pool = pool_alloconly_create("sql dict iterate", 512);
@@ -781,10 +774,7 @@ sql_dict_iterate_init(struct dict *_dict, const char *const *paths,
 	ctx->pool = pool;
 	ctx->flags = flags;
 
-	for (path_count = 0; paths[path_count] != NULL; path_count++) ;
-	ctx->paths = p_new(pool, const char *, path_count + 1);
-	for (i = 0; i < path_count; i++)
-		ctx->paths[i] = p_strdup(pool, paths[i]);
+	ctx->path = p_strdup(pool, path);
 
 	ctx->key = str_new(pool, 256);
 	return &ctx->ctx;

src/lib-dict/dict-client.c
@@ -98,7 +98,7 @@ struct client_dict_iter_result {
 struct client_dict_iterate_context {
 	struct dict_iterate_context ctx;
 	char *error;
-	const char **paths;
+	char *path;
 	enum dict_iterate_flags flags;
 	int refcount;
 
@@ -1145,7 +1145,7 @@ client_dict_iter_async_callback(struct client_dict_cmd *cmd,
 }
 
 static struct dict_iterate_context *
-client_dict_iterate_init(struct dict *_dict, const char *const *paths,
+client_dict_iterate_init(struct dict *_dict, const char *path,
 			 enum dict_iterate_flags flags)
 {
         struct client_dict_iterate_context *ctx;
@@ -1154,7 +1154,7 @@ client_dict_iterate_init(struct dict *_dict, const char *const *paths,
 	ctx->ctx.dict = _dict;
 	ctx->results_pool = pool_alloconly_create("client dict iteration", 512);
 	ctx->flags = flags;
-	ctx->paths = p_strarray_dup(system_pool, paths);
+	ctx->path = i_strdup(path);
 	ctx->refcount = 1;
 	i_array_init(&ctx->results, 64);
 	return &ctx->ctx;
@@ -1165,17 +1165,14 @@ client_dict_iterate_cmd_send(struct client_dict_iterate_context *ctx)
 {
 	struct client_dict *dict = (struct client_dict *)ctx->ctx.dict;
 	struct client_dict_cmd *cmd;
-	unsigned int i;
 	string_t *query = t_str_new(256);
 
 	/* we can't do this query in _iterate_init(), because
 	   _set_limit() hasn't been called yet at that point. */
 	str_printfa(query, "%c%d\t%"PRIu64, DICT_PROTOCOL_CMD_ITERATE,
 		    ctx->flags, ctx->ctx.max_rows);
-	for (i = 0; ctx->paths[i] != NULL; i++) {
-		str_append_c(query, '\t');
-		str_append(query, str_tabescape(ctx->paths[i]));
-	}
+	str_append_c(query, '\t');
+	str_append_tabescaped(query, ctx->path);
 
 	cmd = client_dict_cmd_init(dict, str_c(query));
 	cmd->iter = ctx;
@@ -1239,7 +1236,7 @@ static int client_dict_iterate_deinit(struct dict_iterate_context *_ctx,
 	*error_r = t_strdup(ctx->error);
 	array_free(&ctx->results);
 	pool_unref(&ctx->results_pool);
-	i_free(ctx->paths);
+	i_free(ctx->path);
 	client_dict_iterate_unref(ctx);
 
 	client_dict_add_timeout(dict);

src/lib-dict/dict-fail.c
@@ -40,7 +40,7 @@ static int dict_fail_lookup(struct dict *dict ATTR_UNUSED, pool_t pool ATTR_UNUS
 }
 
 static struct dict_iterate_context *
-dict_fail_iterate_init(struct dict *dict ATTR_UNUSED, const char *const *paths ATTR_UNUSED,
+dict_fail_iterate_init(struct dict *dict ATTR_UNUSED, const char *path ATTR_UNUSED,
 		       enum dict_iterate_flags flags ATTR_UNUSED)
 {
 	return &dict_iter_unsupported;

src/lib-dict/dict-file.c
@@ -43,7 +43,7 @@ struct file_dict_iterate_context {
 	pool_t pool;
 
 	struct hash_iterate_context *iter;
-	struct file_dict_iterate_path *paths;
+	struct file_dict_iterate_path *path;
 
 	enum dict_iterate_flags flags;
 	const char *values[2];
@@ -210,12 +210,11 @@ static int file_dict_lookup(struct dict *_dict, pool_t pool, const char *key,
 }
 
 static struct dict_iterate_context *
-file_dict_iterate_init(struct dict *_dict, const char *const *paths,
+file_dict_iterate_init(struct dict *_dict, const char *path,
 		       enum dict_iterate_flags flags)
 {
         struct file_dict_iterate_context *ctx;
 	struct file_dict *dict = (struct file_dict *)_dict;
-	unsigned int i, path_count;
 	const char *error;
 	pool_t pool;
 
@@ -224,12 +223,9 @@ file_dict_iterate_init(struct dict *_dict, const char *const *paths,
 	ctx->ctx.dict = _dict;
 	ctx->pool = pool;
 
-	for (path_count = 0; paths[path_count] != NULL; path_count++) ;
-	ctx->paths = p_new(pool, struct file_dict_iterate_path, path_count + 1);
-	for (i = 0; i < path_count; i++) {
-		ctx->paths[i].path = p_strdup(pool, paths[i]);
-		ctx->paths[i].len = strlen(paths[i]);
-	}
+	ctx->path = p_new(pool, struct file_dict_iterate_path, 1);
+	ctx->path->path = p_strdup(pool, path);
+	ctx->path->len = strlen(path);
 	ctx->flags = flags;
 
 	if (file_dict_refresh(dict, &error) < 0)
@@ -239,32 +235,28 @@ file_dict_iterate_init(struct dict *_dict, const char *const *paths,
 	return &ctx->ctx;
 }
 
-static const struct file_dict_iterate_path *
-file_dict_iterate_find_path(struct file_dict_iterate_context *ctx,
-			    const char *key)
+static bool
+file_dict_iterate_key_matches(struct file_dict_iterate_context *ctx,
+			      const char *key)
 {
-	unsigned int i;
-
-	for (i = 0; ctx->paths[i].path != NULL; i++) {
-		if (strncmp(ctx->paths[i].path, key, ctx->paths[i].len) == 0)
-			return &ctx->paths[i];
-	}
-	return NULL;
+	if (strncmp(ctx->path->path, key, ctx->path->len) == 0)
+		return TRUE;
+	return FALSE;
 }
+
 
 static bool file_dict_iterate(struct dict_iterate_context *_ctx,
 			      const char **key_r, const char *const **values_r)
 {
 	struct file_dict_iterate_context *ctx =
 		(struct file_dict_iterate_context *)_ctx;
-	const struct file_dict_iterate_path *path;
+	const struct file_dict_iterate_path *path = ctx->path;
 	char *key, *value;
 
 	while (hash_table_iterate(ctx->iter,
 				  ((struct file_dict *)_ctx->dict)->hash,
 				  &key, &value)) {
-		path = file_dict_iterate_find_path(ctx, key);
-		if (path == NULL)
+		if (!file_dict_iterate_key_matches(ctx, key))
 			continue;
 
 		if ((ctx->flags & DICT_ITERATE_FLAG_RECURSE) != 0) {

src/lib-dict/dict-private.h
@@ -18,7 +18,7 @@ struct dict_vfuncs {
 		      const char **error_r);
 
 	struct dict_iterate_context *
-		(*iterate_init)(struct dict *dict, const char *const *paths,
+		(*iterate_init)(struct dict *dict, const char *path,
 				enum dict_iterate_flags flags);
 	bool (*iterate)(struct dict_iterate_context *ctx,
 			const char **key_r, const char *const **values_r);

src/lib-dict/dict.c
@@ -344,19 +344,15 @@ dict_iterate_init(struct dict *dict, const struct dict_op_settings *set ATTR_UNU
 		  const char *path, enum dict_iterate_flags flags)
 {
 	struct dict_iterate_context *ctx;
-	const char *paths[2];
 
 	i_assert(path != NULL);
 	i_assert(dict_key_prefix_is_valid(path));
-
-	paths[0] = path;
-	paths[1] = NULL;
 
 	if (dict->v.iterate_init == NULL) {
 		/* not supported by backend */
 		ctx = &dict_iter_unsupported;
 	} else {
-		ctx = dict->v.iterate_init(dict, paths, flags);
+		ctx = dict->v.iterate_init(dict, path, flags);
 	}
 	/* the dict in context can differ from the dict
 	   passed as parameter, e.g. it can be dict-fail when
@@ -364,9 +360,9 @@ dict_iterate_init(struct dict *dict, const struct dict_op_settings *set ATTR_UNU
 	ctx->event = event_create(dict->event);
 	ctx->flags = flags;
 
-	event_add_str(ctx->event, "key", paths[0]);
+	event_add_str(ctx->event, "key", path);
 	event_set_name(ctx->event, "dict_iteration_started");
-	e_debug(ctx->event, "Iterating prefix %s", paths[0]);
+	e_debug(ctx->event, "Iterating prefix %s", path);
 	ctx->dict->iter_count++;
 	return ctx;
 }

commit 0f9d0b643dc025557b0784bc8f6b92e526556f63

lib-dict: dict-fs - Allocate key from iteration pool
Path gets allocated from stack memory, which causes it to
become invalidated before used by clients.

Broken in 32e8bd5b5a9d5bc04e5ac6c65ac132ba5c386d64

src/lib-dict-extra/dict-fs.c
@@ -204,12 +204,12 @@ static bool fs_dict_iterate(struct dict_iterate_context *ctx,
 	}
 	iter->key_count++;
 
-	path = t_strconcat(iter->path, *key_r, NULL);
+	p_clear(iter->value_pool);
+	path = p_strconcat(iter->value_pool, iter->path, *key_r, NULL);
 	if ((iter->flags & DICT_ITERATE_FLAG_NO_VALUE) != 0) {
 		*key_r = path;
 		return TRUE;
 	}
-	p_clear(iter->value_pool);
 	struct dict_op_settings set = {
 		.username = ctx->set.username,
 	};