e0369f9eba9c808181ce910dd32570c126161fc6
Stephan Bosch <stephan.bosch@open-xchange.com>
2023-11-02 13:32:27 +0100

    auth: test-auth - Run the test as a master service

--- src/auth/test-main.c
+++ src/auth/test-main.c
@@ -1,15 +1,21 @@
 /* Copyright (c) 2017-2018 Dovecot authors, see the included COPYING file */
 
 #include "lib.h"
+#include "master-service.h"
 #include "auth-settings.h"
 #include "test-common.h"
 #include "test-auth.h"
 #include "password-scheme.h"
 #include "passdb.h"
 
-int main(int argc, const char *argv[])
+int main(int argc, char *argv[])
 {
 	const char *match = "";
+	const enum master_service_flags service_flags =
+		MASTER_SERVICE_FLAG_NO_CONFIG_SETTINGS |
+		MASTER_SERVICE_FLAG_STANDALONE |
+		MASTER_SERVICE_FLAG_STD_CLIENT |
+		MASTER_SERVICE_FLAG_DONT_SEND_STATS;
 	int ret;
 	static const struct named_test test_functions[] = {
 #if defined(BUILTIN_LDAP) || defined(PLUGIN_BUILD)
@@ -26,6 +32,10 @@ int main(int argc, const char *argv[])
 		{ NULL, NULL }
 	};
 
+	master_service = master_service_init("test-auth",
+		service_flags, &argc, &argv, "");
+	master_service_init_finish(master_service);
+
 	password_schemes_init();
 	passdbs_init();
 	passdb_mock_mod_init();
@@ -38,6 +48,8 @@ int main(int argc, const char *argv[])
 	passdb_mock_mod_deinit();
 	password_schemes_deinit();
 	passdbs_deinit();
+
+	master_service_deinit(&master_service);
 
 	return ret;
 }
