8bc047262bad20c8255bc003dcb9e3485fefeedc
Karl Fleischmann <karl.fleischmann@open-xchange.com>
2024-01-16 13:31:14 +0100

    imap-login: Convert imap_id_send to a strlist setting

--- src/imap-login/imap-login-settings.h
+++ src/imap-login/imap-login-settings.h
@@ -4,7 +4,7 @@
 struct imap_login_settings {
 	pool_t pool;
 	const char *imap_capability;
-	const char *imap_id_send;
+	ARRAY_TYPE(const_string) imap_id_send;
 	bool imap_literal_minus;
 	bool imap_id_retain;
 };
