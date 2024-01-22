dovecot/core

commit 9a1a1cc222d7b67abf3ff621b70ed92b1b6d8d39

lmtp: Include local_name in auth request, so it ends up lib-storage

src/lmtp/lmtp-client.c
@@ -158,6 +158,8 @@ struct client *client_create(int fd_in, int fd_out,
 	client->real_remote_ip = conn->real_remote_ip;
 	client->real_remote_port = conn->real_remote_port;
 	client->state_pool = pool_alloconly_create("client state", 4096);
+	if (conn->haproxy.ssl)
+		client->local_name = conn->haproxy.hostname;
 
 	client->event = event_create(NULL);
 	event_add_category(client->event, &event_category_lmtp);
@@ -242,11 +244,16 @@ void client_destroy(struct client **_client, const char *enh_code,
 		    const char *reason)
 {
 	struct client *client = *_client;
+	struct smtp_server_connection *conn = client->conn;
 
 	*_client = NULL;
 
-	smtp_server_connection_terminate(&client->conn,
+	smtp_server_connection_terminate(&conn,
 		(enh_code == NULL ? "4.0.0" : enh_code), reason);
+	/* smtp_server_connection_terminate() calls
+	   client_connection_state_changed(), which may still access
+	   client->conn. Don't clear it before that. */
+	client->conn = NULL;
 }
 
 static void
@@ -318,6 +325,11 @@ client_connection_state_changed(void *context,
 	client->state.state = new_state;
 	client->state.args = i_strdup(new_args);
 
+	if (client->local_name == NULL) {
+		const char *local_name =
+			smtp_server_connection_get_server_name(client->conn);
+		client->local_name = p_strdup(client->pool, local_name);
+	}
 	if (clients_count == 1)
 		refresh_proctitle();
 }

src/lmtp/lmtp-client.h
@@ -80,6 +80,7 @@ struct client {
 
 	struct ip_addr remote_ip, local_ip, real_local_ip, real_remote_ip;
 	in_port_t remote_port, local_port, real_local_port, real_remote_port;
+	const char *local_name;
 
 	struct mail_user *raw_mail_user;
 	const char *my_domain;

src/lmtp/lmtp-local.c
@@ -311,6 +311,7 @@ int lmtp_local_rcpt(struct client *client,
 	input.local_port = client->local_port;
 	input.remote_port = client->remote_port;
 	input.session_id = lrcpt->session_id;
+	input.local_name = client->local_name;
 	input.end_client_tls_secured =
 		client->end_client_tls_secured_set ?
 		client->end_client_tls_secured :

src/lmtp/lmtp-proxy.c
@@ -687,6 +687,7 @@ lmtp_proxy_rcpt_init_auth_user_info(struct lmtp_recipient *lrcpt,
 	info_r->remote_port = client->remote_port;
 	info_r->real_remote_port = client->real_remote_port;
 	info_r->forward_fields = lrcpt->forward_fields;
+	info_r->local_name = client->local_name;
 }
 
 static void

commit b579a89a67cab367bba6a7952a9f8d6afe5cd331

lmtp: lmtp-client - Fix potential segfault occurring at service termination
Invalid write of size 8 found by Valgrind.
Broken by 9a1a1cc222d7b67abf3ff621b70ed92b1b6d8d39.

src/lmtp/lmtp-client.c
@@ -270,12 +270,14 @@ void client_destroy(struct client **_client, const char *enh_code,
 
 	*_client = NULL;
 
+	client_ref(client);
 	smtp_server_connection_terminate(&conn,
 		(enh_code == NULL ? "4.0.0" : enh_code), reason);
 	/* smtp_server_connection_terminate() calls
 	   client_connection_state_changed(), which may still access
 	   client->conn. Don't clear it before that. */
 	client->conn = NULL;
+	client_unref(&client);
 }
 
 static void