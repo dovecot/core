548d4e6dc6c356fe2791be2f569a2fddb6a39788
Marco Bettini <marco.bettini@open-xchange.com>
2024-01-31 11:55:49 +0000

    lib-language: Copy plugins/fts/fts-user.* into lang-user.*

---
+++ src/lib-storage/lang-user.h
@@ -0,0 +1,29 @@
+#ifndef FTS_USER_H
+#define FTS_USER_H
+
+#include "fts-settings.h"
+
+struct fts_user_language {
+	const struct language *lang;
+	struct lang_filter *filter;
+	struct lang_tokenizer *index_tokenizer, *search_tokenizer;
+};
+ARRAY_DEFINE_TYPE(fts_user_language, struct fts_user_language *);
+
+struct fts_user_language *
+fts_user_language_find(struct mail_user *user,
+                       const struct language *lang);
+struct language_list *fts_user_get_language_list(struct mail_user *user);
+const ARRAY_TYPE(fts_user_language) *
+fts_user_get_all_languages(struct mail_user *user);
+struct fts_user_language *fts_user_get_data_lang(struct mail_user *user);
+const ARRAY_TYPE(fts_user_language) *
+fts_user_get_data_languages(struct mail_user *user);
+
+const struct fts_settings *fts_user_get_settings(struct mail_user *user);
+
+int fts_mail_user_init(struct mail_user *user, bool initialize_libfts,
+		       const char **error_r);
+void fts_mail_user_deinit(struct mail_user *user);
+
+#endif
