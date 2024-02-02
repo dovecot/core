dovecot/core

commit 720a6e0e67c3f04dcfee4c80b34f0291c7217cfa

lib: io_loop_stop() should stop the ioloop immediately
Other timeouts or IOs shouldn't be called anymore.

src/lib/ioloop-kqueue.c
@@ -150,6 +150,9 @@ void io_loop_handler_run_internal(struct ioloop *ioloop)
 	/* execute timeout handlers */
 	io_loop_handle_timeouts(ioloop);
 
+	if (!ioloop->running)
+		return;
+
 	for (i = 0; i < ret; i++) {
 		/* io_loop_handle_add() may cause events array reallocation,
 		   so we have use array_idx() */
@@ -157,8 +160,11 @@ void io_loop_handler_run_internal(struct ioloop *ioloop)
 		io = (void *)event->udata;
 
 		/* callback is NULL if io_remove() was already called */
-		if (io->io.callback != NULL)
+		if (io->io.callback != NULL) {
 			io_loop_call_io(&io->io);
+			if (!ioloop->running)
+				break;
+		}
 
 		i_assert(io->refcount > 0);
 		if (--io->refcount == 0)

src/lib/ioloop-epoll.c
@@ -218,8 +218,11 @@ void io_loop_handler_run_internal(struct ioloop *ioloop)
 			else if ((io->io.condition & IO_ERROR) != 0)
 				call = (event->events & IO_EPOLL_ERROR) != 0;
 
-			if (call)
+			if (call) {
 				io_loop_call_io(&io->io);
+				if (!ioloop->running)
+					return;
+			}
 		}
 	}
 }

src/lib/ioloop-poll.c
@@ -209,8 +209,11 @@ void io_loop_handler_run_internal(struct ioloop *ioloop)
 			if (pollfd->revents == 0)
 				ret--;
 
-			if (call)
+			if (call) {
 				io_loop_call_io(&io->io);
+				if (!ioloop->running)
+					return;
+			}
 		}
 	}
 }

src/lib/ioloop-select.c
@@ -139,6 +139,8 @@ void io_loop_handler_run_internal(struct ioloop *ioloop)
 		} else if (io_check_condition(ctx, io->fd, io->io.condition)) {
 			ret--;
 			io_loop_call_io(&io->io);
+			if (!ioloop->running)
+				break;
 		}
 	}
 }

src/lib/ioloop.c
@@ -618,7 +618,8 @@ static void io_loop_handle_timeouts_real(struct ioloop *ioloop)
 	ioloop_time = ioloop_timeval.tv_sec;
 	tv_call = ioloop_timeval;
 
-	while ((item = priorityq_peek(ioloop->timeouts)) != NULL) {
+	while (ioloop->running &&
+	       (item = priorityq_peek(ioloop->timeouts)) != NULL) {
 		struct timeout *timeout = (struct timeout *)item;
 
 		/* use tv_call to make sure we don't get to infinite loop in

commit dfcdac4583afc87b8cd03a9ad8124f1fb679a2ea

lib: ioloop-kqueue - Fix memory leak when ioloop stops before all IO callbacks are called
Broken by 720a6e0e67c3f04dcfee4c80b34f0291c7217cfa

src/lib/ioloop-kqueue.c
@@ -163,9 +163,7 @@ void io_loop_handler_run_internal(struct ioloop *ioloop)
 	/* execute timeout handlers */
 	io_loop_handle_timeouts(ioloop);
 
-	if (!ioloop->running)
-		return;
-
+	bool call_callbacks = ioloop->running;
 	for (i = 0; i < ret; i++) {
 		/* io_loop_handle_add() may cause events array reallocation,
 		   so we have use array_idx() */
@@ -173,10 +171,14 @@ void io_loop_handler_run_internal(struct ioloop *ioloop)
 		io = (void *)event->udata;
 
 		/* callback is NULL if io_remove() was already called */
-		if (io->io.callback != NULL) {
+		if (io->io.callback != NULL && call_callbacks) {
 			io_loop_call_io(&io->io);
-			if (!ioloop->running)
-				break;
+			if (!ioloop->running) {
+				/* Don't call any further callbacks, but the
+				   rest of the IOs still need to be
+				   unreferenced. */
+				call_callbacks = FALSE;
+			}
 		}
 
 		i_assert(io->refcount > 0);