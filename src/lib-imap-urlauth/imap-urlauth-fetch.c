dovecot/core

commit bf8165cea2450c854da39710715983e5ed44b480

lib-imap-urlauth: imap-urlauth-fetch - Fix potential NULL dereference
Found by Coverity.

src/lib-imap-urlauth/imap-urlauth-fetch.c
@@ -310,7 +310,13 @@ imap_urlauth_fetch_request_callback(struct imap_urlauth_fetch_reply *reply,
 	imap_urlauth_fetch_ref(ufetch);
 
 	if (!ufetch->failed) {
+		struct imap_urlauth_fetch_reply error_reply;
 		bool last = ufetch->pending_requests == 0 || reply == NULL;
+
+		if (reply == NULL) {
+			i_zero(&error_reply);
+			reply = &error_reply;
+		}
 		ret = ufetch->callback(reply, last, ufetch->context);
 	}
 

commit b9cfdad20f09d5de75f5db2027601a5e3ab724a7

lib-imap-urlauth: imap-urlauth-fetch - Fix comparing out of scope pointer
Practically this wouldn't matter, but Coverity still complains.

Broken by bf8165cea2450c854da39710715983e5ed44b480

src/lib-imap-urlauth/imap-urlauth-fetch.c
@@ -284,6 +284,7 @@ imap_urlauth_fetch_request_callback(struct imap_urlauth_fetch_reply *reply,
 {
 	struct imap_urlauth_fetch *ufetch =
 		(struct imap_urlauth_fetch *)context;
+	struct imap_urlauth_fetch_reply error_reply;
 	int ret = 1;
 
 	if (ufetch->waiting_local && reply != NULL) {
@@ -310,7 +311,6 @@ imap_urlauth_fetch_request_callback(struct imap_urlauth_fetch_reply *reply,
 	imap_urlauth_fetch_ref(ufetch);
 
 	if (!ufetch->failed) {
-		struct imap_urlauth_fetch_reply error_reply;
 		bool last = ufetch->pending_requests == 0 || reply == NULL;
 
 		if (reply == NULL) {