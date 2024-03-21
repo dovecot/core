ec62f222344077e7bfed5cc2bb7743e0f5563f15
Timo Sirainen <timo.sirainen@open-xchange.com>
2024-03-22 00:03:53 +0200

    dict: Rename dict_init_cache_get() to dict_init_cache_get_legacy()

--- src/dict/dict-init-cache.h
+++ src/dict/dict-init-cache.h
@@ -1,9 +1,9 @@
 #ifndef DICT_INIT_CACHE_H
 #define DICT_INIT_CACHE_H
 
-int dict_init_cache_get(const char *dict_name, const char *uri,
-			const struct dict_legacy_settings *set,
-			struct dict **dict_r, const char **error_r);
+int dict_init_cache_get_legacy(const char *dict_name, const char *uri,
+			       const struct dict_legacy_settings *set,
+			       struct dict **dict_r, const char **error_r);
 void dict_init_cache_unref(struct dict **dict);
 
 void dict_init_cache_wait_all(void);
