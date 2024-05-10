dovecot/core

commit db8a542f3d95c7e03ad160fd8f73d4b01994c906

lib-imap-client: Fix automatic reconnecting with delayed logins
reconnect_command_count was always set to 1, because there was always
the command that triggered the connection. It was never reset to 0,
which caused reconnects to never happen.

src/lib-imap-client/imapc-connection.c
@@ -1883,11 +1883,14 @@ void imapc_connection_connect(struct imapc_connection *conn)
 		return;
 	}
 
-	conn->reconnecting = FALSE;
-	/* if we get disconnected before we've finished all the pending
-	   commands, don't reconnect */
-	conn->reconnect_command_count = array_count(&conn->cmd_wait_list) +
-		array_count(&conn->cmd_send_queue);
+	if (conn->reconnecting) {
+		conn->reconnecting = FALSE;
+		/* if we get disconnected before we've finished all the pending
+		   commands, don't reconnect */
+		conn->reconnect_command_count =
+			array_count(&conn->cmd_wait_list) +
+			array_count(&conn->cmd_send_queue);
+	}
 
 	imapc_connection_input_reset(conn);
 	conn->last_connect = ioloop_timeval;

commit 17db6fa52b9e62399f7de3fe53bf991ebf3f978e

lib-imap-client: imapc_command_abort() - Remove command from connection's arrays before freeing it
Broken by db8a542f3d9

src/lib-imap-client/imapc-connection.c
@@ -2011,11 +2011,34 @@ const char *imapc_command_get_tag(struct imapc_command *cmd)
 	return t_strdup_printf("%u", cmd->tag);
 }
 
+static bool imapc_cmd_remove(ARRAY_TYPE(imapc_command) *entries,
+			     struct imapc_command *cmd)
+{
+	if (array_is_empty(entries))
+		return FALSE;
+
+	unsigned int count;
+	struct imapc_command *const *items = array_get(entries, &count);
+	for (unsigned int ndx = 0; ndx < count; ndx++) {
+		if (items[ndx] == cmd) {
+			array_delete(entries, ndx, 1);
+			return TRUE;
+		}
+	}
+	return FALSE;
+}
+
 void imapc_command_abort(struct imapc_command **_cmd)
 {
 	struct imapc_command *cmd = *_cmd;
 
+	if (cmd == NULL)
+		return;
 	*_cmd = NULL;
+
+	if (!imapc_cmd_remove(&cmd->conn->cmd_send_queue, cmd))
+		(void)imapc_cmd_remove(&cmd->conn->cmd_wait_list, cmd);
+
 	imapc_command_free(cmd);
 }
 