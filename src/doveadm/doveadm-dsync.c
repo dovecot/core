dovecot/core

commit a76faea3eb26c4cd67886fbe02c604f74d54be8c

dsync: Try to commit transactions every dsync_commit_msgs_interval messages
This was first attempted to be implemented by
ec0cc8fa647794e44a1afaa448f495a713048dc4, but it was later partially
reverted by 5973d496b16721af6d2c1fa90b016aacddf13554. This current
commit should fix its problems.

src/doveadm/doveadm-dsync.c
@@ -93,6 +93,7 @@ struct dsync_cmd_context {
 	const char *error;
 
 	unsigned int lock_timeout;
+	unsigned int import_commit_msgs_interval;
 
 	bool lock:1;
 	bool purge_remote:1;
@@ -592,6 +593,7 @@ cmd_dsync_run(struct doveadm_mail_cmd_context *_ctx, struct mail_user *user)
 	set.virtual_all_box = ctx->virtual_all_box;
 	memcpy(set.sync_box_guid, ctx->mailbox_guid, sizeof(set.sync_box_guid));
 	set.lock_timeout_secs = ctx->lock_timeout;
+	set.import_commit_msgs_interval = ctx->import_commit_msgs_interval;
 	set.state = ctx->state_input;
 	set.mailbox_alt_char = doveadm_settings->dsync_alt_char[0];
 
@@ -1111,6 +1113,7 @@ static struct doveadm_mail_cmd_context *cmd_dsync_alloc(void)
 	p_array_init(&ctx->namespace_prefixes, ctx->ctx.pool, 4);
         if ((doveadm_settings->parsed_features & DSYNC_FEATURE_EMPTY_HDR_WORKAROUND) != 0)
                 ctx->empty_hdr_workaround = TRUE;
+	ctx->import_commit_msgs_interval = doveadm_settings->dsync_commit_msgs_interval;
 	return &ctx->ctx;
 }
 

src/doveadm/dsync/dsync-brain.c
@@ -221,6 +221,7 @@ dsync_brain_master_init(struct mail_user *user, struct dsync_ibc *ibc,
 	memcpy(brain->sync_box_guid, set->sync_box_guid,
 	       sizeof(brain->sync_box_guid));
 	brain->lock_timeout = set->lock_timeout_secs;
+	brain->import_commit_msgs_interval = set->import_commit_msgs_interval;
 	brain->master_brain = TRUE;
 	dsync_brain_set_flags(brain, flags);
 
@@ -260,6 +261,7 @@ dsync_brain_master_init(struct mail_user *user, struct dsync_ibc *ibc,
 	ibc_set.sync_type = sync_type;
 	ibc_set.hdr_hash_v2 = TRUE;
 	ibc_set.lock_timeout = set->lock_timeout_secs;
+	ibc_set.import_commit_msgs_interval = set->import_commit_msgs_interval;
 	/* reverse the backup direction for the slave */
 	ibc_set.brain_flags = flags & ~(DSYNC_BRAIN_FLAG_BACKUP_SEND |
 					DSYNC_BRAIN_FLAG_BACKUP_RECV);

src/doveadm/dsync/dsync-brain.h
@@ -79,6 +79,9 @@ struct dsync_brain_settings {
 
 	/* If non-zero, use dsync lock file for this user */
 	unsigned int lock_timeout_secs;
+	/* If non-zero, importing will attempt to commit transaction after
+	   saving this many messages. */
+	unsigned int import_commit_msgs_interval;
 	/* Input state for DSYNC_BRAIN_SYNC_TYPE_STATE */
 	const char *state;
 };

src/doveadm/dsync/dsync-ibc-stream.c
@@ -78,7 +78,7 @@ static const struct {
 	  	"send_mail_requests backup_send backup_recv lock_timeout "
 	  	"no_mail_sync no_mailbox_renames no_backup_overwrite purge_remote "
 		"no_notify sync_since_timestamp sync_max_size sync_flags sync_until_timestamp"
-	  	"virtual_all_box empty_hdr_workaround"
+	  	"virtual_all_box empty_hdr_workaround import_commit_msgs_interval"
 	},
 	{ .name = "mailbox_state",
 	  .chr = 'S',
@@ -704,6 +704,10 @@ dsync_ibc_stream_send_handshake(struct dsync_ibc *_ibc,
 		dsync_serializer_encode_add(encoder, "lock_timeout",
 			t_strdup_printf("%u", set->lock_timeout));
 	}
+	if (set->import_commit_msgs_interval > 0) {
+		dsync_serializer_encode_add(encoder, "import_commit_msgs_interval",
+			t_strdup_printf("%u", set->import_commit_msgs_interval));
+	}
 	if (set->sync_since_timestamp > 0) {
 		dsync_serializer_encode_add(encoder, "sync_since_timestamp",
 			t_strdup_printf("%ld", (long)set->sync_since_timestamp));
@@ -817,6 +821,14 @@ dsync_ibc_stream_recv_handshake(struct dsync_ibc *_ibc,
 		    set->lock_timeout == 0) {
 			dsync_ibc_input_error(ibc, decoder,
 				"Invalid lock_timeout: %s", value);
+			return DSYNC_IBC_RECV_RET_TRYAGAIN;
+		}
+	}
+	if (dsync_deserializer_decode_try(decoder, "import_commit_msgs_interval", &value)) {
+		if (str_to_uint(value, &set->import_commit_msgs_interval) < 0 ||
+		    set->import_commit_msgs_interval == 0) {
+			dsync_ibc_input_error(ibc, decoder,
+				"Invalid import_commit_msgs_interval: %s", value);
 			return DSYNC_IBC_RECV_RET_TRYAGAIN;
 		}
 	}

src/doveadm/dsync/dsync-ibc.h
@@ -69,6 +69,7 @@ struct dsync_ibc_settings {
 	enum dsync_brain_flags brain_flags;
 	bool hdr_hash_v2;
 	unsigned int lock_timeout;
+	unsigned int import_commit_msgs_interval;
 };
 
 void dsync_ibc_init_pipe(struct dsync_ibc **ibc1_r,

src/doveadm/doveadm-settings.c
@@ -72,6 +72,7 @@ static const struct setting_define doveadm_setting_defines[] = {
 	DEF(SET_STR, director_username_hash),
 	DEF(SET_STR, doveadm_api_key),
 	DEF(SET_STR, dsync_features),
+	DEF(SET_UINT, dsync_commit_msgs_interval),
 	DEF(SET_STR, doveadm_http_rawlog_dir),
 
 	{ SET_STRLIST, "plugin", offsetof(struct doveadm_settings, plugin_envs), NULL },
@@ -95,6 +96,7 @@ const struct doveadm_settings doveadm_default_settings = {
 	.dsync_alt_char = "_",
 	.dsync_remote_cmd = "ssh -l%{login} %{host} doveadm dsync-server -u%u -U",
 	.dsync_features = "",
+	.dsync_commit_msgs_interval = 100,
 	.ssl_client_ca_dir = "",
 	.ssl_client_ca_file = "",
 	.director_username_hash = "%Lu",

src/doveadm/doveadm-settings.h
@@ -29,6 +29,7 @@ struct doveadm_settings {
 	const char *director_username_hash;
 	const char *doveadm_api_key;
 	const char *dsync_features;
+	unsigned int dsync_commit_msgs_interval;
 	const char *doveadm_http_rawlog_dir;
 	enum dsync_features parsed_features;
 	ARRAY(const char *) plugin_envs;

src/doveadm/dsync/dsync-brain-mailbox.c
@@ -236,6 +236,7 @@ dsync_brain_sync_mailbox_init_remote(struct dsync_brain *brain,
 					  brain->sync_until_timestamp,
 					  brain->sync_max_size,
 					  brain->sync_flag,
+					  brain->import_commit_msgs_interval,
 					  import_flags);
 }
 

commit 3001c1410e72440e48c285890dd31625b7e12555

dsync: Don't commit transaction after every mail on slave side
dsync_commit_msgs_interval setting worked only on the master side. On the
slave side it was always 0, which caused it to commit after every mail save.

Also, dsync_commit_msgs_interval should be handled independently on each
side. Master shouldn't send its dsync_commit_msgs_interval setting to the
slave side.

Broken by a76faea3eb26c4cd67886fbe02c604f74d54be8c

src/doveadm/doveadm-dsync.c
@@ -508,7 +508,8 @@ cmd_dsync_run_local(struct dsync_cmd_context *ctx, struct mail_user *user,
 	}
 
 	brain2 = dsync_brain_slave_init(user2, ibc2, TRUE, "",
-					doveadm_settings->dsync_alt_char[0]);
+					doveadm_settings->dsync_alt_char[0],
+					doveadm_settings->dsync_commit_msgs_interval);
 	mail_user_unref(&user2);
 
 	brain1_running = brain2_running = TRUE;
@@ -1280,7 +1281,8 @@ cmd_dsync_server_run(struct doveadm_mail_cmd_context *_ctx,
 
 	ibc = cmd_dsync_ibc_stream_init(ctx, name, str_c(temp_prefix));
 	brain = dsync_brain_slave_init(user, ibc, FALSE, process_title_prefix,
-				       doveadm_settings->dsync_alt_char[0]);
+				       doveadm_settings->dsync_alt_char[0],
+				       doveadm_settings->dsync_commit_msgs_interval);
 
 	io_loop_run(current_ioloop);
 	/* io_loop_run() deactivates the context - put it back */

src/doveadm/dsync/dsync-brain.c
@@ -280,7 +280,6 @@ dsync_brain_master_init(struct mail_user *user, struct dsync_ibc *ibc,
 	ibc_set.sync_type = sync_type;
 	ibc_set.hdr_hash_v2 = TRUE;
 	ibc_set.lock_timeout = set->lock_timeout_secs;
-	ibc_set.import_commit_msgs_interval = set->import_commit_msgs_interval;
 	ibc_set.hashed_headers = set->hashed_headers;
 	/* reverse the backup direction for the slave */
 	ibc_set.brain_flags = flags & ENUM_NEGATE(DSYNC_BRAIN_FLAG_BACKUP_SEND |
@@ -302,7 +301,8 @@ dsync_brain_master_init(struct mail_user *user, struct dsync_ibc *ibc,
 struct dsync_brain *
 dsync_brain_slave_init(struct mail_user *user, struct dsync_ibc *ibc,
 		       bool local, const char *process_title_prefix,
-		       char default_alt_char)
+		       char default_alt_char,
+		       unsigned int import_commit_msgs_interval)
 {
 	struct dsync_ibc_settings ibc_set;
 	struct dsync_brain *brain;
@@ -314,6 +314,7 @@ dsync_brain_slave_init(struct mail_user *user, struct dsync_ibc *ibc,
 	brain->process_title_prefix =
 		p_strdup(brain->pool, process_title_prefix);
 	brain->state = DSYNC_STATE_SLAVE_RECV_HANDSHAKE;
+	brain->import_commit_msgs_interval = import_commit_msgs_interval;
 
 	if (local) {
 		/* both master and slave are running within the same process,

src/doveadm/dsync/dsync-brain.h
@@ -110,7 +110,8 @@ dsync_brain_master_init(struct mail_user *user, struct dsync_ibc *ibc,
 struct dsync_brain *
 dsync_brain_slave_init(struct mail_user *user, struct dsync_ibc *ibc,
 		       bool local, const char *process_title_prefix,
-		       char default_alt_char);
+		       char default_alt_char,
+		       unsigned int import_commit_msgs_interval);
 /* Returns 0 if everything was successful, -1 if syncing failed in some way */
 int dsync_brain_deinit(struct dsync_brain **brain, enum mail_error *error_r);
 

src/doveadm/dsync/dsync-ibc-stream.c
@@ -78,7 +78,7 @@ static const struct {
 	  	"send_mail_requests backup_send backup_recv lock_timeout "
 	  	"no_mail_sync no_backup_overwrite purge_remote "
 		"no_notify sync_since_timestamp sync_max_size sync_flags sync_until_timestamp "
-		"virtual_all_box empty_hdr_workaround import_commit_msgs_interval "
+		"virtual_all_box empty_hdr_workaround "
 		"hashed_headers alt_char"
 	},
 	{ .name = "mailbox_state",
@@ -708,10 +708,6 @@ dsync_ibc_stream_send_handshake(struct dsync_ibc *_ibc,
 		dsync_serializer_encode_add(encoder, "lock_timeout",
 			t_strdup_printf("%u", set->lock_timeout));
 	}
-	if (set->import_commit_msgs_interval > 0) {
-		dsync_serializer_encode_add(encoder, "import_commit_msgs_interval",
-			t_strdup_printf("%u", set->import_commit_msgs_interval));
-	}
 	if (set->sync_since_timestamp > 0) {
 		dsync_serializer_encode_add(encoder, "sync_since_timestamp",
 			t_strdup_printf("%ld", (long)set->sync_since_timestamp));
@@ -835,14 +831,6 @@ dsync_ibc_stream_recv_handshake(struct dsync_ibc *_ibc,
 		    set->lock_timeout == 0) {
 			dsync_ibc_input_error(ibc, decoder,
 				"Invalid lock_timeout: %s", value);
-			return DSYNC_IBC_RECV_RET_TRYAGAIN;
-		}
-	}
-	if (dsync_deserializer_decode_try(decoder, "import_commit_msgs_interval", &value)) {
-		if (str_to_uint(value, &set->import_commit_msgs_interval) < 0 ||
-		    set->import_commit_msgs_interval == 0) {
-			dsync_ibc_input_error(ibc, decoder,
-				"Invalid import_commit_msgs_interval: %s", value);
 			return DSYNC_IBC_RECV_RET_TRYAGAIN;
 		}
 	}

src/doveadm/dsync/dsync-ibc.h
@@ -73,7 +73,6 @@ struct dsync_ibc_settings {
 	bool hdr_hash_v2;
 	bool hdr_hash_v3;
 	unsigned int lock_timeout;
-	unsigned int import_commit_msgs_interval;
 };
 
 void dsync_ibc_init_pipe(struct dsync_ibc **ibc1_r,