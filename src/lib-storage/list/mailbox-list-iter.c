dovecot/core

commit 48df71ee56482db1d2631f6b34fabbb434563545

lib-storage: Convert mailboxes to named array filter

src/lib-storage/list/mailbox-list-iter.c
@@ -3,8 +3,9 @@
 #include "lib.h"
 #include "array.h"
 #include "hash.h"
+#include "settings.h"
 #include "imap-match.h"
-#include "mail-storage.h"
+#include "mail-storage-private.h"
 #include "mailbox-tree.h"
 #include "mailbox-list-subscriptions.h"
 #include "mailbox-list-private.h"
@@ -79,9 +80,9 @@ int mailbox_list_iter_subscriptions_refresh(struct mailbox_list *list)
 	return ns->list->v.subscriptions_refresh(ns->list, list);
 }
 
-static struct mailbox_settings *
+static const struct mailbox_settings *
 mailbox_settings_add_ns_prefix(pool_t pool, struct mail_namespace *ns,
-			       struct mailbox_settings *in_set)
+			       const struct mailbox_settings *in_set)
 {
 	struct mailbox_settings *out_set;
 
@@ -100,20 +101,22 @@ mailbox_settings_add_ns_prefix(pool_t pool, struct mail_namespace *ns,
 	return out_set;
 }
 
-static void
+static int
 mailbox_list_iter_init_autocreate(struct mailbox_list_iterate_context *ctx)
 {
 	struct mail_namespace *ns = ctx->list->ns;
 	struct mailbox_list_autocreate_iterate_context *actx;
-	struct mailbox_settings *const *box_sets, *set;
+	const struct mailbox_settings *box_set, *set;
+	const char *error, *const *box_names;
 	struct autocreate_box *autobox;
 	unsigned int i, count;
 
 	if (!array_is_created(&ns->set->mailboxes))
-		return;
-	box_sets = array_get(&ns->set->mailboxes, &count);
+		return 0;
+
+	box_names = array_get(&ns->set->mailboxes, &count);
 	if (count == 0)
-		return;
+		return 0;
 
 	actx = p_new(ctx->pool, struct mailbox_list_autocreate_iterate_context, 1);
 	ctx->autocreate_ctx = actx;
@@ -125,14 +128,24 @@ mailbox_list_iter_init_autocreate(struct mailbox_list_iterate_context *ctx)
 	p_array_init(&actx->box_sets, ctx->pool, 16);
 	p_array_init(&actx->all_ns_box_sets, ctx->pool, 16);
 	for (i = 0; i < count; i++) {
-		if (strcmp(box_sets[i]->autocreate, MAILBOX_SET_AUTO_NO) == 0)
+		if (settings_get_filter(ns->list->event,
+					SETTINGS_EVENT_MAILBOX_NAME_WITHOUT_PREFIX, box_names[i],
+					&mailbox_setting_parser_info, 0,
+					&box_set, &error) < 0) {
+			mailbox_list_set_critical(ctx->list, "%s", error);
+			return -1;
+		}
+		if (strcmp(box_set->autocreate, MAILBOX_SET_AUTO_NO) == 0) {
+			settings_free(box_set);
 			continue;
+		}
 
-		set = mailbox_settings_add_ns_prefix(ctx->pool,
-						     ns, box_sets[i]);
+		set = mailbox_settings_add_ns_prefix(ctx->pool, ns, box_set);
 
 		/* autocreate mailbox belongs to listed namespace */
 		array_push_back(&actx->all_ns_box_sets, &set);
+		pool_add_external_ref(ctx->pool, box_set->pool);
+		settings_free(box_set);
 		if ((ctx->flags & MAILBOX_LIST_ITER_SELECT_SUBSCRIBED) == 0 ||
 		    strcmp(set->autocreate, MAILBOX_SET_AUTO_SUBSCRIBE) == 0) {
 			array_push_back(&actx->box_sets, &set);
@@ -146,6 +159,7 @@ mailbox_list_iter_init_autocreate(struct mailbox_list_iterate_context *ctx)
 			}
 		}
 	}
+	return 0;
 }
 
 struct mailbox_list_iterate_context *
@@ -164,8 +178,12 @@ mailbox_list_iter_init_multiple(struct mailbox_list *list,
 	}
 
 	ctx = list->v.iter_init(list, patterns, flags);
-	if ((flags & MAILBOX_LIST_ITER_NO_AUTO_BOXES) == 0)
-		mailbox_list_iter_init_autocreate(ctx);
+	if ((flags & MAILBOX_LIST_ITER_NO_AUTO_BOXES) == 0) {
+		if (mailbox_list_iter_init_autocreate(ctx) < 0) {
+			list->v.iter_deinit(ctx);
+			return &mailbox_list_iter_failed;
+		}
+	}
 	return ctx;
 }
 
@@ -807,7 +825,7 @@ autocreate_box_match(const ARRAY_TYPE(mailbox_settings) *boxes,
 		     struct mail_namespace *ns, const char *name,
 		     bool only_subscribed, unsigned int *idx_r)
 {
-	struct mailbox_settings *const *sets;
+	const struct mailbox_settings *const *sets;
 	unsigned int i, count;
 	size_t len, name_len = strlen(name);
 	enum autocreate_match_result result = 0;
@@ -1006,6 +1024,7 @@ mailbox_list_iter_next_call(struct mailbox_list_iterate_context *ctx)
 {
 	const struct mailbox_info *info;
 	const struct mailbox_settings *set;
+	int ret;
 
 	info = ctx->list->v.iter_next(ctx);
 	if (info == NULL)
@@ -1013,14 +1032,26 @@ mailbox_list_iter_next_call(struct mailbox_list_iterate_context *ctx)
 
 	ctx->list->ns->flags |= NAMESPACE_FLAG_USABLE;
 	if ((ctx->flags & MAILBOX_LIST_ITER_RETURN_SPECIALUSE) != 0) {
-		set = mailbox_settings_find(ctx->list->ns, info->vname);
-		if (set != NULL && *set->special_use != '\0') {
+		/* NOTE: ctx->list is fake - don't use it directly */
+		const char *error;
+		struct event *event = mail_storage_mailbox_create_event(
+			info->ns->list->event, info->ns->list, info->vname);
+		ret = settings_get(event, &mailbox_setting_parser_info, 0,
+				   &set, &error);
+		event_unref(&event);
+		if (ret < 0) {
+			mailbox_list_set_critical(info->ns->list, "%s", error);
+			ctx->failed = TRUE;
+			return NULL;
+		}
+		if (*set->special_use != '\0') {
 			ctx->specialuse_info = *info;
 			ctx->specialuse_info.special_use =
 				*set->special_use == '\0' ? NULL :
 				set->special_use;
 			info = &ctx->specialuse_info;
 		}
+		settings_free(set);
 	}
 
 	return mailbox_list_iter_autocreate_filter(ctx, info);

src/config/config-dump-full.c
@@ -101,6 +101,8 @@ config_dump_full_append_filter_query(string_t *str,
 		if (leaf)
 			str_append_c(str, '(');
 		const char *filter_key = t_strdup_until(filter->filter_name, p);
+		if (strcmp(filter_key, SETTINGS_EVENT_MAILBOX_NAME_WITH_PREFIX) == 0)
+			filter_key = SETTINGS_EVENT_MAILBOX_NAME_WITHOUT_PREFIX;
 		str_printfa(str, "%s=\"%s\"", filter_key, str_escape(p + 1));
 		if (leaf) {
 			/* the filter_name is used by settings_get_filter() for

src/config/config-parser.c
@@ -485,7 +485,16 @@ config_filter_add_new_filter(struct config_parser_context *ctx,
 			}
 			if (strcmp(key, "namespace") == 0 &&
 			    parent->filter_name_array &&
-			    str_begins_with(parent->filter_name, "namespace/")) {
+			    (str_begins_with(parent->filter_name, "namespace/") ||
+			     str_begins_with(parent->filter_name, "mailbox/"))) {
+				ctx->error = p_strdup_printf(ctx->pool,
+					"%s { .. } not allowed under %s { .. }",
+					key, t_strcut(parent->filter_name, '/'));
+				return FALSE;
+			}
+			if (strcmp(key, "mailbox") == 0 &&
+			    parent->filter_name_array &&
+			    str_begins_with(parent->filter_name, "mailbox/")) {
 				ctx->error = p_strdup_printf(ctx->pool,
 					"%s { .. } not allowed under %s { .. }",
 					key, t_strcut(parent->filter_name, '/'));

src/lib-settings/settings.c
@@ -682,8 +682,11 @@ settings_override_get_value(struct setting_parser_context *parser,
 		/* Try filter/name/key -> filter_key. Do this before the
 		   non-prefixed check, so e.g. inet_listener/imap/ssl won't
 		   try to change the global ssl setting. */
+		const char *key_prefix = set->last_filter_key;
+		if (strcmp(key_prefix, SETTINGS_EVENT_MAILBOX_NAME_WITHOUT_PREFIX) == 0)
+			key_prefix = SETTINGS_EVENT_MAILBOX_NAME_WITH_PREFIX;
 		const char *prefixed_key =
-			t_strdup_printf("%s_%s", set->last_filter_key, key);
+			t_strdup_printf("%s_%s", key_prefix, key);
 		old_value = settings_parse_get_value(parser, prefixed_key, &value_type);
 		if (old_value != NULL)
 			key = prefixed_key;
@@ -1064,19 +1067,14 @@ settings_override_get_filter(struct settings_override *set, pool_t pool,
 	size_t last_filter_key_pos = 0;
 	while ((value = strchr(key, '/')) != NULL &&
 	       (next = strchr(value + 1, '/')) != NULL) {
-		if (str_begins_with(key, "mailbox/")) {
-			/* FIXME: temporary kludge - removed when mailboxes
-			   are named array filters */
-			value = NULL;
-			break;
-		}
-
 		if (filter == NULL)
 			filter = t_str_new(64);
 		else
 			str_append(filter, " AND ");
 
 		last_filter_key = t_strdup_until(key, value);
+		if (strcmp(last_filter_key, SETTINGS_EVENT_MAILBOX_NAME_WITH_PREFIX) == 0)
+			last_filter_key = SETTINGS_EVENT_MAILBOX_NAME_WITHOUT_PREFIX;
 		last_filter_value = t_strdup_until(value + 1, next);
 		last_filter_key_pos = str_len(filter);
 		str_printfa(filter, "\"%s\"=\"%s\"",

src/lib-settings/settings.h
@@ -45,6 +45,13 @@ enum settings_get_flags {
    event_set_ptr(event, SETTINGS_EVENT_FILTER_NAME, "auth_policy"); */
 #define SETTINGS_EVENT_FILTER_NAME "settings_filter_name"
 
+/* The "mailbox" event field contains the full mailbox with namespace prefix.
+   However, for settings we need to use the mailbox name without the namespace
+   prefix. Internally convert the "mailbox" named filters to "mailbox_subname",
+   so the matching works for the event. */
+#define SETTINGS_EVENT_MAILBOX_NAME_WITH_PREFIX "mailbox"
+#define SETTINGS_EVENT_MAILBOX_NAME_WITHOUT_PREFIX "mailbox_subname"
+
 /* Set struct var_expand_table to be used for settings expansion. The table is
    expected to be accessible until the event is freed or the table is cleared
    from the event. Usage:

src/lib-storage/list/Makefile.am
@@ -2,6 +2,7 @@ noinst_LTLIBRARIES = libstorage_list.la
 
 AM_CPPFLAGS = \
 	-I$(top_srcdir)/src/lib \
+	-I$(top_srcdir)/src/lib-settings \
 	-I$(top_srcdir)/src/lib-fs \
 	-I$(top_srcdir)/src/lib-mail \
 	-I$(top_srcdir)/src/lib-index \

src/lib-storage/list/mailbox-list-iter-private.h
@@ -12,12 +12,16 @@ struct autocreate_box {
 	bool child_listed;
 };
 
-ARRAY_DEFINE_TYPE(mailbox_settings, struct mailbox_settings *);
+ARRAY_DEFINE_TYPE(mailbox_settings, const struct mailbox_settings *);
 struct mailbox_list_autocreate_iterate_context {
 	unsigned int idx;
 	struct mailbox_info new_info;
+	/* Autocreated mailboxes that match the filters */
 	ARRAY(struct autocreate_box) boxes;
+	/* References to settings in the boxes array. This is just for
+	   convenience. */
 	ARRAY_TYPE(mailbox_settings) box_sets;
+	/* References to all mailbox { ... } settings in the namespace. */
 	ARRAY_TYPE(mailbox_settings) all_ns_box_sets;
 	HASH_TABLE(char *, char *) duplicate_vnames;
 	bool listing_autoboxes:1;

src/lib-storage/mail-autoexpunge.c
@@ -2,6 +2,7 @@
 
 #include "lib.h"
 #include "ioloop.h"
+#include "settings.h"
 #include "mailbox-list-iter.h"
 #include "mail-storage-private.h"
 #include "mail-namespace.h"
@@ -214,7 +215,7 @@ mailbox_autoexpunge_wildcards(struct mail_namespace *ns,
 
 static void
 mailbox_autoexpunge_name(struct mail_namespace *ns,
-			 struct mailbox_settings *box_set,
+			 const struct mailbox_settings *box_set,
 			 unsigned int *expunged_count)
 {
 	const char *vname;
@@ -237,22 +238,32 @@ static bool
 mail_namespace_autoexpunge(struct mail_namespace *ns, struct file_lock **lock,
 			   unsigned int *expunged_count)
 {
-	struct mailbox_settings *box_set;
+	const struct mailbox_settings *box_set;
+	const char *box_name, *error;
 
 	if (!array_is_created(&ns->set->mailboxes))
 		return TRUE;
 
-	array_foreach_elem(&ns->set->mailboxes, box_set) {
+	array_foreach_elem(&ns->set->mailboxes, box_name) {
+		if (settings_get_filter(mailbox_list_get_event(ns->list),
+					SETTINGS_EVENT_MAILBOX_NAME_WITHOUT_PREFIX, box_name,
+					&mailbox_setting_parser_info, 0,
+					&box_set, &error) < 0) {
+			e_error(mailbox_list_get_event(ns->list), "%s", error);
+			break;
+		}
+
 		if (box_set->autoexpunge == 0 &&
-		    box_set->autoexpunge_max_mails == 0)
-			continue;
-
-		if (!mailbox_autoexpunge_lock(ns->user, lock))
+		    box_set->autoexpunge_max_mails == 0) {
+			/* no autoexpunging needed */
+		} else if (!mailbox_autoexpunge_lock(ns->user, lock)) {
+			/* another process is already autoexpunging */
+			settings_free(box_set);
 			return FALSE;
-
-		T_BEGIN {
+		} else T_BEGIN {
 			mailbox_autoexpunge_name(ns, box_set, expunged_count);
 		} T_END;
+		settings_free(box_set);
 	}
 	return TRUE;
 }

commit 75e3a2234da863721b0aa61850782b822b23d4e5

lib-storage: Fix settings leak when mailbox list iteration fails in settings lookup
Broken by 48df71ee56482db1d2631f6b34fabbb434563545

src/lib-storage/list/mailbox-list-iter.c
@@ -180,6 +180,7 @@ mailbox_list_iter_init_multiple(struct mailbox_list *list,
 	ctx = list->v.iter_init(list, patterns, flags);
 	if ((flags & MAILBOX_LIST_ITER_NO_AUTO_BOXES) == 0) {
 		if (mailbox_list_iter_init_autocreate(ctx) < 0) {
+			hash_table_destroy(&ctx->autocreate_ctx->duplicate_vnames);
 			list->v.iter_deinit(ctx);
 			return &mailbox_list_iter_failed;
 		}