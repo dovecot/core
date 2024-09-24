dovecot/core

commit 5ec4fc44e8d4e2160f07b1a7f4fce1ccfec3f6c1

lib: Automatically flush ostream when it's closed
Also verify that it unexpectedly didn't get fully flushed.

src/lib/ostream.c
@@ -45,6 +45,14 @@ const char *o_stream_get_error(struct ostream *stream)
 
 static void o_stream_close_full(struct ostream *stream, bool close_parents)
 {
+	/* Ideally o_stream_finish() would be called for all non-failed
+	   ostreams, but strictly requiring it would cause unnecessary
+	   complexity for many callers. Just require that at this point
+	   after flushing there isn't anything in the output buffer or that
+	   we're ignoring all errors. */
+	if (o_stream_flush(stream) == 0)
+		i_assert(stream->real_stream->error_handling_disabled);
+
 	if (!stream->closed && !stream->real_stream->closing) {
 		/* first mark the stream as being closed so the
 		   o_stream_copy_error_from_parent() won't recurse us back

src/lib/ostream.h
@@ -100,7 +100,13 @@ void o_stream_remove_destroy_callback(struct ostream *stream,
 				      void (*callback)());
 
 /* Mark the stream and all of its parent streams closed. Nothing will be
-   sent after this call. */
+   sent after this call. When using ostreams that require writing a trailer,
+   o_stream_finish() must be used before the stream is closed. When ostream
+   is destroyed, it's also closed but its parents aren't.
+
+   Closing the ostream (also via destroy) will first flush the ostream, and
+   afterwards requires one of: a) stream has failed, b) there is no more
+   buffered data, c) o_stream_set_no_error_handling() has been called. */
 void o_stream_close(struct ostream *stream);
 
 /* Set IO_WRITE callback. Default will just try to flush the output and

commit 4b1e2a465c9f637ffe68b69c0ba8d28e46354860

lib: o_stream_close() - Don't reset error checking on automatic flush
Otherwise it will be difficult to find missing o_stream_flush() calls.
The missing error handling would be found only when the flushing fails,
which would result in assert-crash.

This is how the code was originally intended to behave. Broken by
5ec4fc44e8d4e2160f07b1a7f4fce1ccfec3f6c1

src/lib/ostream.c
@@ -55,8 +55,16 @@ static void o_stream_close_full(struct ostream *stream, bool close_parents)
 	   complexity for many callers. Just require that at this point
 	   after flushing there isn't anything in the output buffer or that
 	   we're ignoring all errors. */
+	bool last_errors_not_checked =
+		stream->real_stream->last_errors_not_checked;
+
 	if (o_stream_flush(stream) == 0)
 		i_assert(stream->real_stream->error_handling_disabled);
+
+	/* We don't want this auto-flushing to remove the need for
+	   proper error checking. */
+	if (last_errors_not_checked)
+		stream->real_stream->last_errors_not_checked = TRUE;
 
 	if (!stream->closed && !stream->real_stream->closing) {
 		/* first mark the stream as being closed so the