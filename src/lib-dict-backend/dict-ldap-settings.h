29cb0f0b6a65e731337d4436738fdb2e57938abf
Marco Bettini <marco.bettini@open-xchange.com>
2024-10-25 16:26:19 +0000

    dict-ldap: dict_ldap_map_settings - Var expand values

--- src/lib-dict-backend/dict-ldap-settings.h
+++ src/lib-dict-backend/dict-ldap-settings.h
@@ -5,11 +5,12 @@ struct dict_ldap_map_settings {
 	pool_t pool;
 
 	const char *pattern;
-	ARRAY_TYPE(const_string) values;
 	const char *base;
 	const char *scope;
 
 	/* parsed */
+
+	ARRAY_TYPE(const_string) parsed_attributes;
 
 	/* attributes sorted by the position in parsed_pattern. */
 	ARRAY_TYPE(const_string) parsed_pattern_keys;
@@ -24,6 +25,18 @@ struct dict_ldap_map_pre_settings {
 	const char *filter;
 };
 
+struct dict_ldap_map_post_settings {
+	pool_t pool;
+	const char *value;
+
+	/* parsed */
+
+	/* This is preliminary support for supporting multiple values.
+	   For now the array contains only the single value coming
+	   from 'value' above. */
+	ARRAY_TYPE(const_string) values;
+};
+
 struct dict_ldap_settings {
 	pool_t pool;
 	ARRAY_TYPE(const_string) maps;
@@ -34,6 +47,7 @@ struct dict_ldap_settings {
 
 extern const struct setting_parser_info dict_ldap_map_setting_parser_info;
 extern const struct setting_parser_info dict_ldap_map_pre_setting_parser_info;
+extern const struct setting_parser_info dict_ldap_map_post_setting_parser_info;
 extern const struct setting_parser_info dict_ldap_setting_parser_info;
 
 int dict_ldap_settings_get(struct event *event,
