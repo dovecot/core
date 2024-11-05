d645e0ad42c7396696a5f1799ef20ff506024f72
Aki Tuomi <aki.tuomi@open-xchange.com>
2024-11-05 13:19:49 +0200

    lib: uri - Allow parameters directly with semicolon
    Fixes AAA validation.

--- src/lib/uri-util.c
+++ src/lib/uri-util.c
@@ -789,6 +789,10 @@ uri_do_parse_authority(struct uri_parser *parser, struct uri_authority *auth,
 	switch (*parser->cur) {
 	case ':': case '/': case '?': case '#':
 		break;
+	case ';':
+		if (parser->semicolon_params)
+			break;
+		/* fall-through */
 	default:
 		if (parser->parse_prefix)
 			break;
@@ -808,6 +812,10 @@ uri_do_parse_authority(struct uri_parser *parser, struct uri_authority *auth,
 		switch (*parser->cur) {
 		case '/': case '?': case '#':
 			break;
+		case ';':
+			if (parser->semicolon_params)
+				break;
+			/* fall-through */
 		default:
 			if (parser->parse_prefix)
 				break;
@@ -1009,7 +1017,9 @@ int uri_parse_query(struct uri_parser *parser, const char **query_r)
 	   query         = *( pchar / "/" / "?" )
 	   pchar         = unreserved / pct-encoded / sub-delims / ":" / "@"
 	 */
-	if (parser->cur >= parser->end || *parser->cur != '?')
+	if (parser->cur >= parser->end ||
+	    (!parser->semicolon_params && *parser->cur != '?') ||
+	    (parser->semicolon_params && *parser->cur != ';'))
 		return 0;
 	parser->cur++;
 
@@ -1135,11 +1145,16 @@ int uri_parse_absolute_generic(struct uri_parser *parser,
 	 */
 
 	/* scheme ":" */
-	if ((flags & URI_PARSE_SCHEME_EXTERNAL) == 0 &&
-	    (ret = uri_parse_scheme(parser, NULL)) <= 0) {
-		if (ret == 0)
-			parser->error = "Missing scheme";
-		return -1;
+	if ((flags & URI_PARSE_SCHEME_EXTERNAL) == 0) {
+		const char *scheme;
+		if ((ret = uri_parse_scheme(parser, &scheme)) <= 0) {
+			if (ret == 0)
+				parser->error = "Missing scheme";
+			return -1;
+		}
+		if (strcmp(scheme, "aaa") == 0 ||
+		    (flags & URI_PARSE_SEMICOLON_PARAMS) != 0)
+			parser->semicolon_params = TRUE;
 	}
 
 	/* "//" authority */
