dovecot/core

commit a9d9eb4297a2cccec0cf698a45efb4f1987bdc63

Add lib-var-expand

src/lib-var-expand/expansion-statement.c
@@ -1,0 +1,65 @@
+/* Copyright (c) 2024 Dovecot authors, see the included COPYING file */
+
+#include "lib.h"
+#include "var-expand-private.h"
+#include "expansion.h"
+
+bool var_expand_execute_stmt(struct var_expand_state *state,
+			     const struct var_expand_statement *stmt,
+			     bool first, const char **error_r)
+{
+	const char *error;
+	char *delayed_error = NULL;
+	var_expand_filter_func_t *fn;
+
+	/* We allow first function to be either variable or function,
+	   so that you can do simple lookups, like %{variable}.
+	   Also we prefer variables first, to avoid cumbersome things like
+	   having to write lookup('domain') every time you wanted domain.
+	*/
+	if (first) {
+		const char *value = NULL;
+		if (var_expand_state_lookup_variable(state, stmt->function,
+						     &value, &error) < 0) {
+			/* ignore this error now, but leave transfer unset. */
+			/* allows default to pick this up */
+			var_expand_state_unset_transfer(state);
+			i_free(delayed_error);
+			delayed_error = i_strdup(error);
+		} else {
+			i_assert(value != NULL);
+			var_expand_state_set_transfer(state, value);
+			return TRUE;
+		}
+	}
+
+	if (var_expand_find_filter(stmt->function, &fn) == 0) {
+		int ret;
+		T_BEGIN {
+			ret = (*fn)(stmt, state, &error);
+		} T_END_PASS_STR_IF(ret < 0, &error);
+		/* this is to allow e.g. defaut to work correctly */
+		if (ret < 0) {
+			var_expand_state_unset_transfer(state);
+			if (state->delayed_error != NULL) {
+				*error_r = t_strdup(state->delayed_error);
+				return FALSE;
+			}
+			i_free(delayed_error);
+			delayed_error =
+				i_strdup_printf("%s: %s", stmt->function, error);
+		} else {
+			i_free(delayed_error);
+		}
+		/* this was already handled in the first branch, so just ignore
+		   the error here */
+	} else if (!first) {
+		i_free(delayed_error);
+		*error_r = t_strdup_printf("No such function '%s'", stmt->function);
+		return FALSE;
+	}
+
+	i_free(state->delayed_error);
+	state->delayed_error = delayed_error;
+	return TRUE;
+}

.gitignore
@@ -132,3 +132,6 @@ src/lib-imap/fuzz-imap-utf7
 src/lib-imap/fuzz-imap-bodystructure
 src/lib-mail/fuzz-message-parser
 src/lib-smtp/fuzz-smtp-server
+src/lib-var-expand/var-expand-parser.c
+src/lib-var-expand/var-expand-parser.h
+src/lib-var-expand/var-expand-lexer.c

configure.ac
@@ -557,6 +557,7 @@ LIBDOVECOT_LA_LIBS='\
 	$(top_builddir)/src/lib-oauth2/liboauth2.la \
 	$(top_builddir)/src/lib-smtp/libsmtp.la \
 	$(top_builddir)/src/lib-program-client/libprogram_client.la \
+	$(top_builddir)/src/lib-var-expand/libvar_expand.la \
 	$(top_builddir)/src/lib-master/libmaster.la \
 	$(top_builddir)/src/lib-login/liblogin.la \
 	$(top_builddir)/src/lib-settings/libsettings.la \
@@ -844,6 +845,7 @@ src/lib-storage/index/dbox-multi/Makefile
 src/lib-storage/index/dbox-single/Makefile
 src/lib-storage/index/raw/Makefile
 src/lib-storage/index/shared/Makefile
+src/lib-var-expand/Makefile
 src/anvil/Makefile
 src/auth/Makefile
 src/config/Makefile

src/Makefile.am
@@ -11,6 +11,7 @@ endif
 LIBDOVECOT_SUBDIRS = \
 	lib-test \
 	lib \
+	lib-var-expand \
 	lib-settings \
 	lib-otp \
 	lib-auth \

src/lib-imap-client/imapc-settings.c
@@ -79,7 +79,7 @@ static const struct setting_keyvalue imapc_default_settings_keyvalue[] = {
 	/* We want to have all imapc mailboxes accessible, so escape them if
 	   necessary. */
 	{ "layout_imapc/mailbox_list_visible_escape_char", "~" },
-	{ "layout_imapc/mailbox_list_storage_escape_char", "%%" },
+	{ "layout_imapc/mailbox_list_storage_escape_char", "%" },
 	{ NULL, NULL }
 };
 

src/lib-var-expand/Makefile.am
@@ -1,0 +1,71 @@
+noinst_LTLIBRARIES = libvar_expand.la
+
+# Squelch autoconf error about using .[ly] sources but not defining $(LEX)
+# and $(YACC).  Using false here avoids accidental use.
+LEX=/bin/false
+YACC=/bin/false
+
+# We use custom rules here because we want to use flex and bison instead
+# of lex and yacc (or bison in yacc-compatibility mode).  Both flex and
+# bison can handle properly naming the generated files, and it is simpler
+# and cleaner to make this rule ourselves instead of working around ylwrap
+# and yywrap's antiquated notion of what is hapenning.
+.l.c:
+	$(AM_V_GEN)$(FLEX) -o $@ $<
+
+.y.c:
+	$(AM_V_GEN)$(BISON) -Wcounterexamples -o $@ $<
+
+AM_CPPFLAGS = \
+	-I$(top_srcdir)/src/lib \
+	-I$(top_srcdir)/src/lib-test \
+	-I$(top_srcdir)/src/lib-dcrypt \
+	-Wno-error=unused-function
+
+var-expand-parser.h: var-expand-parser.c
+
+libvar_expand_la_SOURCES = \
+	expansion-parameter.c \
+	expansion-statement.c \
+	expansion-filter.c \
+	expansion-filter-if.c \
+	expansion-program.c \
+	var-expand.c \
+	var-expand-parser.y \
+	var-expand-lexer.l
+
+BUILT_SOURCES = \
+	var-expand-parser.c \
+	var-expand-parser.h \
+	var-expand-lexer.c
+
+noinst_HEADERS = \
+	var-expand-parser-private.h \
+	var-expand-parser.h \
+	expansion.h
+
+headers = \
+	var-expand-new.h \
+	var-expand-private.h
+
+pkginc_libdir=$(pkgincludedir)
+pkginc_lib_HEADERS = $(headers)
+
+test_programs = \
+	test-var-expand
+
+noinst_PROGRAMS = $(test_programs)
+
+test_libs = \
+	libvar_expand.la \
+	../lib-test/libtest.la \
+	../lib/liblib.la \
+	$(MODULE_LIBS)
+
+test_var_expand_SOURCE = test-var-expand.c
+test_var_expand_LDADD = $(test_libs)
+
+check-local:
+	for bin in $(test_programs); do \
+	  if ! $(RUN_TEST) ./$$bin; then exit 1; fi; \
+	done

src/lib-var-expand/expansion-filter-if.c
@@ -1,0 +1,266 @@
+/* Copyright (c) 2024 Dovecot authors, see the included COPYING file */
+
+#include "lib.h"
+#include "var-expand-private.h"
+#include "expansion.h"
+#include "wildcard-match.h"
+
+#include <regex.h>
+
+enum var_expand_if_op {
+	OP_UNKNOWN,
+	OP_NUM_EQ,
+	OP_NUM_LT,
+	OP_NUM_LE,
+	OP_NUM_GT,
+	OP_NUM_GE,
+	OP_NUM_NE,
+/* put all numeric comparisons before this line */
+	OP_STR_EQ,
+	OP_STR_LT,
+	OP_STR_LE,
+	OP_STR_GT,
+	OP_STR_GE,
+	OP_STR_NE,
+	OP_STR_LIKE,
+	OP_STR_NOT_LIKE,
+	OP_STR_REGEXP,
+	OP_STR_NOT_REGEXP,
+/* keep this as last */
+	OP_COUNT
+};
+
+static enum var_expand_if_op var_expand_if_str_to_comp(const char *op)
+{
+	const char *ops[] = {
+		NULL,
+		"==",
+		"<",
+		"<=",
+		">",
+		">=",
+		"!=",
+		"eq",
+		"lt",
+		"le",
+		"gt",
+		"ge",
+		"ne",
+		"*",
+		"!*",
+		"~",
+		"!~",
+	};
+	static_assert_array_size(ops, OP_COUNT);
+	for (enum var_expand_if_op i = 1; i < OP_COUNT; i++) {
+		i_assert(ops[i] != NULL);
+		if (strcmp(op, ops[i]) == 0)
+			return i;
+	}
+	return OP_UNKNOWN;
+}
+
+static int
+fn_if_cmp(struct var_expand_state *state, const struct var_expand_parameter *p_lhs,
+	  enum var_expand_if_op op, const struct var_expand_parameter *p_rhs,
+	  bool *result_r, const char **error_r)
+{
+	bool neg = FALSE;
+	if (op < OP_STR_EQ) {
+		intmax_t a;
+		intmax_t b;
+		if (var_expand_parameter_number_or_var(state, p_lhs, &a, error_r) < 0) {
+			*error_r = t_strdup_printf("Left-hand side: %s", *error_r);
+			return -1;
+		} else if (var_expand_parameter_number_or_var(state, p_rhs, &b, error_r) < 0) {
+			*error_r = t_strdup_printf("Right-hand side: %s", *error_r);
+			return -1;
+		}
+		switch (op) {
+		case OP_NUM_EQ:
+			*result_r = a == b;
+			return 0;
+		case OP_NUM_LT:
+			*result_r = a < b;
+			return 0;
+		case OP_NUM_LE:
+			*result_r = a <= b;
+			return 0;
+		case OP_NUM_GT:
+			*result_r = a > b;
+			return 0;
+		case OP_NUM_GE:
+			*result_r = a >= b;
+			return 0;
+		case OP_NUM_NE:
+			*result_r = a != b;
+			return 0;
+		default:
+			i_panic("Missing numeric comparator %u", op);
+		}
+	}
+
+	const char *lhs, *rhs;
+	if (var_expand_parameter_string_or_var(state, p_lhs, &lhs, error_r) < 0) {
+		*error_r = t_strdup_printf("Left-hand side %s", *error_r);
+		return -1;
+	} else if (var_expand_parameter_string_or_var(state, p_rhs, &rhs, error_r) < 0) {
+		*error_r = t_strdup_printf("Right-hand side %s", *error_r);
+		return -1;
+	}
+
+	switch (op) {
+	case OP_STR_EQ:
+		*result_r = strcmp(lhs,rhs) == 0;
+		return 0;
+	case OP_STR_LT:
+		*result_r = strcmp(lhs,rhs) < 0;
+		return 0;
+	case OP_STR_LE:
+		*result_r = strcmp(lhs,rhs) <= 0;
+		return 0;
+	case OP_STR_GT:
+		*result_r = strcmp(lhs,rhs) > 0;
+		return 0;
+	case OP_STR_GE:
+		*result_r = strcmp(lhs,rhs) >= 0;
+		return 0;
+	case OP_STR_NE:
+		*result_r = strcmp(lhs,rhs) != 0;
+		return 0;
+	case OP_STR_LIKE:
+		*result_r = wildcard_match(lhs, rhs);
+		return 0;
+	case OP_STR_NOT_LIKE:
+		*result_r = !wildcard_match(lhs, rhs);
+		return 0;
+	case OP_STR_NOT_REGEXP:
+		neg = TRUE;
+		/* fall through */
+	case OP_STR_REGEXP: {
+		int ec;
+		bool res;
+		regex_t reg;
+		if ((ec = regcomp(&reg, rhs, REG_EXTENDED)) != 0) {
+			size_t size;
+			char *errbuf;
+			size = regerror(ec, &reg, NULL, 0);
+			errbuf = t_malloc_no0(size);
+			(void)regerror(ec, &reg, errbuf, size);
+			*error_r = t_strdup_printf("regexp() failed: %s",
+						   errbuf);
+			return -1;
+		}
+		if ((ec = regexec(&reg, lhs, 0, 0, 0)) != 0) {
+			i_assert(ec == REG_NOMATCH);
+			res = FALSE;
+		} else {
+			res = TRUE;
+		}
+		regfree(&reg);
+		/* this should be same as neg.
+		   if NOT_REGEXP, neg == TRUE and res should be FALSE
+		   if REGEXP, ned == FALSE, and res should be TRUE
+		 */
+		*result_r = res != neg;
+		return 0;
+	}
+	default:
+		i_panic("Missing generic comparator %u", op);
+	}
+}
+
+int expansion_filter_if(const struct var_expand_statement *stmt,
+			struct var_expand_state *state,
+			const char **error_r)
+{
+	const char *_op;
+	bool use_first_as_lhs = !state->transfer_set;
+
+	const struct var_expand_parameter *p_lhs = NULL;
+	const struct var_expand_parameter *p_rhs = NULL;
+	const struct var_expand_parameter *p_true = NULL;
+	const struct var_expand_parameter *p_false = NULL;
+
+	enum {
+		STATE_LHS = 0,
+		STATE_OP,
+		STATE_RHS,
+		STATE_TRUE,
+		STATE_FALSE,
+		STATE_DONE,
+	} parse_state;
+
+	if (use_first_as_lhs)
+		parse_state = STATE_LHS;
+	else
+		parse_state = STATE_OP;
+
+	struct var_expand_parameter_iter_context *ctx =
+		var_expand_parameter_iter_init(stmt);
+	while (var_expand_parameter_iter_more(ctx)) {
+		const struct var_expand_parameter *par =
+			var_expand_parameter_iter_next(ctx);
+		const char *key = var_expand_parameter_key(par);
+		if (key != NULL) {
+			ERROR_UNSUPPORTED_KEY(key);
+		}
+		switch (parse_state) {
+		case STATE_LHS:	p_lhs = par; break;
+		case STATE_OP:
+			if (var_expand_parameter_string_or_var(state, par, &_op, error_r) < 0) {
+				*error_r = t_strdup_printf("Comparator: %s", *error_r);
+				return -1;
+			}
+			break;
+		case STATE_RHS: p_rhs = par; break;
+		case STATE_TRUE: p_true = par; break;
+		case STATE_FALSE: p_false = par; break;
+		case STATE_DONE: ERROR_TOO_MANY_UNNAMED_PARAMETERS;
+		}
+		parse_state++;
+	}
+
+	if (parse_state != STATE_DONE) {
+		*error_r = "Missing parameters";
+		return -1;
+	}
+
+	enum var_expand_if_op op = var_expand_if_str_to_comp(_op);
+
+	if (op == OP_UNKNOWN) {
+		*error_r = t_strdup_printf("Unsupported comparator '%s'", _op);
+		return -1;
+	}
+
+	if (!use_first_as_lhs) {
+		if (var_expand_parameter_from_state(state, op < OP_STR_EQ,
+						    &p_lhs) < 0) {
+			if (op < OP_STR_EQ) {
+				*error_r = "Input is not a number";
+			} else  {
+				*error_r = "No value to use as left-hand in if";
+			}
+			return -1;
+		}
+	}
+
+	i_assert(p_lhs != NULL);
+
+	bool result;
+	if (fn_if_cmp(state, p_lhs, op, p_rhs, &result, error_r) < 0)
+		return -1;
+	const struct var_expand_parameter *res = result ? p_true : p_false;
+	const char *value;
+
+	if (var_expand_parameter_any_or_var(state, res, &value, error_r) < 0) {
+		*error_r = t_strdup_printf("%s: %s",
+					   result ? "True branch" : "False branch",
+					   *error_r);
+		return -1;
+	}
+
+	var_expand_state_set_transfer(state, value);
+
+	return 0;
+}

commit afa250713e3e77cf077c48ea4145dff73001ae04

lib-var-expand: Fix memory leak if filter fails
Broken in a9d9eb4297a2cccec0cf698a45efb4f1987bdc63

src/lib-var-expand/expansion-statement.c
@@ -38,6 +38,7 @@ bool var_expand_execute_stmt(struct var_expand_state *state,
 		T_BEGIN {
 			ret = (*fn)(stmt, state, &error);
 		} T_END_PASS_STR_IF(ret < 0, &error);
+		i_free(delayed_error);
 		/* this is to allow e.g. defaut to work correctly */
 		if (ret < 0) {
 			var_expand_state_unset_transfer(state);
@@ -45,11 +46,8 @@ bool var_expand_execute_stmt(struct var_expand_state *state,
 				*error_r = t_strdup(state->delayed_error);
 				return FALSE;
 			}
-			i_free(delayed_error);
 			delayed_error =
 				i_strdup_printf("%s: %s", stmt->function, error);
-		} else {
-			i_free(delayed_error);
 		}
 		/* this was already handled in the first branch, so just ignore
 		   the error here */