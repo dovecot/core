dovecot/core

commit 6634e45500e81cfa36932203ee69c81745efd3e6

fts: Fixed memory leak at deinit when using multiple fts plugins at the same time using libfts.

src/plugins/fts/fts-user.c
@@ -16,6 +16,7 @@
 
 struct fts_user {
 	union mail_user_module_context module_ctx;
+	int refcount;
 
 	struct fts_language_list *lang_list;
 	struct fts_tokenizer *index_tokenizer, *search_tokenizer;
@@ -335,9 +336,16 @@ static void fts_user_free(struct fts_user *fuser)
 
 int fts_mail_user_init(struct mail_user *user, const char **error_r)
 {
-	struct fts_user *fuser;
+	struct fts_user *fuser = FTS_USER_CONTEXT(user);
+
+	if (fuser != NULL) {
+		/* multiple fts plugins are loaded */
+		fuser->refcount++;
+		return 0;
+	}
 
 	fuser = p_new(user->pool, struct fts_user, 1);
+	fuser->refcount = 1;
 	p_array_init(&fuser->languages, user->pool, 4);
 
 	if (fts_user_init_languages(user, fuser, error_r) < 0) {
@@ -358,6 +366,9 @@ void fts_mail_user_deinit(struct mail_user *user)
 {
 	struct fts_user *fuser = FTS_USER_CONTEXT(user);
 
-	if (fuser != NULL)
-		fts_user_free(fuser);
+	if (fuser != NULL) {
+		i_assert(fuser->refcount > 0);
+		if (--fuser->refcount == 0)
+			fts_user_free(fuser);
+	}
 }

src/plugins/fts-lucene/fts-lucene-plugin.c
@@ -96,7 +96,8 @@ static void fts_lucene_mail_user_deinit(struct mail_user *user)
 {
 	struct fts_lucene_user *fuser = FTS_LUCENE_USER_CONTEXT(user);
 
-	fts_mail_user_deinit(user);
+	if (fuser->set.use_libfts)
+		fts_mail_user_deinit(user);
 	fuser->module_ctx.super.deinit(user);
 }
 

src/plugins/fts-solr/fts-solr-plugin.c
@@ -54,7 +54,8 @@ static void fts_solr_mail_user_deinit(struct mail_user *user)
 {
 	struct fts_solr_user *fuser = FTS_SOLR_USER_CONTEXT(user);
 
-	fts_mail_user_deinit(user);
+	if (fuser->set.use_libfts)
+		fts_mail_user_deinit(user);
 	fuser->module_ctx.super.deinit(user);
 }
 

commit 3f60f6e99ab5cbe6cb77d252962fafac2b5a1bd3

fts: Use FTS_USER_CONTEXT_REQUIRE in fts_mail_user_init()
User is required to be present.

Broken in 6634e45500e81cfa36932203ee69c81745efd3e6

src/plugins/fts/fts-user.c
@@ -41,7 +41,7 @@ size_t fts_mail_user_message_max_size(struct mail_user *user)
 int fts_mail_user_init(struct mail_user *user, struct event *event,
 		       bool initialize_libfts, const char **error_r)
 {
-	struct fts_user *fuser = FTS_USER_CONTEXT(user);
+	struct fts_user *fuser = FTS_USER_CONTEXT_REQUIRE(user);
 
 	if (fuser->set != NULL)
 		return 0;