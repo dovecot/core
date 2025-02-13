722a5e3011480fd1fa31a7b15f29d9f287d0e2a2
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-02-13 14:10:51 -0500

    spelling: and
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/lib-index/mail-index.h
+++ src/lib-index/mail-index.h
@@ -634,7 +634,7 @@ void mail_index_lookup_first(struct mail_index_view *view,
 void mail_index_append(struct mail_index_transaction *t, uint32_t uid,
 		       uint32_t *seq_r);
 /* Assign new UIDs for mails with uid=0 or uid<min_allowed_uid. All the new
-   UIDs are >= first_new_uid, an also higher than the highest seen uid (i.e. it
+   UIDs are >= first_new_uid, and also higher than the highest seen uid (i.e. it
    doesn't try to fill UID gaps). Assumes that mailbox is locked in a way that
    UIDs can be safely assigned. Returns UIDs for all assigned messages, in
    their sequence order (so UIDs are not necessary ascending). */
