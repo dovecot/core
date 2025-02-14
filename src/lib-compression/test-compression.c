140a442372ef5289aa2cba43dee42536ec17a746
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-02-13 22:37:56 -0500

    spelling: unknown
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/lib-compression/test-compression.c
+++ src/lib-compression/test-compression.c
@@ -803,7 +803,7 @@ static void test_gz(const char *str1, const char *str2, bool autodetect)
 	buffer_t *buf = t_buffer_create(512);
 
 	if (compression_lookup_handler("gz", &gz) <= 0 )
-		return; /* not compiled in or unkown*/
+		return; /* not compiled in or unknown*/
 
 	/* write concatenated output */
 	buf_output = o_stream_create_buffer(buf);
@@ -881,7 +881,7 @@ static void test_gz_header_int(bool autodetect)
 	};
 	struct istream *file_input, *input;
 	if (compression_lookup_handler("gz", &gz) <= 0 )
-		return; /* not compiled in or unkown*/
+		return; /* not compiled in or unknown*/
 
 	test_begin(t_strdup_printf(
 		"gz header (autodetect=%s)", autodetect ? "yes" : "no"));
@@ -917,7 +917,7 @@ static void test_gz_large_header_int(bool autodetect)
 	size_t i;
 
 	if (compression_lookup_handler("gz", &gz) <= 0 )
-		return; /* not compiled in or unkown*/
+		return; /* not compiled in or unknown*/
 
 	test_begin(t_strdup_printf(
 		"gz large header (autodetect=%s)", autodetect ? "yes" : "no"));
@@ -964,7 +964,7 @@ static void test_lz4_small_header(void)
 	struct istream *file_input, *input;
 
 	if (compression_lookup_handler("lz4", &lz4) <= 0)
-		return; /* not compiled in or unkown */
+		return; /* not compiled in or unknown */
 
 	test_begin("lz4 small header");
 
