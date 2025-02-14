397c2f2a014101126cb6f919f5e0540b2a113324
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-02-13 22:21:32 -0500

    spelling: particularly
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/imap/imap-client.h
+++ src/imap/imap-client.h
@@ -130,7 +130,7 @@ struct client_command_context {
 
 struct imap_client_vfuncs {
 	/* Perform client initialization. This is called when client creation is
-	   finished completely. Particulary, at this point the namespaces are
+	   finished completely. Particularly, at this point the namespaces are
 	   fully initialized, which is not the case for the client create hook.
 	 */
 	void (*init)(struct client *client);
