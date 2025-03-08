dovecot/core

commit 19316ac1ff80cb454f2d35614137292d9aa79e1e

lib-language: Rename m4 macros

m4/dovecot.m4
@@ -6,7 +6,7 @@ dnl This file is free software; the authors give
 dnl unlimited permission to copy and/or distribute it, with or without
 dnl modifications, as long as this notice is preserved.
 
-# serial 39
+# serial 40
 
 dnl
 dnl Check for support for D_FORTIFY_SOURCE=2
@@ -395,11 +395,11 @@ AC_DEFUN([DC_DOVECOT],[
 
 	AX_SUBST_L([DISTCHECK_CONFIGURE_FLAGS], [dovecotdir], [dovecot_moduledir], [dovecot_installed_moduledir], [dovecot_pkgincludedir], [dovecot_pkglibexecdir], [dovecot_pkglibdir], [dovecot_docdir], [dovecot_statedir])
 	AX_SUBST_L([DOVECOT_INSTALLED], [DOVECOT_CFLAGS], [DOVECOT_LIBS], [DOVECOT_SSL_LIBS], [DOVECOT_SQL_LIBS], [DOVECOT_COMPRESS_LIBS], [DOVECOT_BINARY_CFLAGS], [DOVECOT_BINARY_LDFLAGS])
-	AX_SUBST_L([LIBDOVECOT], [LIBDOVECOT_LOGIN], [LIBDOVECOT_SQL], [LIBDOVECOT_OPENSSL], [LIBDOVECOT_COMPRESS], [LIBDOVECOT_LDA], [LIBDOVECOT_STORAGE], [LIBDOVECOT_DSYNC], [LIBDOVECOT_LIBFTS])
-	AX_SUBST_L([LIBDOVECOT_DEPS], [LIBDOVECOT_LOGIN_DEPS], [LIBDOVECOT_SQL_DEPS], [LIBDOVECOT_OPENSSL_DEPS], [LIBDOVECOT_COMPRESS_DEPS], [LIBDOVECOT_LDA_DEPS], [LIBDOVECOT_STORAGE_DEPS], [LIBDOVECOT_DSYNC_DEPS], [LIBDOVECOT_LIBFTS_DEPS])
+	AX_SUBST_L([LIBDOVECOT], [LIBDOVECOT_LOGIN], [LIBDOVECOT_SQL], [LIBDOVECOT_OPENSSL], [LIBDOVECOT_COMPRESS], [LIBDOVECOT_LDA], [LIBDOVECOT_STORAGE], [LIBDOVECOT_DSYNC], [LIBDOVECOT_LIBLANG])
+	AX_SUBST_L([LIBDOVECOT_DEPS], [LIBDOVECOT_LOGIN_DEPS], [LIBDOVECOT_SQL_DEPS], [LIBDOVECOT_OPENSSL_DEPS], [LIBDOVECOT_COMPRESS_DEPS], [LIBDOVECOT_LDA_DEPS], [LIBDOVECOT_STORAGE_DEPS], [LIBDOVECOT_DSYNC_DEPS], [LIBDOVECOT_LIBLANG_DEPS])
 	AX_SUBST_L([LIBDOVECOT_INCLUDE], [LIBDOVECOT_LDA_INCLUDE], [LIBDOVECOT_AUTH_INCLUDE], [LIBDOVECOT_DOVEADM_INCLUDE], [LIBDOVECOT_SERVICE_INCLUDE], [LIBDOVECOT_STORAGE_INCLUDE], [LIBDOVECOT_LOGIN_INCLUDE], [LIBDOVECOT_SQL_INCLUDE])
-	AX_SUBST_L([LIBDOVECOT_IMAP_LOGIN_INCLUDE], [LIBDOVECOT_CONFIG_INCLUDE], [LIBDOVECOT_IMAP_INCLUDE], [LIBDOVECOT_POP3_INCLUDE], [LIBDOVECOT_SUBMISSION_INCLUDE], [LIBDOVECOT_LMTP_INCLUDE], [LIBDOVECOT_DSYNC_INCLUDE], [LIBDOVECOT_IMAPC_INCLUDE], [LIBDOVECOT_FTS_INCLUDE])
-	AX_SUBST_L([LIBDOVECOT_NOTIFY_INCLUDE], [LIBDOVECOT_PUSH_NOTIFICATION_INCLUDE], [LIBDOVECOT_ACL_INCLUDE], [LIBDOVECOT_LIBFTS_INCLUDE], [LIBDOVECOT_LUA_INCLUDE])
+	AX_SUBST_L([LIBDOVECOT_IMAP_LOGIN_INCLUDE], [LIBDOVECOT_CONFIG_INCLUDE], [LIBDOVECOT_IMAP_INCLUDE], [LIBDOVECOT_POP3_INCLUDE], [LIBDOVECOT_SUBMISSION_INCLUDE], [LIBDOVECOT_LMTP_INCLUDE], [LIBDOVECOT_DSYNC_INCLUDE], [LIBDOVECOT_IMAPC_INCLUDE], [LIBDOVECOT_LANG_INCLUDE])
+	AX_SUBST_L([LIBDOVECOT_NOTIFY_INCLUDE], [LIBDOVECOT_PUSH_NOTIFICATION_INCLUDE], [LIBDOVECOT_ACL_INCLUDE], [LIBDOVECOT_LIBLANG_INCLUDE], [LIBDOVECOT_LUA_INCLUDE])
 	AX_SUBST_L([DOVECOT_LUA_LIBS], [DOVECOT_LUA_CFLAGS], [LIBDOVECOT_LUA], [LIBDOVECOT_LUA_DEPS])
 
 	AS_IF([test x$DOVECOT_HAVE_MAIL_UTF8 = xyes], [

configure.ac
@@ -146,17 +146,17 @@ AS_HELP_STRING([--with-cassandra], [Build with Cassandra driver support]),
   want_cassandra=no)
 
 AC_ARG_WITH(stemmer,
-AS_HELP_STRING([--with-stemmer], [Build with libstemmer support (for FTS) (auto)]),
+AS_HELP_STRING([--with-stemmer], [Build with libstemmer support (for lib-language) (auto)]),
   TEST_WITH(stemmer, $withval),
   want_stemmer=auto)
 
 AC_ARG_WITH(textcat,
-AS_HELP_STRING([--with-textcat], [Build with libtextcat support (for FTS) (auto)]),
+AS_HELP_STRING([--with-textcat], [Build with libtextcat support (for lib-language) (auto)]),
   TEST_WITH(textcat, $withval),
   want_textcat=auto)
 
 AC_ARG_WITH(icu,
-AS_HELP_STRING([--with-icu], [Build with libicu support (for FTS normalization) (auto)]),
+AS_HELP_STRING([--with-icu], [Build with libicu support (for lib-language normalization) (auto)]),
   want_icu=$withval,
   want_icu=auto)
 
@@ -610,7 +610,7 @@ LIBDOVECOT_STORAGE="$LIBDOVECOT_STORAGE_DEPS"
 LIBDOVECOT_DSYNC='$(top_builddir)/src/doveadm/dsync/libdovecot-dsync.la'
 LIBDOVECOT_SQL='$(top_builddir)/src/lib-sql/libsql.la'
 LIBDOVECOT_COMPRESS='$(top_builddir)/src/lib-compression/libcompression.la'
-LIBDOVECOT_LIBFTS='$(top_builddir)/src/lib-language/libdovecot-language.la'
+LIBDOVECOT_LIBLANG='$(top_builddir)/src/lib-language/libdovecot-language.la'
 
 LIBDOVECOT_OPENSSL='$(top_builddir)/src/lib-ssl-iostream/libssl_iostream_openssl.la'
 LIBDOVECOT_OPENSSL_DEPS="$LIBDOVECOT_OPENSSL"
@@ -626,8 +626,8 @@ AC_SUBST(LIBDOVECOT_COMPRESS)
 AC_SUBST(LIBDOVECOT_DSYNC)
 AC_SUBST(LIBDOVECOT_LDA)
 AC_SUBST(LIBDOVECOT_LDAP)
-AC_SUBST(LIBDOVECOT_LIBFTS)
-AC_SUBST(LIBDOVECOT_LIBFTS_DEPS)
+AC_SUBST(LIBDOVECOT_LIBLANG)
+AC_SUBST(LIBDOVECOT_LIBLANG_DEPS)
 AC_SUBST(LIBDOVECOT_LUA)
 AC_SUBST(LIBDOVECOT_LUA_DEPS)
 AC_SUBST(LIBDOVECOT_OPENSSL)

dovecot-config.in.in
@@ -18,7 +18,7 @@ LIBDOVECOT_COMPRESS="@LIBDOVECOT_COMPRESS@"
 LIBDOVECOT_LDA="@LIBDOVECOT_LDA@"
 LIBDOVECOT_STORAGE="@LIBDOVECOT_STORAGE@"
 LIBDOVECOT_DSYNC="@LIBDOVECOT_DSYNC@"
-LIBDOVECOT_LIBFTS="@LIBDOVECOT_LIBFTS@"
+LIBDOVECOT_LIBLANG="@LIBDOVECOT_LIBLANG@"
 LIBDOVECOT_LUA="@LIBDOVECOT_LUA@"
 LIBDOVECOT_OPENSSL="@LIBDOVECOT_OPENSSL@"
 
@@ -29,7 +29,7 @@ LIBDOVECOT_COMPRESS_DEPS="@LIBDOVECOT_COMPRESS@"
 LIBDOVECOT_LDA_DEPS="@LIBDOVECOT_LDA@"
 LIBDOVECOT_STORAGE_DEPS="@LIBDOVECOT_STORAGE_DEPS@"
 LIBDOVECOT_DSYNC_DEPS="@LIBDOVECOT_DSYNC@"
-LIBDOVECOT_LIBFTS_DEPS="@LIBDOVECOT_LIBFTS@"
+LIBDOVECOT_LIBLANG_DEPS="@LIBDOVECOT_LIBLANG@"
 LIBDOVECOT_LUA_DEPS="@LIBDOVECOT_LUA@"
 LIBDOVECOT_OPENSSL_DEPS="@LIBDOVECOT_OPENSSL@"
 
@@ -52,7 +52,7 @@ LIBDOVECOT_FTS_INCLUDE="-I$(incdir)/src/plugins/fts"
 LIBDOVECOT_NOTIFY_INCLUDE="-I$(incdir)/src/plugins/notify"
 LIBDOVECOT_PUSH_NOTIFICATION_INCLUDE="-I$(incdir)/src/plugins/push-notification"
 LIBDOVECOT_ACL_INCLUDE="-I$(incdir)/src/plugins/acl"
-LIBDOVECOT_LIBFTS_INCLUDE="-I$(incdir)/src/lib-language"
+LIBDOVECOT_LIBLANG_INCLUDE="-I$(incdir)/src/lib-language"
 LIBDOVECOT_LUA_INCLUDE="-I$(incdir)/src/lib-lua"
 
 dovecot_pkgincludedir=

m4/want_icu.m4
@@ -12,7 +12,7 @@ AC_DEFUN([DOVECOT_WANT_ICU], [
   ])
 
   AS_IF([test "$have_icu" != "no"], [
-    AC_DEFINE(HAVE_LIBICU,, [Define if you want ICU normalization support for FTS])
+    AC_DEFINE(HAVE_LIBICU,, [Define if you want ICU normalization support for lib-language])
   ])
 
   AM_CONDITIONAL(BUILD_LIBICU, test "$have_icu" = "yes")

m4/want_stemmer.m4
@@ -2,7 +2,7 @@ AC_DEFUN([DOVECOT_WANT_STEMMER], [
   AS_IF([test "$want_stemmer" != "no"], [
     AC_CHECK_LIB(stemmer, sb_stemmer_new, [
       have_fts_stemmer=yes
-      AC_DEFINE(HAVE_FTS_STEMMER,, [Define if you want stemming support for FTS])
+      AC_DEFINE(HAVE_LANG_STEMMER,, [Define if you want stemming support for lib-language])
     ], [
       AS_IF([test "$want_stemmer" = "yes"], [
         AC_MSG_ERROR(cannot build with stemmer support: libstemmer not found)
@@ -10,5 +10,5 @@ AC_DEFUN([DOVECOT_WANT_STEMMER], [
     ])
   ])
 
-  AM_CONDITIONAL([BUILD_FTS_STEMMER], [test "$have_fts_stemmer" = "yes"])
+  AM_CONDITIONAL([BUILD_LANG_STEMMER], [test "$have_fts_stemmer" = "yes"])
 ])

m4/want_textcat.m4
@@ -3,35 +3,35 @@ AC_DEFUN([DOVECOT_WANT_TEXTCAT], [
     AS_IF([test "$PKG_CONFIG" != "" && $PKG_CONFIG --exists libexttextcat 2>/dev/null], [
       PKG_CHECK_MODULES(LIBEXTTEXTCAT, libexttextcat)
       TEXTCAT_DATADIR=`$PKG_CONFIG --variable=pkgdatadir libexttextcat`
-      AC_DEFINE(HAVE_FTS_EXTTEXTCAT,, [Define if you want exttextcat support for FTS])
-      have_fts_exttextcat=yes
+      AC_DEFINE(HAVE_LANG_EXTTEXTCAT,, [Define if you want exttextcat support for lib-language])
+      have_lang_exttextcat=yes
 
       dnl Debian Wheezy workaround - LIBEXTTEXTCAT_CFLAGS include path is wrong:
       AC_CHECK_HEADERS(libexttextcat/textcat.h)
     ], [
       AC_CHECK_LIB(exttextcat, special_textcat_Init, [
-        have_fts_exttextcat=yes
+        have_lang_exttextcat=yes
         AC_CHECK_HEADERS(libexttextcat/textcat.h)
         LIBEXTTEXTCAT_LIBS=-lexttextcat
-        AC_DEFINE(HAVE_FTS_EXTTEXTCAT,, [Define if you want exttextcat support for FTS])
+        AC_DEFINE(HAVE_LANG_EXTTEXTCAT,, [Define if you want exttextcat support for lib-language])
         TEXTCAT_DATADIR="/usr/share/libexttextcat"
         AC_SUBST(LIBEXTTEXTCAT_LIBS)
       ], [
         AC_CHECK_LIB(textcat, special_textcat_Init, [
-          have_fts_textcat=yes
+          have_lang_textcat=yes
           TEXTCAT_DATADIR="/usr/share/libtextcat"
           AC_CHECK_HEADERS(libtextcat/textcat.h)
         ])
       ])
-      AS_IF([test "$want_textcat" = "yes" && test "$have_fts_exttextcat" != "yes" && test "$have_fts_textcat" != "yes"], [
+      AS_IF([test "$want_textcat" = "yes" && test "$have_lang_exttextcat" != "yes" && test "$have_lang_textcat" != "yes"], [
         AC_MSG_ERROR(cannot build with textcat support: libtextcat or libexttextcat not found)
       ])
     ])
-    AS_IF([test "$have_fts_exttextcat" = "yes" || test "$have_fts_textcat" = "yes"], [
-      AC_DEFINE(HAVE_FTS_TEXTCAT,, [Define if you want textcat support for FTS])
+    AS_IF([test "$have_lang_exttextcat" = "yes" || test "$have_lang_textcat" = "yes"], [
+      AC_DEFINE(HAVE_LANG_TEXTCAT,, [Define if you want textcat support for lib-language])
       AC_DEFINE_UNQUOTED(TEXTCAT_DATADIR, "$TEXTCAT_DATADIR", [Points to textcat pkgdatadir containing the language files])
     ])
   ])
-  AM_CONDITIONAL(BUILD_FTS_TEXTCAT, test "$have_fts_textcat" = "yes")
-  AM_CONDITIONAL(BUILD_FTS_EXTTEXTCAT, test "$have_fts_exttextcat" = "yes")
+  AM_CONDITIONAL(BUILD_LANG_TEXTCAT, test "$have_lang_textcat" = "yes")
+  AM_CONDITIONAL(BUILD_LANG_EXTTEXTCAT, test "$have_lang_exttextcat" = "yes")
 ])

src/lib-language/Makefile.am
@@ -54,14 +54,14 @@ $(srcdir)/word-break-data.c: $(srcdir)/word-properties.pl $(srcdir)/PropList.txt
 	$(AM_V_at)perl $(srcdir)/word-properties.pl breaks $(srcdir)/PropList.txt > $@
 
 
-if BUILD_FTS_STEMMER
+if BUILD_LANG_STEMMER
 STEMMER_LIBS = -lstemmer
 endif
 
-if BUILD_FTS_EXTTEXTCAT
+if BUILD_LANG_EXTTEXTCAT
 TEXTCAT_LIBS = $(LIBEXTTEXTCAT_LIBS)
 else
-if BUILD_FTS_TEXTCAT
+if BUILD_LANG_TEXTCAT
 TEXTCAT_LIBS = -ltextcat
 endif
 endif
@@ -138,7 +138,7 @@ test_lang_filter_SOURCES = test-lang-filter.c
 test_lang_filter_LDADD = liblanguage.la $(test_libs)
 test_lang_filter_DEPENDENCIES = liblanguage.la $(test_deps)
 
-if BUILD_FTS_EXTTEXTCAT
+if BUILD_LANG_EXTTEXTCAT
 TEST_LANGUAGE = test-language
 test_language_SOURCES = test-language.c
 test_language_LDADD = language.lo $(test_libs) $(TEXTCAT_LIBS)

src/lib-language/lang-filter-stemmer-snowball.c
@@ -4,7 +4,7 @@
 #include "language.h"
 #include "lang-filter-private.h"
 
-#ifdef HAVE_FTS_STEMMER
+#ifdef HAVE_LANG_STEMMER
 
 #include <libstemmer.h>
 

commit e756fdb86a2ec4cada018b4a55568ec0c7360a63

m4/dovecot.m4: Fix accidentally renamed LIBDOVECOT_FTS_INCLUDE
Broken by 19316ac1ff80cb454f2d35614137292d9aa79e1e

m4/dovecot.m4
@@ -6,7 +6,7 @@ dnl This file is free software; the authors give
 dnl unlimited permission to copy and/or distribute it, with or without
 dnl modifications, as long as this notice is preserved.
 
-# serial 41
+# serial 42
 
 dnl
 dnl Check for support for D_FORTIFY_SOURCE=2
@@ -398,7 +398,7 @@ AC_DEFUN([DC_DOVECOT],[
 	AX_SUBST_L([LIBDOVECOT], [LIBDOVECOT_LOGIN], [LIBDOVECOT_SQL], [LIBDOVECOT_LDAP], [LIBDOVECOT_OPENSSL], [LIBDOVECOT_COMPRESS], [LIBDOVECOT_LDA], [LIBDOVECOT_STORAGE], [LIBDOVECOT_DSYNC], [LIBDOVECOT_LIBLANG])
 	AX_SUBST_L([LIBDOVECOT_DEPS], [LIBDOVECOT_LOGIN_DEPS], [LIBDOVECOT_SQL_DEPS], [LIBDOVECOT_LDAP_DEPS], [LIBDOVECOT_OPENSSL_DEPS], [LIBDOVECOT_COMPRESS_DEPS], [LIBDOVECOT_LDA_DEPS], [LIBDOVECOT_STORAGE_DEPS], [LIBDOVECOT_DSYNC_DEPS], [LIBDOVECOT_LIBLANG_DEPS])
 	AX_SUBST_L([LIBDOVECOT_INCLUDE], [LIBDOVECOT_LDA_INCLUDE], [LIBDOVECOT_AUTH_INCLUDE], [LIBDOVECOT_DOVEADM_INCLUDE], [LIBDOVECOT_SERVICE_INCLUDE], [LIBDOVECOT_STORAGE_INCLUDE], [LIBDOVECOT_LOGIN_INCLUDE], [LIBDOVECOT_SQL_INCLUDE], [LIBDOVECOT_LDAP_INCLUDE])
-	AX_SUBST_L([LIBDOVECOT_IMAP_LOGIN_INCLUDE], [LIBDOVECOT_CONFIG_INCLUDE], [LIBDOVECOT_IMAP_INCLUDE], [LIBDOVECOT_POP3_INCLUDE], [LIBDOVECOT_SUBMISSION_INCLUDE], [LIBDOVECOT_LMTP_INCLUDE], [LIBDOVECOT_DSYNC_INCLUDE], [LIBDOVECOT_IMAPC_INCLUDE], [LIBDOVECOT_LANG_INCLUDE])
+	AX_SUBST_L([LIBDOVECOT_IMAP_LOGIN_INCLUDE], [LIBDOVECOT_CONFIG_INCLUDE], [LIBDOVECOT_IMAP_INCLUDE], [LIBDOVECOT_POP3_INCLUDE], [LIBDOVECOT_SUBMISSION_INCLUDE], [LIBDOVECOT_LMTP_INCLUDE], [LIBDOVECOT_DSYNC_INCLUDE], [LIBDOVECOT_IMAPC_INCLUDE], [LIBDOVECOT_FTS_INCLUDE])
 	AX_SUBST_L([LIBDOVECOT_NOTIFY_INCLUDE], [LIBDOVECOT_PUSH_NOTIFICATION_INCLUDE], [LIBDOVECOT_ACL_INCLUDE], [LIBDOVECOT_LIBLANG_INCLUDE], [LIBDOVECOT_LUA_INCLUDE])
 	AX_SUBST_L([DOVECOT_LUA_LIBS], [DOVECOT_LUA_CFLAGS], [LIBDOVECOT_LUA], [LIBDOVECOT_LUA_DEPS])
 