dovecot/core

commit 191da0e9f93362fbf1ba8af2461c7df984eb375b

lib-http: Fix storing dns_client_socket_path to http_client_context
It wasn't safe to point it directly to http_client's settings, since the
client could become destroyed.

src/lib-http/http-client.c
@@ -541,7 +541,7 @@ http_client_context_update_settings(struct http_client_context *cctx)
 		    client->set->dns_client_socket_path != NULL &&
 		    client->set->dns_client_socket_path[0] != '\0') {
 			cctx->dns_client_socket_path =
-				client->set->dns_client_socket_path;
+				p_strdup(cctx->pool, client->set->dns_client_socket_path);
 		}
 		if (client->set->dns_ttl_msecs != 0 &&
 		    cctx->dns_ttl_msecs > client->set->dns_ttl_msecs)

commit ea4c73f78b36fbd1310228dcfd0c318296882570

lib-http: Fix increasing memory usage in shared HTTP client context
Broken by 191da0e9f93362fbf1ba8af2461c7df984eb375b

src/lib-http/http-client-private.h
@@ -401,7 +401,7 @@ struct http_client_context {
 	struct ioloop *ioloop;
 
 	struct dns_client *dns_client;
-	const char *dns_client_socket_path;
+	char *dns_client_socket_path;
 	unsigned int dns_ttl_msecs;
 	unsigned int dns_lookup_timeout_msecs;
 

src/lib-http/http-client.c
@@ -492,6 +492,7 @@ void http_client_context_unref(struct http_client_context **_cctx)
 	connection_list_deinit(&cctx->conn_list);
 
 	event_unref(&cctx->event);
+	i_free(cctx->dns_client_socket_path);
 	pool_unref(&cctx->pool);
 }
 
@@ -513,7 +514,7 @@ http_client_context_update_settings(struct http_client_context *cctx)
 
 	/* Revert back to default settings */
 	cctx->dns_client = NULL;
-	cctx->dns_client_socket_path = NULL;
+	i_free(cctx->dns_client_socket_path);
 	cctx->dns_ttl_msecs = UINT_MAX;
 	cctx->dns_lookup_timeout_msecs = UINT_MAX;
 
@@ -534,10 +535,10 @@ http_client_context_update_settings(struct http_client_context *cctx)
 			if (client->set->dns_client_socket_path[0] == '/' ||
 			    str_begins_with(client->set->dns_client_socket_path, "./")) {
 				cctx->dns_client_socket_path =
-					p_strdup(cctx->pool, client->set->dns_client_socket_path);
+					i_strdup(client->set->dns_client_socket_path);
 			} else {
 				cctx->dns_client_socket_path =
-					p_strdup_printf(cctx->pool, "%s/%s",
+					i_strdup_printf("%s/%s",
 							client->set->base_dir,
 							client->set->dns_client_socket_path);
 			}