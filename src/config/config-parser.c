dovecot/core

commit 0301f03cbe9123302a439756f54a713373b86c69

config, lib-settings: Add support for binary config file cache
This is mainly intended for optimizing our CI performance. It shouldn't
be used in production.

src/config/config-parser.c
@@ -57,6 +57,7 @@ struct config_parsed {
 	const char *dovecot_config_version;
 	struct config_filter_parser *const *filter_parsers;
 	struct config_module_parser *module_parsers;
+	ARRAY_TYPE(config_path) seen_paths;
 	ARRAY_TYPE(const_string) errors;
 	HASH_TABLE(const char *, const struct setting_define *) key_hash;
 	HASH_TABLE(const char *, struct config_include_group_filters *) include_groups;
@@ -111,6 +112,29 @@ void config_parser_set_change_counter(struct config_parser_context *ctx,
 				      uint8_t change_counter)
 {
 	ctx->change_counter = change_counter;
+}
+
+static void
+config_parser_add_seen_file(struct config_parser_context *ctx,
+			    const struct stat *st, const char *path)
+{
+	struct config_path *seen_path = array_append_space(&ctx->seen_paths);
+	seen_path->path = p_strdup(ctx->pool, path);
+	seen_path->st = *st;
+}
+
+static int
+config_parser_add_seen_file_fd(struct config_parser_context *ctx,
+			       int fd, const char *path, const char **error_r)
+{
+	struct stat st;
+
+	if (fstat(fd, &st) < 0) {
+		*error_r = t_strdup_printf("fstat(%s) failed: %m", path);
+		return -1;
+	}
+	config_parser_add_seen_file(ctx, &st, path);
+	return 0;
 }
 
 static struct config_section_stack *
@@ -669,6 +693,7 @@ static int config_apply_file(struct config_parser_context *ctx,
 			     const struct config_line *line,
 			     const char *path, const char **output_r)
 {
+	struct stat st;
 	const char *full_path, *error;
 
 	if (path[0] == '\0') {
@@ -686,13 +711,15 @@ static int config_apply_file(struct config_parser_context *ctx,
 	/* preserve original relative path in doveconf output */
 	if (full_path != path && ctx->expand_values)
 		path = full_path;
-	if (settings_parse_read_file(full_path, path, ctx->pool, NULL,
+	if (settings_parse_read_file(full_path, path, ctx->pool, &st,
 				     output_r, &error) < 0) {
 		ctx->error = p_strdup(ctx->pool, error);
 		if (config_apply_error(ctx, line->key) < 0)
 			return -1;
 		/* delayed error */
 		*output_r = "";
+	} else {
+		config_parser_add_seen_file(ctx, &st, full_path);
 	}
 	return 0;
 }
@@ -1684,8 +1711,8 @@ config_all_parsers_check(struct config_parser_context *ctx,
 }
 
 static int
-str_append_file(string_t *str, const char *key, const char *path,
-		const char **error_r)
+str_append_file(struct config_parser_context *ctx, string_t *str,
+		const char *key, const char *path, const char **error_r)
 {
 	unsigned char buf[1024];
 	int fd;
@@ -1699,6 +1726,8 @@ str_append_file(string_t *str, const char *key, const char *path,
 					   key, path);
 		return -1;
 	}
+	if (config_parser_add_seen_file_fd(ctx, fd, path, error_r) < 0)
+		return -1;
 	while ((ret = read(fd, buf, sizeof(buf))) > 0)
 		str_append_data(str, buf, ret);
 	if (ret < 0) {
@@ -1732,6 +1761,8 @@ static int settings_add_include(struct config_parser_context *ctx, const char *p
 					   path);
 		return -1;
 	}
+	if (config_parser_add_seen_file_fd(ctx, fd, path, error_r) < 0)
+		return -1;
 
 	new_input = p_new(ctx->pool, struct input_stack, 1);
 	new_input->prev = ctx->cur_input;
@@ -2343,6 +2374,7 @@ config_parse_finish(struct config_parser_context *ctx,
 	pool_ref(new_config->pool);
 	new_config->dovecot_config_version = ctx->dovecot_config_version;
 	p_array_init(&new_config->errors, ctx->pool, 1);
+	new_config->seen_paths = ctx->seen_paths;
 
 	array_sort(&ctx->all_filter_parsers, config_parser_filter_cmp);
 	array_append_zero(&ctx->all_filter_parsers);
@@ -2498,7 +2530,7 @@ static int config_write_value(struct config_parser_context *ctx,
 						line->key) :
 				line->key;
 			path = fix_relative_path(line->value, ctx->cur_input);
-			if (str_append_file(ctx->value, key_with_path, path,
+			if (str_append_file(ctx, ctx->value, key_with_path, path,
 					    &error) < 0) {
 				/* file reading failed */
 				ctx->error = p_strdup(ctx->pool, error);
@@ -2904,6 +2936,11 @@ int config_parse_file(const char *path, enum config_parse_flags flags,
 	ctx.delay_errors = (flags & CONFIG_PARSE_FLAG_DELAY_ERRORS) != 0;
 	ctx.ignore_unknown = (flags & CONFIG_PARSE_FLAG_IGNORE_UNKNOWN) != 0;
 	hash_table_create(&ctx.all_keys, ctx.pool, 500, str_hash, strcmp);
+	p_array_init(&ctx.seen_paths, ctx.pool, 8);
+	if (fd != -1) {
+		if (config_parser_add_seen_file_fd(&ctx, fd, path, error_r) < 0)
+			return -1;
+	}
 
 	for (count = 0; all_infos[count] != NULL; count++) ;
 	config_parser_set_change_counter(&ctx, CONFIG_PARSER_CHANGE_DEFAULTS);
@@ -3272,6 +3309,12 @@ bool config_parsed_get_includes(struct config_parsed *config,
 			array_push_back(groups, group);
 	}
 	return array_count(groups) > 0;
+}
+
+const ARRAY_TYPE(config_path) *
+config_parsed_get_paths(struct config_parsed *config)
+{
+	return &config->seen_paths;
 }
 
 void config_parsed_free(struct config_parsed **_config)

src/config/config-dump-full.c
@@ -8,6 +8,7 @@
 #include "safe-mkstemp.h"
 #include "ostream.h"
 #include "settings.h"
+#include "master-service-settings.h"
 #include "config-parser.h"
 #include "config-request.h"
 #include "config-filter.h"
@@ -24,6 +25,16 @@
 
    "DOVECOT-CONFIG\t1.0\n"
    <64bit big-endian: settings full size>
+
+   <32bit big-endian: number of paths used for caching>
+   Repeat for "number of paths":
+     <NUL-terminated string: path>
+     <64bit big-endian: inode>
+     <64bit big-endian: size>
+     <32bit big-endian: mtime UNIX timestamp>
+     <32bit big-endian: mtime nsecs>
+     <32bit big-endian: ctime UNIX timestamp>
+     <32bit big-endian: ctime nsecs>
 
    <32bit big-endian: event filter strings count>
    Repeat for "event filter strings count":
@@ -114,6 +125,54 @@ static int output_blob_size(struct ostream *output, uoff_t blob_size_offset)
 		return -1;
 	}
 	return 0;
+}
+
+static const char *config_get_cache_dir(enum config_dump_flags flags)
+{
+	if ((flags & CONFIG_DUMP_FLAG_WRITE_BINARY_CACHE) == 0)
+		return NULL;
+	return getenv("DOVECOT_CONFIG_CACHE");
+}
+
+static void
+config_dump_full_write_cache_paths(struct ostream *output,
+				   struct config_parsed *config,
+				   enum config_dump_flags flags,
+				   const char **cache_path_r)
+{
+	const struct config_path *path;
+	const char *cache_dir = config_get_cache_dir(flags);
+	uint32_t num32 = 0;
+
+	if (cache_dir == NULL) {
+		/* no config caching - nobody cares about the paths */
+		o_stream_nsend(output, &num32, sizeof(num32));
+		*cache_path_r = NULL;
+		return;
+	}
+
+	num32 = cpu32_to_be(array_count(config_parsed_get_paths(config)));
+	o_stream_nsend(output, &num32, sizeof(num32));
+	array_foreach(config_parsed_get_paths(config), path) {
+		o_stream_nsend(output, path->path, strlen(path->path) + 1);
+		uint64_t num64 = cpu32_to_be(path->st.st_ino);
+		o_stream_nsend(output, &num64, sizeof(num64));
+		num64 = cpu32_to_be(path->st.st_size);
+		o_stream_nsend(output, &num64, sizeof(num64));
+		num32 = cpu32_to_be(path->st.st_mtime);
+		o_stream_nsend(output, &num32, sizeof(num32));
+		num32 = cpu32_to_be(ST_MTIME_NSEC(path->st));
+		o_stream_nsend(output, &num32, sizeof(num32));
+		num32 = cpu32_to_be(path->st.st_ctime);
+		o_stream_nsend(output, &num32, sizeof(num32));
+		num32 = cpu32_to_be(ST_CTIME_NSEC(path->st));
+		o_stream_nsend(output, &num32, sizeof(num32));
+	}
+
+	const struct config_path *main_path =
+		array_front(config_parsed_get_paths(config));
+	*cache_path_r = master_service_get_binary_config_cache_path(cache_dir,
+								    main_path->path);
 }
 
 static void
@@ -674,7 +733,8 @@ int config_dump_full(struct config_parsed *config,
 			str_free(&dump_ctx.delayed_output);
 			return -1;
 		}
-		if (dest == CONFIG_DUMP_FULL_DEST_TEMPDIR)
+		if (dest == CONFIG_DUMP_FULL_DEST_TEMPDIR &&
+		    config_get_cache_dir(flags) == NULL)
 			i_unlink(str_c(path));
 		dump_ctx.output = o_stream_create_fd_autoclose(&fd, IO_BLOCK_SIZE);
 		o_stream_set_name(dump_ctx.output, str_c(path));
@@ -697,6 +757,11 @@ int config_dump_full(struct config_parsed *config,
 		settings_full_size_offset = output->offset;
 		o_stream_nsend(output, &blob_size, sizeof(blob_size));
 
+		const char *cache_path;
+		config_dump_full_write_cache_paths(output, config, flags,
+						   &cache_path);
+		if (cache_path != NULL)
+			final_path = cache_path;
 		config_dump_full_write_filters(output, config);
 	}
 

src/config/config-parser-private.h
@@ -78,6 +78,7 @@ struct config_parser_context {
 	pool_t pool;
 	const char *path;
 
+	ARRAY_TYPE(config_path) seen_paths;
 	HASH_TABLE(const char *, struct config_parser_key *) all_keys;
 	ARRAY(struct config_filter_parser *) all_filter_parsers;
 	HASH_TABLE(struct config_filter *,

src/config/config-parser.h
@@ -3,6 +3,8 @@
 
 #include "config-filter.h"
 
+#include <sys/stat.h>
+
 #define CONFIG_MODULE_DIR MODULEDIR"/settings"
 
 /* change_counter used for default settings created internally */
@@ -66,6 +68,12 @@ struct config_module_parser {
 };
 ARRAY_DEFINE_TYPE(config_module_parsers, struct config_module_parser *);
 
+struct config_path {
+	const char *path;
+	struct stat st;
+};
+ARRAY_DEFINE_TYPE(config_path, struct config_path);
+
 extern struct module *modules;
 
 int config_parse_net(const char *value, struct ip_addr *ip_r,
@@ -116,7 +124,9 @@ bool config_parsed_get_includes(struct config_parsed *config,
 				const struct config_filter_parser *filter,
 				unsigned int parser_idx,
 				ARRAY_TYPE(config_include_group) *groups);
-
+/* Get all paths used for generating the config */
+const ARRAY_TYPE(config_path) *
+config_parsed_get_paths(struct config_parsed *config);
 void config_parsed_free(struct config_parsed **config);
 
 void config_parse_load_modules(bool dump_config_import);

src/config/config-request.h
@@ -24,6 +24,8 @@ enum config_dump_scope {
 };
 
 enum config_dump_flags {
+	/* Write config binary cache if it's enabled. */
+	CONFIG_DUMP_FLAG_WRITE_BINARY_CACHE	= 0x01,
 	CONFIG_DUMP_FLAG_DEDUPLICATE_KEYS	= 0x08,
 };
 

src/config/doveconf.c
@@ -1251,7 +1251,8 @@ int main(int argc, char *argv[])
 	if (dump_full && exec_args != NULL) {
 		int temp_fd = config_dump_full(config,
 					       CONFIG_DUMP_FULL_DEST_TEMPDIR,
-					       0, &import_environment);
+					       CONFIG_DUMP_FLAG_WRITE_BINARY_CACHE,
+					       &import_environment);
 		if (getenv(DOVECOT_PRESERVE_ENVS_ENV) != NULL) {
 			/* Standalone binary is getting its configuration via
 			   doveconf. Clean the environment before calling it.

src/lib-master/master-service-settings.c
@@ -7,6 +7,9 @@
 #include "fdpass.h"
 #include "write-full.h"
 #include "str.h"
+#include "sha2.h"
+#include "hex-binary.h"
+#include "restrict-access.h"
 #include "syslog-util.h"
 #include "eacces-error.h"
 #include "env-util.h"
@@ -581,4 +584,28 @@ master_service_get_import_environment_keyvals(struct master_service *service)
 			str_append_c(keyvals, ' ');
 	}
 	return str_c(keyvals);
+}
+
+const char *
+master_service_get_binary_config_cache_path(const char *cache_dir,
+					    const char *main_path)
+{
+	struct sha256_ctx hash;
+	sha256_init(&hash);
+	sha256_loop(&hash, main_path, strlen(main_path) + 1);
+	uid_t euid = geteuid();
+	sha256_loop(&hash, &euid, sizeof(euid));
+	gid_t egid = getegid();
+	sha256_loop(&hash, &egid, sizeof(egid));
+	unsigned int groups_count;
+	const gid_t *groups = restrict_get_groups_list(&groups_count);
+	sha256_loop(&hash, groups, sizeof(*groups) * groups_count);
+	unsigned char digest[SHA256_RESULTLEN];
+	sha256_result(&hash, digest);
+
+	string_t *cache_path = t_str_new(128);
+	str_append(cache_path, cache_dir);
+	str_append(cache_path, "/binary.conf.");
+	binary_to_hex_append(cache_path, digest, sizeof(digest));
+	return str_c(cache_path);
 }

src/lib-master/master-service-settings.h
@@ -95,4 +95,8 @@ master_service_get_service_settings(struct master_service *service);
 const char *
 master_service_get_import_environment_keyvals(struct master_service *service);
 
+const char *
+master_service_get_binary_config_cache_path(const char *cache_dir,
+					    const char *main_path);
+
 #endif

commit 6b96fe9f8e48e547c5e2ad9220d645dc663b42d8

config: Fix file descriptor leak if "set = <file" handling fails due to fstat()
Broken by 0301f03cbe9123302a439756f54a713373b86c69

src/config/config-parser.c
@@ -1726,8 +1726,10 @@ str_append_file(struct config_parser_context *ctx, string_t *str,
 					   key, path);
 		return -1;
 	}
-	if (config_parser_add_seen_file_fd(ctx, fd, path, error_r) < 0)
+	if (config_parser_add_seen_file_fd(ctx, fd, path, error_r) < 0) {
+		i_close_fd(&fd);
 		return -1;
+	}
 	while ((ret = read(fd, buf, sizeof(buf))) > 0)
 		str_append_data(str, buf, ret);
 	if (ret < 0) {