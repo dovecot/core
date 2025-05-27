dovecot/core

commit 894610212596c35aade07a4d0af9d5e7fd6245c7

lib-master: Use pooled memory for haproxy

src/lib-master/master-service-haproxy.c
@@ -66,6 +66,8 @@ struct haproxy_data_v2 {
 struct master_service_haproxy_conn {
 	struct master_service_connection conn;
 
+	pool_t pool;
+
 	struct master_service_haproxy_conn *prev, *next;
 	
 	struct master_service *service;
@@ -83,7 +85,7 @@ master_service_haproxy_conn_free(struct master_service_haproxy_conn *hpconn)
 
 	io_remove(&hpconn->io);
 	timeout_remove(&hpconn->to);
-	i_free(hpconn);
+	pool_unref(&hpconn->pool);
 }
 
 static void
@@ -492,6 +494,7 @@ void master_service_haproxy_new(struct master_service *service,
 				struct master_service_connection *conn)
 {
 	struct master_service_haproxy_conn *hpconn;
+	pool_t pool;
 
 	if (!master_service_haproxy_conn_is_trusted(service, conn)) {
 		i_warning("haproxy: Client not trusted (rip=%s)",
@@ -500,7 +503,9 @@ void master_service_haproxy_new(struct master_service *service,
 		return;
 	}
 
-	hpconn = i_new(struct master_service_haproxy_conn, 1);
+	pool = pool_alloconly_create("haproxy connection", 128);
+	hpconn = p_new(pool, struct master_service_haproxy_conn, 1);
+	hpconn->pool = pool;
 	hpconn->conn = *conn;
 	hpconn->service = service;
 	DLLIST_PREPEND(&service->haproxy_conns, hpconn);

commit 888ff933db9c7ea5393d6ef83cbb11b6de94ca5e

lib-master: Copy haproxy provided fields before calling callback
Otherwise fields get lost, broken in 894610212596c35aade07a4d0af9d5e7fd6245c7

src/lib-master/master-service-haproxy.c
@@ -139,6 +139,14 @@ master_service_haproxy_conn_success(struct master_service_haproxy_conn *hpconn)
 	struct master_service *service = hpconn->service;
 	struct master_service_connection conn = hpconn->conn;
 
+	/* copy values */
+	conn.haproxy.cert_common_name = t_strdup(hpconn->conn.haproxy.cert_common_name);
+	if (hpconn->conn.haproxy.alpn_size > 0)
+		conn.haproxy.alpn = t_memdup(hpconn->conn.haproxy.alpn, hpconn->conn.haproxy.alpn_size);
+	else
+		conn.haproxy.alpn = NULL;
+	conn.haproxy.hostname = t_strdup(hpconn->conn.haproxy.hostname);
+
 	master_service_haproxy_conn_free(hpconn);
 	master_service_client_connection_callback(service, &conn);
 }