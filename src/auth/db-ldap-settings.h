dovecot/core

commit 961275fdb54878fdfa4ee1b9f1a4f00e82bf4a83

auth: db-ldap - Move validation to settings

src/auth/db-ldap.c
@@ -1700,32 +1700,6 @@ struct ldap_connection *db_ldap_init(struct event *event)
 	conn->default_bind_msgid = -1;
 	conn->fd = -1;
 
-	if (conn->set->base == NULL)
-		i_fatal("LDAP: No ldap_base given");
-
-	if (conn->set->uris == NULL && conn->set->hosts == NULL)
-		i_fatal("LDAP: Neither ldap_uris nor ldap_hosts set");
-#ifndef LDAP_HAVE_INITIALIZE
-	if (conn->set->uris != NULL) {
-		i_fatal("LDAP: ldap_uris set, but Dovecot compiled without support for LDAP uris "
-			"(ldap_initialize() not supported by LDAP library)");
-	}
-#endif
-#ifndef LDAP_HAVE_START_TLS_S
-	if (conn->set->starttls)
-		i_fatal("LDAP: ldap_starttls=yes, but your LDAP library doesn't support TLS");
-#endif
-#ifndef HAVE_LDAP_SASL
-	if (conn->set->sasl_bind)
-		i_fatal("LDAP: ldap_sasl_bind=yes but no SASL support compiled in");
-#endif
-	if (conn->set->version < 3) {
-		if (conn->set->sasl_bind)
-			i_fatal("LDAP: ldap_sasl_bind=yes requires ldap_version=3");
-		if (conn->set->starttls)
-			i_fatal("LDAP: ldap_starttls=yes requires ldap_version=3");
-	}
-
 	conn->event = event_create(auth_event);
 	event_set_append_log_prefix(conn->event, "ldap: ");
 

src/auth/db-ldap-settings.c
@@ -170,6 +170,49 @@ static bool ldap_setting_check(void *_set, pool_t pool ATTR_UNUSED,
 	}
 #endif
 
+	if (*set->base == '\0') {
+		*error_r = "No ldap_base given";
+		return FALSE;
+	}
+
+	if (*set->uris == '\0' && *set->hosts == '\0') {
+		*error_r = "Neither ldap_uris nor ldap_hosts set";
+		return FALSE;
+	}
+
+#ifndef LDAP_HAVE_INITIALIZE
+	if (*set->uris != '\0') {
+		*error_r = "ldap_uris set, but Dovecot compiled without support for LDAP uris "
+			   "(ldap_initialize() not supported by LDAP library)";
+		return FALSE;
+	}
+#endif
+
+#ifndef LDAP_HAVE_START_TLS_S
+	if (set->starttls) {
+		*error_r = "ldap_starttls=yes, but your LDAP library doesn't support TLS";
+		return FALSE;
+	}
+#endif
+
+#ifndef HAVE_LDAP_SASL
+	if (set->sasl_bind) {
+		*error_r = "ldap_sasl_bind=yes but no SASL support compiled in";
+		return FALSE;
+	}
+#endif
+
+	if (set->version < 3) {
+		if (set->sasl_bind) {
+			*error_r = "ldap_sasl_bind=yes requires ldap_version=3";
+			return FALSE;
+		}
+		if (set->starttls) {
+			*error_r = "ldap_starttls=yes requires ldap_version=3";
+			return FALSE;
+		}
+	}
+
 	return TRUE;
 }
 

commit 431e328b3b035ddb187526cd13bccf29833aed90

auth: Fix LDAP SASL support
The settings code didn't see the necessary defines.

Based on patch by Jakob Haufe

Broken by 961275fdb54878fdfa4ee1b9f1a4f00e82bf4a83

src/auth/db-ldap-settings.h
@@ -1,6 +1,20 @@
 #ifndef DB_LDAP_SETTINGS_H
 #define DB_LDAP_SETTINGS_H
 
+/* <settings checks> */
+#define HAVE_LDAP_SASL
+#ifdef HAVE_SASL_SASL_H
+#  include <sasl/sasl.h>
+#elif defined (HAVE_SASL_H)
+#  include <sasl.h>
+#else
+#  undef HAVE_LDAP_SASL
+#endif
+#if !defined(SASL_VERSION_MAJOR) || SASL_VERSION_MAJOR < 2
+#  undef HAVE_LDAP_SASL
+#endif
+/* </settings checks> */
+
 enum db_ldap_lookup_type {
 	DB_LDAP_LOOKUP_TYPE_PASSDB,
 	DB_LDAP_LOOKUP_TYPE_USERDB,

src/auth/db-ldap.c
@@ -22,19 +22,8 @@
 
 #include <unistd.h>
 
-#define HAVE_LDAP_SASL
-#ifdef HAVE_SASL_SASL_H
-#  include <sasl/sasl.h>
-#elif defined (HAVE_SASL_H)
-#  include <sasl.h>
-#else
-#  undef HAVE_LDAP_SASL
-#endif
 #ifdef LDAP_OPT_X_TLS
 #  define OPENLDAP_TLS_OPTIONS
-#endif
-#if !defined(SASL_VERSION_MAJOR) || SASL_VERSION_MAJOR < 2
-#  undef HAVE_LDAP_SASL
 #endif
 
 #ifndef LDAP_SASL_QUIET