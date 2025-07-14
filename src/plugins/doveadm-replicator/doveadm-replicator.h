d92e2145168281ffa52febe03aa31625f873abbc
Patrick Cernko <pcernko@mpi-klsb.mpg.de>
2025-07-14 12:49:38 +0200

    removed all replication specific code leaving only generic doveadm-dsync and doveadm-protocol specific API changes

--- src/plugins/doveadm-replicator/doveadm-replicator.h
+++
@@ -1,6 +0,0 @@
-#ifndef DOVEADM_REPLICATOR_H
-#define DOVEADM_REPLICATOR_H
-
-void doveadm_register_replicator_commands(void);
-
-#endif
--- src/plugins/replication/replication-plugin.h
+++
@@ -1,9 +0,0 @@
-#ifndef REPLICATION_PLUGIN_H
-#define REPLICATION_PLUGIN_H
-
-extern const char *replication_plugin_dependencies[];
-
-void replication_plugin_init(struct module *module);
-void replication_plugin_deinit(void);
-
-#endif
