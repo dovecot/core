165d1886b7a1fce8398fa0b37cff49f95d3aecca
Timo Sirainen <timo.sirainen@open-xchange.com>
2025-08-08 14:42:48 +0300

    lib-ssl-iostream: Fix indent

--- src/lib-ssl-iostream/iostream-openssl.c
+++ src/lib-ssl-iostream/iostream-openssl.c
@@ -623,8 +623,8 @@ static int openssl_iostream_handshake(struct ssl_iostream *ssl_io)
 			openssl_iostream_set_error(ssl_io, error);
 			ssl_io->handshake_failed = TRUE;
 		}
-       } else if (ssl_io->connected_host != NULL && !ssl_io->handshake_failed &&
-		  !ssl_io->allow_invalid_cert) {
+	} else if (ssl_io->connected_host != NULL && !ssl_io->handshake_failed &&
+		   !ssl_io->allow_invalid_cert) {
 		if (ssl_iostream_check_cert_validity(ssl_io, ssl_io->connected_host,
 						     &reason) != SSL_IOSTREAM_CERT_VALIDITY_OK) {
 			openssl_iostream_set_error(ssl_io, reason);
