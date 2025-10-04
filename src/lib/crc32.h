1a144b99fcd85d851addf0580b23a07cf3737d7a
Michael M Slusarz <michael.slusarz@open-xchange.com>
2025-10-04 14:24:54 -0600

    lib: crc32 - Use zlib's built-in CRC32 function
    Performance-wise, according to fastmail's crc32 test suite, this is 10x
    faster than the previous code.

--- src/lib/crc32.h
+++ src/lib/crc32.h
@@ -1,10 +1,18 @@
 #ifndef CRC32_H
 #define CRC32_H
 
-uint32_t crc32_data(const void *data, size_t size) ATTR_PURE;
-uint32_t crc32_str(const char *str) ATTR_PURE;
+#define CRC32_INIT 0
 
+uint32_t crc32_data(const void *data, size_t size) ATTR_PURE;
 uint32_t crc32_data_more(uint32_t crc, const void *data, size_t size) ATTR_PURE;
-uint32_t crc32_str_more(uint32_t crc, const char *str) ATTR_PURE;
+
+static inline uint32_t crc32_str(const char *str)
+{
+	return crc32_data_more(CRC32_INIT, str, strlen(str));
+}
+static inline uint32_t crc32_str_more(uint32_t crc, const char *str)
+{
+	return crc32_data_more(crc, str, strlen(str));
+}
 
 #endif
