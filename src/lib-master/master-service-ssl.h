ae04b508ce3ddc05a4c1ad42f6f9f33062c8a9e2
Timo Sirainen <timo.sirainen@open-xchange.com>
2025-10-05 18:56:10 +0300

    lib-master: Allow including master-service-ssl.h without master-service.h

--- src/lib-master/master-service-ssl.h
+++ src/lib-master/master-service-ssl.h
@@ -1,6 +1,8 @@
 #ifndef MASTER_SERVICE_SSL_H
 #define MASTER_SERVICE_SSL_H
 
+struct master_service;
+
 bool master_service_ssl_is_enabled(struct master_service *service);
 
 void master_service_ssl_ctx_init(struct master_service *service);
