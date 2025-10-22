dovecot/core

commit 776d3e6f408ae5c4f903c4ed9e906d84e3e741a1

lib-storage: Make sure shared INBOX mailbox is always uppercased
This fixes issues where if shared/user/INBOX was accessed without
uppercasing the INBOX, it wasn't handled consistently.

src/lib-storage/mail-storage.c
@@ -1146,6 +1146,21 @@ struct mailbox *mailbox_alloc(struct mailbox_list *list, const char *vname,
 			errstr = t_strdup(errstr);
 		}
 
+		if ((new_list->ns->flags & NAMESPACE_FLAG_INBOX_ANY) != 0 &&
+		    str_begins(vname, new_list->ns->prefix, &suffix) &&
+		    strcasecmp(suffix, "INBOX") == 0 &&
+		    strcmp(suffix, "INBOX") != 0) {
+			/* Accessing INBOX explicitly in a shared namespace -
+			   convert to uppercase. This is done regardless of the
+			   mail_shared_explicit_inbox setting, since even with
+			   "no" adding INBOX to the namespace prefix accesses
+			   the same INBOX. When accessing the shared INBOX
+			   as the namespace prefix, the uppercase INBOX is
+			   appended when getting the storage_name in mailbox
+			   list code. */
+			vname = t_strconcat(new_list->ns->prefix, "INBOX", NULL);
+		}
+
 		box = storage->v.mailbox_alloc(storage, new_list, vname, flags);
 		if (open_error != 0) {
 			box->open_error = open_error;

commit e46fbea7182056fa341cd0bfeb20e00a30d722f9

lib-storage: Use case-insensitive ns_prefix/INBOX only for shared namespace
For example OtherNamespace/Inbox shouldn't be converted to INBOX.

Broken by 776d3e6f408ae5c4f903c4ed9e906d84e3e741a1

src/lib-storage/mail-storage.c
@@ -1147,6 +1147,7 @@ struct mailbox *mailbox_alloc(struct mailbox_list *list, const char *vname,
 		}
 
 		if ((new_list->ns->flags & NAMESPACE_FLAG_INBOX_ANY) != 0 &&
+		    new_list->ns->type == MAIL_NAMESPACE_TYPE_SHARED &&
 		    str_begins(vname, new_list->ns->prefix, &suffix) &&
 		    strcasecmp(suffix, "INBOX") == 0 &&
 		    strcmp(suffix, "INBOX") != 0) {