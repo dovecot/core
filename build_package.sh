c415d9a3ea449ec1028ae75e81545b568cd222f6
Ilya Talipov <talipov.ilya@r7-office.ru>
2025-11-25 13:56:38 +0300

    add sudo in build package script

--- build_package.sh
+++ build_package.sh
@@ -5,5 +5,5 @@ DIR_FILES=$2
 sudo apt update
 sudo apt-get install -y zip unzip
 
-cd $DIR_FILES/Pack/src/ && zip -r $DIR_FILES/Packages/r7mdaserver_${VERSION}.zip *
-md5sum $DIR_FILES/Packages/r7mdaserver_${VERSION}.zip >> $DIR_FILES/Packages/md5.txt
+cd $DIR_FILES/Pack/src/ && sudo zip -r $DIR_FILES/Packages/r7mdaserver_${VERSION}.zip *
+sudo md5sum $DIR_FILES/Packages/r7mdaserver_${VERSION}.zip >> $DIR_FILES/Packages/md5.txt
