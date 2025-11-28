3046b61c47017543d175fab1636bf9bee0f25349
Ilya Talipov <talipov.ilya@r7-office.ru>
2025-11-28 13:39:04 +0300

    change script

--- build_package.sh
+++ build_package.sh
@@ -7,4 +7,5 @@ sudo apt-get install -y zip unzip
 
 cd $DIR_FILES/Pack/src/ && sudo zip -r $DIR_FILES/Packages/r7mdaserver_${VERSION}.zip *
 sudo md5sum $DIR_FILES/Packages/r7mdaserver_${VERSION}.zip >> $DIR_FILES/Packages/md5.txt
-cd $DIR_FILES && sudo zip -r $DIR_FILES/Packages/src_dovecot_${VERSION}.zip * -x $DIR_FILES/Packages/src_dovecot_${VERSION}.zip
+#Deprecated in build-server
+#cd $DIR_FILES && sudo zip -r $DIR_FILES/Packages/src_dovecot_${VERSION}.zip * -x $DIR_FILES/Packages/src_dovecot_${VERSION}.zip
