f1ce373c1b9d42da720893cd2127242d5a18615d
Ilya Talipov <talipov.ilya@r7-office.ru>
2025-11-28 15:23:12 +0300

    add deb build

--- build_package.sh
+++ build_package.sh
@@ -5,7 +5,13 @@ DIR_FILES=$2
 sudo apt update
 sudo apt-get install -y zip unzip
 
-cd $DIR_FILES/Pack/src/ && sudo zip -r $DIR_FILES/Packages/r7mdaserver_${VERSION}.zip *
-sudo md5sum $DIR_FILES/Packages/r7mdaserver_${VERSION}.zip >> $DIR_FILES/Packages/md5.txt
+sed -i "s/Version:.*/Version: ${VERSION}/" $DIR_FILES/Pack/src/DEBIAN/control
+sed -i "s/Source:.*/Source: r7mdaserver (${VERSION})/" $DIR_FILES/Pack/src/DEBIAN/control
+sed -i "s/Package:.*/Package: dovecot/" $DIR_FILES/Pack/src/DEBIAN/control
+
+dpkg-deb -b $DIR_FILES/Pack/src $DIR_FILES/Packages/dovecot_${VERSION}.deb
+
+#cd $DIR_FILES/Pack/src/ && sudo zip -r $DIR_FILES/Packages/r7mdaserver_${VERSION}.zip *
+#sudo md5sum $DIR_FILES/Packages/r7mdaserver_${VERSION}.zip >> $DIR_FILES/Packages/md5.txt
 #Deprecated in build-server
 #cd $DIR_FILES && sudo zip -r $DIR_FILES/Packages/src_dovecot_${VERSION}.zip * -x $DIR_FILES/Packages/src_dovecot_${VERSION}.zip
