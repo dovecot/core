6d6724bd529493c448d5aa02e70aee9aaff4f2c3
Timo Sirainen <timo.sirainen@open-xchange.com>
2025-12-07 07:05:25 +0200

    push-notification: Fix using %variables in push_notification_ox_url

--- src/plugins/push-notification/push-notification-settings.c
+++ src/plugins/push-notification/push-notification-settings.c
@@ -103,7 +103,9 @@ push_notification_ox_settings_check(void *_set, pool_t pool,
 	struct push_notification_ox_settings *set = _set;
 	const char *error;
 
-	if (set->url[0] != '\0') {
+	/* value can be set_value_unknown in config/doveconf if it contains
+	   variables that couldn't be expanded. */
+	if (set->url[0] != '\0' && set->url != set_value_unknown) {
 		if (http_url_parse(set->url, NULL, HTTP_URL_ALLOW_USERINFO_PART,
 				   pool, &set->parsed_url, &error) < 0) {
 			*error_r = t_strdup_printf(
