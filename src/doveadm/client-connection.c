dovecot/core

commit 1d1d368f4547b35082e53d684e5ebefcba4cc364

doveadm: Add local_ip and remote_ip fields to connection event
These will be required to preserve support for local {) and remote {}
config filters after the following change.

src/doveadm/client-connection.c
@@ -62,8 +62,10 @@ int client_connection_init(struct client_connection *conn,
 	conn->type = type;
 	conn->pool = pool;
 
-	(void)net_getsockname(fd, &conn->local_ip, &conn->local_port);
-	(void)net_getpeername(fd, &conn->remote_ip, &conn->remote_port);
+	if (net_getsockname(fd, &conn->local_ip, &conn->local_port) == 0)
+		event_add_ip(conn->event, "local_ip", &conn->local_ip);
+	if (net_getpeername(fd, &conn->remote_ip, &conn->remote_port) == 0)
+		event_add_ip(conn->event, "remote_ip", &conn->local_ip);
 
 	ip = net_ip2addr(&conn->remote_ip);
 	if (ip[0] != '\0')

commit 49a056727a3ba9cfe9fa68081eb8c6820696553c

doveadm: Log tcp client IP instead of server IP as remote IP
Broken in 1d1d368f4547b35082e53d684e5ebefcba4cc364

src/doveadm/client-connection.c
@@ -55,7 +55,7 @@ int client_connection_init(struct client_connection *conn,
 	if (net_getsockname(fd, &conn->local_ip, &conn->local_port) == 0)
 		event_add_ip(conn->event, "local_ip", &conn->local_ip);
 	if (net_getpeername(fd, &conn->remote_ip, &conn->remote_port) == 0)
-		event_add_ip(conn->event, "remote_ip", &conn->local_ip);
+		event_add_ip(conn->event, "remote_ip", &conn->remote_ip);
 
 	ip = net_ip2addr(&conn->remote_ip);
 	if (ip[0] != '\0')