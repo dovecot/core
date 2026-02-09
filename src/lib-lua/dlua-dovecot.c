dovecot/core

commit a487559b11294c1c519a64dd42a970456e1493a0

lib-lua: Automatically abort leaked passthrough events
Log an error about the leak.

src/lib-lua/dlua-dovecot.c
@@ -6,6 +6,7 @@
 #include "str.h"
 #include "base64.h"
 #include "time-util.h"
+#include "lib-event-private.h"
 #include "dlua-script-private.h"
 #include "dict-lua.h"
 #include "doveadm-client-lua.h"
@@ -180,10 +181,12 @@ static int dlua_event_pt_inc_int(lua_State *L)
 
 static int dlua_event_pt_log_debug(lua_State *L)
 {
+	struct dlua_script *script = dlua_script_from_state(L);
 	DLUA_REQUIRE_ARGS(L, 2);
 	struct event_passthrough *event = dlua_check_event_passthrough(L, 1);
 	const char *str = luaL_checkstring(L, 2);
 
+	script->pending_event_passthrough = NULL;
 	dlua_event_log(L, event->event(), LOG_TYPE_DEBUG, str);
 
 	lua_pushvalue(L, 1);
@@ -193,10 +196,12 @@ static int dlua_event_pt_log_debug(lua_State *L)
 
 static int dlua_event_pt_log_info(lua_State *L)
 {
+	struct dlua_script *script = dlua_script_from_state(L);
 	DLUA_REQUIRE_ARGS(L, 2);
 	struct event_passthrough *event = dlua_check_event_passthrough(L, 1);
 	const char *str = luaL_checkstring(L, 2);
 
+	script->pending_event_passthrough = NULL;
 	dlua_event_log(L, event->event(), LOG_TYPE_INFO, str);
 
 	lua_pushvalue(L, 1);
@@ -206,10 +211,12 @@ static int dlua_event_pt_log_info(lua_State *L)
 
 static int dlua_event_pt_log_warning(lua_State *L)
 {
+	struct dlua_script *script = dlua_script_from_state(L);
 	DLUA_REQUIRE_ARGS(L, 2);
 	struct event_passthrough *event = dlua_check_event_passthrough(L, 1);
 	const char *str = luaL_checkstring(L, 2);
 
+	script->pending_event_passthrough = NULL;
 	dlua_event_log(L, event->event(), LOG_TYPE_WARNING, str);
 
 	lua_pushvalue(L, 1);
@@ -219,10 +226,12 @@ static int dlua_event_pt_log_warning(lua_State *L)
 
 static int dlua_event_pt_log_error(lua_State *L)
 {
+	struct dlua_script *script = dlua_script_from_state(L);
 	DLUA_REQUIRE_ARGS(L, 2);
 	struct event_passthrough *event = dlua_check_event_passthrough(L, 1);
 	const char *str = luaL_checkstring(L, 2);
 
+	script->pending_event_passthrough = NULL;
 	dlua_event_log(L, event->event(), LOG_TYPE_ERROR, str);
 
 	lua_pushvalue(L, 1);
@@ -476,18 +485,38 @@ static int dlua_event_log_error(lua_State *L)
 	return 1;
 }
 
+void dlua_event_passthrough_abort(struct dlua_script *script)
+{
+	if (script->pending_event_passthrough != NULL) {
+		struct event *event =
+			script->pending_event_passthrough->event();
+		const char *source_filename = event->source_filename;
+		int source_linenum = event->source_linenum;
+		event_send_abort(event);
+		script->pending_event_passthrough = NULL;
+
+		e_error(script->event,
+			"Passthrough event leaked by Lua script at %s:%d",
+			source_filename, source_linenum);
+	}
+}
+
 #undef event_create_passthrough
 #undef event_create
 static int dlua_event_passthrough_event(lua_State *L)
 {
+	struct dlua_script *script = dlua_script_from_state(L);
 	DLUA_REQUIRE_ARGS(L, 1);
 	struct event *event = dlua_check_event(L, 1);
 	const char *file;
 	unsigned int line;
 
+	dlua_event_passthrough_abort(script);
+
 	dlua_get_file_line(L, 1, &file, &line);
 	struct event_passthrough *e =
 		event_create_passthrough(event, file, line);
+	script->pending_event_passthrough = e;
 	dlua_push_event_passthrough(L, e);
 
 	return 1;

src/lib-lua/dlua-resume.c
@@ -124,6 +124,7 @@ static void dlua_pcall_yieldable_continue(lua_State *L)
 	dlua_tls_clear(L, RESUME_NARGS);
 
 	ret = lua_resume(L, L, nargs, &nresults);
+	dlua_event_passthrough_abort(dlua_script_from_state(L));
 	if (ret == LUA_YIELD) {
 		/*
 		 * thread yielded - nothing to do
@@ -192,6 +193,7 @@ int dlua_pcall_yieldable(lua_State *L, const char *func_name, int nargs,
 
 	/* stack: func, args (top) */
 	ret = lua_resume(L, L, nargs, &nresults);
+	dlua_event_passthrough_abort(dlua_script_from_state(L));
 	if (ret == LUA_YIELD) {
 		/*
 		 * thread yielded - nothing to do

src/lib-lua/dlua-script-private.h
@@ -53,6 +53,7 @@ struct dlua_script {
 	lua_State *L; /* base lua context */
 
 	struct event *event;
+	struct event_passthrough *pending_event_passthrough;
 	const char *filename;
 	struct istream *in;
 	ssize_t last_read;
@@ -93,6 +94,8 @@ struct dlua_script *dlua_script_from_state(lua_State *L);
 
 /* register 'dovecot' global */
 void dlua_dovecot_register(struct dlua_script *script);
+
+void dlua_event_passthrough_abort(struct dlua_script *script);
 
 /* push 'dovecot' global on top of stack */
 void dlua_get_dovecot(lua_State *L);

src/lib-lua/dlua-script.c
@@ -154,6 +154,8 @@ int dlua_pcall(lua_State *L, const char *func_name, int nargs, int nresults,
 				nresults = lua_gettop(L) - top;
 			ret = nresults;
 		}
+		struct dlua_script *script = dlua_script_from_state(L);
+		dlua_event_passthrough_abort(script);
 	} else {
 		/* ensure stack is clean, remove function and arguments */
 		lua_pop(L, nargs + 1);
@@ -280,6 +282,7 @@ static int dlua_run_script(struct dlua_script *script, const char **error_r)
 		loaded script as function
 	*/
 	int err = lua_pcall(script->L, 0, 0, 1);
+	dlua_event_passthrough_abort(script);
 	if (err != LUA_OK) {
 		*error_r = t_strdup_printf("lua_pcall(%s) failed: %s",
 					   script->filename,

commit ff7c94f369453b29f838641caf8bfb1bba83c475

lib-lua: Fix accesing freed memory if Lua script leaks passthrough event
Broken by a487559b11294c1c519a64dd42a970456e1493a0

src/lib-lua/dlua-dovecot.c
@@ -490,7 +490,7 @@ void dlua_event_passthrough_abort(struct dlua_script *script)
 	if (script->pending_event_passthrough != NULL) {
 		struct event *event =
 			script->pending_event_passthrough->event();
-		const char *source_filename = event->source_filename;
+		const char *source_filename = t_strdup(event->source_filename);
 		int source_linenum = event->source_linenum;
 		event_send_abort(event);
 		script->pending_event_passthrough = NULL;