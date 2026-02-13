dovecot/core

commit 66bc85d333c53c29ef17b92b7964b86fc5f6d186

imap: Don't assert-crash on FETCH BODY[MIME] (return BAD instead)

src/lib-imap-storage/imap-msgpart.c
@@ -254,6 +254,8 @@ int imap_msgpart_parse(const char *section, struct imap_msgpart **msgpart_r)
 	section = t_str_ucase(section);
 
 	if (strcmp(section, "MIME") == 0) {
+		if (msgpart->section_number[0] == '\0')
+			return -1;
 		msgpart->fetch_type = FETCH_MIME;
 		msgpart->wanted_fields |= MAIL_FETCH_STREAM_BODY;
 	} else if (strcmp(section, "TEXT") == 0) {

commit 35c48f4c10dc3a2c7f5f6237e410ccdf9d88fe7a

lib-imap-storage: Free msgpart always on parsing error
Fixes memory leak introduced in 66bc85d333c53c29ef17b92b7964b86fc5f6d186

src/lib-imap-storage/imap-msgpart.c
@@ -258,8 +258,10 @@ int imap_msgpart_parse(const char *section, struct imap_msgpart **msgpart_r)
 	section = t_str_ucase(section);
 
 	if (strcmp(section, "MIME") == 0) {
-		if (msgpart->section_number[0] == '\0')
+		if (msgpart->section_number[0] == '\0') {
+			imap_msgpart_free(&msgpart);
 			return -1;
+		}
 		msgpart->fetch_type = FETCH_MIME;
 		msgpart->wanted_fields |= MAIL_FETCH_STREAM_BODY;
 	} else if (strcmp(section, "TEXT") == 0) {