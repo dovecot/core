addccc582eaf51bda83ae1ec85c59b466b6c5494
uwezkhan06 <uwezkhan053@gmail.com>
2026-05-13 18:25:47 +0530

    updated

--- src/lib/test-bits.c
+++ src/lib/test-bits.c
@@ -76,9 +76,8 @@ static void test_nearest_power(void)
 	test_assert_idx(nearest_power(num  ) == num,    b);
 	/* i_assert()s: test_assert_idx(nearest_power(num+1) == num<<1, b); */
 	/* Test edge case: nearest_power should never return a value smaller than input */
-	num = ((size_t)1 << (sizeof(size_t) * 8 - 1));
+	num = ((size_t)1 << (CHAR_BIT * sizeof(size_t) - 1));
 	test_assert(nearest_power(num) >= num);
-	test_assert(nearest_power(num + 1) >= num + 1);
 	test_end();
 }
 
@@ -283,4 +282,17 @@ void test_bits(void)
 	test_bits_rotr64();
 	test_sum_overflows();
 	test_bit_tests();
+}
+
+enum fatal_test_state fatal_bits(unsigned int stage)
+{
+	size_t num = ((size_t)1 << (CHAR_BIT * sizeof(size_t) - 1));
+	switch (stage) {
+	case 0:
+		test_begin("nearest_power() overflow");
+		test_expect_fatal_string("calculation would overflow");
+		(void)nearest_power(num + 1);
+		return FATAL_TEST_FAILURE;
+	}
+	return FATAL_TEST_FINISHED;
 }
