dovecot/core

commit d0b4a58cb934731e4b64934895a319b16e0d66e8

auth: Use new var_expand

src/auth/auth-request-var-expand.c
@@ -13,46 +13,45 @@ struct auth_request_var_expand_ctx {
 
 const struct var_expand_table
 auth_request_var_expand_static_tab[] = {
-	{ 'u', NULL, "user" },
-	{ 'n', NULL, "username" },
-	{ 'd', NULL, "domain" },
-	{ '\0', NULL, "protocol" },
-	{ 'h', NULL, "home" },
-	{ 'l', NULL, "local_ip" },
-	{ 'r', NULL, "remote_ip" },
-	{ 'p', NULL, "client_pid" },
-	{ 'w', NULL, "password" },
-	{ '!', NULL, NULL },
-	{ 'm', NULL, "mechanism" },
-	{ 'c', NULL, "secured" },
-	{ 'a', NULL, "local_port" },
-	{ 'b', NULL, "remote_port" },
-	{ 'k', NULL, "cert" },
-	{ '\0', NULL, "login_user" },
-	{ '\0', NULL, "login_username" },
-	{ '\0', NULL, "login_domain" },
-	{ '\0', NULL, "session" },
-	{ '\0', NULL, "real_local_ip" },
-	{ '\0', NULL, "real_remote_ip" },
-	{ '\0', NULL, "real_local_port" },
-	{ '\0', NULL, "real_remote_port" },
-	{ '\0', NULL, "domain_first" },
-	{ '\0', NULL, "domain_last" },
-	{ '\0', NULL, "master_user" },
-	{ '\0', NULL, "session_pid" },
-	{ '\0', NULL, "original_user" },
-	{ '\0', NULL, "original_username" },
-	{ '\0', NULL, "original_domain" },
-	{ '\0', NULL, "auth_user" },
-	{ '\0', NULL, "auth_username" },
-	{ '\0', NULL, "auth_domain" },
-	{ '\0', NULL, "local_name" },
-	{ '\0', NULL, "client_id" },
-	{ '\0', NULL, "ssl_ja3_hash" },
-	{ '\0', NULL, "owner_user" },
-
+	{ .key = "user", .value = NULL },
+	{ .key = "username", .value = NULL },
+	{ .key = "domain", .value = NULL },
+	{ .key = "protocol", .value = NULL },
+	{ .key = "home", .value = NULL },
+	{ .key = "local_ip", .value = NULL },
+	{ .key = "remote_ip", .value = NULL },
+	{ .key = "client_pid", .value = NULL },
+	{ .key = "password", .value = NULL },
+	{ .key = "id", .value = NULL },
+	{ .key = "mechanism", .value = NULL },
+	{ .key = "secured", .value = NULL },
+	{ .key = "local_port", .value = NULL },
+	{ .key = "remote_port", .value = NULL },
+	{ .key = "cert", .value = NULL },
+	{ .key = "login_user", .value = NULL },
+	{ .key = "login_username", .value = NULL },
+	{ .key = "login_domain", .value = NULL },
+	{ .key = "session", .value = NULL },
+	{ .key = "real_local_ip", .value = NULL },
+	{ .key = "real_remote_ip", .value = NULL },
+	{ .key = "real_local_port", .value = NULL },
+	{ .key = "real_remote_port", .value = NULL },
+	{ .key = "domain_first", .value = NULL },
+	{ .key = "domain_last", .value = NULL },
+	{ .key = "master_user", .value = NULL },
+	{ .key = "session_pid", .value = NULL },
+	{ .key = "original_user", .value = NULL },
+	{ .key = "original_username", .value = NULL },
+	{ .key = "original_domain", .value = NULL },
+	{ .key = "auth_user", .value = NULL },
+	{ .key = "auth_username", .value = NULL },
+	{ .key = "auth_domain", .value = NULL },
+	{ .key = "local_name", .value = NULL },
+	{ .key = "client_id", .value = NULL },
+	{ .key = "ssl_ja3_hash", .value = NULL },
+	{ .key = "owner_user", .value = NULL },
+	VAR_EXPAND_TABLE_END
 	/* be sure to update AUTH_REQUEST_VAR_TAB_COUNT */
-	{ '\0', NULL, NULL }
 };
 static_assert_array_size(auth_request_var_expand_static_tab,
 			 AUTH_REQUEST_VAR_TAB_COUNT+1);
@@ -74,7 +73,6 @@ auth_request_str_escape(const char *string,
 struct var_expand_table *
 auth_request_get_var_expand_table_full(const struct auth_request *auth_request,
 				       const char *username,
-				       auth_request_escape_func_t *escape_func,
 				       unsigned int *count)
 {
 	const struct auth_request_fields *fields = &auth_request->fields;
@@ -82,9 +80,6 @@ auth_request_get_var_expand_table_full(const struct auth_request *auth_request,
 		N_ELEMENTS(auth_request_var_expand_static_tab);
 	struct var_expand_table *tab, *ret_tab;
 	const char *orig_user, *auth_user;
-
-	if (escape_func == NULL)
-		escape_func = escape_none;
 
 	/* keep the extra fields at the beginning. the last static_tab field
 	   contains the ending NULL-fields. */
@@ -98,158 +93,154 @@ auth_request_get_var_expand_table_full(const struct auth_request *auth_request,
 
 	if (username == NULL)
 		username = "";
-	tab[0].value = tab[36].value = escape_func(username, auth_request);
-	tab[1].value = escape_func(t_strcut(username, '@'),
-				   auth_request);
-	tab[2].value = i_strchr_to_next(username, '@');
-	if (tab[2].value != NULL)
-		tab[2].value = escape_func(tab[2].value, auth_request);
-	tab[3].value = escape_func(fields->protocol, auth_request);
-	/* tab[4] = we have no home dir */
-	if (fields->local_ip.family != 0)
-		tab[5].value = net_ip2addr(&fields->local_ip);
-	if (fields->remote_ip.family != 0)
-		tab[6].value = net_ip2addr(&fields->remote_ip);
-	tab[7].value = dec2str(auth_request->client_pid);
-	if (auth_request->mech_password != NULL) {
-		tab[8].value = escape_func(auth_request->mech_password,
-					   auth_request);
+
+	var_expand_table_set_value(tab, "user", username);
+	var_expand_table_set_value(tab, "username", t_strcut(username, '@'));
+	var_expand_table_set_value(tab, "domain", i_strchr_to_next(username, '@'));
+	var_expand_table_set_value(tab, "protocol", fields->protocol);
+	/* tab['home'] = we have no home dir */
+	if (fields->local_ip.family != 0) {
+		var_expand_table_set_value(tab, "local_ip",
+				net_ip2addr(&fields->local_ip));
 	}
+	if (fields->remote_ip.family != 0) {
+		var_expand_table_set_value(tab, "remote_ip",
+			net_ip2addr(&fields->remote_ip));
+	}
+	var_expand_table_set_value(tab, "client_pid",
+			dec2str(auth_request->client_pid));
+	var_expand_table_set_value(tab, "password", auth_request->mech_password);
 	if (auth_request->userdb_lookup) {
-		tab[9].value = auth_request->userdb == NULL ? "" :
-			dec2str(auth_request->userdb->userdb->id);
+		var_expand_table_set_value(tab, "id",
+				auth_request->userdb == NULL ? "" :
+				dec2str(auth_request->userdb->userdb->id));
 	} else {
-		tab[9].value = auth_request->passdb == NULL ? "" :
-			dec2str(auth_request->passdb->passdb->id);
+		var_expand_table_set_value(tab, "id",
+				auth_request->passdb == NULL ? "" :
+				dec2str(auth_request->passdb->passdb->id));
 	}
-	tab[10].value = fields->mech_name == NULL ? "" :
-		escape_func(fields->mech_name, auth_request);
+
+	var_expand_table_set_value(tab, "mechanism", fields->mech_name);
+
 	switch (fields->conn_secured) {
-	case AUTH_REQUEST_CONN_SECURED_NONE: tab[11].value = ""; break;
-	case AUTH_REQUEST_CONN_SECURED: tab[11].value = "secured"; break;
-	case AUTH_REQUEST_CONN_SECURED_TLS: tab[11].value = "TLS"; break;
-	default: tab[11].value = ""; break;
+	case AUTH_REQUEST_CONN_SECURED_NONE:
+		var_expand_table_set_value(tab, "secured", ""); break;
+	case AUTH_REQUEST_CONN_SECURED:
+		var_expand_table_set_value(tab, "secured", "secured"); break;
+	case AUTH_REQUEST_CONN_SECURED_TLS:
+		var_expand_table_set_value(tab, "secured", "TLS"); break;
+	default:
+		var_expand_table_set_value(tab, "secured", ""); break;
 	};
-	tab[12].value = dec2str(fields->local_port);
-	tab[13].value = dec2str(fields->remote_port);
-	tab[14].value = fields->valid_client_cert ? "valid" : "";
+
+	var_expand_table_set_value(tab, "local_port", dec2str(fields->local_port));
+	var_expand_table_set_value(tab, "remote_port", dec2str(fields->remote_port));
+	var_expand_table_set_value(tab, "cert",
+			fields->valid_client_cert ? "valid" : "");
 
 	if (fields->requested_login_user != NULL) {
 		const char *login_user = fields->requested_login_user;
 
-		tab[15].value = escape_func(login_user, auth_request);
-		tab[16].value = escape_func(t_strcut(login_user, '@'),
-					    auth_request);
-		tab[17].value = i_strchr_to_next(login_user, '@');
-		if (tab[17].value != NULL) {
-			tab[17].value = escape_func(tab[17].value,
-						    auth_request);
-		}
+		var_expand_table_set_value(tab, "login_user", login_user);
+		var_expand_table_set_value(tab, "login_username",
+					   t_strcut(login_user, '@'));
+		var_expand_table_set_value(tab, "login_domain",
+					   i_strchr_to_next(login_user, '@'));
 	}
-	tab[18].value = fields->session_id == NULL ? NULL :
-		escape_func(fields->session_id, auth_request);
-	if (fields->real_local_ip.family != 0)
-		tab[19].value = net_ip2addr(&fields->real_local_ip);
-	if (fields->real_remote_ip.family != 0)
-		tab[20].value = net_ip2addr(&fields->real_remote_ip);
-	tab[21].value = dec2str(fields->real_local_port);
-	tab[22].value = dec2str(fields->real_remote_port);
-	tab[23].value = i_strchr_to_next(username, '@');
-	if (tab[23].value != NULL) {
-		tab[23].value = escape_func(t_strcut(tab[23].value, '@'),
-					    auth_request);
+
+	var_expand_table_set_value(tab, "session", fields->session_id);
+	if (fields->real_local_ip.family != 0) {
+		var_expand_table_set_value(tab, "real_local_ip",
+			net_ip2addr(&fields->real_local_ip));
 	}
-	tab[24].value = strrchr(username, '@');
-	if (tab[24].value != NULL)
-		tab[24].value = escape_func(tab[24].value+1, auth_request);
-	tab[25].value = fields->master_user == NULL ? NULL :
-		escape_func(fields->master_user, auth_request);
-	tab[26].value = auth_request->session_pid == (pid_t)-1 ? NULL :
-		dec2str(auth_request->session_pid);
+	if (fields->real_remote_ip.family != 0) {
+		var_expand_table_set_value(tab, "real_remote_ip",
+			net_ip2addr(&fields->real_remote_ip));
+	}
+	var_expand_table_set_value(tab, "real_local_port",
+				   dec2str(fields->real_local_port));
+	var_expand_table_set_value(tab, "real_remote_port",
+				   dec2str(fields->real_remote_port));
+
+	const char *domain_first = i_strchr_to_next(username, '@');
+	if (domain_first != NULL)
+		domain_first = t_strcut(domain_first, '@');
+	var_expand_table_set_value(tab, "domain_first", domain_first);
+	const char *domain_last = strrchr(username, '@');
+	if (domain_last != NULL)
+		domain_last++;
+	var_expand_table_set_value(tab, "domain_last", domain_last);
+	var_expand_table_set_value(tab, "master_user", fields->master_user);
+
+	const char *session_pid = "";
+	if (auth_request->session_pid != (pid_t)-1)
+		session_pid = dec2str(auth_request->session_pid);
+	var_expand_table_set_value(tab, "session_pid", session_pid);
 
 	orig_user = fields->original_username != NULL ?
 		fields->original_username : username;
-	tab[27].value = escape_func(orig_user, auth_request);
-	tab[28].value = escape_func(t_strcut(orig_user, '@'), auth_request);
-	tab[29].value = i_strchr_to_next(orig_user, '@');
-	if (tab[29].value != NULL)
-		tab[29].value = escape_func(tab[29].value, auth_request);
+	var_expand_table_set_value(tab, "original_user", orig_user);
+	var_expand_table_set_value(tab, "original_username",
+				   t_strcut(orig_user, '@'));
+	var_expand_table_set_value(tab, "original_domain",
+				   i_strchr_to_next(orig_user, '@'));
 
-	if (fields->master_user != NULL)
-		auth_user = fields->master_user;
-	else
-		auth_user = orig_user;
-	tab[30].value = escape_func(auth_user, auth_request);
-	tab[31].value = escape_func(t_strcut(auth_user, '@'), auth_request);
-	tab[32].value = i_strchr_to_next(auth_user, '@');
-	if (tab[32].value != NULL)
-		tab[32].value = escape_func(tab[32].value, auth_request);
-	if (fields->local_name != NULL)
-		tab[33].value = escape_func(fields->local_name, auth_request);
-	if (fields->client_id != NULL)
-		tab[34].value = escape_func(fields->client_id, auth_request);
-	if (fields->ssl_ja3_hash != NULL)
-		tab[35].value = escape_func(fields->ssl_ja3_hash, auth_request);
+	auth_user = fields->master_user != NULL ?
+		fields->master_user : orig_user;
+	var_expand_table_set_value(tab, "auth_user", auth_user);
+	var_expand_table_set_value(tab, "auth_username",
+				   t_strcut(auth_user, '@'));
+	var_expand_table_set_value(tab, "auth_domain",
+				   i_strchr_to_next(auth_user, '@'));
+	var_expand_table_set_value(tab, "local_name", fields->local_name);
+	var_expand_table_set_value(tab, "client_id", fields->client_id);
+	var_expand_table_set_value(tab, "ssl_ja3_hash", fields->ssl_ja3_hash);
+	var_expand_table_set_value(tab, "owner_user", username);
 	return ret_tab;
 }
 
 const struct var_expand_table *
-auth_request_get_var_expand_table(const struct auth_request *auth_request,
-				  auth_request_escape_func_t *escape_func)
+auth_request_get_var_expand_table(const struct auth_request *auth_request)
 {
 	unsigned int count = 0;
 
 	return auth_request_get_var_expand_table_full(auth_request,
-		auth_request->fields.user, escape_func, &count);
-}
-
-static const char *field_get_default(const char *data)
-{
-	const char *p;
-
-	p = strchr(data, ':');
-	if (p == NULL)
-		return "";
-	else {
-		/* default value given */
-		return p+1;
-	}
+		auth_request->fields.user, &count);
 }
 
 static int
-auth_request_var_expand_func_passdb(const char *data, void *context,
-				    const char **value_r,
+auth_request_var_expand_func_passdb(const char *field_name, const char **value_r,
+				    void *context,
 				    const char **error_r ATTR_UNUSED)
 {
 	struct auth_request_var_expand_ctx *ctx = context;
-	const char *field_name = t_strcut(data, ':');
 	const char *value;
 
 	value = auth_fields_find(ctx->auth_request->fields.extra_fields, field_name);
-	*value_r = ctx->escape_func(value != NULL ? value : field_get_default(data),
-				    ctx->auth_request);
-	return 1;
+	if (value == NULL)
+		value = "";
+	*value_r = value;
+	return 0;
 }
 
 static int
-auth_request_var_expand_func_userdb(const char *data, void *context,
-				    const char **value_r,
-				    const char **error_r ATTR_UNUSED)
+auth_request_var_expand_func_userdb(const char *field_name, const char **value_r,
+				    void *context, const char **error_r ATTR_UNUSED)
 {
 	struct auth_request_var_expand_ctx *ctx = context;
-	const char *field_name = t_strcut(data, ':');
 	const char *value;
 
 	value = ctx->auth_request->fields.userdb_reply == NULL ? NULL :
 		auth_fields_find(ctx->auth_request->fields.userdb_reply, field_name);
-	*value_r = ctx->escape_func(value != NULL ? value : field_get_default(data),
-				    ctx->auth_request);
-	return 1;
+	if (value == NULL)
+		value = "";
+	*value_r = value;
+	return 0;
 }
 
-const struct var_expand_func_table auth_request_var_funcs_table[] = {
-	{ "passdb", auth_request_var_expand_func_passdb },
-	{ "userdb", auth_request_var_expand_func_userdb },
+const struct var_expand_provider auth_request_var_expand_providers[] = {
+	{ .key = "passdb", .func = auth_request_var_expand_func_passdb },
+	{ .key = "userdb", .func = auth_request_var_expand_func_userdb },
 	{ NULL, NULL }
 };
 
@@ -259,7 +250,7 @@ int auth_request_var_expand(string_t *dest, const char *str,
 			    const char **error_r)
 {
 	return auth_request_var_expand_with_table(dest, str, auth_request,
-		auth_request_get_var_expand_table(auth_request, escape_func),
+		auth_request_get_var_expand_table(auth_request),
 		escape_func, error_r);
 }
 
@@ -274,20 +265,28 @@ int auth_request_var_expand_with_table(string_t *dest, const char *str,
 	i_zero(&ctx);
 	ctx.auth_request = auth_request;
 	ctx.escape_func = escape_func == NULL ? escape_none : escape_func;
-	return var_expand_with_funcs(dest, str, table,
-				     auth_request_var_funcs_table, &ctx, error_r);
+	const struct var_expand_params params = {
+		.table = table,
+		.providers = auth_request_var_expand_providers,
+		.escape_func = (var_expand_escape_func_t *)ctx.escape_func,
+		.context = &ctx,
+		.escape_context = (void *)auth_request,
+		.event = auth_request->event,
+	};
+
+	return var_expand_new(dest, str, &params, error_r) < 0 ? -1 : 1;
 }
 
 int t_auth_request_var_expand(const char *str,
-			      const struct auth_request *auth_request ATTR_UNUSED,
-			      auth_request_escape_func_t *escape_func ATTR_UNUSED,
+			      const struct auth_request *auth_request,
+			      auth_request_escape_func_t *escape_func,
 			      const char **value_r, const char **error_r)
 {
 	string_t *dest = t_str_new(128);
 	int ret = auth_request_var_expand(dest, str, auth_request,
 					  escape_func, error_r);
 	*value_r = str_c(dest);
-	return ret;
+	return ret < 0 ? ret : 1;
 }
 
 static void
@@ -296,10 +295,9 @@ auth_request_event_var_expand_callback(void *context,
 {
 	struct auth_request_var_expand_ctx *ctx = context;
 
-	params_r->table = auth_request_get_var_expand_table(ctx->auth_request,
-							    ctx->escape_func);
-	params_r->func_table = auth_request_var_funcs_table;
-	params_r->func_context = ctx;
+	params_r->table = auth_request_get_var_expand_table(ctx->auth_request);
+	params_r->providers = auth_request_var_expand_providers;
+	params_r->context = ctx;
 }
 
 void auth_request_event_set_var_expand(struct auth_request *auth_request)

src/auth/auth-request-var-expand.h
@@ -12,17 +12,14 @@ auth_request_escape_func_t(const char *string,
 extern const struct var_expand_table
 auth_request_var_expand_static_tab[AUTH_REQUEST_VAR_TAB_COUNT+1];
 
-extern const struct var_expand_func_table auth_request_var_funcs_table[];
+extern const struct var_expand_provider auth_request_var_expand_providers[];
 
 const struct var_expand_table *
-auth_request_get_var_expand_table(const struct auth_request *auth_request,
-				  auth_request_escape_func_t *escape_func)
-	ATTR_NULL(2);
+auth_request_get_var_expand_table(const struct auth_request *auth_request);
 struct var_expand_table *
 auth_request_get_var_expand_table_full(const struct auth_request *auth_request,
 				       const char *username,
-				       auth_request_escape_func_t *escape_func,
-				       unsigned int *count) ATTR_NULL(3);
+				       unsigned int *count);
 
 int auth_request_var_expand(string_t *dest, const char *str,
 			    const struct auth_request *auth_request,

src/auth/db-passwd-file.c
@@ -56,6 +56,23 @@ const struct setting_parser_info passwd_file_setting_parser_info = {
 	.pool_offset1 = 1 + offsetof(struct passwd_file_settings, pool),
 };
 
+static int db_passwd_file_expand(const char *key, const char **value_r,
+				 void *context, const char **error_r)
+{
+	struct auth_fields *pwd_fields = context;
+	*value_r = auth_fields_find(pwd_fields, key);
+	if (*value_r == NULL) {
+		*error_r = t_strdup_printf("No such field '%s'", key);
+		return -1;
+	}
+	return 0;
+}
+
+const struct var_expand_provider db_passwd_file_var_expand_fn[] = {
+	{ .key = "passwd_file", .func = db_passwd_file_expand },
+	VAR_EXPAND_TABLE_END
+};
+
 static struct db_passwd_file *passwd_files;
 
 static void ATTR_NULL(3)
@@ -343,9 +360,8 @@ static void db_passwd_file_set_userdb(struct db_passwd_file *db)
 struct db_passwd_file *
 db_passwd_file_init(const char *path, bool userdb, bool debug)
 {
+	const char *error;
 	struct db_passwd_file *db;
-	const char *p;
-	bool percents = FALSE;
 
 	db = db_passwd_file_find(path);
 	if (db != NULL) {
@@ -362,30 +378,18 @@ db_passwd_file_init(const char *path, bool userdb, bool debug)
 	db->event = event_create(auth_event);
 	event_set_forced_debug(db->event, debug);
 
-	for (p = path; *p != '\0'; p++) {
-		if (*p == '%' && p[1] != '\0') {
-			if (var_get_key(++p) == '%')
-				percents = TRUE;
-			else
-				db->vars = TRUE;
-		}
-	}
+	struct var_expand_program *prog;
+	if (var_expand_program_create(path, &prog, &error) < 0)
+		i_fatal("Invalid path '%s' for passwd-file", error);
 
-	if (percents && !db->vars) {
-		/* just extra escaped % chars. remove them. */
-		struct var_expand_table empty_table[1] = {
-			{ .key = '\0' },
-		};
-		string_t *dest;
-		const char *error;
-
-		dest = t_str_new(256);
-		if (var_expand_with_table(dest, path, empty_table, &error) <= 0)
-			i_unreached();
-		path = str_c(dest);
-	}
-
+	const char *const *vars = var_expand_program_variables(prog);
 	db->path = i_strdup(path);
+
+	if (*vars != NULL) {
+		db->vars = TRUE;
+		db->prog = prog;
+	} else
+		var_expand_program_free(&prog);
 	if (db->vars) {
 		hash_table_create(&db->files, default_pool, 0,
 				  str_hash, strcmp);
@@ -428,6 +432,7 @@ void db_passwd_file_unref(struct db_passwd_file **_db)
 			break;
 		}
 	}
+	var_expand_program_free(&db->prog);
 
 	if (db->default_file != NULL)
 		passwd_file_free(db->default_file);
@@ -444,8 +449,7 @@ void db_passwd_file_unref(struct db_passwd_file **_db)
 }
 
 static const char *
-path_fix(const char *path,
-	 const struct auth_request *auth_request ATTR_UNUSED)
+path_fix(const char *path, void *context ATTR_UNUSED)
 {
 	const char *p;
 
@@ -471,9 +475,14 @@ int db_passwd_file_lookup(struct db_passwd_file *db,
 	if (!db->vars)
 		pw = db->default_file;
 	else {
+		const struct var_expand_params params = {
+			.table = auth_request_get_var_expand_table(request),
+			.providers = auth_request_var_expand_providers,
+			.context = request,
+			.escape_func = path_fix,
+		};
 		dest = t_str_new(256);
-		if (auth_request_var_expand(dest, db->path, request, path_fix,
-					    &error) <= 0) {
+		if (var_expand_program_execute(dest, db->prog, &params, &error) < 0) {
 			e_error(authdb_event(request),
 				"Failed to expand passwd-file path %s: %s",
 				db->path, error);

src/auth/userdb-passwd-file.c
@@ -38,7 +38,7 @@ passwd_file_add_extra_fields(struct auth_request *request,
 	unsigned int i;
 	int ret = 0;
 
-	table = auth_request_get_var_expand_table(request, NULL);
+	table = auth_request_get_var_expand_table(request);
 
 	for (i = 0; fields[i] != NULL; i++) {
 		if (!str_begins(fields[i], "userdb_", &key))
@@ -64,7 +64,8 @@ passwd_file_add_extra_fields(struct auth_request *request,
 			auth_request_set_userdb_field(request, key, value);
 		auth_fields_add(pwd_fields, key, value, 0);
 	}
-	if (ret == 0 && auth_request_set_userdb_fields(request, pwd_fields) < 0)
+	if (ret == 0 && auth_request_set_userdb_fields_ex(request, pwd_fields,
+							  db_passwd_file_var_expand_fn) < 0)
 		ret = -1;
 	return ret;
 }

src/auth/Makefile.am
@@ -222,7 +222,6 @@ test_auth_SOURCES = \
 	test-auth-request-var-expand.c \
 	test-auth-request-fields.c \
 	test-username-filter.c \
-	test-db-ldap.c \
 	test-lua.c \
 	test-mock.c \
 	test-main.c

commit 08b6f6da08053b3e128a73d8982a187e49186494

auth: Wrap var_expand_program_execute() for auth_request var expansion
The var_expand providers expect params.context to be a struct
auth_request_var_expand_ctx and dereference ctx->auth_request, but the
passwd-file backends erroneously passed raw auth_request as params.context,
giving garbage %{passdb:...} and %{userdb:...} lookups and potential crashes.

Add auth_request_var_expand_program_execute(), which builds the context
internally and enforces it to be of the proper type.

Broken since d0b4a58cb9 ("auth: Use new var_expand").

src/auth/auth-request-var-expand.c
@@ -269,6 +269,24 @@ int t_auth_request_var_expand(const char *str,
 	return ret;
 }
 
+int auth_request_var_expand_program_execute(string_t *dest,
+					    const struct var_expand_program *program,
+					    const struct auth_request *auth_request,
+					    const char **error_r)
+{
+	struct auth_request_var_expand_ctx ctx = {
+		.auth_request = auth_request,
+	};
+	const struct var_expand_params params = {
+		.table = auth_request_get_var_expand_table(auth_request),
+		.providers = auth_request_var_expand_providers,
+		.context = &ctx,
+		.event = authdb_event(auth_request),
+	};
+
+	return var_expand_program_execute(dest, program, &params, error_r);
+}
+
 static void
 auth_request_event_var_expand_callback(void *context,
 				       struct var_expand_params *params_r)

src/auth/auth-request-var-expand.h
@@ -32,7 +32,10 @@ int t_auth_request_var_expand(const char *str,
 			      const struct auth_request *auth_request,
 			      auth_request_escape_func_t *escape_func,
 			      const char **value_r, const char **error_r);
-
+int auth_request_var_expand_program_execute(string_t *dest,
+					    const struct var_expand_program *program,
+					    const struct auth_request *auth_request,
+					    const char **error_r);
 const char *auth_request_str_escape(const char *string,
 				    const struct auth_request *request);
 

src/auth/db-passwd-file.c
@@ -486,14 +486,9 @@ int db_passwd_file_lookup(struct db_passwd_file *db,
 	if (!db->vars)
 		pw = db->default_file;
 	else {
-		const struct var_expand_params params = {
-			.table = auth_request_get_var_expand_table(request),
-			.providers = auth_request_var_expand_providers,
-			.context = request,
-			.event = authdb_event(request),
-		};
 		dest = t_str_new(256);
-		if (var_expand_program_execute(dest, db->prog, &params, &error) < 0) {
+		if (auth_request_var_expand_program_execute(dest, db->prog,
+							    request, &error) < 0) {
 			e_error(authdb_event(request),
 				"Failed to expand passwd-file path %s: %s",
 				db->path, error);

src/auth/userdb-passwd-file.c
@@ -144,16 +144,10 @@ passwd_file_iterate_init(struct auth_request *auth_request,
 	ctx->ctx.context = context;
 	ctx->skip_passdb_entries = !module->pwf->userdb_warn_missing;
 	if (module->pwf->default_file == NULL) {
-		const struct var_expand_params params = {
-			.table = auth_request_get_var_expand_table(auth_request),
-			.providers = auth_request_var_expand_providers,
-			.context = auth_request,
-			.event = authdb_event(auth_request),
-		};
 		const char *error;
 		string_t *dest = t_str_new(32);
-		if (var_expand_program_execute(dest, module->pwf->prog, &params,
-					       &error) < 0) {
+		if (auth_request_var_expand_program_execute(dest,
+				module->pwf->prog, auth_request, &error) < 0) {
 			e_error(authdb_event(auth_request),
 				"passwd-file: User iteration failed: "
 				"Cannot expand '%s': %s", module->pwf->path, error);