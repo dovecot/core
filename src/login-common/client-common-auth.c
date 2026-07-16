dovecot/core

commit 6335dab970771b13689918637122360ff7021662

login-common: Improve logging for proxy connection failures
When a proxy connection to a destination fails, the Info log
line stating this now includes the destination to which the
connection failed.

src/login-common/client-common-auth.c
@@ -567,6 +567,9 @@ void client_common_proxy_failed(struct client *client,
 
 	client->proxy_last_failure = type;
 	client->proxy_failed = TRUE;
+	if (client->login_proxy != NULL)
+		client->proxy_last_host = i_strdup(login_proxy_get_hostport(
+			client->login_proxy));
 	client_proxy_failed(client);
 }
 

src/login-common/client-common.c
@@ -1558,7 +1558,9 @@ bool client_get_extra_disconnect_reason(struct client *client,
 		}
 		/* Authentication to the next hop failed. */
 		*event_reason_r = t_strdup_printf("proxy_dest_%s", event_reason);
-		last_reason = t_strdup_printf("proxy dest %s", last_reason);
+		last_reason = t_strdup_printf("proxy dest %s to %s",
+					      last_reason,
+					      client->proxy_last_host == NULL ? "(unknown)" : client->proxy_last_host);
 	} else if (client->auth_login_limit_reached) {
 		*event_reason_r = "connection_limit";
 		last_reason = "connection limit reached";

src/login-common/client-common.h
@@ -268,6 +268,9 @@ struct client {
 	   Can also be NULL if there are no user_* fields. */
 	const char **alt_usernames;
 
+	/* Last host we tried to connect to */
+	const char *proxy_last_host;
+
 	bool create_finished:1;
 	bool disconnected:1;
 	bool destroyed:1;

commit 2de8aba806e7b835083b7106de32def517ed5721

login-common: Fix memory leak of proxy_last_host
Broken in 6335dab970771b13689918637122360ff7021662.

src/login-common/client-common-auth.c
@@ -567,9 +567,11 @@ void client_common_proxy_failed(struct client *client,
 
 	client->proxy_last_failure = type;
 	client->proxy_failed = TRUE;
-	if (client->login_proxy != NULL)
+	if (client->login_proxy != NULL) {
+		i_free(client->proxy_last_host);
 		client->proxy_last_host = i_strdup(login_proxy_get_hostport(
 			client->login_proxy));
+	}
 	client_proxy_failed(client);
 }
 

src/login-common/client-common.c
@@ -648,6 +648,7 @@ bool client_unref(struct client **_client)
 	i_free(client->virtual_auth_user);
 	i_free(client->auth_mech_name);
 	i_free(client->last_proxy_auth_failure_reason);
+	i_free(client->proxy_last_host);
 	i_free(client->master_data_prefix);
 	pool_unref(&client->pool);
 

src/login-common/client-common.h
@@ -269,7 +269,7 @@ struct client {
 	const char **alt_usernames;
 
 	/* Last host we tried to connect to */
-	const char *proxy_last_host;
+	char *proxy_last_host;
 
 	bool create_finished:1;
 	bool disconnected:1;