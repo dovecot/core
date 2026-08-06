dovecot/core

commit 3b8e0ff9c1279e05e8bbb233eb92c8e95f613fe2

dsync: backup - Delete mailbox attributes that don't exist in the source
The backup destination is supposed to become identical to the source,
but attributes that existed only in the destination were never deleted.
For example a Sieve script created in the backup destination stayed
there forever.

This requires the source to send all of its attributes, so it's done
only when the remote dsync protocol version supports it.

src/doveadm/dsync/dsync-mailbox-import.c
@@ -91,6 +91,9 @@ struct dsync_mailbox_importer {
 	/* UID => struct dsync_mail_change */
 	HASH_TABLE_TYPE(dsync_uid_mail_change) local_changes;
 	HASH_TABLE_TYPE(dsync_attr_change) local_attr_changes;
+	/* Attribute keys received from the remote, prefixed with the
+	   attribute type character. */
+	HASH_TABLE(char *, char *) remote_attr_keys;
 
 	ARRAY_TYPE(seq_range) maybe_expunge_uids;
 	ARRAY(struct dsync_mail_change *) maybe_saves;
@@ -131,6 +134,7 @@ struct dsync_mailbox_importer {
 	bool delete_mailbox:1;
 	bool empty_hdr_workaround:1;
 	bool no_header_hashes:1;
+	bool delete_unknown_attrs:1;
 };
 
 static const char *dsync_mail_change_type_names[] = {
@@ -270,6 +274,12 @@ dsync_mailbox_import_init(struct mailbox *box,
 		(set->flags & DSYNC_MAILBOX_IMPORT_FLAG_MASTER_BRAIN) != 0;
 	importer->revert_local_changes =
 		(set->flags & DSYNC_MAILBOX_IMPORT_FLAG_REVERT_LOCAL_CHANGES) != 0;
+	importer->delete_unknown_attrs =
+		(set->flags & DSYNC_MAILBOX_IMPORT_FLAG_DELETE_UNKNOWN_ATTRS) != 0;
+	if (importer->delete_unknown_attrs) {
+		hash_table_create(&importer->remote_attr_keys, pool, 0,
+				  str_hash, strcmp);
+	}
 	importer->mails_have_guids =
 		(set->flags & DSYNC_MAILBOX_IMPORT_FLAG_MAILS_HAVE_GUIDS) != 0;
 	importer->mails_use_guid128 =
@@ -572,12 +582,121 @@ dsync_mailbox_import_attribute_real(struct dsync_mailbox_importer *importer,
 	return 0;
 }
 
+static char *
+dsync_mailbox_import_attr_hash_key(pool_t pool, enum mail_attribute_type type,
+				   const char *key)
+{
+	char type_chr = type == MAIL_ATTRIBUTE_TYPE_PRIVATE ? 'p' : 's';
+
+	return p_strdup_printf(pool, "%c%s", type_chr, key);
+}
+
+static void
+dsync_mailbox_import_attr_remember(struct dsync_mailbox_importer *importer,
+				   const struct dsync_mailbox_attribute *attr)
+{
+	char *hash_key;
+
+	hash_key = dsync_mailbox_import_attr_hash_key(importer->pool,
+						      attr->type, attr->key);
+	if (hash_table_lookup(importer->remote_attr_keys, hash_key) == NULL)
+		hash_table_insert(importer->remote_attr_keys, hash_key, hash_key);
+}
+
+static int
+dsync_mailbox_import_delete_attrs(struct dsync_mailbox_importer *importer,
+				  enum mail_attribute_type type)
+{
+	struct mailbox_attribute_iter *iter;
+	struct mail_attribute_value value;
+	ARRAY_TYPE(const_string) keys;
+	const char *key, *const *keyp;
+	bool skip;
+	int ret = 0;
+
+	t_array_init(&keys, 8);
+	iter = mailbox_attribute_iter_init(importer->box, type, "");
+	while ((key = mailbox_attribute_iter_next(iter)) != NULL) {
+		if (hash_table_lookup(importer->remote_attr_keys,
+			dsync_mailbox_import_attr_hash_key(pool_datastack_create(),
+							   type, key)) != NULL)
+			continue;
+
+		if (mailbox_attribute_get_stream(importer->box, type,
+						 key, &value) < 0) {
+			e_error(importer->event,
+				"Failed to get attribute %s: %s", key,
+				mailbox_get_last_internal_error(
+					importer->box, &importer->mail_error));
+			ret = -1;
+			break;
+		}
+		/* readonly attributes can't be deleted, and attributes
+		   without a value have nothing to delete */
+		skip = (value.flags & MAIL_ATTRIBUTE_VALUE_FLAG_READONLY) != 0 ||
+			(value.value == NULL && value.value_stream == NULL);
+		if (value.value_stream != NULL)
+			i_stream_unref(&value.value_stream);
+		if (skip)
+			continue;
+
+		key = t_strdup(key);
+		array_push_back(&keys, &key);
+	}
+	if (mailbox_attribute_iter_deinit(&iter) < 0) {
+		e_error(importer->event, "Mailbox attribute iteration failed: %s",
+			mailbox_get_last_internal_error(importer->box,
+							&importer->mail_error));
+		ret = -1;
+	}
+	if (ret < 0)
+		return -1;
+
+	array_foreach(&keys, keyp) {
+		if (mailbox_attribute_unset(importer->trans, type, *keyp) < 0) {
+			e_error(importer->event,
+				"Failed to unset attribute %s: %s", *keyp,
+				mailbox_get_last_internal_error(importer->box,
+								NULL));
+			/* the attributes aren't vital, don't fail everything
+			   just because of them. */
+		} else {
+			e_debug(importer->event,
+				"Delete attribute %s: Nonexistent remotely",
+				*keyp);
+		}
+	}
+	return 0;
+}
+
+int dsync_mailbox_import_attributes_finish(struct dsync_mailbox_importer *importer)
+{
+	int ret = 0;
+
+	if (!importer->delete_unknown_attrs)
+		return 0;
+
+	T_BEGIN {
+		if (dsync_mailbox_import_delete_attrs(importer,
+				MAIL_ATTRIBUTE_TYPE_PRIVATE) < 0 ||
+		    dsync_mailbox_import_delete_attrs(importer,
+				MAIL_ATTRIBUTE_TYPE_SHARED) < 0)
+			ret = -1;
+	} T_END;
+	if (ret < 0)
+		importer->failed = TRUE;
+	return ret;
+}
+
 int dsync_mailbox_import_attribute(struct dsync_mailbox_importer *importer,
 				   const struct dsync_mailbox_attribute *attr)
 {
 	struct dsync_mailbox_attribute *local_attr;
 	const char *result = "";
 	int ret;
+
+	if (importer->delete_unknown_attrs)
+		dsync_mailbox_import_attr_remember(importer, attr);
 
 	if (dsync_mailbox_import_lookup_attr(importer, attr->type,
 					     attr->key, &local_attr) < 0)

src/doveadm/dsync/dsync-brain-mailbox.c
@@ -204,8 +204,13 @@ dsync_brain_sync_mailbox_init_remote(struct dsync_brain *brain,
 		import_flags |= DSYNC_MAILBOX_IMPORT_FLAG_WANT_MAIL_REQUESTS;
 	if (brain->master_brain)
 		import_flags |= DSYNC_MAILBOX_IMPORT_FLAG_MASTER_BRAIN;
-	if (brain->backup_recv && !brain->no_backup_overwrite)
+	if (brain->backup_recv && !brain->no_backup_overwrite) {
 		import_flags |= DSYNC_MAILBOX_IMPORT_FLAG_REVERT_LOCAL_CHANGES;
+		if (brain->remote_backup_full_attrs) {
+			import_flags |=
+				DSYNC_MAILBOX_IMPORT_FLAG_DELETE_UNKNOWN_ATTRS;
+		}
+	}
 	if (brain->local_dsync_box.have_save_guids &&
 	    (remote_dsync_box->have_save_guids ||
 	     (brain->backup_recv && remote_dsync_box->have_guids)))

src/doveadm/dsync/dsync-brain-mails.c
@@ -94,6 +94,9 @@ static bool dsync_brain_recv_mailbox_attribute(struct dsync_brain *brain)
 	if ((ret = dsync_ibc_recv_mailbox_attribute(brain->ibc, &attr)) == 0)
 		return FALSE;
 	if (ret == DSYNC_IBC_RECV_RET_FINISHED) {
+		if (dsync_mailbox_import_attributes_finish(
+					brain->box_importer) < 0)
+			brain->failed = TRUE;
 		brain->box_recv_state = DSYNC_BOX_STATE_CHANGES;
 		return TRUE;
 	}

src/doveadm/dsync/dsync-brain-private.h
@@ -113,6 +113,8 @@ struct dsync_brain {
 	bool mail_requests:1;
 	bool backup_send:1;
 	bool backup_recv:1;
+	/* Remote sends all of its mailbox attributes in backup mode */
+	bool remote_backup_full_attrs:1;
 	bool purge:1;
 	bool debug:1;
 	bool sync_visible_namespaces:1;

src/doveadm/dsync/dsync-brain.c
@@ -498,6 +498,7 @@ static bool dsync_brain_master_recv_handshake(struct dsync_brain *brain)
 		}
 	}
 	dsync_brain_set_hdr_hash_version(brain, ibc_set);
+	brain->remote_backup_full_attrs = ibc_set->backup_full_attrs;
 
 	brain->state = brain->sync_type == DSYNC_BRAIN_SYNC_TYPE_STATE ?
 		DSYNC_STATE_MASTER_SEND_LAST_COMMON :
@@ -516,6 +517,7 @@ static bool dsync_brain_slave_recv_handshake(struct dsync_brain *brain)
 	if (dsync_ibc_recv_handshake(brain->ibc, &ibc_set) == 0)
 		return FALSE;
 	dsync_brain_set_hdr_hash_version(brain, ibc_set);
+	brain->remote_backup_full_attrs = ibc_set->backup_full_attrs;
 
 	if (ibc_set->lock_timeout > 0) {
 		brain->lock_timeout = ibc_set->lock_timeout;

src/doveadm/dsync/dsync-ibc-pipe.c
@@ -194,6 +194,8 @@ dsync_ibc_pipe_recv_handshake(struct dsync_ibc *ibc,
 	if (item == NULL)
 		return DSYNC_IBC_RECV_RET_TRYAGAIN;
 
+	/* both sides are running the same version */
+	item->u.set.backup_full_attrs = TRUE;
 	*set_r = &item->u.set;
 	return DSYNC_IBC_RECV_RET_OK;
 }

src/doveadm/dsync/dsync-ibc-stream.c
@@ -25,14 +25,15 @@
 #define DSYNC_IBC_STREAM_OUTBUF_THROTTLE_SIZE (1024*128)
 
 #define DSYNC_PROTOCOL_VERSION_MAJOR 3
-#define DSYNC_PROTOCOL_VERSION_MINOR 5
-#define DSYNC_HANDSHAKE_VERSION "VERSION\tdsync\t3\t5\n"
+#define DSYNC_PROTOCOL_VERSION_MINOR 6
+#define DSYNC_HANDSHAKE_VERSION "VERSION\tdsync\t3\t6\n"
 
 #define DSYNC_PROTOCOL_MINOR_HAVE_ATTRIBUTES 1
 #define DSYNC_PROTOCOL_MINOR_HAVE_SAVE_GUID 2
 #define DSYNC_PROTOCOL_MINOR_HAVE_FINISH 3
 #define DSYNC_PROTOCOL_MINOR_HAVE_HDR_HASH_V2 4
 #define DSYNC_PROTOCOL_MINOR_HAVE_HDR_HASH_V3 5
+#define DSYNC_PROTOCOL_MINOR_HAVE_BACKUP_FULL_ATTRS 6
 
 enum item_type {
 	ITEM_NONE,
@@ -886,6 +887,8 @@ dsync_ibc_stream_recv_handshake(struct dsync_ibc *_ibc,
 		set->hashed_headers = (const char*const*)p_strsplit_tabescaped(pool, value);
 	set->hdr_hash_v2 = ibc->minor_version >= DSYNC_PROTOCOL_MINOR_HAVE_HDR_HASH_V2;
 	set->hdr_hash_v3 = ibc->minor_version >= DSYNC_PROTOCOL_MINOR_HAVE_HDR_HASH_V3;
+	set->backup_full_attrs =
+		ibc->minor_version >= DSYNC_PROTOCOL_MINOR_HAVE_BACKUP_FULL_ATTRS;
 
 	*set_r = set;
 	return DSYNC_IBC_RECV_RET_OK;

src/doveadm/dsync/dsync-ibc.h
@@ -72,6 +72,10 @@ struct dsync_ibc_settings {
 	enum dsync_brain_flags brain_flags;
 	bool hdr_hash_v2;
 	bool hdr_hash_v3;
+	/* The remote exports all of its mailbox attributes when it is the
+	   backup source, so attributes that don't exist in it can be deleted
+	   from the backup destination. */
+	bool backup_full_attrs;
 	unsigned int lock_timeout;
 };
 

commit 15496381c26037d21395afb2fab500b18a7c4ba9

dsync: Fix leaking the remote attribute keys hash table
Broken by 3b8e0ff9c1279e05e8bbb233eb92c8e95f613fe2

src/doveadm/dsync/dsync-mailbox-import.c
@@ -3101,6 +3101,7 @@ int dsync_mailbox_import_deinit(struct dsync_mailbox_importer **_importer,
 
 	hash_table_destroy(&importer->import_guids);
 	hash_table_destroy(&importer->import_uids);
+	hash_table_destroy(&importer->remote_attr_keys);
 	array_free(&importer->maybe_expunge_uids);
 	array_free(&importer->maybe_saves);
 	array_free(&importer->wanted_uids);